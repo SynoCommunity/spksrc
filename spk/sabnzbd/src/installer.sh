a32a9980c83e26cb920f6e0b55f5d4c80705f4b9
Antoine Bertin <diaoulael@gmail.com>
2011-11-14 17:21:56 +0100

    Review sabnzbd and sickbeard packages
    - Remove extra compressing stuff
    - Add default nice and ionice parameters for sickbeard
    - Rename references to superzebulon.org to packages.nas-forum.com
    - Use symlinks to ${SYNOPKG_PKGDEST} instead of copy when possible
    - Don't put anything in /usr/local/var except run scripts

--- spk/sabnzbd/src/installer.sh
+++ spk/sabnzbd/src/installer.sh
@@ -10,7 +10,6 @@ PYTHON_DIR="/usr/local/python27"
 
 # Common variables
 INSTALL_DIR="/usr/local/${PACKAGE}"
-VAR_DIR="/usr/local/var/${PACKAGE}"
 UPGRADE="/tmp/${PACKAGE}.upgrade"
 PATH="${INSTALL_DIR}/bin:${PYTHON_DIR}/bin:/bin:/usr/bin:/usr/syno/bin" # Avoid ipkg commands
 
@@ -30,66 +29,49 @@ postinst ()
 {
     # Installation directories
     mkdir -p ${INSTALL_DIR}
-    mkdir -p ${VAR_DIR}
     mkdir -p /usr/local/bin
 
-    # Remove the DSM user
-    if ${SYNOUSER} --enum local | grep "^${PACKAGE}$" >/dev/null
-    then
-    	# Keep the existing uid
-        uid=`grep ${PACKAGE} /etc/passwd | cut -d: -f3`
-        ${SYNOUSER} --del ${PACKAGE} 2> /dev/null
-        UID_PARAM="-u ${uid}"
-    fi
+    # Link folders
+    for dir in ${SYNOPKG_PKGDEST}/*; do
+        ln -s ${SYNOPKG_PKGDEST}/`basename ${dir}` ${INSTALL_DIR}/`basename ${dir}`
+    done
 
-    # Extract the files to the installation directory
-    ${PYTHON_DIR}/bin/xzdec -c ${SYNOPKG_PKGDEST}/package.txz | \
-        tar xpf - -C ${INSTALL_DIR}
-    # Remove the installer archive to save space
-    rm ${SYNOPKG_PKGDEST}/package.txz
+    # Create a link in /usr/local/bin
+    ln -s /var/packages/SABnzbd/scripts/start-stop-status /usr/local/bin/${PACKAGE}-ctl
 
-    ln -s /var/packages/SABnzbd/scripts/start-stop-status /usr/local/bin/${PACKAGE}-ctl 
-
-    # Install the application in the main interface.
-    if [ -d ${SYNO3APP} ]
-    then
+    # Install the application in the main interface
+    if [ -d ${SYNO3APP} ]; then
         rm -f ${SYNO3APP}/${PACKAGE}
-        ln -s ${INSTALL_DIR}/share/synoman ${SYNO3APP}/${PACKAGE}
+        ln -s ${SYNOPKG_PKGDEST}/share/synoman ${SYNO3APP}/${PACKAGE}
     fi
 
-    # Copy the default configuration if needed
-    if [ -f ${VAR_DIR}/config.ini ]
-    then
-        true
+    # Restore the config file if we're upgrading, use default otherwise
+    if [ -f ${UPGRADE} ]; then
+        mv /tmp/config.ini ${INSTALL_DIR}/
     else
-        # No config file, copy default one
-        cp ${SYNOPKG_PKGDEST}/var/config.ini ${VAR_DIR}/config.ini
+        cp ${SYNOPKG_PKGDEST}/etc/config.ini ${INSTALL_DIR}/config.ini
     fi
 
-    # Ensure that only the sabnzbd user can access this file, as some
-    # password are stored in clear text
-    chmod 600 ${VAR_DIR}/config.ini
+    # Ensure that only the sabnzbd user can access this file, as some password are stored in clear text
+    chmod 600 ${INSTALL_DIR}/config.ini
 
     # Install the nice and ionice hardlinks
     ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
 
     # Create the service user if needed
-    if grep "^${PACKAGE}:" /etc/passwd >/dev/null
-    then
-        true
-    else
-        adduser -h ${VAR_DIR} -g "${DNAME} User" -G users -D -H ${UID_PARAM} -s /bin/sh ${PACKAGE}
+    if ! grep "^${PACKAGE}:" /etc/passwd >/dev/null; then
+        adduser -h ${INSTALL_DIR} -g "${DNAME} User" -G users -D -H ${UID_PARAM} -s /bin/sh ${PACKAGE}
     fi
 
-    # Correct the files ownership    
-    chown -Rh ${PACKAGE}:users ${INSTALL_DIR} ${VAR_DIR}
+    # Correct the files ownership
+    chown -Rh ${PACKAGE}:users ${INSTALL_DIR}
 
     exit 0
 }
 
 preuninst ()
 {
-    # Make sure the package is not running while we are removing it.
+    # Make sure the package is not running while we are removing it
     /usr/local/bin/${PACKAGE}-ctl stop
 
     exit 0
@@ -97,23 +79,20 @@ preuninst ()
 
 postuninst ()
 {
-    # Keep the user data and settings during the upgrade
-    if [ -f ${UPGRADE} ]
-    then
-        true 
+    # Save the config file and the user if we're upgrading, delete the user otherwise
+    if [ -f ${UPGRADE} ]; then
+        cp ${INSTALL_DIR}/config.ini /tmp/
     else
         deluser ${PACKAGE}
-        rm -fr ${VAR_DIR}
     fi
 
-    # Remove the application from the main interface if it was previously added.
-    if [ -h ${SYNO3APP}/${PACKAGE} ]
-    then
+    # Remove the application from the main interface if it was previously added
+    if [ -h ${SYNO3APP}/${PACKAGE} ]; then
         rm ${SYNO3APP}/${PACKAGE}
     fi
 
     # Remove symlinks to utils
-    rm /usr/local/bin/${PACKAGE}-ctl 
+    rm /usr/local/bin/${PACKAGE}-ctl
 
     # Remove the installation directory
     rm -fr ${INSTALL_DIR}
@@ -123,16 +102,9 @@ postuninst ()
 
 preupgrade ()
 {
-    # The package manager only check the version when installing, not upgrading. So do it here the old way.
-    if [ -e ${PYTHON_DIR}/bin/adduser ]
-    then
-        touch ${UPGRADE}
-    else
-        echo "Please uppdate Python26 before updating this package"
-        false
-    fi
+    touch ${UPGRADE}
 
-    exit $?
+    exit 0
 }
 
 postupgrade ()
