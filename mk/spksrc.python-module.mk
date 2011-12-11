df7e18ec4512c43507a07442f2ba26ab82ed59b5
Antoine Bertin <diaoulael@gmail.com>
2011-12-11 18:22:53 +0100

    Improve python-module rules

--- mk/spksrc.python-module.mk
+++ mk/spksrc.python-module.mk
@@ -1,22 +1,26 @@
 # Python module targets
-CONFIGURE_TARGET = nop
+ifeq ($(strip $(CONFIGURE_TARGET)),)
+CONFIGURE_TARGET = nope
+endif
+ifeq ($(strip $(COMPILE_TARGET)),)
 COMPILE_TARGET = compile_python_module
+endif
+ifeq ($(strip $(INSTALL_TARGET)),)
 INSTALL_TARGET = install_python_module
-
-# Fetch python variables
-include $(WORK_DIR)/python-cc.mk
+endif
 
 # Resume with standard spksrc.cross-cc.mk
 include ../../mk/spksrc.cross-cc.mk
+
+# Fetch python variables
+include $(WORK_DIR)/python-cc.mk
 
 # Python module variables
 PYTHONPATH = $(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_DIR)/site-packages/
 
 
 ### Python module rules
-nop: ;
-
-compile_python_module: $(PRE_COMPILE_PYTHON_MODULE)
+compile_python_module:
 	@if [ ! -d $(PYTHON_LIB_CROSS).bak ]; then \
 		mv $(PYTHON_LIB_CROSS) $(PYTHON_LIB_CROSS).bak; \
 	fi
@@ -32,8 +36,8 @@ fix_shebang_python_module:
 	do \
 	  case $${type} in \
 	    bin) \
-	      echo "Fixing shebang for $${file}" ; \
-	      sed -i -e 's|^#!.*$$|#!$(INSTALL_PREFIX)/bin/python|g' $(INSTALL_DIR)$(INSTALL_PREFIX)/$${file} \
+	      echo -n "Fixing shebang for $${file}... " ; \
+	      sed -i -e 's|^#!.*$$|#!$(INSTALL_PREFIX)/bin/python|g' $(INSTALL_DIR)$(INSTALL_PREFIX)/$${file} > /dev/null 2>&1 && echo "ok" || echo "failed!" \
 	      ;; \
 	  esac ; \
 	done
