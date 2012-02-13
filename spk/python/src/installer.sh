c88fef2ad63f014de6012d7a48dddde957ba169c
Antoine Bertin <diaoulael@gmail.com>
2012-02-13 22:21:40 +0100

    Only provide python SPK to latest version in stable branch

---
+++ spk/python/src/installer.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Package
+PACKAGE="python"
+DNAME="Python"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+UPGRADE="/tmp/${PACKAGE}.upgrade"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Byte-compile
+    ${INSTALL_DIR}/bin/python -m compileall -q -f ${INSTALL_DIR}/lib/python2.7
+    ${INSTALL_DIR}/bin/python -OO -m compileall -q -f ${INSTALL_DIR}/lib/python2.7
+
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
+
