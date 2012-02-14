a97849ba3a4c303bf108723690a830617a5c0a20
Antoine Bertin <diaoulael@gmail.com>
2012-02-14 11:43:00 +0100

    Update umurmur SPK

--- spk/umurmur/src/dsm-control.sh
+++ spk/umurmur/src/dsm-control.sh
@@ -1,64 +1,34 @@
 #!/bin/sh
 
-#########################################
-# A few variables to make things readable
-
-# Package specific variables
+# Package
 PACKAGE="umurmur"
 DNAME="uMurmur"
 
-# Common variables
+# Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
-PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin" # Avoid ipkg commands
-
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 RUNAS="root"
 UMURMUR="${INSTALL_DIR}/bin/umurmurd"
 PID_FILE="${INSTALL_DIR}/var/umurmur.pid"
 LOG_FILE="${INSTALL_DIR}/var/umurmurd.log"
 CFG_FILE="${INSTALL_DIR}/etc/umurmur.conf"
 
+
 start_daemon ()
 {
-    # Launch the application in the background
     su - ${RUNAS} -c "PATH=${PATH} ${UMURMUR} -r -c ${CFG_FILE} -p ${PID_FILE}"
-    counter=20
-    while [ ${counter} -gt 0 ]; do
-        daemon_status && break
-        let counter=counter-1
-        sleep 1
-    done
 }
 
 stop_daemon ()
 {
-    # Kill the application
     kill `cat ${PID_FILE}`
-
-    # Wait until uMurmur is really dead (may take some time)
-    counter=20
-    while [ $counter -gt 0 ]; do
-        daemon_status || exit 0
-        let counter=counter-1
-        sleep 1
-    done
-
-    exit 1
-}
-
-reload_daemon ()
-{
-    kill -s HUP `cat ${PID_FILE}`
+    rm ${PID_FILE}
 }
 
 daemon_status ()
 {
-    if [ -f ${PID_FILE} ]; then
-        if [ -d /proc/`cat ${PID_FILE}` ]; then
-            return 0
-        else
-            # PID file exists, but no process has this PID
-            rm ${PID_FILE}
-    	fi
+    if [ -f ${PID_FILE} ] && [ -d /proc/`cat ${PID_FILE}` ]; then
+        return 0
     fi
     return 1
 }
@@ -93,10 +63,6 @@ case $1 in
             echo ${DNAME} is not running
             exit 1
         fi
-        ;;
-    console)
-        run_in_console
-        exit $?
         ;;
     log)
         echo ${LOG_FILE}
