727b69ad68724ef9f3abb08be3e84c19384be48f
Antoine Bertin <diaoulael@gmail.com>
2012-04-04 21:49:05 +0200

    Improve umurmur dsm-control script

--- spk/umurmur/src/dsm-control.sh
+++ spk/umurmur/src/dsm-control.sh
@@ -22,7 +22,8 @@ start_daemon ()
 stop_daemon ()
 {
     kill `cat ${PID_FILE}`
-    rm ${PID_FILE}
+    wait_for_status 1 20
+    rm -f ${PID_FILE}
 }
 
 daemon_status ()
@@ -31,6 +32,17 @@ daemon_status ()
         return 0
     fi
     return 1
+}
+
+wait_for_status()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && break
+        let counter=counter-1
+        sleep 1
+    done
 }
 
 
