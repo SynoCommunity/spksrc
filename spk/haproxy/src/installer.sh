cd5656f0c9facd55a7a370e24bfd0924c782ebd1
Emmanuel BEZAGU <emmanuel.bezagu@gmail.com>
2012-04-19 13:37:05 +0200

    Add first version of haproxy

---
+++ spk/haproxy/src/installer.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+# Package
+PACKAGE="haproxy"
+DNAME="haproxy"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+RUNAS="nobody"
+CFG_FILE="${INSTALL_DIR}/var/haproxy.conf"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    exit 0
+}
+
+preuninst ()
+{
+    # Remove the user (if not upgrading)
+    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
+        deluser ${RUNAS}
+    fi
+
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    # Save some stuff
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    exit 0
+}
+
+postupgrade ()
+{
+    # Restore some stuff
+    rm -fr ${INSTALL_DIR}/var
+    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
+    exit 0
+}
