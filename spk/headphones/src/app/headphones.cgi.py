b06590e93a8598abdfdc105c5cc8f1f5345a79fa
Antoine Bertin <diaoulael@gmail.com>
2012-05-20 18:25:48 +0200

    Update headphones

---
+++ spk/headphones/src/app/headphones.cgi.py
@@ -0,0 +1,12 @@
+#!/usr/local/headphones/env/bin/python
+import os
+import ConfigParser
+
+
+config = ConfigParser.ConfigParser()
+config.read('/usr/local/headphones/var/config.ini')
+protocol = 'http'
+port = int(config.get('General', 'http_port'))
+
+print 'Location: %s://%s:%d' % (protocol, os.environ['SERVER_NAME'], port)
+print
