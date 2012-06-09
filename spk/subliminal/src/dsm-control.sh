6f8ec0f8e007e994130fd44d2b84aec21e32a2cf
Antoine Bertin <diaoulael@gmail.com>
2012-06-09 09:01:44 +0200

    Add subliminal SPK
    Need to wait for 0.6.0 for scan_filter to be available

---
+++ spk/subliminal/src/dsm-control.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Package
+PACKAGE="subliminal"
+DNAME="Subliminal"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+RUNAS="root"
+PYTHON="${INSTALL_DIR}/env/bin/python"
+SCHEDULER="${INSTALL_DIR}/app/scheduler.py"
+PID_FILE="${INSTALL_DIR}/var/scheduler.pid"
+
+
+start_daemon()
+{
+    su - ${RUNAS} -c "PATH=${PATH} ${PYTHON} ${SCHEDULER}"
+}
+
+stop_daemon()
+{
+    kill `cat ${PID_FILE}`
+    wait_for_status 1 20
+    rm -f ${PID_FILE}
+}
+
+daemon_status()
+{
+    if [ -f ${PID_FILE} ] && [ -d /proc/`cat ${PID_FILE}` ]; then
+        return 0
+    fi
+    rm -f ${PID_FILE}
+    return 1
+}
+
+wait_for_status()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && break
+        let counter=counter-1
+        sleep 1
+    done
+}
+
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+            exit 0
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+            exit $?
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+            exit $?
+        else
+            echo ${DNAME} is not running
+            exit 0
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    *)
+        exit 1
+        ;;
+esac
---
+++ spk/subliminal/src/installer.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Package
+PACKAGE="subliminal"
+DNAME="Subliminal"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/syno/sbin:/usr/syno/bin"
+RUNAS="root"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env > /dev/null
+
+    # Install the bundle
+    ${INSTALL_DIR}/env/bin/pip install -U -b ${INSTALL_DIR}/var/build ${INSTALL_DIR}/share/requirements.pybundle > /dev/null
+    rm -fr ${INSTALL_DIR}/var/build
+
+    # Setup the database
+    ${INSTALL_DIR}/env/bin/python ${INSTALL_DIR}/app/setup.py
+
+    # Correct the files ownership
+    chown -R ${RUNAS}:root ${SYNOPKG_PKGDEST}
+
+	# Index help files
+	pkgindexer_add ${INSTALL_DIR}/app/index.conf > /dev/null
+	pkgindexer_add ${INSTALL_DIR}/app/helptoc.conf > /dev/null
+
+    exit 0
+}
+
+preuninst ()
+{
+	# Remove help files
+	pkgindexer_del ${INSTALL_DIR}/app/index.conf > /dev/null
+	pkgindexer_del ${INSTALL_DIR}/app/helptoc.conf > /dev/null
+
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    # Save some stuff
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    exit 0
+}
+
+postupgrade ()
+{
+    # Restore some stuff
+    rm -fr ${INSTALL_DIR}/var
+    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
+    exit 0
+}
