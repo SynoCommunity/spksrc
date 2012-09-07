5879c8ce014e3620d49d77290dba6d3ddb0d76cf
manu <manu@debian.nantes.dgi>
2012-09-07 16:58:44 +0200

    Haproxy now autoconfigure itself. Modify routing in order to redirect port 80 to your syno on port 30080 and 443 to 30443. If your set DDNS with <your domain> as hosname name, some subdomains are now available : - ssh : <your domain>:443 - dsm : dsm.<your domain>, HTTP is redirected to HTTPS - surveillance station : cam.<your domain>, set dedicated ports in DSM control pannel - file station : file.<your domain>, set dedicated ports in DSM control pannel - sabnzbd : sab.<your domain> - gateone : gateone.<your domain> - webdav : dav.<your domain>
    File /usr/local/haproxy/var/haproxy.conf can be modify manualy, it will be preserved when upgrading haproxy spk.
    Uninstall et reinstall haproxy spk if you want haproxy.conf to be rebuilt.

---
+++ spk/haproxy/src/var/genConf.pl
@@ -0,0 +1,141 @@
+#!/usr/bin/perl
+
+use strict;
+use File::Copy;
+
+my $l;
+my %params;
+my @services;
+my @services_idx;
+
+my $use_backend_http=q{	use_backend http_@SERVICE_NAME@ if { hdr_dom(host) -i @SERVICE_NAME@.@DDNS@ }};
+my $use_backend_https=q{	use_backend https_@SERVICE_NAME@ if { req_ssl_sni @SERVICE_NAME@.@DDNS@ }};
+my $backend=q{
+backend http_@SERVICE_NAME@
+	@SERVICE_HTTP_ENABLED@
+	mode http
+	option forwardfor
+	timeout server 30s
+	timeout connect 4s
+	server http_@SERVICE_NAME@ 127.0.0.1:@SERVICE_HTTP_PORT@ check inter 30000 downinter 1000
+
+backend https_@SERVICE_NAME@
+	@SERVICE_HTTPS_ENABLED@
+	timeout server 2h
+	timeout connect 4s
+	option ssl-hello-chk
+	server https_@SERVICE_NAME@ 127.0.0.1:@SERVICE_HTTPS_PORT@ check inter 30000 downinter 1000
+};
+
+# read ini file
+my $i = 0;
+my $nolig=0;
+open(IN,"haproxy.ini");
+while($l=<IN>) {
+	$nolig++;
+
+	chomp $l;
+	if ($l !~ /^(HTTP_PORT=[0-9]+|HTTPS_PORT=[0-9]+|DDNS=([^|]*)\|(.*)|SSH_(PORT=[0-9]+|ENABLED=(enabled|disabled))|(DSM|SERVICE[0-9]+)_(NAME=[a-z0-9_]+|ENABLED=(enabled|disabled)|HTTPS?_PORT=([0-9]+|([^|]*)\|(.*))))?$/) {
+		print "Ligne $nolig invalide : $l\n";
+	}
+
+	if ($l =~ /([^=]+)=([^|]*)\|(.*)/) {
+		my $value=getValue($2,$3);
+		if ($1 =~ /SERVICE([0-9]+)_([A-Z_]+)/)
+		{
+			my $idx = $1;			
+			$services[$idx]{$2}=$value;
+			if ( $2 =~ /(HTTPS?)_PORT/ ) {
+				if ( $value == 65535 ) {
+					$services[$idx]{$1."_ENABLED"}="disabled";
+				} else {
+					$services[$idx]{$1."_ENABLED"}="enabled";
+				}
+			}
+		}
+		else
+		{
+			if ($1 eq "DDNS" && $value == 65535) {
+				$value = "localdomain";
+			}
+			$params{$1}=$value;
+		}
+	} else	{
+		if ($l =~ /([^=]+)=(.*)/) {
+			my $value=$2;
+
+			if ($1 =~ /SERVICE([0-9]+)_([A-Z_]+)/)
+			{
+				$services[$1]{$2}=$value;
+				if ($2 eq "NAME") {
+					$services_idx[$i] = $1;
+					$i++;
+				}
+			}
+			else
+			{
+				$params{$1}=$value;
+			}
+		}
+	}
+}
+close(IN);
+
+# build conf file from parameters
+open(IN,"haproxy.tpl");
+open(OUT,">haproxy.conf");
+while($l=<IN>) {
+	if ($l =~ /#backend_http#/) {
+		for $i (0 .. $#services_idx) {
+			my $ligne = $use_backend_http;
+			my $idx = $services_idx[$i];
+			$ligne =~ s/\@SERVICE_([^@]+)@/$services[$idx]{$1}/g;
+			$ligne =~ s/@([^@]+)@/$params{$1}/g;
+			print OUT $ligne."\n";
+		}
+	} else {
+		if ($l =~ /#backend_https#/) {
+			for $i (0 .. $#services_idx) {
+				my $ligne = $use_backend_https;
+				my $idx = $services_idx[$i];
+				$ligne =~ s/\@SERVICE_([^@]+)@/$services[$idx]{$1}/g;
+				$ligne =~ s/@([^@]+)@/$params{$1}/g;
+				print OUT $ligne."\n";
+			}
+		} else {
+			if ($l =~ /#backend#/) {
+				for $i (0 .. $#services_idx) {
+					my $ligne = $backend;
+					my $idx = $services_idx[$i];
+					$ligne =~ s/\@SERVICE_([^@]+)@/$services[$idx]{$1}/g;
+					$ligne =~ s/@([^@]+)@/$params{$1}/g;
+					print OUT $ligne."\n";
+				}
+			} else {
+				$l =~ s/@([^@]+)@/$params{$1}/g;
+				print OUT $l;
+			}
+		}
+	}
+}
+close(IN);
+close(OUT);
+
+sub getValue
+{
+	my $file = $_[0];
+	my $regexp = $_[1];
+	if (open (FIC,$file)) {
+		while($l=<FIC>)
+		{
+			if ($l =~ $regexp)
+			{
+				close(FIC);
+				return $1;
+			}
+		}
+	} else {
+		return 65535;
+	}
+	return 65535;
+}
