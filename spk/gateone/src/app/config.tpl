3cab71c79b9a24b3438ab2634a71185431e149b0
Emmanuel BEZAGU <emmanuel.bezagu@gmail.com>
2012-09-09 23:32:22 +0200

    Add GateOne spk

---
+++ spk/gateone/src/app/config.tpl
@@ -0,0 +1,12 @@
+{
+    ".url": {
+        "com.synocommunity.packages.gateone": {
+            "title": "GateOne",
+            "desc": "GateOne Web UI",
+            "icon": "images/gateone-{0}.png",
+            "type": "legacy",
+            "url": "https://gateone.==:DDNS:==/",
+            "allUsers": true
+        }
+    }
+}
---
+++ spk/gateone/src/installer.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# Package
+PACKAGE="gateone"
+DNAME="GateOne"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+PYTHON="${INSTALL_DIR}/env/bin/python"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+CFG_FILE="${INSTALL_DIR}/var/config.ini"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    perl ${INSTALL_DIR}/var/conf/setConf.pl
+
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env > /dev/null
+
+    # Install the bundle
+    ${INSTALL_DIR}/env/bin/pip install -U -b ${INSTALL_DIR}/var/build ${INSTALL_DIR}/share/requirements.pybundle > /dev/null
+    rm -fr ${INSTALL_DIR}/var/build
+
+    PATH=${PATH} ${PYTHON} ${INSTALL_DIR}/share/GateOne/setup.py install --prefix=${INSTALL_DIR}
+
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    # Save some stuff
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    exit 0
+}
+
+postupgrade ()
+{
+    # Restore some stuff
+    rm -fr ${INSTALL_DIR}/var
+    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
+    exit 0
+}
+
