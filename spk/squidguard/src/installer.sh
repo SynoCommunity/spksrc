9307720d3db8c8cebba78faee25c03b6394b3c1b
manu <manu@debian.nantes.dgi>
2012-10-05 16:43:57 +0200

    Add squidclamav and squidguardmanager to squid

--- spk/squidguard/src/installer.sh
+++ spk/squidguard/src/installer.sh
@@ -10,6 +10,8 @@ PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 SQUID="${INSTALL_DIR}/sbin/squid"
 RUNAS="squid"
 CFG_FILE="${INSTALL_DIR}/etc/squid.conf"
+WWW_DIR="/var/packages/${PACKAGE}/target/share/www/squidguardmgr"
+WEBMAN_DIR="/usr/syno/synoman/webman/3rdparty"
 
 preinst ()
 {
@@ -33,9 +35,22 @@ postinst ()
     # Init squid cache directory
     su - ${RUNAS} -c "${SQUID} -z -f ${CFG_FILE}"
 
-    # Init squidGuad DB
+    # Init squidGuard DB
     su - ${RUNAS} -c "${INSTALL_DIR}/bin/update_db.sh > ${INSTALL_DIR}/var/logs/update_db.log"
 
+    # Install webman
+    su - ${RUNAS} -c "ln -s $WWW_DIR ${WEBMAN_DIR}/${PACKAGE}"
+    su - ${RUNAS} -c "ln -s ${INSTALL_DIR}/etc/squiguardmgr.conf ${WEBMAN_DIR}/${PACKAGE}/"
+      
+    # Init crontab : update squidguard DB each day at 1 a.m
+    nb=`grep ${PACKAGE} /etc/crontab`
+    if [ "x$nb" eq "x" ]; then
+        echo "0 1       *       *       *       root    su - ${RUNAS} -c \"${INSTALL_DIR}/bin/update_db.sh > ${INSTALL_DIR}/var/logs/update_db.log\"" >> /etc/crontab
+        /usr/syno/etc/rc.d/S04crond.sh stop
+        sleep 1
+        /usr/syno/etc/rc.d/S04crond.sh start
+    fi
+    
     exit 0
 }
 
@@ -48,7 +63,9 @@ postuninst ()
 {
     # Remove link
     rm -f ${INSTALL_DIR}
-
+    rm -Rf ${WEBMAN_DIR}/${PACKAGE}
+    sed "/${PACKAGE}/d" /etc/crontab
+    
     exit 0
 }
 
