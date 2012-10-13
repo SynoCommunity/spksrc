9ddbca21ed04e6537ebcc8b8322db7d9ae832eb7
Angelo Tata <angelo.tata@gmail.com>
2012-10-14 01:43:43 +0200

    OPEN - task 257: Python and the Package center (suggestions) http://github.com/SynoCommunity/spksrc/issues/issue/257
    Changed Python post-install script to save version information to a
    file.
    
    Added Python package start-stop-status script, to enable the Package
    Manager to read that file and, besides, show the package status.
    
    Updated Python package makefile, to include the start-stop-status script
    and remove the start-stop button.

--- spk/python/src/installer.sh
+++ spk/python/src/installer.sh
@@ -26,6 +26,16 @@ postinst ()
     ${INSTALL_DIR}/bin/python -m compileall -q -f ${INSTALL_DIR}/lib/python2.7
     ${INSTALL_DIR}/bin/python -OO -m compileall -q -f ${INSTALL_DIR}/lib/python2.7
 
+    # Save some information about the newly installed package
+    ${INSTALL_DIR}/bin/python --version > ${SYNOPKG_PKGDEST}/output.log 2>&1
+    echo >> ${SYNOPKG_PKGDEST}/output.log
+    echo System installed modules: >> ${SYNOPKG_PKGDEST}/output.log
+    ${INSTALL_DIR}/bin/pip freeze >> ${SYNOPKG_PKGDEST}/output.log
+
+    echo "NOTE - Ignore that last sentence. This package does *not* start and stop like other packages. "
+    echo "Python is correctly installed if you can see the version number "
+    echo "and module information in the package Log tab."
+
     exit 0
 }
 
