b6f2f5b68bf71ae0a51d49de2cc9061f4cf08373
Manu <manu@XUbuntu1.(none)>
2012-11-09 12:59:22 +0100

    mc now works

---
+++ spk/mc/src/dsm-control.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Package
+PACKAGE="midnightcommander"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+
+
+case $1 in
+    start)
+        exit 0
+    ;;
+    stop)
+        exit 0
+    ;;
+    status)
+        exit 1
+    ;;
+    log)
+        echo "${INSTALL_DIR}/install.log"
+        exit 0
+    ;;
+esac
