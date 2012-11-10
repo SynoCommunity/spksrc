e4bb8af51827d340986fe252d52a8dafc2050574
Antoine Bertin <diaoulael@gmail.com>
2012-11-10 13:36:10 +0100

    Improve dsm-control of python

--- spk/python/src/dsm-control.sh
+++ spk/python/src/dsm-control.sh
@@ -12,15 +12,18 @@ PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 case $1 in
     start)
         exit 0
-    ;;
+        ;;
     stop)
         exit 0
-    ;;
+        ;;
     status)
         exit 1
-    ;;
+        ;;
     log)
         echo "${INSTALL_DIR}/install.log"
         exit 0
-    ;;
+        ;;
+    *)
+        exit 1
+        ;;
 esac
