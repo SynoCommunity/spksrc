27fbd8c98d3a4bd30bcc1e0fb227997a151a8ba1
Antoine Bertin <diaoulael@gmail.com>
2012-11-11 14:01:00 +0100

    Add lazylibrarian spk

---
+++ spk/lazylibrarian/src/app/lazylibrarian.cgi.py
@@ -0,0 +1,12 @@
+#!/usr/local/lazylibrarian/env/bin/python
+import os
+import ConfigParser
+
+
+config = ConfigParser.ConfigParser()
+config.read('/usr/local/lazylibrarian/var/config.ini')
+protocol = 'http'
+port = int(config.get('General', 'http_port'))
+
+print 'Location: %s://%s:%d' % (protocol, os.environ['SERVER_NAME'], port)
+print
