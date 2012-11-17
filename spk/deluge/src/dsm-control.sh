0adbb73e5795223019d5f1ba374dcdb953de73cc
Antoine Bertin <diaoulael@gmail.com>
2012-11-18 00:24:49 +0100

    Add deluge spk

---
+++ spk/deluge/src/dsm-control.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+# Package
+PACKAGE="deluge"
+DNAME="Deluge"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+RUNAS="deluge"
+PYTHON="${INSTALL_DIR}/env/bin/python"
+DELUGED="${INSTALL_DIR}/env/bin/deluged"
+DELUGE_WEB="${INSTALL_DIR}/env/bin/deluge-web"
+CFG_DIR="${INSTALL_DIR}/var/"
+DELUGED_PID="${INSTALL_DIR}/var/deluged.pid"
+DELUGE_WEB_PID="${INSTALL_DIR}/var/deluge-web.pid"
+DELUGED_LOG="${INSTALL_DIR}/var/deluged.log"
+DELUGE_WEB_LOG="${INSTALL_DIR}/var/deluge-web.log"
+
+
+start_daemon()
+{
+    start-stop-daemon -S -q -x ${DELUGED} -c ${RUNAS} -u ${RUNAS} -p ${DELUGED_PID} \
+      -- --quiet --config ${CFG_DIR} --logfile ${DELUGED_LOG} --loglevel info --pidfile ${DELUGED_PID}
+    sleep 3
+    start-stop-daemon -S -q -b -m -x ${DELUGE_WEB} -c ${RUNAS} -u ${RUNAS} -p ${DELUGE_WEB_PID} \
+      -- --quiet --config ${CFG_DIR} --logfile ${DELUGE_WEB_LOG} --loglevel info
+}
+
+stop_daemon()
+{
+    start-stop-daemon -K -q -u ${RUNAS} -p ${DELUGE_WEB_PID}
+    start-stop-daemon -K -q -u ${RUNAS} -p ${DELUGED_PID}
+    wait_for_status 1 20
+    if [ $? -eq 1 ]; then
+        start-stop-daemon -K -s 9 -q -u ${RUNAS} -p ${DELUGE_WEB_PID}
+        start-stop-daemon -K -s 9 -q -u ${RUNAS} -p ${DELUGED_PID}
+    fi
+    rm -f ${DELUGED_PID} ${DELUGE_WEB_PID}
+}
+
+daemon_status()
+{
+    start-stop-daemon -K -q -t -u ${RUNAS} -p ${DELUGE_WEB_PID}
+    DELUGE_WEB_RETVAL=$?
+    start-stop-daemon -K -q -t -u ${RUNAS} -p ${DELUGED_PID}
+    DELUGED_RETVAL=$?
+    [ ${DELUGED_RETVAL} -eq 0 -a ${DELUGE_WEB_RETVAL} -eq 0 ] || return 1
+}
+
+wait_for_status()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        if [ $? -eq $1 ]; then
+            return
+        fi
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
+}
+
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    log)
+        echo ${LOG_FILE}
+        ;;
+    *)
+        exit 1
+        ;;
+esac
