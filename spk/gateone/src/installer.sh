a7ed1ecf219549232b7a38b09f3ddcd4f1df4eb5
Antoine Bertin <diaoulael@gmail.com>
2012-11-18 01:22:54 +0100

    Improve gateone and remove unused dependency to busybox

--- spk/gateone/src/installer.sh
+++ spk/gateone/src/installer.sh
@@ -6,6 +6,7 @@ DNAME="GateOne"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
+SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PYTHON_DIR="/usr/local/python"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 PYTHON="${INSTALL_DIR}/env/bin/python"
@@ -66,6 +67,9 @@ postuninst ()
 
 preupgrade ()
 {
+    # Stop the package
+    ${SSS} stop > /dev/null
+
     # Save some stuff
     rm -fr ${TMP_DIR}/${PACKAGE}
     mkdir -p ${TMP_DIR}/${PACKAGE}
