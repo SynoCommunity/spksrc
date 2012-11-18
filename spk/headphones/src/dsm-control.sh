da1af1f1382cda23f8716681c5169b5d407f2cba
Antoine Bertin <diaoulael@gmail.com>
2012-11-18 01:16:54 +0100

    Improve headphones

--- spk/headphones/src/dsm-control.sh
+++ spk/headphones/src/dsm-control.sh
@@ -24,14 +24,14 @@ start_daemon()
 stop_daemon()
 {
     kill `cat ${PID_FILE}`
-    wait_for_status 1 20
+    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
     rm -f ${PID_FILE}
 }
 
 daemon_status()
 {
-    if [ -f ${PID_FILE} ] && [ -d /proc/`cat ${PID_FILE}` ]; then
-        return 0
+    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+        return
     fi
     return 1
 }
@@ -41,10 +41,11 @@ wait_for_status()
     counter=$2
     while [ ${counter} -gt 0 ]; do
         daemon_status
-        [ $? -eq $1 ] && break
+        [ $? -eq $1 ] && return
         let counter=counter-1
         sleep 1
     done
+    return 1
 }
 
 
@@ -81,4 +82,3 @@ case $1 in
         exit 1
         ;;
 esac
-
