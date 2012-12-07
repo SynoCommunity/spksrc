410bababcc81a74cfa50829283671f4e1a5a0b99
Antoine Bertin <diaoulael@gmail.com>
2012-12-08 00:45:33 +0100

    Add a configurable extract path for the extract target

--- mk/spksrc.extract.mk
+++ mk/spksrc.extract.mk
@@ -1,18 +1,19 @@
 ### Extract rules
-#   Extract the $(DIST_FILE) in $(WORK_DIR)
+#   Extract the $(DIST_FILE) in $(EXTRACT_PATH)
+# Override EXTRACT_PATH to specify path to use instead of $(WORK_DIR)
 # Target are executed in the following order:
 #  extract_msg_target
 #  pre_extract_target   (override with PRE_EXTRACT_TARGET)
 #  extract_target       (override with EXTRACT_TARGET)
 #  post_extract_target  (override with POST_EXTRACT_TARGET)
-
+EXTRACT_PATH ?= $(WORK_DIR)
 # Extract commands
-EXTRACT_CMD.tgz = tar -xzpf $(DIST_FILE) -C $(WORK_DIR)
-EXTRACT_CMD.tar.gz = tar -xzpf $(DIST_FILE) -C $(WORK_DIR)
-EXTRACT_CMD.tar.bz2 = tar -xjpf $(DIST_FILE) -C $(WORK_DIR)
-EXTRACT_CMD.tar.xz = tar -xJpf $(DIST_FILE) -C $(WORK_DIR)
-EXTRACT_CMD.tar.lzma = tar --lzma -xpf $(DIST_FILE) -C $(WORK_DIR)
-EXTRACT_CMD.zip = unzip $(DIST_FILE) -d $(WORK_DIR)
+EXTRACT_CMD.tgz = tar -xzpf $(DIST_FILE) -C $(EXTRACT_PATH)
+EXTRACT_CMD.tar.gz = tar -xzpf $(DIST_FILE) -C $(EXTRACT_PATH)
+EXTRACT_CMD.tar.bz2 = tar -xjpf $(DIST_FILE) -C $(EXTRACT_PATH)
+EXTRACT_CMD.tar.xz = tar -xJpf $(DIST_FILE) -C $(EXTRACT_PATH)
+EXTRACT_CMD.tar.lzma = tar --lzma -xpf $(DIST_FILE) -C $(EXTRACT_PATH)
+EXTRACT_CMD.zip = unzip $(DIST_FILE) -d $(EXTRACT_PATH)
 
 EXTRACT_CMD = $(EXTRACT_CMD.$(DIST_EXT)) 
 
@@ -44,7 +45,7 @@ extract_msg:
 pre_extract_target: extract_msg
 
 extract_target: $(PRE_EXTRACT_TARGET)
-	@mkdir -p $(WORK_DIR)
+	@mkdir -p $(EXTRACT_PATH)
 	$(EXTRACT_CMD)
 
 post_extract_target: $(EXTRACT_TARGET) 
@@ -58,4 +59,3 @@ $(EXTRACT_COOKIE): $(POST_EXTRACT_TARGET)
 else
 extract: ;
 endif
-
