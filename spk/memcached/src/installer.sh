ea86234ab8f7652e7fa9526dcfe9e325879a83ed
Antoine Bertin <diaoulael@gmail.com>
2012-12-08 11:16:22 +0100

    Add memcached spk

---
+++ spk/memcached/src/installer.sh
@@ -0,0 +1,89 @@
+#!/bin/sh
+
+# Package
+PACKAGE="memcached"
+DNAME="Memcached"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+WEB_DIR="/var/services/web"
+SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+USER="memcached"
+GROUP="nobody"
+MEMCACHED="${INSTALL_DIR}/bin/memcached"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Install the web interface
+    cp -R ${INSTALL_DIR}/share/phpMemcachedAdmin ${WEB_DIR}
+
+    # Install busybox stuff
+    ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
+
+    # Create user
+    adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
+
+    # Correct the files ownership
+    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
+
+    exit 0
+}
+
+preuninst ()
+{
+    # Stop the package
+    ${SSS} stop > /dev/null
+
+    # Remove the user (if not upgrading)
+    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
+        delgroup ${USER} ${GROUP}
+        deluser ${USER}
+    fi
+
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove the web interface
+    rm -fr ${WEB_DIR}/phpMemcachedAdmin
+
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    # Stop the package
+    ${SSS} stop > /dev/null
+
+    # Save some stuff
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    exit 0
+}
+
+postupgrade ()
+{
+    # Restore some stuff
+    rm -fr ${INSTALL_DIR}/var
+    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
+    exit 0
+}
