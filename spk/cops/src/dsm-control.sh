3df28b27ed70d11bf4fd288238d5023c4f60a678
Sébastien Lucas <sebastien@slucas.fr>
2013-01-13 21:13:34 +0100

    Add a new spk for COPS
    Mainly made from copy/paste with sed so let's hope it's all right
    
    For now Icon is Calibre (no icon for COPS).

---
+++ spk/cops/src/dsm-control.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Package
+PACKAGE="cops"
+DNAME="COPS"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+WEB_DIR="/var/services/web"
+ENABLED_FILE="/var/packages/${PACKAGE}/enabled"
+
+daemon_status ()
+{
+    if [ -f ${ENABLED_FILE} ]; then
+        return
+    fi
+    return 1
+}
+
+
+case $1 in
+    start)
+        exit 0
+        ;;
+    stop)
+        exit 0
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    log)
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
+esac
