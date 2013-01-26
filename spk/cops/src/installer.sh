cfb63e937d325c869687e3971ebf247aa590908d
Sébastien Lucas <sebastien@slucas.fr>
2013-01-26 14:11:09 +0100

    * Add a default config in the spk  * Add a wizard to configure automatically the paths  * Add a french description

--- spk/cops/src/installer.sh
+++ spk/cops/src/installer.sh
@@ -6,7 +6,9 @@ DNAME="COPS"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
+DEFAULT_CFG_FILE="/usr/local/${PACKAGE}/config_local.php.synology"
 WEB_DIR="/var/services/web"
+CFG_FILE="${WEB_DIR}/${PACKAGE}/config_local.php"
 
 
 preinst ()
@@ -23,9 +25,12 @@ postinst ()
     cp -R ${INSTALL_DIR}/share/${PACKAGE} ${WEB_DIR}
 
     # Create a default configuration file
-    if [ ! -f ${WEB_DIR}/${PACKAGE}/config_local.php ]; then
-      echo -e "<?php\nif (\0041isset(\$config)) \$config = array();\n\$config['calibre_directory'] = './';\n?>" > ${WEB_DIR}/${PACKAGE}/config_local.php
-      chmod ga+w ${WEB_DIR}/${PACKAGE}/config_local.php
+    if [ ! -f ${CFG_FILE} ]; then
+      cp ${DEFAULT_CFG_FILE} ${CFG_FILE}
+      sed -i -e "s|@calibre_dir@|${wizard_calibre_dir:=/volume1/calibre}|g" ${CFG_FILE}
+      sed -i -e "s|@cops_title@|${wizard_cops_title:=COPS}|g" ${CFG_FILE}
+      sed -i -e "s|@use_url_rewriting@|${wizard_use_url_rewriting:=0}|g" ${CFG_FILE}
+      chmod ga+w ${CFG_FILE}
     fi
 
     exit 0
