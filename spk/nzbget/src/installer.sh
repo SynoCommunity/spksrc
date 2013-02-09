c30623d7d9f477e4c7fa5d7584bda8fb4adde950
Antoine Bertin <diaoulael@gmail.com>
2013-02-09 11:05:42 +0100

    Require uninstallation for nzbget 9.1
    Changes to the post-process script and configuration file
    are required

--- spk/nzbget/src/installer.sh
+++ spk/nzbget/src/installer.sh
@@ -67,8 +67,8 @@ preupgrade ()
     # Stop the package
     ${SSS} stop > /dev/null
 
-    # Revision 4 introduces backward incompatible changes
-    if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -lt 4 ]; then
+    # Revision 7 introduces backward incompatible changes
+    if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -lt 7 ]; then
         echo "Please uninstall previous version, no update possible"
         exit 1
     fi
