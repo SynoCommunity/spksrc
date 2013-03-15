9e76b06ab831dbc85f26079347a89b220275d8a5
Antoine Bertin <diaoulael@gmail.com>
2013-03-15 04:07:31 +0100

    Update newznab spk

--- spk/newznab/src/dsm-control.sh
+++ spk/newznab/src/dsm-control.sh
@@ -6,24 +6,56 @@ DNAME="Newznab"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
-WEB_DIR="/var/services/web"
-ENABLED_FILE="/var/packages/${PACKAGE}/enabled"
+PATH="${INSTALL_DIR}/bin:${PATH}"
+NEWZNAB="${INSTALL_DIR}/bin/newznab.sh"
+PID_FILE="${INSTALL_DIR}/var/newznab.pid"
+
+
+start_daemon ()
+{
+    start-stop-daemon -S -q -m -x ${NEWZNAB} -p ${PID_FILE}
+}
+
+stop_daemon ()
+{
+    start-stop-daemon -K -q -p ${PID_FILE}
+    wait_for_status 1 20 || start-stop-daemon -K -s 9 -q -p ${PID_FILE}
+}
 
 daemon_status ()
 {
-    if [ -f ${ENABLED_FILE} ]; then
-        return
-    fi
+    start-stop-daemon -K -q -t -p ${PID_FILE}
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
     return 1
 }
 
 
 case $1 in
     start)
-        exit 0
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+        fi
         ;;
     stop)
-        exit 0
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
         ;;
     status)
         if daemon_status; then
