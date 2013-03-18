63ea8239f5a78b06b629c75817a958c6bd586121
Antoine Bertin <diaoulael@gmail.com>
2013-03-18 01:17:13 +0100

    Add selfoss spk

---
+++ spk/selfoss/src/dsm-control.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+# Package
+PACKAGE="selfoss"
+DNAME="Selfoss"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+WEB_DIR="/var/services/web"
+PATH="${INSTALL_DIR}/bin:${PATH}"
+USER="nobody"
+SELFOSS="${INSTALL_DIR}/bin/selfoss.sh"
+PID_FILE="${INSTALL_DIR}/var/selfoss.pid"
+
+
+start_daemon ()
+{
+    start-stop-daemon -S -q -m -b -N 10 -x ${SELFOSS} -c ${USER} -u ${USER} -p ${PID_FILE}
+}
+
+stop_daemon ()
+{
+    start-stop-daemon -K -q -u ${USER} -p ${PID_FILE}
+    wait_for_status 1 20 || start-stop-daemon -K -s 9 -q -p ${PID_FILE}
+}
+
+daemon_status ()
+{
+    start-stop-daemon -K -q -t -u ${USER} -p ${PID_FILE}
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
+}
+
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    log)
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
+esac
