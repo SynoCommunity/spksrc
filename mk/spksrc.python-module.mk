1ddd3a0aba88abec025cf6e484e8777c6c518012
Antoine Bertin <diaoulael@gmail.com>
2013-05-29 18:57:36 +0200

    Update python to 2.7.5

--- mk/spksrc.python-module.mk
+++ mk/spksrc.python-module.mk
@@ -20,7 +20,7 @@ include ../../mk/spksrc.cross-cc.mk
 -include $(WORK_DIR)/python-cc.mk
 
 # Python module variables
-PYTHONPATH = $(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_DIR)/site-packages/
+PYTHONPATH = $(PYTHON_LIB_NATIVE):$(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_DIR)/site-packages/
 
 
 ### Python module rules
