21bfa276fc04bb08378482afdbaeb85c4e94c935
Antoine Bertin <diaoulael@gmail.com>
2013-06-02 17:15:52 +0200

    Update bicbucstriim to 1.1.0, add wizard and remove beta

--- spk/bicbucstriim/src/dsm-control.sh
+++ spk/bicbucstriim/src/dsm-control.sh
@@ -4,19 +4,6 @@
 PACKAGE="bicbucstriim"
 DNAME="BicBucStriim"
 
-# Others
-INSTALL_DIR="/usr/local/${PACKAGE}"
-WEB_DIR="/var/services/web"
-ENABLED_FILE="/var/packages/${PACKAGE}/enabled"
-
-daemon_status ()
-{
-    if [ -f ${ENABLED_FILE} ]; then
-        return
-    fi
-    return 1
-}
-
 
 case $1 in
     start)
@@ -26,13 +13,7 @@ case $1 in
         exit 0
         ;;
     status)
-        if daemon_status; then
-            echo ${DNAME} is running
-            exit 0
-        else
-            echo ${DNAME} is not running
-            exit 1
-        fi
+        exit 0
         ;;
     log)
         exit 1
