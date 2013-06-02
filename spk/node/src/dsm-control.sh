33d3ab7199d8ea8233f9031470b787d3980bf12f
Antoine Bertin <diaoulael@gmail.com>
2013-06-02 11:58:09 +0200

    Update node scripts

--- spk/node/src/dsm-control.sh
+++ spk/node/src/dsm-control.sh
@@ -4,20 +4,24 @@
 PACKAGE="node"
 DNAME="Node.js"
 
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+
 
 case $1 in
     start)
-	exit 0
-       ;;
+        exit 0
+        ;;
     stop)
-	exit 0
-       ;;
+        exit 0
+        ;;
     status)
-	exit 0
-       ;;
+        exit 0
+        ;;
     log)
         exit 0
         ;;
     *)
+        exit 1
         ;;
 esac
