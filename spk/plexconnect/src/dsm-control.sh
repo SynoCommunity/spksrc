b62d55bf33d60ef7859ca3a0737a4f2e0528027f
Bert Wynants <bwynants+drupal-git@gmail.com>
2013-07-04 21:06:57 +0200

    daemonize mode

--- spk/plexconnect/src/dsm-control.sh
+++ spk/plexconnect/src/dsm-control.sh
@@ -12,56 +12,72 @@ PYTHON="${PYTHON_DIR}/bin/python"
 PATH="${PYTHON_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 RUNAS="${PACKAGE}"
 PROG_PY="${INSTALL_DIR}/share/PlexConnect/PlexConnect.py"
+PID_FILE="${INSTALL_DIR}/var/PlexConnect.pid"
 LOG_FILE="${INSTALL_DIR}/share/PlexConnect/PlexConnect.log"
 
 start_daemon ()
 {
     # Launch the application in the background as root so we can open the dns port
-    su -c "PATH=${PATH} ${PYTHON} ${PROG_PY}" &
+    su -c "PATH=${PATH} ${PYTHON} ${PROG_PY} --daemon --pidfile ${PID_FILE}"
 }
 
 stop_daemon ()
 {
-    # Kill the application
-    kill `ps w | grep ${PACKAGE} | grep -v -E 'stop|grep' | awk '{print $1}'`
+    kill `cat ${PID_FILE}`
+    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
+    rm -f ${PID_FILE}
 }
 
 
 daemon_status ()
 {
-   ${INSTALL_DIR}/app/updateInfo
-   if [ `ps w | grep ${PACKAGE} | grep -v -E 'status|grep' | wc -l` -gt 0 ]
-    then
-        return 0
-    else
-        return 1
+    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+        return
     fi
+    rm -f ${PID_FILE}
+    return 1
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
 }
 
 case $1 in
     start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
             echo Starting ${DNAME} ...
             start_daemon
-            exit $?
+        fi
         ;;
     stop)
+        if daemon_status; then
             echo Stopping ${DNAME} ...
-			stop_daemon
-            exit 0
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
         ;;
-	status)
-			if daemon_status
-			then
-				echo ${DNAME} is running
-				exit 0
-			else
-				echo ${DNAME} is not running
-				exit 1
-			fi
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
         ;;
     log)
-			echo ${LOG_FILE}
-			exit 0
+        echo ${LOG_FILE}
         ;;
     *)
         exit 1
