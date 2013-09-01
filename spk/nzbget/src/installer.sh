85dd573baa3bf87441647463baff91d16b578173
Antoine Bertin <diaoulael@gmail.com>
2013-09-01 10:14:14 +0200

    Update nzbget to 11.0

--- spk/nzbget/src/installer.sh
+++ spk/nzbget/src/installer.sh
@@ -31,8 +31,10 @@ postinst ()
     adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
 
     # Edit the configuration according to the wizard
-    sed -i -e "s|@download_dir@|${wizard_download_dir:=/volume1/downloads}|g" ${CFG_FILE}
-    sed -i -e "s/@server_password@/${wizard_server_password:=nzbget}/g" ${CFG_FILE}
+    sed -i -e "s|@download_dir@|${wizard_download_dir:=/volume1/downloads}|g" \
+           -e "s/@control_username@/${wizard_control_username:=nzbget}/g" \
+           -e "s/@control_password@/${wizard_control_password:=nzbget}/g" \
+           ${CFG_FILE}
 
     # Correct the files ownership
     chown -R ${USER}:root ${SYNOPKG_PKGDEST}
@@ -67,8 +69,8 @@ preupgrade ()
     # Stop the package
     ${SSS} stop > /dev/null
 
-    # Revision 8 introduces backward incompatible changes
-    if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -lt 8 ]; then
+    # Revision 9 introduces backward incompatible changes
+    if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -lt 9 ]; then
         echo "Please uninstall previous version, no update possible"
         exit 1
     fi
