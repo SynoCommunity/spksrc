5e75c0c5cc98b5ff70396bbcc7abc5b1b8eb7bd2
Bastien Nocera <hadess@hadess.net>
2013-09-25 14:28:42 +0200

    Add a get_iplayer SPK

---
+++ spk/get_iplayer/src/installer.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Package
+PACKAGE="get_iplayer"
+DNAME="get_iplayer"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
