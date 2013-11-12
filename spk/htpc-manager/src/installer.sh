e121b2beb35cd76319db7c935a989f958e72455d
cytec <iamcytec@googlemail.com>
2013-11-12 13:14:05 +0100

    fixed installer.sh

--- spk/htpc-manager/src/installer.sh
+++ spk/htpc-manager/src/installer.sh
@@ -9,7 +9,7 @@ INSTALL_DIR="/usr/local/${PACKAGE}"
 SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PYTHON_DIR="/usr/local/python"
 GIT_DIR="/usr/local/git"
-PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin:${PATH}"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
 USER="htpcmanager"
 GROUP="users"
 GIT="${GIT_DIR}/bin/git"
@@ -27,18 +27,11 @@ postinst ()
     # Link
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
-    #make log writeable
-    chmod 777 /var/log/lighttpd/access.log
-
     # Create a Python virtualenv
     ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env > /dev/null
 
     #clone the repository
     ${GIT} clone -q git://github.com/styxit/HTPC-Manager.git ${INSTALL_DIR}/share/HTPC-Manager
-
-    # Install configobj
-    ${INSTALL_DIR}/env/bin/pip install -U -b ${INSTALL_DIR}/var/build sqlite3 > /dev/null
-    rm -fr ${INSTALL_DIR}/var/build
 
     # Create user
     adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
