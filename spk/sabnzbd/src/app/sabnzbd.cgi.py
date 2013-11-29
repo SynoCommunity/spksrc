c37ed3621ec1b6f3ff5a72235a66752ee53b9ee1
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-11-29 17:23:01 +0000

    [sabnzdb] fix typo.

--- spk/sabnzbd/src/app/sabnzbd.cgi.py
+++ spk/sabnzbd/src/app/sabnzbd.cgi.py
@@ -3,7 +3,7 @@ import os
 import configobj
 
 
-config = configobj.ConfigObj('/var/services/homes/oscam/config.ini')
+config = configobj.ConfigObj('/var/services/homes/sabnzbd/config.ini')
 protocol = 'https' if int(config['misc']['enable_https']) else 'http'
 https_port = int(config['misc']['port']) if len(config['misc']['https_port']) == 0 else int(config['misc']['https_port'])
 port = https_port if protocol == 'https' else int(config['misc']['port'])
