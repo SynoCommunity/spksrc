f5f3b9ea52c6e73e3ee1fdd061ea3ddd8a821e07
Dr-Bean <github@beanpoint.com>
2013-12-04 18:11:56 +0100

    spksrc: Firewall/port-forwarding support by adding FWPORTS See "Install Package Related Ports Information into DSM" of Synology Developers Guide
    Usage:
    Makefile:
    - Add FWPORTS = src/${SPK_NAME}.sc
    Installer.sh:
    - Add FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
    - Add SERVICETOOL="/usr/syno/bin/servicetool"
    - postinst ():
        # Add port-forwarding config
        ${SERVICETOOL} --install-configure-file --package ${FWPORTS}
    - preuninst ():
        # Remove port-forwarding config
        if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
            ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc
        fi

--- mk/spksrc.spk.mk
+++ mk/spksrc.spk.mk
@@ -153,6 +153,7 @@ ifneq ($(strip $(SSS_SCRIPT)),)
 DSM_SCRIPTS_ += start-stop-status
 endif
 DSM_SCRIPTS_ += installer
+DSM_SCRIPTS_ += $(notdir $(FWPORTS))
 DSM_SCRIPTS_ += $(notdir $(basename $(ADDITIONAL_SCRIPTS)))
 
 DSM_SCRIPTS = $(addprefix $(DSM_SCRIPTS_DIR)/,$(DSM_SCRIPTS_))
@@ -190,7 +191,9 @@ $(DSM_SCRIPTS_DIR)/start-stop-status: $(SSS_SCRIPT)
 	@$(dsm_script_copy)
 $(DSM_SCRIPTS_DIR)/installer: $(INSTALLER_SCRIPT)
 	@$(dsm_script_copy)
-$(DSM_SCRIPTS_DIR)/%: $(filter %.sh,$(ADDITIONAL_SCRIPTS)) 
+$(DSM_SCRIPTS_DIR)/%: $(filter %.sc,$(FWPORTS))
+	@$(dsm_script_copy)	
+$(DSM_SCRIPTS_DIR)/%: $(filter %.sh,$(ADDITIONAL_SCRIPTS))
 	@$(dsm_script_copy)
 
 
