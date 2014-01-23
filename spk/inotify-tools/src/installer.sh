8274b82e0294ad3ef7737d00e9f5a05b84164d24
Francis Besset <francis.besset@gmail.com>
2014-01-23 16:57:48 +0100

    Added inotify-tools

---
+++ spk/inotify-tools/src/installer.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Package
+PACKAGE="inotify-tools"
+DNAME="inotify-tools"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:${PATH}"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Put mc in the PATH
+    mkdir -p /usr/local/bin
+    ln -s ${INSTALL_DIR}/bin/inotifywait /usr/local/bin/inotifywait
+    ln -s ${INSTALL_DIR}/bin/inotifywatch /usr/local/bin/inotifywatch
+
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+    rm -f /usr/local/bin/inotifywait
+    rm -f /usr/local/bin/inotifywatch
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
