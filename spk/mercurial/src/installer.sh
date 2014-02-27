293beda2c92ae2d7c263ecc11676a198a8a69074
Dr-Bean <github@beanpoint.com>
2014-02-27 11:44:36 +0100

    Add Mercurial SPK using wheel

---
+++ spk/mercurial/src/installer.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Package
+PACKAGE="mercurial"
+DNAME="Mercurial"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+PATH="${INSTALL_DIR}/env/bin:${INSTALL_DIR}/bin:${PYTHON_DIR}/bin:${PATH}"
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+    
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env > /dev/null
+
+    # Install the wheels
+    ${INSTALL_DIR}/env/bin/pip install --use-wheel --no-deps --no-index -U --force-reinstall -f ${INSTALL_DIR}/share/wheelhouse -r ${INSTALL_DIR}/share/wheelhouse/requirements.txt > /dev/null 2>&1
+
+    exit 0
+}
+
+preuninst ()
+{
+
+    exit 0
+}
+
+postuninst ()
+{
+
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
