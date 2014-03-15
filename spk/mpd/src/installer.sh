5c748bdc5503cce3adeb52bbc7aa0b5595a2972a
Maxime Gauduin <alucryd@gmail.com>
2014-03-15 21:16:55 +0100

    Add mpd and mpdscribble

---
+++ spk/mpd/src/installer.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Package
+PACKAGE="mpd"
+DNAME="Music Player Daemon"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
+PATH="${INSTALL_DIR}/bin:${PATH}"
+USER="mpd"
+GROUP="users"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Install busybox stuff
+    ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
+
+    # Create user
+    adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
+
+    # Correct the files ownership
+    chown -R ${USER}:${GROUP} ${SYNOPKG_PKGDEST}/var
+
+    exit 0
+}
+
+preuninst ()
+{
+    # Stop the package
+    ${SSS} stop > /dev/null
+
+    # Remove the user (if not upgrading)
+    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
+        delgroup ${USER} ${GROUP}
+        deluser ${USER}
+    fi
+
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    # Stop the package
+    ${SSS} stop > /dev/null
+
+    # Save some stuff
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    exit 0
+}
+
+postupgrade ()
+{
+    # Restore some stuff
+    rm -fr ${INSTALL_DIR}/var
+    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
+    exit 0
+}
