ee6f8ad4843fbc9b976ec960104f083187ab383a
Marcel Dopita <m@rcel.cz>
2014-03-29 10:32:17 +0100

    Fix DSM 5 compatibility for jappix

--- spk/jappix/src/installer.sh
+++ spk/jappix/src/installer.sh
@@ -7,7 +7,7 @@ DNAME="Jappix"
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
 WEB_DIR="/var/services/web"
-USER="nobody"
+USER="$([ $(grep buildnumber /etc.defaults/VERSION | cut -d"\"" -f2) -ge 4418 ] && echo -n http || echo -n nobody)"
 TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
 
 
