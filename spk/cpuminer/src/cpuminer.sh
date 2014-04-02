0d97fba3c78e14f9c81c44bfadbc86ed3c874239
TuXedMe <tuxedme@gmail.com>
2014-04-02 17:43:54 +0200

    removed busybox daemon option

--- spk/cpuminer/src/cpuminer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-INSTALL_DIR="/usr/local/${PACKAGE}"
-CPUMINER="${INSTALL_DIR}/bin/minerd"
-CFG_FILE="${INSTALL_DIR}/var/settings.json"
-LOG_FILE="${INSTALL_DIR}/var/cpuminer.log"
-
-${CPUMINER} -c ${CFG_FILE} -t 1 2> ${LOG_FILE}
--- spk/cpuminer/src/dsm-control.sh
+++ spk/cpuminer/src/dsm-control.sh
@@ -8,24 +8,31 @@ DNAME="CPUMiner"
 INSTALL_DIR="/usr/local/${PACKAGE}"
 PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 USER="cpuminer"
-CPUMINER="${INSTALL_DIR}/bin/cpuminer.sh"
+CPUMINER="${INSTALL_DIR}/bin/minerd"
+CFG_FILE="${INSTALL_DIR}/var/settings.json"
 LOG_FILE="${INSTALL_DIR}/var/cpuminer.log"
-PID_FILE="${INSTALL_DIR}/var/cpuminer.pid"
+DAEMON_MINER="minerd"
 
 start_daemon ()
 {
-    start-stop-daemon -S -q -m -b -N 10 -x ${CPUMINER} -c ${USER} -u ${USER} -p ${PID_FILE} > /dev/null
+    su - ${USER} -c "${CPUMINER} -c ${CFG_FILE} -t 1 2> ${LOG_FILE}"
 }
 
 stop_daemon ()
 {
-    start-stop-daemon -K -q -u ${USER} -p ${PID_FILE}
-    wait_for_status 1 20 || start-stop-daemon -K -s 9 -q -p ${PID_FILE}
+    PIDS=`pidof ${DAEMON_MINER}`
+    kill ${PIDS}
+    wait_for_status 1 20 || kill -9 ${PIDS}
 }
 
 daemon_status ()
 {
-    start-stop-daemon -K -q -t -u ${USER} -p ${PID_FILE}
+
+    PIDS=`pidof ${DAEMON_MINER}`
+    if [ ${PIDS} ]; then
+        return
+    fi
+    return 1
 }
 
 wait_for_status ()
@@ -37,9 +44,8 @@ wait_for_status ()
         let counter=counter-1
         sleep 1
     done
-return 1
+    return 1
 }
-
 
 case $1 in
     start)
