6280b07cf3048277e5ee4fdf7881e99c3e096325
TuXedMe <tuxedme@gmail.com>
2014-04-02 17:43:39 +0200

    remove threads variable, and quiet param from configuration

--- spk/cpuminer/src/cpuminer.sh
+++ spk/cpuminer/src/cpuminer.sh
@@ -3,5 +3,6 @@
 INSTALL_DIR="/usr/local/${PACKAGE}"
 CPUMINER="${INSTALL_DIR}/bin/minerd"
 CFG_FILE="${INSTALL_DIR}/var/settings.json"
+LOG_FILE="${INSTALL_DIR}/var/cpuminer.log"
 
-${CPUMINER} -c ${CFG_FILE} -t 4
+${CPUMINER} -c ${CFG_FILE} -t 1 2> ${LOG_FILE}
