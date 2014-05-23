SynoCommunity/spksrc

commit 47c63baae001fb37956f7a254136877cb4f9cd73

Fix native env so native python/glibc can be compiled from within spk
Should prevent reports like #659, #746, #978 etc.

mk/spksrc.native-env.mk
@@ -3,5 +3,6 @@ PKG_CONFIG_LIBDIR = $(INSTALL_DIR)/$(INSTALL_PREFIX)/lib/pkgconfig
 INSTALL_DIR = $(WORK_DIR)/install
 INSTALL_PREFIX = /usr/local
 
-ENV += LD= CPP= NM= CC= RANLIB= CXX= AR= STRIP= OBJDUM= CFLAGS= CPPFLAGS= LDFLAGS=
+ENV += -u PKG_CONFIG -u PKG_CONFIG_LIBDIR -u PKG_CONFIG_PATH
+ENV += LD= CPP= NM= CC= RANLIB= CXX= AR= STRIP= OBJDUM= CFLAGS= CPPFLAGS= LDFLAGS= CXXFLAGS= LDSHARED= READELF= AS= OBJDUMP=
 ENV += INSTALL_PREFIX=$(INSTALL_PREFIX)

commit 322335707f1c3ca4739df7249ade7a8149f696da

Fix ruby native compile
Broken by 47c63baae001fb37956f7a254136877cb4f9cd73

mk/spksrc.native-env.mk
@@ -3,6 +3,6 @@ PKG_CONFIG_LIBDIR = $(INSTALL_DIR)/$(INSTALL_PREFIX)/lib/pkgconfig
 INSTALL_DIR = $(WORK_DIR)/install
 INSTALL_PREFIX = /usr/local
 
-ENV += -u PKG_CONFIG -u PKG_CONFIG_LIBDIR -u PKG_CONFIG_PATH
-ENV += LD= CPP= NM= CC= RANLIB= CXX= AR= STRIP= OBJDUM= CFLAGS= CPPFLAGS= LDFLAGS= CXXFLAGS= LDSHARED= READELF= AS= OBJDUMP=
+ENV += -u PKG_CONFIG -u PKG_CONFIG_LIBDIR -u PKG_CONFIG_PATH -u LDSHARED
+ENV += LD= CPP= NM= CC= RANLIB= CXX= AR= STRIP= OBJDUM= CFLAGS= CPPFLAGS= LDFLAGS= CXXFLAGS= READELF= AS= OBJDUMP=
 ENV += INSTALL_PREFIX=$(INSTALL_PREFIX)