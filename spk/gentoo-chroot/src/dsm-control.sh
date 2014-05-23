86d16f6151e32d5d33d1f09fcacd9ab4386a78f0
Joe Nyland <joenyland@me.com>
2014-05-23 14:55:23 +0100

    Improve dsm-control.sh script.
    Mount ${CHROOTTARGET}/tmp to be tmpfs, so that /tmp is created at start the chroot and cleared up when stopping the chroot, as per http://wiki.gentoo.org/wiki/Chroot.
    Added warning to dsm-control.sh script for chroot if not started.
    Fix for /dev/fd errors in emerge.
    Unmount all mount points in the chroot env with a for loop. Some filesystems are mounted automatically by services, such as sysfs (which mounts ${CHROOTTARGET}/sys/fs/fuse/connections) and this stops dsm-control.sh from being able to unmount all filesystems when the chroot is stopped.
    Add restart functionality to control script.
    Add ability to mount custom directories in chroot.
    Add log case to dsm-control.sh to allow the display of the log file /var/log/messages from inside the chroot in DSM Package Center.
    Print message to user if dsm-control.sh is called with no arguments supplied.

--- spk/gentoo-chroot/src/dsm-control.sh
+++ spk/gentoo-chroot/src/dsm-control.sh
@@ -9,7 +9,6 @@ INSTALL_DIR="/usr/local/${PACKAGE}"
 PATH="${INSTALL_DIR}/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/syno/sbin:/usr/syno/bin"
 CHROOTTARGET=`realpath ${INSTALL_DIR}/var/chroottarget`
 
-
 start_daemon ()
 {
     # Mount if install is finished
@@ -19,7 +18,20 @@ start_daemon ()
         grep -q "${CHROOTTARGET}/sys " /proc/mounts || mount -t sysfs sys ${CHROOTTARGET}/sys
         grep -q "${CHROOTTARGET}/dev " /proc/mounts || mount -o bind /dev ${CHROOTTARGET}/dev
         grep -q "${CHROOTTARGET}/dev/pts " /proc/mounts || mount -o bind /dev/pts ${CHROOTTARGET}/dev/pts
-        
+        test -d "${CHROOTTARGET}/dev/fd" || ln -s '../proc/self/fd' ${CHROOTTARGET}/dev/fd
+        grep -q "${CHROOTTARGET}/tmp " /proc/mounts || mount -t tmpfs /tmp ${CHROOTTARGET}/tmp
+
+    # Mount user specified directories, if specified
+    if [ -f ${INSTALL_DIR}/etc/mounts ]; then
+        if [ `sed -e 's/#.*$//g' -e '/^$/d' ${INSTALL_DIR}/etc/mounts | wc -l` != 0 ]; then
+            sed -e 's/#.*$//g' -e '/^$/d' ${INSTALL_DIR}/etc/mounts | while read mount; do
+                SRC=$(echo $mount | awk '{ print $1 }')
+                DST=$(echo $mount | awk '{ print $2 }' | cut -c 2-)
+                grep -q "${CHROOTTARGET}/${DST} " /proc/mounts || mount -o bind $SRC ${CHROOTTARGET}/${DST}
+                done
+        fi
+    fi
+
         # Start all services
         ${INSTALL_DIR}/app/start.py
     fi
@@ -31,15 +43,14 @@ stop_daemon ()
     ${INSTALL_DIR}/app/stop.py
 
     # Unmount
-    umount ${CHROOTTARGET}/dev/pts
-    umount ${CHROOTTARGET}/dev
-    umount ${CHROOTTARGET}/sys
-    umount ${CHROOTTARGET}/proc
+    test -L ${CHROOTTARGET}/dev/fd && rm ${CHROOTTARGET}/dev/fd
+    for mount in `grep ${CHROOTTARGET} /proc/mounts | awk '{ print $2 }'`; do umount -l $mount; done
+
 }
 
 daemon_status ()
 {
-    `grep -q "${CHROOTTARGET}/proc " /proc/mounts` && `grep -q "${CHROOTTARGET}/sys " /proc/mounts` && `grep -q "${CHROOTTARGET}/dev " /proc/mounts` && `grep -q "${CHROOTTARGET}/dev/pts " /proc/mounts`
+    `grep -q "${CHROOTTARGET}/proc " /proc/mounts` && `grep -q "${CHROOTTARGET}/sys " /proc/mounts` && `grep -q "${CHROOTTARGET}/dev " /proc/mounts` && `grep -q "${CHROOTTARGET}/dev/pts " /proc/mounts` && `grep -q "${CHROOTTARGET}/tmp " /proc/mounts` && `test -d "${CHROOTTARGET}/dev/fd"`
 }
 
 
@@ -64,6 +75,18 @@ case $1 in
             exit 0
         fi
         ;;
+    restart)
+        if daemon_status; then
+            echo Stopping ${DNAME} ... && \
+            stop_daemon && \
+            echo Starting ${DNAME} ... && \
+            start_daemon && \
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
     status)
         if daemon_status; then
             echo ${DNAME} is running
@@ -74,9 +97,30 @@ case $1 in
         fi
         ;;
     chroot)
-        chroot ${CHROOTTARGET}/ /bin/bash
+        if daemon_status; then
+            echo Entering chroot...
+            chroot ${CHROOTTARGET}/ /bin/bash
+        else
+            echo WARNING: ${DNAME} is not running
+            echo WARNING: The chroot environment will not have access to /proc, /dev, /sys or /tmp which could cause problems!
+            echo Entering chroot...
+            chroot ${CHROOTTARGET}/ /bin/bash
+            exit 1
+        fi
+        ;;
+    log)
+        if [ -f ${CHROOTTARGET}/var/log/syslog ]; then
+            echo ${CHROOTTARGET}/var/log/syslog
+            exit 0
+        elif [ -f ${CHROOTTARGET}/var/log/messages ]; then
+            echo ${CHROOTTARGET}/var/log/messages
+            exit 0
+        else
+            exit 1
+        fi
         ;;
     *)
+        echo "You must provide an argument to this script; either 'start', 'stop', 'restart', 'status' or 'chroot'"
         exit 1
         ;;
 esac
