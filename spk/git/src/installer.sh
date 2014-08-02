554896a958b16fe2b675873e6c092c744448b0f2
tcavallari <tommaso@tommasocavallari.com>
2014-08-02 23:58:22 +0200

    git: add symlinks to the programs into /usr/local/bin upon installation

--- spk/git/src/installer.sh
+++ spk/git/src/installer.sh
@@ -21,6 +21,14 @@ postinst ()
     # Install busybox stuff
     ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
 
+    # Put symlinks in the PATH
+    mkdir -p /usr/local/bin
+    ln -s ${INSTALL_DIR}/bin/git /usr/local/bin/git
+    ln -s ${INSTALL_DIR}/bin/git-receive-pack /usr/local/bin/git-receive-pack
+    ln -s ${INSTALL_DIR}/bin/git-shell /usr/local/bin/git-shell
+    ln -s ${INSTALL_DIR}/bin/git-upload-archive /usr/local/bin/git-upload-archive
+    ln -s ${INSTALL_DIR}/bin/git-upload-pack /usr/local/bin/git-upload-pack
+
     exit 0
 }
 
@@ -33,6 +41,11 @@ postuninst ()
 {
     # Remove link
     rm -f ${INSTALL_DIR}
+    rm -f /usr/local/bin/git
+    rm -f /usr/local/bin/git-receive-pack
+    rm -f /usr/local/bin/git-shell
+    rm -f /usr/local/bin/git-upload-archive
+    rm -f /usr/local/bin/git-upload-pack
 
     exit 0
 }
