0c09a2a633b448684a881f5e035afd56556ea2eb
tcavallari <tommaso@tommasocavallari.com>
2014-08-03 01:17:31 +0200

    ruby: added symlinks and includes to package

--- spk/ruby/src/installer.sh
+++ spk/ruby/src/installer.sh
@@ -19,6 +19,15 @@ postinst ()
     # Link
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
+    mkdir -p /usr/local/bin
+    ln -s ${INSTALL_DIR}/bin/erb /usr/local/bin/erb
+    ln -s ${INSTALL_DIR}/bin/gem /usr/local/bin/gem
+    ln -s ${INSTALL_DIR}/bin/irb /usr/local/bin/irb
+    ln -s ${INSTALL_DIR}/bin/rake /usr/local/bin/rake
+    ln -s ${INSTALL_DIR}/bin/rdoc /usr/local/bin/rdoc
+    ln -s ${INSTALL_DIR}/bin/ri /usr/local/bin/ri
+    ln -s ${INSTALL_DIR}/bin/ruby /usr/local/bin/ruby
+
     exit 0
 }
 
@@ -31,6 +40,14 @@ postuninst ()
 {
     # Remove link
     rm -f ${INSTALL_DIR}
+
+    rm -f /usr/local/bin/erb
+    rm -f /usr/local/bin/gem
+    rm -f /usr/local/bin/irb
+    rm -f /usr/local/bin/rake
+    rm -f /usr/local/bin/rdoc
+    rm -f /usr/local/bin/ri
+    rm -f /usr/local/bin/ruby
 
     exit 0
 }
