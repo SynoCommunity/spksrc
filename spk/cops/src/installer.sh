eaddcff29bdab0732c7125207830fb675070ff7c
Sébastien Lucas <sebastien@slucas.fr>
2014-09-10 17:38:38 +0200

    Fix package upgrade by moving all configuration in an INSTALL conditionnal. Also fix the pre/postupgrade to properly backup/restore site configuration.

--- spk/cops/src/installer.sh
+++ spk/cops/src/installer.sh
@@ -25,28 +25,30 @@ postinst ()
     # Install the web interface
     cp -pR ${INSTALL_DIR}/share/${PACKAGE} ${WEB_DIR}
 
-    # Configure open_basedir
-    if [ "${USER}" == "nobody" ]; then
-        echo -e "<Directory \"${WEB_DIR}/${PACKAGE}\">\nphp_admin_value open_basedir ${WEB_DIR}/${PACKAGE}:${wizard_calibre_dir:=/volume1/calibre/} \n</Directory>" > /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf
-    else
-        echo -e "[PATH=${WEB_DIR}/${PACKAGE}]\nopen_basedir = ${WEB_DIR}/${PACKAGE}:${wizard_calibre_dir:=/volume1/calibre/}" > /etc/php/conf.d/${PACKAGE_NAME}.ini
-    fi
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        # Configure open_basedir
+        if [ "${USER}" == "nobody" ]; then
+            echo -e "<Directory \"${WEB_DIR}/${PACKAGE}\">\nphp_admin_value open_basedir ${WEB_DIR}/${PACKAGE}:${wizard_calibre_dir:=/volume1/calibre/} \n</Directory>" > /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf
+        else
+            echo -e "[PATH=${WEB_DIR}/${PACKAGE}]\nopen_basedir = ${WEB_DIR}/${PACKAGE}:${wizard_calibre_dir:=/volume1/calibre/}" > /etc/php/conf.d/${PACKAGE_NAME}.ini
+        fi
 
-    # Create a default configuration file
-    if [ ! -f ${CFG_FILE} ]; then
-      cp ${DEFAULT_CFG_FILE} ${CFG_FILE}
-      url_rewriting=$([ "${wizard_use_url_rewriting}" == "true" ] && echo "1" || echo "0")
-      sed -i -e "s|@calibre_dir@|${wizard_calibre_dir:=/volume1/calibre/}|g" ${CFG_FILE}
-      sed -i -e "s|@cops_title@|${wizard_cops_title:=COPS}|g" ${CFG_FILE}
-      sed -i -e "s|@use_url_rewriting@|${url_rewriting:=0}|g" ${CFG_FILE}
-      chmod ga+w ${CFG_FILE}
-    fi
+        # Create a default configuration file
+        if [ ! -f ${CFG_FILE} ]; then
+          cp ${DEFAULT_CFG_FILE} ${CFG_FILE}
+          url_rewriting=$([ "${wizard_use_url_rewriting}" == "true" ] && echo "1" || echo "0")
+          sed -i -e "s|@calibre_dir@|${wizard_calibre_dir:=/volume1/calibre/}|g" ${CFG_FILE}
+          sed -i -e "s|@cops_title@|${wizard_cops_title:=COPS}|g" ${CFG_FILE}
+          sed -i -e "s|@use_url_rewriting@|${url_rewriting:=0}|g" ${CFG_FILE}
+          chmod ga+w ${CFG_FILE}
+        fi
 
-    # Set permissions
-    chown ${USER}:root ${wizard_calibre_dir:=/volume1/calibre/}
-    chmod u+rw ${wizard_calibre_dir:=/volume1/calibre/}
-    chown ${USER}:root ${wizard_calibre_dir:=/volume1/calibre/}/metadata.db
-    chmod u+rw ${wizard_calibre_dir:=/volume1/calibre/}/metadata.db
+        # Set permissions
+        chown ${USER} ${wizard_calibre_dir:=/volume1/calibre/}
+        chmod u+rw ${wizard_calibre_dir:=/volume1/calibre/}
+        chown ${USER} ${wizard_calibre_dir:=/volume1/calibre/}/metadata.db
+        chmod u+rw ${wizard_calibre_dir:=/volume1/calibre/}/metadata.db
+    fi
 
     exit 0
 }
@@ -77,6 +79,11 @@ preupgrade ()
     rm -fr ${TMP_DIR}/${PACKAGE}
     mkdir -p ${TMP_DIR}/${PACKAGE}
     mv ${CFG_FILE} ${TMP_DIR}/${PACKAGE}/
+    if [ "${USER}" == "nobody" ]; then
+        mv /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf ${TMP_DIR}/${PACKAGE}/
+    else
+        mv /etc/php/conf.d/${PACKAGE_NAME}.ini ${TMP_DIR}/${PACKAGE}/
+    fi
 
     exit 0
 }
@@ -86,6 +93,8 @@ postupgrade ()
     # Restore some stuff
     rm -f ${CFG_FILE}
     mv ${TMP_DIR}/${PACKAGE}/config_local.php ${CFG_FILE}
+    mv ${TMP_DIR}/${PACKAGE}/${PACKAGE}.conf /usr/syno/etc/sites-enabled-user
+    mv ${TMP_DIR}/${PACKAGE}/${PACKAGE_NAME}.ini /etc/php/conf.d/
     rm -fr ${TMP_DIR}/${PACKAGE}
 
     exit 0
