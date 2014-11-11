5a2e922e99433a082dc55bd830ac7203d57670d5
sirjeannot <sirjeannot@gmail.com>
2014-11-11 19:00:52 +0100

    Update dsm-control.sh

--- spk/librsync/src/dsm-control.sh
+++ spk/librsync/src/dsm-control.sh
@@ -1,24 +1,3 @@
-PKG_NAME = librsync
-PKG_VERS = 0.9.7
-PKG_EXT = tar.gz
-PKG_DIR = $(PKG_NAME)-$(PKG_VERS)
-PKG_DIST_NAME = $(PKG_DIR).$(PKG_EXT)
-PKG_DIST_SITE = http://downloads.sourceforge.net/project/librsync/$(PKG_NAME)/$(PKG_VERS)
-
-DEPENDS =
-
-HOMEPAGE = http://librsync.sourceforge.net/
-COMMENT  = librsync is a free software library that implements the rsync remote-delta algorithm
-LICENSE  =
-
-GNU_CONFIGURE = 1
-CONFIGURE_ARGS = --disable-static --enable-shared
-
-include ../../mk/spksrc.cross-cc.mk
-root@debian-x86:~/backup/spksrc/spk/librsync# cd src/
-root@debian-x86:~/backup/spksrc/spk/librsync/src# ls
-dsm-control.sh  installer.sh  librsync.png
-root@debian-x86:~/backup/spksrc/spk/librsync/src# cat dsm-control.sh
 #!/bin/sh
 
 # Package
