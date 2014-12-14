907853b56f25010d187768db8c80099dec524e25
AustinSaintAubin <austinsaintaubin@gmail.com>
2014-12-14 12:24:44 -0600

    Ready for review.
    Fixed permission issues with Synology ACL. Web Services works. Firewall
    rules added.

--- spk/logstash/src/dsm-control.sh
+++ spk/logstash/src/dsm-control.sh
@@ -13,39 +13,89 @@ USER="logstash"
 GROUP="users"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/usr/bin:${PATH}"
 
+# Common Functions
+checkFolder() { [ -d "$@" ] || mkdir -p "$@"; }
+
 # Logstash File Path Varables
 PACKAGE_CONF_PATH="${INSTALL_DIR}/var/package.conf"
-source "${PACKAGE_CONF_PATH}"  # Load Logstash File Path Varables
+source "${PACKAGE_CONF_PATH}" # Load Logstash File Path Varables
+# Check Config & Load from stock if needed
+[ -e "${LOGSTASH_LOG_PATH}" ] || ( checkFolder "$(dirname "${LOGSTASH_LOG_PATH}")"; touch "${LOGSTASH_LOG_PATH}"; LOGSTASH_LOG_MESSAGE="$LOGSTASH_LOG_MESSAGE\nCreating Logfile." )
+[ -e "${LOGSTASH_CONFIG_PATH}" ] || ( checkFolder "$(dirname "${LOGSTASH_CONFIG_PATH}")"; cp "${INSTALL_DIR}/logstash-sample.conf" "${LOGSTASH_CONFIG_PATH}"; LOGSTASH_LOG_MESSAGE="$LOGSTASH_LOG_MESSAGE\nLogstash Config not found, loading sample config." )
+[ -e "${JAVA_CONF_PATH}" ] || ( checkFolder "$(dirname "${JAVA_CONF_PATH}")"; cp "${INSTALL_DIR}/var/logstash-java.conf" "${JAVA_CONF_PATH}"; LOGSTASH_LOG_MESSAGE="$LOGSTASH_LOG_MESSAGE\nJava Config not found, loading sample config." )
+checkFolder "${LOGSTASH_DATABASE_DIR}"
 
-# Java Varables
+# Java Varables & Tuning
 # JAVA_BINARY="$(which java)"
+JAVA_ARGUMENTS=""  # Clear Java Arguments
 source "${JAVA_CONF_PATH}"  # Load Java Settings
 LOGSTASH_BIN_PATH="${INSTALL_DIR}/logstash/bin/logstash"
-LOGSTASH_BIN_ARGS="agent --verbose --config $LOGSTASH_CONFIG_PATH --log $LOGSTASH_LOG_PATH $LOGSTASH_PARAMETERS_TUNING"
-[ -n "$JAVA_HEAP_SIZE_MAX" ] && JAVA_ARGUMENTS="LS_HEAP_SIZE=$JAVA_HEAP_SIZE_MAX ${JAVA_ARGUMENTS}"  # Set Java Heap Size into string varable
+LOGSTASH_BIN_ARGS="agent --verbose --config "${LOGSTASH_CONFIG_PATH}" --log "${LOGSTASH_LOG_PATH}" ${LOGSTASH_PARAMETERS_TUNING}"
+[ -n "$JAVA_HEAP_SIZE_MAX" ] && JAVA_ARGUMENTS="-XX:MaxHeapSize=$JAVA_HEAP_SIZE_MAX ${JAVA_ARGUMENTS}"  # Set Java Heap Size into string varable
 [ -n "$JAVA_HEAP_SIZE_INITIAL" ] && JAVA_ARGUMENTS="-XX:InitialHeapSize=$JAVA_HEAP_SIZE_INITIAL ${JAVA_ARGUMENTS}"
+JAVA_ARGUMENTS="-Des.path.data=${LOGSTASH_DATABASE_DIR} ${JAVA_ARGUMENTS}"  # Set Database Location
 
 # Start & Stop Varables
 PID_FILE="/var/run/${PACKAGE}.pid"
 
-fix_permissions() {
-	# Set group and permissions on configuration files & logs
-	chgrp users "${LOGSTASH_CONFIG_PATH}"
-	chmod g+rw "${LOGSTASH_CONFIG_PATH}"
-	
-	chgrp -R users "${LOGSTASH_DATABASE_DIR}"
-	chmod -R g+rw "${LOGSTASH_DATABASE_DIR}"
-	
-	touch "${LOGSTASH_LOG_PATH}"
-	chgrp users "${LOGSTASH_LOG_PATH}"
-	chmod g+rw "${LOGSTASH_LOG_PATH}"
-	
-	chgrp users "${JAVA_CONF_PATH}"
-	chmod g+rw "${JAVA_CONF_PATH}"
-	
-	touch "${PID_FILE}"
-	chgrp users "${PID_FILE}"
-	chmod g+rw "${PID_FILE}"
+fix_permissions() {	
+	# Set group and permissions on config files and database dir for DSM5
+	if [ `/bin/get_key_value /etc.defaults/VERSION buildnumber` -ge "4418" ]; then
+		
+		# Internal Field Separator
+		IFS_TEMP="$IFS" # backup curent internal field separator
+		IFS="|"  # Set Seperator for Files List
+		
+		# Allow root traversal for config files, this is needed to logstash can acccess these files
+		FILES="${LOGSTASH_CONFIG_PATH}|${LOGSTASH_LOG_PATH}|${JAVA_CONF_PATH}|${LOGSTASH_DATABASE_DIR}"
+		for FILE in ${FILES}; do
+			
+			# Set Share Permissions
+			SHARE_DIR=$(echo "${FILE}" | awk -F/ '{print "/"$2"/"$3}')
+			
+			# Set Share Permissions
+			if [ ! "$(synoacltool -get "${SHARE_DIR}" | grep "user:$USER:allow:--x----------:fd--")" ]; then
+				if [ "$(synoacltool -get "${SHARE_DIR}" | grep "user:$USER:allow")" ]; then
+					synoacltool -replace "${FILE}" $(synoacltool -get "${FILE}" | grep "user:$USER" | awk 'BEGIN { OFS=" "; } { gsub(/[^[:alnum:]]/, "", $1); print $1;}' | head -1) user:$USER:allow:--x----------:---n &> /dev/null
+				else
+# 	 				synoacltool -add "$SHARE_DIR" user:$USER:allow:--x----------:---n &> /dev/null
+					synoacltool -add "$SHARE_DIR" user:$USER:allow:--x----------:df-- &> /dev/null
+				fi
+			fi
+			
+			# Set Directory Path / Host DIRECTORY Permissions
+			if [ ! -d "${FILE}" ]; then
+				DIRECTORY="$(dirname "${FILE}")"
+				
+				if [ -f "${FILE}" ] && [ ! "$(synoacltool -get "${DIRECTORY}" | grep "user:$USER:allow:rwxpdDaARWc--:fd--")" ]; then
+					synoacltool -enforce-inherit "$SHARE_DIR"  # Clear Permisions & Allow Inherited
+					synoacltool -add "${DIRECTORY}" user:$USER:allow:rwxpdDaARWc--:fd-- &> /dev/null
+				fi
+			fi
+			
+			# Set File Permissions
+			if ([ -f "${FILE}" ] || [ -d "${FILE}" ]) && [ ! "$(synoacltool -get "${FILE}" | grep "user:$USER:allow:rwxpdDaARWc--:fd--")" ]; then
+				synoacltool -enforce-inherit "$SHARE_DIR"  # Clear Permisions & Allow Inherited
+				synoacltool -add "${FILE}" user:$USER:allow:rwxpdDaARWc--:fd-- &> /dev/null
+			fi
+		done
+		
+		IFS="$IFS_TEMP" # restore curent internal field separator
+	else  # Older systems not using Synology ACLs
+		# Set group and permissions on configuration files & logs		
+		chgrp users "${LOGSTASH_CONFIG_PATH}"
+		chmod g+rw "${LOGSTASH_CONFIG_PATH}"
+		
+		chgrp -R users "${LOGSTASH_DATABASE_DIR}"
+		chmod -R g+rw "${LOGSTASH_DATABASE_DIR}"
+		
+		chown -R ${USER}:root "${LOGSTASH_LOG_PATH}"
+		chgrp users "${LOGSTASH_LOG_PATH}"
+		chmod g+rw "${LOGSTASH_LOG_PATH}"
+		
+		chgrp users "${JAVA_CONF_PATH}"
+		chmod g+rw "${JAVA_CONF_PATH}"
+	fi
 }
 
 daemon_debug() {
@@ -53,26 +103,29 @@ daemon_debug() {
 		echo ${DNAME} is already running
 		exit $?
 	else
+		# Clear Log
+		echo "Starting [${DNAME}] #DEBUGING# with user [${USER}] using command: ( JAVA_OPTS=${JAVA_ARGUMENTS} ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS} )" > "$LOGSTASH_LOG_PATH"
+		
+		# Start Debuging
+		echo "Starting [${DNAME}] with user [${USER}] using command: ( JAVA_OPTS=${JAVA_ARGUMENTS} ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS} )"
+		echo "$LOGSTASH_LOG_MESSAGE"
+		
 		# Fix Configuration File Permissions
 		fix_permissions
- 	
-		# Start Debuging
-		echo "Starting [${DNAME}] with user [${USER}] using command: ( JAVA_OPTS=-Des.path.data=${LOGSTASH_DATABASE_DIR} $JAVA_ARGUMENTS ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS} )"	
 		
 		# Run Logstash
-		su - ${USER} -c "JAVA_OPTS=\"-Des.path.data=${LOGSTASH_DATABASE_DIR} $JAVA_ARGUMENTS\" ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS}"
+		su - ${USER} -c "JAVA_OPTS=\"$JAVA_ARGUMENTS\" ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS}"
+		
 		exit 0
 	fi
 }
 
 start_daemon () {
 	# Logstash Bin & Java Varables
-	rm -f "$LOGSTASH_LOG_PATH"
-	echo "Starting ${DNAME}. $(date) [${DNAME}] ( $LOGSTASH_BIN_PATH )" >> "$LOGSTASH_LOG_PATH"
+	echo "Starting ${DNAME}. $(date) [${DNAME}] ( $LOGSTASH_BIN_PATH )" > "$LOGSTASH_LOG_PATH"
 	echo " ...Might take a moment to be fully initialize logstash... please wait." >> "$LOGSTASH_LOG_PATH"
-	[ -e "${LOGSTASH_CONFIG_PATH}" ] || ( cp "${INSTALL_DIR}/logstash-sample.conf" "${LOGSTASH_CONFIG_PATH}"; echo "Logstash Config not found, loading sample config." >> "$LOGSTASH_LOG_PATH" )
-	[ -e "${JAVA_CONF_PATH}" ] || ( cp "${INSTALL_DIR}/var/logstash-java.conf" "${JAVA_CONF_PATH}"; echo "Java Config not found, loading sample config." >> "$LOGSTASH_LOG_PATH" )
-	echo "Starting with: ( JAVA_OPTS=-Des.path.data=${LOGSTASH_DATABASE_DIR} $JAVA_ARGUMENTS ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS} )" >> "$LOGSTASH_LOG_PATH"
+	echo "$LOGSTASH_LOG_MESSAGE"
+	echo "Starting with: ( JAVA_OPTS=${JAVA_ARGUMENTS} ${LOGSTASH_BIN_PATH} ${LOGSTASH_BIN_ARGS} )" >> "$LOGSTASH_LOG_PATH"
 	echo "Using Elasticsearch database at ${LOGSTASH_DATABASE_DIR}." >> "$LOGSTASH_LOG_PATH"
 	
 	# Fix Configuration File Permissions
@@ -80,8 +133,7 @@ start_daemon () {
 	
 	# Setup Enverment Varables
 	export HOME=${INSTALL_DIR}/var
-	export JAVA_OPTS="-Des.path.data=${LOGSTASH_DATABASE_DIR} $JAVA_ARGUMENTS" 
-	export $JAVA_LS_HEAP_SIZE
+	export JAVA_OPTS="$JAVA_ARGUMENTS" 
 	
 	# Run Logstash
 	start-stop-daemon -S -q -m -b -N 19 -c ${USER} -u ${USER} -p ${PID_FILE} -x ${LOGSTASH_BIN_PATH} -- ${LOGSTASH_BIN_ARGS}  # Run Logstash inside of java using start-stop-daemon as user logstash
--- spk/logstash/src/installer.sh
+++ spk/logstash/src/installer.sh
@@ -13,6 +13,7 @@ TMP_DIR="${PACKAGE}/../../@tmp"
 FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
 USER="logstash"
 GROUP="users"
+APACHE_USER="$([ $(grep buildnumber /etc.defaults/VERSION | cut -d"\"" -f2) -ge 4418 ] && echo -n http || echo -n nobody)"
 
 # Get Envirmental Varables (needed to detect if java is installed)
 source /root/.profile  # Get Environment Variables from Root Profile
@@ -63,17 +64,23 @@ postinst() {
 	# Install busybox stuff
 	${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
 	
-	# Configure package varables file based on wizard, or use defaults (used for scripts like the start-stop-status)
-	sed -i -e "s|@logstash_config_path@|${wizard_logstash_config_path:=/var/packages/logstash/target/var/logstash.conf}|g" "${PACKAGE_CONF_PATH}"
-	sed -i -e "s|@logstash_database_dir@|${wizard_logstash_database_dir:=/var/packages/logstash/target/var/database}|g" "${PACKAGE_CONF_PATH}"
-	sed -i -e "s|@logstash_log_path@|${wizard_logstash_log_path:=/var/packages/logstash/target/var/logstash.log}|g" "${PACKAGE_CONF_PATH}"
-	sed -i -e "s|@java_config_path@|${wizard_java_config_path:=/var/packages/logstash/target/var/logstash-java.conf}|g" "${PACKAGE_CONF_PATH}"
+	# Create user
+	adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
 	
-	# Configure java settigns file based on wizard, or use defaults
-	sed -i -e "s|@java_heap_size_initial@|${wizard_java_heap_size_initial}|g" "${JAVA_CONF_PATH}"
-	sed -i -e "s|@java_heap_size_max@|${wizard_java_heap_size_max}|g" "${JAVA_CONF_PATH}"
-	sed -i -e "s|@java_arguments_tuning@|${wizard_java_arguments_tuning}|g" "${JAVA_CONF_PATH}"
-	sed -i -e "s|@logstash_parameters_tuning@|${wizard_logstash_parameters_tuning}|g" "${JAVA_CONF_PATH}"
+	# Configure files
+	if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+		# Configure package varables file based on wizard, or use defaults (used for scripts like the start-stop-status)
+		sed -i -e "s|@logstash_config_path@|${wizard_logstash_config_path:=/var/packages/logstash/target/var/logstash.conf}|g" "${PACKAGE_CONF_PATH}"
+		sed -i -e "s|@logstash_database_dir@|${wizard_logstash_database_dir:=/var/packages/logstash/target/var/database}|g" "${PACKAGE_CONF_PATH}"
+		sed -i -e "s|@logstash_log_path@|${wizard_logstash_log_path:=/var/packages/logstash/target/var/logstash.log}|g" "${PACKAGE_CONF_PATH}"
+		sed -i -e "s|@java_config_path@|${wizard_java_config_path:=/var/packages/logstash/target/var/logstash-java.conf}|g" "${PACKAGE_CONF_PATH}"
+		
+		# Configure java settigns file based on wizard, or use defaults
+		sed -i -e "s|@java_heap_size_initial@|${wizard_java_heap_size_initial}|g" "${JAVA_CONF_PATH}"
+		sed -i -e "s|@java_heap_size_max@|${wizard_java_heap_size_max}|g" "${JAVA_CONF_PATH}"
+		sed -i -e "s|@java_arguments_tuning@|${wizard_java_arguments_tuning}|g" "${JAVA_CONF_PATH}"
+		sed -i -e "s|@logstash_parameters_tuning@|${wizard_logstash_parameters_tuning}|g" "${JAVA_CONF_PATH}"
+	fi
 	
 	# Check Logstash Config, Log, & Database
 	checkFolder "$(dirname "${wizard_logstash_config_path}")"  # Create Folder if Needed
@@ -84,51 +91,27 @@ postinst() {
 	[ -e "${wizard_java_config_path}" ] || cp "${JAVA_CONF_PATH}" "${wizard_java_config_path}"
 	
 	# Symbolic Link to Kibana
-	ln -s "${INSTALL_DIR}/logstash/vendor/kibana/" "${INSTALL_DIR}/app/"
-# 	ln -s "${WEB_DIR}/${PACKAGE}" "${INSTALL_DIR}/app/"
-# 
-#	# Fix permissions
-#	chown -R ${USER} ${WEB_DIR}/${PACKAGE}
-# 	
-# 	# Move Kibana to Web Services
-# # 	rm -rf $WEBSITE_ROOT  # Remove old webdir.
-# # 	cp -pR "${KIBANA_DIR}" "${WEB_DIR}"
-# 	mkdir "${WEB_DIR}/${PACKAGE}"
-# 	mv -f "${KIBANA_DIR}" "${WEB_DIR}"
-# 	chown -R 1023:1023 "${WEB_DIR}/${PACKAGE}"
-# 	
-# 	# Configure open_basedir
-#	if [ "${USER}" == "nobody" ]; then
-#		echo -e "<Directory \"${WEB_DIR}/${PACKAGE}\">\nphp_admin_value open_basedir none\n</Directory>" > /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf
-#	else
-#		echo -e "extension = fileinfo.so\n[PATH=${WEB_DIR}/${PACKAGE}]\nopen_basedir = Null" > /etc/php/conf.d/${PACKAGE_NAME}.ini
-#		echo -e "<Directory \"${WEB_DIR}/${PACKAGE}\">\nXSendFilePath /\n</Directory>" > /etc/httpd/sites-enabled-user/${PACKAGE_NAME}.conf
-#	fi
-#	
-# <VirtualHost *:80>
-# ServerName 1test.nas.austinsaintaubin.me
-# DocumentRoot "/var/services/web/test1"
-# ErrorDocument 403 "/webdefault/error.html"
-# ErrorDocument 404 "/webdefault/error.html"
-# ErrorDocument 500 "/webdefault/error.html"
-# </VirtualHost>
+# 	ln -s "${INSTALL_DIR}/logstash/vendor/kibana/" "${INSTALL_DIR}/app/"
+	ln -s "${WEB_DIR}/kibana" "${INSTALL_DIR}/app/"
 
-	# Create user
-	adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
+	# Move Kibana to Web Services
+	cp -pR ${KIBANA_DIR} ${WEB_DIR}
+	
+	# Configure open_basedir
+	if [ "${APACHE_USER}" == "nobody" ]; then
+		echo -e "<Directory \"${WEB_DIR}/kibana\">\nphp_admin_value open_basedir none\n</Directory>" > /usr/syno/etc/sites-enabled-user/${PACKAGE}.conf
+	else
+		echo -e "[PATH=${WEB_DIR}/kibana]\nopen_basedir = Null" > /etc/php/conf.d/${PACKAGE_NAME}.ini
+	fi
+	
+	# Set group and permissions on config files and database dir for DSM5
+	if [ `/bin/get_key_value /etc.defaults/VERSION buildnumber` -ge "4418" ]; then
+		chown -R ${USER}:root ${SYNOPKG_PKGDEST}
+	fi
 	
 	# Correct the files ownership
-	chown -R ${USER}:root ${INSTALL_DIR}
-	
-	# Set group and permissions on configuration files & logs
-	source "${PACKAGE_CONF_PATH}"  # Load Logstash File Path Varables
-	chgrp users ${LOGSTASH_CONFIG_PATH}
-	chmod g+rw ${LOGSTASH_CONFIG_PATH}
-	chgrp -R users ${LOGSTASH_DATABASE_DIR}
-	chmod -R g+rw ${LOGSTASH_DATABASE_DIR}
-	chgrp users ${LOGSTASH_LOG_PATH}
-	chmod g+rw ${LOGSTASH_LOG_PATH}
-	chgrp users ${JAVA_CONF_PATH}
-	chmod g+rw ${JAVA_CONF_PATH}
+	chown -R ${USER}:root ${SYNOPKG_PKGDEST}
+	chown -R ${APACHE_USER}:${APACHE_USER} ${WEB_DIR}/kibana
 	
 	# Add firewall config
 	${SERVICETOOL} --install-configure-file --package ${FWPORTS} >> /dev/null
@@ -161,6 +144,43 @@ preuninst() {
 	# Stop the package
 	${SSS} stop > /dev/null
 	
+	# Remove the users permisions from the share
+	if [ `/bin/get_key_value /etc.defaults/VERSION buildnumber` -ge "4418" ]; then
+		
+		# Internal Field Separator
+		IFS_TEMP="$IFS" # backup curent internal field separator
+		IFS="|"  # Set Seperator for Files List
+		
+		# Allow root traversal for config files, this is needed to logstash can acccess these files
+		FILES="${LOGSTASH_CONFIG_PATH}|${LOGSTASH_LOG_PATH}|${JAVA_CONF_PATH}|${LOGSTASH_DATABASE_DIR}"
+		for FILE in ${FILES}; do
+			
+			# Set Share Permissions
+			SHARE_DIR=$(echo "${FILE}" | awk -F/ '{print "/"$2"/"$3}')
+			
+			# Set Share Permissions
+			if [ "$(synoacltool -get "${SHARE_DIR}" | grep "user:$USER:allow")" ]; then
+				synoacltool -del "${FILE}" $(synoacltool -get "${FILE}" | grep "user:$USER" | awk 'BEGIN { OFS=" "; } { gsub(/[^[:alnum:]]/, "", $1); print $1;}' | head -1) &> /dev/null
+			fi
+			
+			# Set Directory Path / Host DIRECTORY Permissions
+			if [ ! -d "${FILE}" ]; then
+				DIRECTORY="$(dirname "${FILE}")"
+				
+				if [ -f "${FILE}" ] && [ "$(synoacltool -get "${DIRECTORY}" | grep "user:$USER:allow")" ]; then
+					synoacltool -del "${DIRECTORY}" $(synoacltool -get "${FILE}" | grep "user:$USER" | awk 'BEGIN { OFS=" "; } { gsub(/[^[:alnum:]]/, "", $1); print $1;}' | head -1) &> /dev/null
+				fi
+			fi
+			
+			# Set File Permissions
+			if ([ -f "${FILE}" ] || [ -d "${FILE}" ]) && [ ! "$(synoacltool -get "${FILE}" | grep "user:$USER:allow")" ]; then
+				synoacltool -del "${FILE}" $(synoacltool -get "${FILE}" | grep "user:$USER" | awk 'BEGIN { OFS=" "; } { gsub(/[^[:alnum:]]/, "", $1); print $1;}' | head -1) &> /dev/null
+			fi
+		done
+		
+		IFS="$IFS_TEMP" # restore curent internal field separator
+	fi
+	
 	# Remove the user (if not upgrading)
 	if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
 		delgroup ${USER} ${GROUP}
@@ -178,6 +198,9 @@ preuninst() {
 postuninst() {
 	# Remove Symbolic link
 	rm "${INSTALL_DIR}/app/kibana"
+	
+	# Remove Kibana Web Interface
+	rm -fr ${WEB_DIR}/kibana
 	
 	exit 0
 }
