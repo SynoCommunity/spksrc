1891dc237f55c0e013de349f47b700755eb1980c
Dr-Bean <github@beanpoint.com>
2015-02-01 14:15:14 +0100

    Revert "Remove unmaintained darkstat"
    This reverts commit 586f5c41d8d31c5f4ef9f9da2de0c8f5a548118a.

---
+++ spk/darkstat/src/dsm-control.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Package
+PACKAGE="darkstat"
+DNAME="DarkStat"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin:/usr/local/sbin"
+USER="root"
+DARKSTAT="${INSTALL_DIR}/sbin/darkstat"
+PID_FILE="${INSTALL_DIR}/var/darkstat.pid"
+
+start_daemon ()
+{
+    su - ${USER} -c "PATH=${PATH} ${DARKSTAT} -i eth0 --chroot ${INSTALL_DIR}/var --pidfile darkstat.pid --import darkstat.data --export darkstat.data"
+}
+
+stop_daemon ()
+{
+    kill `cat ${PID_FILE}`
+    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
+    rm -f ${PID_FILE}
+}
+
+daemon_status ()
+{
+    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+        return
+    fi
+    rm -f ${PID_FILE}
+    return 1
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
+}
+
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+            exit 0
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+            exit 0
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+            exit $?
+        else
+            echo ${DNAME} is not running
+            exit 0
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    *)
+        exit 1
+        ;;
+esac
