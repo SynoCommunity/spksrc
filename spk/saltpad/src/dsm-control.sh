424abd7dcacb6abff5223312fba6390ba66afe6a
Diaoul <diaoulael@gmail.com>
2015-03-21 19:07:08 +0100

    Add saltpad spk

---
+++ spk/saltpad/src/dsm-control.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+
+# Package
+PACKAGE="saltpad"
+DNAME="SaltPad"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PYTHON_DIR="/usr/local/python"
+PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
+USER="saltpad"
+GROUP="nobody"
+PYTHON="${INSTALL_DIR}/env/bin/python"
+UWSGI="${PYTHON_DIR}/bin/uwsgi"
+PID_FILE="${INSTALL_DIR}/var/saltpad.pid"
+LOG_FILE="${INSTALL_DIR}/var/uwsgi.log"
+
+
+start_daemon ()
+{
+    ${UWSGI} --die-on-term --http :8283 --virtualenv ${INSTALL_DIR}/env --chdir ${INSTALL_DIR}/share/saltpad/saltpad --module app:app --master --processes 2 --pidfile ${PID_FILE} --daemonize ${LOG_FILE} --uid ${USER} --gid ${GROUP}
+}
+
+stop_daemon ()
+{
+    kill `cat ${PID_FILE}`
+    wait_for_status 1 20 || kill -9 `cat ${PID_FILE}`
+    rm -f ${PID_FILE}
+}
+
+daemon_status ()
+{
+    if [ -f ${PID_FILE} ] && kill -0 `cat ${PID_FILE}` > /dev/null 2>&1; then
+        return
+    fi
+    rm -f ${PID_FILE}
+    return 1
+}
+
+wait_for_status ()
+{
+    counter=$2
+    while [ ${counter} -gt 0 ]; do
+        daemon_status
+        [ $? -eq $1 ] && return
+        let counter=counter-1
+        sleep 1
+    done
+    return 1
+}
+
+
+case $1 in
+    start)
+        if daemon_status; then
+            echo ${DNAME} is already running
+        else
+            echo Starting ${DNAME} ...
+            start_daemon
+        fi
+        ;;
+    stop)
+        if daemon_status; then
+            echo Stopping ${DNAME} ...
+            stop_daemon
+        else
+            echo ${DNAME} is not running
+        fi
+        ;;
+    status)
+        if daemon_status; then
+            echo ${DNAME} is running
+            exit 0
+        else
+            echo ${DNAME} is not running
+            exit 1
+        fi
+        ;;
+    log)
+        echo ${LOG_FILE}
+        ;;
+    *)
+        exit 1
+        ;;
+esac
---
+++ spk/saltpad/src/saltpad.sc
@@ -0,0 +1,5 @@
+[saltpad]
+title="SaltPad"
+desc="SaltPad"
+port_forward="yes"
+dst.ports="8283/tcp"
