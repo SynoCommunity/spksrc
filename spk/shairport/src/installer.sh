b4c9f65ec2c6d38c5796d7e9d7345e26cb484da4
Lost Carrier <github@losty.ch>
2015-04-21 17:26:47 -0400

    Using pre-generated alsa.conf.

--- spk/shairport/src/installer.sh
+++ spk/shairport/src/installer.sh
@@ -21,14 +21,6 @@ postinst ()
     # Link
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
-	# TODO maybe add an spk/shairport/src/alsa.conf with the correct configuration, and place that file during installation.
-    # persuade alsa to let use use that playback device
-    mv ${INSTALL_DIR}/share/alsa/alsa.conf ${INSTALL_DIR}/share/alsa/alsa.original.bak
-    cat ${INSTALL_DIR}/share/alsa/alsa.original.bak | grep -v "defaults.pcm.dmix.rate" > ${INSTALL_DIR}/share/alsa/alsa.conf
-    rm ${INSTALL_DIR}/share/alsa/alsa.original.bak 
-    echo "defaults.pcm.dmix.rate 44100" >> ${INSTALL_DIR}/share/alsa/alsa.conf
-    echo "defaults.pcm.ipc_gid    \"root\"" >> ${INSTALL_DIR}/share/alsa/alsa.conf
-
     # Install busybox stuff
     ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
 
