3c8ab89f00a68744722f6051d188b07ad41bfb75
Dr-Bean <github@beanpoint.com>
2015-05-03 00:21:25 +0200

    BBS changes
    - Set open_basedir to None/Null
    - Remove wizard
    - Backup data during package upgrade

--- spk/bicbucstriim/src/installer.sh
+++ spk/bicbucstriim/src/installer.sh
@@ -3,10 +3,14 @@
 # Package
 PACKAGE="bicbucstriim"
 DNAME="BicBucStriim"
+SHORTNAME="bbs"
+PACKAGE_NAME="com.synocommunity.packages.${SHORTNAME}"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
 WEB_DIR="/var/services/web"
+USER="$([ $(grep buildnumber /etc.defaults/VERSION | cut -d"\"" -f2) -ge 4418 ] && echo -n http || echo -n nobody)"
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
 
 
 preinst ()
@@ -20,10 +24,18 @@ postinst ()
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
     # Install the web interface
-    cp -R ${INSTALL_DIR}/share/bbs ${WEB_DIR}
+    cp -pR ${INSTALL_DIR}/share/${SHORTNAME} ${WEB_DIR}
+
+    # Configure open_basedir
+    if [ "${USER}" == "nobody" ]; then
+        echo -e "<Directory \"${WEB_DIR}/${SHORTNAME}\">\nphp_admin_value open_basedir none\n</Directory>" > /usr/syno/etc/sites-enabled-user/${SHORTNAME}.conf
+    else
+        echo -e "extension = pdo_sqlite.so\nextension = sqlite3.so\n[PATH=${WEB_DIR}/${SHORTNAME}]\nopen_basedir = Null" > /etc/php/conf.d/${PACKAGE_NAME}.ini
+    fi
 
     # Fix permissions
-    chmod -R ga+w ${WEB_DIR}/bbs/data
+    chown -R ${USER} ${WEB_DIR}/${SHORTNAME}
+    chmod -R u+rw ${WEB_DIR}/${SHORTNAME}/data
 
     exit 0
 }
@@ -38,18 +50,33 @@ postuninst ()
     # Remove link
     rm -f ${INSTALL_DIR}
 
+    # Remove open_basedir configuration
+    rm -f /usr/syno/etc/sites-enabled-user/${SHORTNAME}.conf
+    rm -f /etc/php/conf.d/${PACKAGE_NAME}.ini
+
     # Remove the web interface
-    rm -fr ${WEB_DIR}/bbs
+    rm -fr ${WEB_DIR}/${SHORTNAME}
 
     exit 0
 }
 
 preupgrade ()
 {
+    # Save data
+    rm -fr ${TMP_DIR}/${PACKAGE}
+    mkdir -p ${TMP_DIR}/${PACKAGE}
+    mv ${WEB_DIR}/${SHORTNAME}/data/authors ${TMP_DIR}/${PACKAGE}/data/
+    mv ${WEB_DIR}/${SHORTNAME}/data/titles ${TMP_DIR}/${PACKAGE}/data/
+
     exit 0
 }
 
 postupgrade ()
 {
+    # Restore data
+    mv ${TMP_DIR}/${PACKAGE}/data/authors ${WEB_DIR}/${SHORTNAME}/data/
+    mv ${TMP_DIR}/${PACKAGE}/data/titles ${WEB_DIR}/${SHORTNAME}/data/
+    rm -fr ${TMP_DIR}/${PACKAGE}
+
     exit 0
 }
