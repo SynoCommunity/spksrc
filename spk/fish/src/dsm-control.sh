cc524718b7149535bbf153c0c70007cda8f4960d
tmaone <marios.daoutis@gmail.com>
2015-06-14 13:41:52 +0200

    fish-shell package, initial release

---
+++ spk/fish/src/dsm-control.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Package
+PACKAGE="fish"
+DNAME="fish"
+
+case $1 in
+    start)
+        exit 0
+        ;;
+    stop)
+        exit 0
+        ;;
+    status)
+        exit 0
+        ;;
+    log)
+        exit 1
+        ;;
+    *)
+        exit 1
+        ;;
+esac
---
+++ spk/fish/src/installer.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Package
+PACKAGE="fish"
+DNAME="fish"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+
+    # Put zsh in the PATH
+    mkdir -p /usr/local/bin
+    ln -s ${INSTALL_DIR}/bin/fish /usr/local/bin/fish
+
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+    rm -f /usr/local/bin/zsh
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
