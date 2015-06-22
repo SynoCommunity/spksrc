507b6ebbedd388647ff01571ea7a595ad4671760
Pieter Janssens <piejanssens@users.noreply.github.com>
2015-06-22 14:35:41 +0200

    Migration entry for cfg tpl

--- spk/haproxy/src/installer.sh
+++ spk/haproxy/src/installer.sh
@@ -98,6 +98,7 @@ preupgrade ()
     # Revision 18 runs as root, but requires 'haproxy' user entry in conf
     if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -le 18 ]; then
         sed -ie "/^global$/a\	user haproxy" ${CFG_FILE}
+        sed -ie "/^global$/a\	user haproxy" ${CFG_FILE}.tpl
     fi
 
     # Save some stuff
