d4dc712abca4690c21f8a045cb5cc3e6877e5700
Roman Tymoshyk <tymoshyk.roman@gmail.com>
2015-11-10 16:40:17 +0200

    Fixed whitespaces in revision and updated installer scenario

--- spk/fuse-exfat/src/installer.sh
+++ spk/fuse-exfat/src/installer.sh
@@ -19,8 +19,14 @@ postinst ()
     
     # Put exfat & fusermount in the PATH
     mkdir -p /usr/local/bin
-    ln -s ${INSTALL_DIR}/bin/mount.exfat /usr/local/bin/mount.exfat
-	ln -s ${INSTALL_DIR}/bin/fusermount /usr/local/bin/fusermount
+    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/local/bin/mount.exfat
+    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/local/bin/mount.exfat-fuse
+    ln -s ${INSTALL_DIR}/sbin/dumpexfat /usr/local/bin/dumpexfat
+    ln -s ${INSTALL_DIR}/sbin/exfatfsck /usr/local/bin/exfatfsck
+    ln -s ${INSTALL_DIR}/sbin/exfatlabel /usr/local/bin/exfatlabel
+	ln -s ${INSTALL_DIR}/sbin/exfatfsck /usr/local/bin/fsck.exfat
+	ln -s ${INSTALL_DIR}/sbin/mkexfatfs /usr/local/bin/mkexfatfs
+	ln -s ${INSTALL_DIR}/sbin/mkexfatfs /usr/local/bin/mkfs.exfat
 
     exit 0
 }
@@ -35,7 +41,14 @@ postuninst ()
     # Remove links
     rm -f ${INSTALL_DIR}
     rm -f /usr/local/bin/mount.exfat
-	rm -f /usr/local/bin/fusermount
+    rm -f /usr/local/bin/mount.exfat-fuse
+	rm -f /usr/local/bin/dumpexfat
+	rm -f /usr/local/bin/exfatfsck
+	rm -f /usr/local/bin/exfatlabel
+	rm -f /usr/local/bin/fsck.exfat
+	rm -f /usr/local/bin/mkexfatfs
+	rm -f /usr/local/bin/mkfs.exfat
+	
     exit 0
 }
 
