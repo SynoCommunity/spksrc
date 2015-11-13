2a78743ae4d7a95f7712d48fc01152f7d4d70e91
Roman Tymoshyk <tymoshyk.roman@gmail.com>
2015-11-13 17:07:13 +0200

    Fixed DNAME in install script and removed extra links

--- spk/fuse-exfat/src/installer.sh
+++ spk/fuse-exfat/src/installer.sh
@@ -2,7 +2,7 @@
 
 # Package
 PACKAGE="exfat"
-DNAME="exfat"
+DNAME="exFAT"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
@@ -19,14 +19,8 @@ postinst ()
     
     # Put fuse-exfat & exfat-utils in the PATH
     mkdir -p /usr/local/bin
-    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/sbin/mount.exfat
-    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/sbin/mount.exfat-fuse
-    ln -s ${INSTALL_DIR}/sbin/dumpexfat /usr/local/bin/dumpexfat
-    ln -s ${INSTALL_DIR}/sbin/exfatfsck /usr/local/bin/exfatfsck
-    ln -s ${INSTALL_DIR}/sbin/exfatlabel /usr/local/bin/exfatlabel
-    ln -s ${INSTALL_DIR}/sbin/exfatfsck /usr/local/bin/fsck.exfat
-    ln -s ${INSTALL_DIR}/sbin/mkexfatfs /usr/local/bin/mkexfatfs
-    ln -s ${INSTALL_DIR}/sbin/mkexfatfs /usr/local/bin/mkfs.exfat
+    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/local/bin/mount.exfat
+    ln -s ${INSTALL_DIR}/sbin/mount.exfat-fuse /usr/local/bin/mount.exfat-fuse
 
     exit 0
 }
@@ -40,14 +34,8 @@ postuninst ()
 {
     # Remove links
     rm -f ${INSTALL_DIR}
-    rm -f /usr/sbin/mount.exfat
-    rm -f /usr/sbin/mount.exfat-fuse
-    rm -f /usr/local/bin/dumpexfat
-    rm -f /usr/local/bin/exfatfsck
-    rm -f /usr/local/bin/exfatlabel
-    rm -f /usr/local/bin/fsck.exfat
-    rm -f /usr/local/bin/mkexfatfs
-    rm -f /usr/local/bin/mkfs.exfat
+    rm -f /usr/local/bin/mount.exfat
+    rm -f /usr/local/bin/mount.exfat-fuse
 	
     exit 0
 }
