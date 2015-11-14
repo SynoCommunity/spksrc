95d0d417e514eb775553e4ab161040497fcc41a9
Daniel Lorch <dlorch@gmail.com>
2015-11-14 19:29:38 +0000

    Add igmp-querier 1.0

---
+++ spk/igmp-querier/src/installer.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Package
+PACKAGE="igmp-querier"
+DNAME="IGMPQuerier"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PATH="${INSTALL_DIR}/sbin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Link
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+    
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove link
+    rm -f ${INSTALL_DIR}
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
