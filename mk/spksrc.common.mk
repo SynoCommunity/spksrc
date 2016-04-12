990d85a575a9cc8b54347d24e30a9bd7f8e882ad
Dr-Bean <github@beanpoint.com>
2016-04-12 11:29:57 +0200

    Fix typos

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -13,7 +13,7 @@ MSG = echo "===> "
 # Launch command in the working dir of a software with the right environment
 RUN = cd $(WORK_DIR)/$(PKG_DIR) && env $(ENV)
 
-# Pip command
+# Pip command
 PIP ?= pip
 PIP_WHEEL = $(RUN) $(PIP) wheel --no-use-wheel --no-deps --wheel-dir $(STAGING_DIR)/wheelhouse
 
@@ -24,7 +24,7 @@ LANGUAGES = chs cht csy dan enu fre ger hun ita jpn krn nld nor plk ptb ptg rus 
 AVAILABLE_TCS = $(notdir $(wildcard ../../toolchains/syno-*))
 AVAILABLE_ARCHS = $(notdir $(subst syno-,/,$(AVAILABLE_TCS)))
 
-#Toolchain filters
+# Toolchain filters
 SUPPORTED_ARCHS = $(sort $(filter-out 88f5281% powerpc% ppc824% ppc854x%, $(AVAILABLE_ARCHS)))
 LEGACY_ARCHS = $(sort $(filter-out $(SUPPORTED_ARCHS), $(AVAILABLE_ARCHS)))
 
