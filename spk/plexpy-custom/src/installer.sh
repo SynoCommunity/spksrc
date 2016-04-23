693ee6d9c5771cd8ade39eacc5b0f7fbb7f3caea
rjblake <richard.blake@gmail.com>
2016-04-23 15:31:01 +0200

    Update installer.sh

--- spk/plexpy-custom/src/installer.sh
+++ spk/plexpy-custom/src/installer.sh
@@ -11,39 +11,13 @@ PYTHON_DIR="/usr/local/python"
 GIT_DIR="/usr/local/git"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
 USER="plexpy-custom"
-GROUP="users"
+GROUP="nobody"
 GIT="${GIT_DIR}/bin/git"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
 
 SERVICETOOL="/usr/syno/bin/servicetool"
 FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
-
-SYNO_GROUP="sc-media"
-SYNO_GROUP_DESC="SynoCommunity's media related group"
-
-syno_group_create ()
-{
-    # Create syno group (Does nothing when group already exists)
-    synogroup --add ${SYNO_GROUP} ${USER} > /dev/null
-    # Set description of the syno group
-    synogroup --descset ${SYNO_GROUP} "${SYNO_GROUP_DESC}"
-
-    # Add user to syno group (Does nothing when user already in the group)
-    addgroup ${USER} ${SYNO_GROUP}
-}
-
-syno_group_remove ()
-{
-    # Remove user from syno group
-    delgroup ${USER} ${SYNO_GROUP}
-
-    # Check if syno group is empty
-    if ! synogroup --get ${SYNO_GROUP} | grep -q "0:"; then
-        # Remove syno group
-        synogroup --del ${SYNO_GROUP} > /dev/null
-    fi
-}
 
 preinst ()
 {
@@ -72,8 +46,6 @@ postinst ()
     # Create user
     adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
 
-    syno_group_create
-
     # Correct the files ownership
     chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
@@ -90,7 +62,6 @@ preuninst ()
 
     # Remove the user if uninstalling
     if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
-        syno_group_remove
 
         delgroup ${USER} ${GROUP}
         deluser ${USER}
