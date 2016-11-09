633a5cb7491e2787199a53d3f7107801c709c83b
torrentalle <albert.moreno.t@gmail.com>
2016-11-09 23:11:24 +0000

    Add initial UI files

--- spk/gogs/src/installer.sh
+++ spk/gogs/src/installer.sh
@@ -17,8 +17,28 @@ WORKDIR="${INSTALL_DIR}/var"
 GROUP_SYNC="sc-syncthing"
 GROUP_SYNC_DESC="SynoCommunity's Syncthing group"
 
+MYSQL="$([ $(/bin/get_key_value /etc.defaults/VERSION buildnumber) -ge 7135 ] && echo -n /bin/mysql || echo -n /usr/syno/mysql/bin/mysql)"
+MYSQL_USER=${PACKAGE}
+MYSQL_DATABASE=${PACKAGE}
+
+
 preinst ()
 {
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        if ! ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e quit > /dev/null 2>&1; then
+            echo "Incorrect MySQL root password"
+            exit 1
+        fi
+        if ${MYSQL} -u root -p"${wizard_mysql_password_root}" mysql -e "SELECT User FROM user" | grep ^${MYSQL_USER}$ > /dev/null 2>&1; then
+            echo "MySQL user ${MYSQL_USER} already exists"
+            exit 1
+        fi
+        if ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "SHOW DATABASES" | grep ^${MYSQL_DATABASE}$ > /dev/null 2>&1; then
+            echo "MySQL database ${MYSQL_DATABASE} already exists"
+            exit 1
+        fi
+    fi
+
     exit 0
 }
 
@@ -32,6 +52,12 @@ postinst ()
 
     # Create user
     adduser -H -h ${WORKDIR} -g "${DNAME} User" -G ${GROUP} -s /bin/sh -D ${USER}
+
+     # Setup database and run installer
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+        ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${wizard_mysql_password_fengoffice:=gogs}';"
+    fi
 
     # Create custom config
     #mkdir -p ${WORKDIR}/custom/conf
@@ -49,15 +75,14 @@ postinst ()
 
 preuninst ()
 {
+    # Check database
+    if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ] && ! ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e quit > /dev/null 2>&1; then
+        echo "Incorrect MySQL root password"
+        exit 1
+    fi
+
     # Stop the package
     ${SSS} stop > /dev/null
-
-    # Remove the user (if not upgrading)
-    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
-        #sync_group_remove
-        #delgroup ${USER} ${GROUP}
-        deluser ${USER}
-    fi
 
     # Remove firewall config
     if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
@@ -71,6 +96,12 @@ postuninst ()
 {
     # Remove link
     rm -f ${INSTALL_DIR}
+    
+    # Remove the data and user (if not upgrading)
+    if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
+        ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "DROP DATABASE ${MYSQL_DATABASE}; DROP USER '${MYSQL_USER}'@'localhost';"
+        deluser ${USER}
+    fi   
 
     exit 0
 }
@@ -79,8 +110,6 @@ preupgrade ()
 {
     # Stop the package
     ${SSS} stop > /dev/null
-
-#    sync_group_create
 
     # Save some stuff
     rm -fr ${TMP_DIR}/${PACKAGE}
