380c0efc0d39e1b66e605968cb895071a41987d6
Synology <synology@synology.localdomain>
2016-11-12 09:55:54 +0100

    Update Tcl to v8.6.6-6
    Changes according to Dr-Bean's comments

--- spk/tcl/src/installer.sh
+++ spk/tcl/src/installer.sh
@@ -6,6 +6,9 @@ DNAME="Tcl"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
+PACKAGE_DIR="/var/packages/${PACKAGE}/target"
+LOG_FILE="/tmp/${PACKAGE}-sss.log"
+
 
 
 preinst ()
@@ -16,11 +19,10 @@ preinst ()
 postinst ()
 {
 	# Create symlinks
-	ln -s ${SYNOPKG_PKGDEST}/bin/tclsh8.6 /usr/local/bin/tclsh
-	ln -s ${SYNOPKG_PKGDEST}/lib/libtcl8.6.so /usr/local/lib/libtcl.so
-	ln -s ${SYNOPKG_PKGDEST}/lib/libtcl8.6.so /usr/local/lib/libtcl8.6.so
-	ln -s ${SYNOPKG_PKGDEST}/lib/tcl8.6/ /usr/local/lib/tcl8.6
-	export LD_LIBRARY_PATH=/usr/local/lib/
+	ln -s ${PACKAGE_DIR}/bin/tclsh8.6 /usr/local/bin/tclsh >> ${LOG_FILE} 2>&1
+	ln -s ${PACKAGE_DIR}/lib/libtcl8.6.so /usr/local/lib/libtcl.so >> ${LOG_FILE} 2>&1
+	ln -s ${PACKAGE_DIR}/lib/libtcl8.6.so /usr/local/lib/libtcl8.6.so >> ${LOG_FILE} 2>&1
+	ln -s ${PACKAGE_DIR}/ /usr/local/${PACKAGE} >> ${LOG_FILE} 2>&1
 
 	exit 0
 }
@@ -33,10 +35,13 @@ preuninst ()
 postuninst ()
 {
 	# Remove symlinks
+	rm -f /usr/local/${PACKAGE}
 	rm -f /usr/local/bin/tclsh
 	rm -f /usr/local/lib/libtcl.so
 	rm -f /usr/local/lib/libtcl8.6.so
-	rm -f /usr/local/lib/tcl8.6
+
+	# Remove logfile
+	rm -f ${LOG_FILE}
 
 	exit 0
 }
