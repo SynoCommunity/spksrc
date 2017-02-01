1667dfbb7d5187cc6229c96d5ae2348363f7b80b
Dr-Bean <github@beanpoint.com>
2017-02-01 21:47:09 +0100

    subliminal

--- spk/subliminal/src/installer.sh
+++ spk/subliminal/src/installer.sh
@@ -9,10 +9,41 @@ INSTALL_DIR="/usr/local/${PACKAGE}"
 SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PYTHON_DIR="/usr/local/python"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
-USER="subliminal"
-GROUP="users"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+SERVICETOOL="/usr/syno/bin/servicetool"
+BUILDNUMBER="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
+FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
+
+DSM6_UPGRADE="${INSTALL_DIR}/var/.dsm6_upgrade"
+SC_USER="sc-subliminal"
+SC_GROUP="sc-media"
+SC_GROUP_DESC="SynoCommunity's media related group"
+LEGACY_USER="subliminal"
+LEGACY_GROUP="users"
+USER="$([ "${BUILDNUMBER}" -ge "7321" ] && echo -n ${SC_USER} || echo -n ${LEGACY_USER})"
+
+
+syno_group_create ()
+{
+    # Create syno group
+    synogroup --add ${SC_GROUP} ${USER} > /dev/null
+    # Set description of the syno group
+    synogroup --descset ${SC_GROUP} "${SC_GROUP_DESC}"
+    # Add user to syno group
+    addgroup ${USER} ${SC_GROUP}
+}
+
+syno_group_remove ()
+{
+    # Remove user from syno group
+    delgroup ${USER} ${SC_GROUP}
+    # Check if syno group is empty
+    if ! synogroup --get ${SC_GROUP} | grep -q "0:"; then
+        # Remove syno group
+        synogroup --del ${SC_GROUP} > /dev/null
+    fi
+}
 
 
 preinst ()
@@ -25,8 +56,12 @@ postinst ()
     # Link
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
-    # Create user
-    adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
+    # Create legacy user
+    if [ "${BUILDNUMBER}" -lt "7321" ]; then
+        adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${LEGACY_GROUP} -s /bin/sh -S -D ${LEGACY_USER}
+    fi
+    
+    syno_group_create
 
     # Create a Python virtualenv
     ${VIRTUALENV} --system-site-packages ${INSTALL_DIR}/env > /dev/null
@@ -48,10 +83,14 @@ preuninst ()
     # Stop the package
     ${SSS} stop > /dev/null
 
-    # Remove the user (if not upgrading)
     if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
-        delgroup ${USER} ${GROUP}
+        # Remove the user (if not upgrading)
+        syno_group_remove
+        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
         deluser ${USER}
+
+        # Remove firewall configuration
+        ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc >> /dev/null
     fi
 
     exit 0
@@ -69,6 +108,13 @@ preupgrade ()
 {
     # Stop the package
     ${SSS} stop > /dev/null
+
+    # DSM6 Upgrade handling
+    if [ "${BUILDNUMBER}" -ge "7321" ] && [ ! -f ${DSM6_UPGRADE} ]; then
+        echo "Deleting legacy user" > ${DSM6_UPGRADE}
+        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
+        deluser ${LEGACY_USER}
+    fi
 
     # Revision 12 introduces backward incompatible changes
     if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -le 11 ]; then
@@ -90,6 +136,9 @@ postupgrade ()
     rm -fr ${INSTALL_DIR}/var
     mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
     rm -fr ${TMP_DIR}/${PACKAGE}
+
+    # Ensure file ownership is correct after upgrade
+    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
     exit 0
 }
