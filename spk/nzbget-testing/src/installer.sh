a4dcffea8dff442d4dca2714ed3feac80da98142
Dr-Bean <github@beanpoint.com>
2017-02-01 21:42:37 +0100

    nzbget-testing

--- spk/nzbget-testing/src/installer.sh
+++ spk/nzbget-testing/src/installer.sh
@@ -10,59 +10,60 @@ SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PATH="${INSTALL_DIR}/bin:${PATH}"
 CFG_FILE="${INSTALL_DIR}/var/nzbget.conf"
 TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
-
+BUILDNUMBER="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
 SERVICETOOL="/usr/syno/bin/servicetool"
 FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
 
-DSM6_UPGRADE="${INSTALL_DIR}/var/dsm6_upgrade"
-USER="sc-nzbget-testing"
+DSM6_UPGRADE="${INSTALL_DIR}/var/.dsm6_upgrade"
+SC_USER="sc-nzbget-testing"
+SC_GROUP="sc-download"
+SC_GROUP_DESC="SynoCommunity's download related group"
 LEGACY_USER="nzbget-testing"
 LEGACY_GROUP="users"
-PACKAGE_USER="$([ "${BUILDNUMBER}" -ge "7321" ] && echo -n ${USER} || echo -n ${LEGACY_USER})"
+USER="$([ "${BUILDNUMBER}" -ge "7321" ] && echo -n ${SC_USER} || echo -n ${LEGACY_USER})"
 
-SYNO_GROUP="sc-download"
-SYNO_GROUP_DESC="SynoCommunity's download related group"
 
 syno_group_create ()
 {
     # Create syno group
-    synogroup --add ${SYNO_GROUP} ${PACKAGE_USER} > /dev/null
+    synogroup --add ${SC_GROUP} ${USER} > /dev/null
     # Set description of the syno group
-    synogroup --descset ${SYNO_GROUP} "${SYNO_GROUP_DESC}"
+    synogroup --descset ${SC_GROUP} "${SC_GROUP_DESC}"
     # Add user to syno group
-    addgroup ${PACKAGE_USER} ${SYNO_GROUP}
+    addgroup ${USER} ${SC_GROUP}
 }
 
 syno_group_remove ()
 {
     # Remove user from syno group
-    delgroup ${PACKAGE_USER} ${SYNO_GROUP}
+    delgroup ${USER} ${SC_GROUP}
     # Check if syno group is empty
-    if ! synogroup --get ${SYNO_GROUP} | grep -q "0:"; then
+    if ! synogroup --get ${SC_GROUP} | grep -q "0:"; then
         # Remove syno group
-        synogroup --del ${SYNO_GROUP} > /dev/null
+        synogroup --del ${SC_GROUP} > /dev/null
     fi
 }
 
 set_syno_permissions ()
 {
-    # Sets recursive permissions for ${SYNO_GROUP} on specified directory
+    # Sets recursive permissions for ${SC_GROUP} on specified directory
     # Usage: set_syno_permissions "${wizard_download_dir}"
     DIRNAME=$1
     VOLUME=`echo $1 | awk -F/ '{print "/"$2}'`
-    # Set read/write permissions for SYNO_GROUP on target directory
-    if [ ! "`synoacltool -get "${DIRNAME}"| grep "group:${SYNO_GROUP}:allow:rwxpdDaARWc--:fd--"`" ]; then
-        synoacltool -add "${DIRNAME}" "group:${SYNO_GROUP}:allow:rwxpdDaARWc--:fd--" > /dev/null 2>&1
+    # Set read/write permissions for SC_GROUP on target directory
+    if [ ! "`synoacltool -get "${DIRNAME}"| grep "group:${SC_GROUP}:allow:rwxpdDaARWc--:fd--"`" ]; then
+        synoacltool -add "${DIRNAME}" "group:${SC_GROUP}:allow:rwxpdDaARWc--:fd--" > /dev/null 2>&1
     fi
     # Walk up the tree and set traverse permissions up to VOLUME
     DIRNAME="$(dirname "${DIRNAME}")"
     while [ "${DIRNAME}" != "${VOLUME}" ]; do
-        if [ ! "`synoacltool -get "${DIRNAME}"| grep "group:${SYNO_GROUP}:allow:..x"`" ]; then
-            synoacltool -add "${DIRNAME}" "group:${SYNO_GROUP}:allow:--x----------:---n" > /dev/null 2>&1
+        if [ ! "`synoacltool -get "${DIRNAME}"| grep "group:${SC_GROUP}:allow:..x"`" ]; then
+            synoacltool -add "${DIRNAME}" "group:${SC_GROUP}:allow:--x----------:---n" > /dev/null 2>&1
         fi
         DIRNAME="$(dirname "${DIRNAME}")"
     done
 }
+
 
 preinst ()
 {
@@ -98,9 +99,8 @@ postinst ()
                -e "s/@control_username@/${wizard_control_username:=nzbget}/g" \
                -e "s/@control_password@/${wizard_control_password:=nzbget}/g" \
                ${CFG_FILE}
-        # Permissions handling for download dir
+        # Permissions handling
         if [ "${BUILDNUMBER}" -ge "7321" ]; then
-            # No longer using 'users' group for DSM6+
             set_syno_permissions "${wizard_download_dir:=/volume1/downloads}"
         else
             chgrp users ${wizard_download_dir:=/volume1/downloads}
@@ -109,7 +109,7 @@ postinst ()
     fi
 
     # Correct the files ownership
-    chown -R ${PACKAGE_USER}:root ${SYNOPKG_PKGDEST}
+    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
     # Add firewall config
     ${SERVICETOOL} --install-configure-file --package ${FWPORTS} >> /dev/null
@@ -126,7 +126,7 @@ preuninst ()
         # Remove the user (if not upgrading)
         syno_group_remove
         delgroup ${LEGACY_USER} ${LEGACY_GROUP}
-        deluser ${PACKAGE_USER}
+        deluser ${USER}
 
         # Remove firewall configuration
         ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc >> /dev/null
@@ -177,7 +177,7 @@ postupgrade ()
     rm -fr ${TMP_DIR}/${PACKAGE}
 
     # Ensure file ownership is correct after upgrade
-    chown -R ${PACKAGE_USER}:root ${SYNOPKG_PKGDEST}
+    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
     exit 0
 }
