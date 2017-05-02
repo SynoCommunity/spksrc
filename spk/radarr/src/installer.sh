2bda735a6b8ff279faea2cdbe13a6d74c5b46c98
maxrogers <maxrogers@gmail.com>
2017-05-02 06:16:36 -0600

    Radarr DSM6 Compatibility: var->.config + version checking fix (#2746)

--- spk/radarr/src/installer.sh
+++ spk/radarr/src/installer.sh
@@ -6,48 +6,52 @@ DNAME="Radarr"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
-TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
-INSTALL_LOG="${INSTALL_DIR}/var/install.log"
-TMP_INSTALL_LOG="${TMP_DIR}/${PACKAGE}/var/install.log"
 SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
 PATH="${INSTALL_DIR}/bin:${PATH}"
-USER="${PACKAGE}"
-GROUP="users"
-PID_FILE="${INSTALL_DIR}/var/.config/${DNAME}/nzbdrone.pid"
 MONO_PATH="/usr/local/mono/bin"
 MONO="${MONO_PATH}/mono"
-RADARR="${INSTALL_DIR}/share/${DNAME}/Radarr.exe"
-SPK_RADARR="${SYNOPKG_PKGINST_TEMP_DIR}/share/${DNAME}/Radarr.exe"
-COMMAND="env PATH=${MONO_PATH}:${PATH} LD_LIBRARY_PATH=${INSTALL_DIR}/lib ${MONO}"
-
+TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
 SERVICETOOL="/usr/syno/bin/servicetool"
+BUILDNUMBER="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
 FWPORTS="/var/packages/${PACKAGE}/scripts/${PACKAGE}.sc"
 
-SYNO_GROUP="sc-media"
-SYNO_GROUP_DESC="SynoCommunity's media related group"
+RADARR="${INSTALL_DIR}/share/Radarr/Radarr.exe"
+SPK_RADARR="${SYNOPKG_PKGINST_TEMP_DIR}/share/Radarr/Radarr.exe"
+COMMAND="env PATH=${MONO_PATH}:${PATH} LD_LIBRARY_PATH=${INSTALL_DIR}/lib ${MONO}"
+PID_FILE="${INSTALL_DIR}/.config/Radarr/nzbdrone.pid"
+INSTALL_LOG="${INSTALL_DIR}/.config/install.log"
+TMP_INSTALL_LOG="${TMP_DIR}/${PACKAGE}/.config/install.log"
+
+DSM6_UPGRADE="${INSTALL_DIR}/.config/.dsm6_upgrade"
+SC_USER="sc-radarr"
+SC_GROUP="sc-media"
+SC_GROUP_DESC="SynoCommunity's media related group"
+LEGACY_USER="radarr"
+LEGACY_GROUP="users"
+USER="$([ "${BUILDNUMBER}" -ge "7321" ] && echo -n ${SC_USER} || echo -n ${LEGACY_USER})"
+
 
 syno_group_create ()
 {
-    # Create syno group (Does nothing when group already exists)
-    synogroup --add ${SYNO_GROUP} ${USER} > /dev/null
+    # Create syno group
+    synogroup --add ${SC_GROUP} ${USER} > /dev/null
     # Set description of the syno group
-    synogroup --descset ${SYNO_GROUP} "${SYNO_GROUP_DESC}"
-
-    # Add user to syno group (Does nothing when user already in the group)
-    addgroup ${USER} ${SYNO_GROUP}
+    synogroup --descset ${SC_GROUP} "${SC_GROUP_DESC}"
+    # Add user to syno group
+    addgroup ${USER} ${SC_GROUP}
 }
 
 syno_group_remove ()
 {
     # Remove user from syno group
-    delgroup ${USER} ${SYNO_GROUP}
-
+    delgroup ${USER} ${SC_GROUP}
     # Check if syno group is empty
-    if ! synogroup --get ${SYNO_GROUP} | grep -q "0:"; then
+    if ! synogroup --get ${SC_GROUP} | grep -q "0:"; then
         # Remove syno group
-        synogroup --del ${SYNO_GROUP} > /dev/null
+        synogroup --del ${SC_GROUP} > /dev/null
     fi
 }
+
 
 preinst ()
 {
@@ -60,16 +64,22 @@ postinst ()
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
     # Logging Install
-    mkdir -p ${INSTALL_DIR}/var
+    mkdir -p ${INSTALL_DIR}/.config
     echo "|| Installing package $(grep "version" /var/packages/${PACKAGE}/INFO) - $(date) ||" >> ${INSTALL_LOG}
 
     # Install busybox stuff
     ${INSTALL_DIR}/bin/busybox --install ${INSTALL_DIR}/bin
 
-    # Create user
-    adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
+    # Create legacy user
+    if [ "${BUILDNUMBER}" -lt "7321" ]; then
+        adduser -h ${INSTALL_DIR}/.config -g "${DNAME} User" -G ${LEGACY_GROUP} -s /bin/sh -S -D ${LEGACY_USER}
+    fi
 
     syno_group_create
+
+    # Move config.xml to .config
+    mkdir -p ${INSTALL_DIR}/.config/Radarr
+    mv ${INSTALL_DIR}/app/config.xml ${INSTALL_DIR}/.config/Radarr/config.xml
 
     # Correct the files ownership
     chown -R ${USER}:root ${SYNOPKG_PKGDEST}
@@ -88,16 +98,13 @@ preuninst ()
     # Stop the package
     ${SSS} stop > /dev/null
 
-    # Remove the user (if not upgrading)
     if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
+        # Remove the user (if not upgrading)
         syno_group_remove
-
-        delgroup ${USER} ${GROUP}
+        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
         deluser ${USER}
-    fi
 
-    # Remove firewall config
-    if [ "${SYNOPKG_PKG_STATUS}" == "UNINSTALL" ]; then
+        # Remove firewall configuration
         ${SERVICETOOL} --remove-configure-file --package ${PACKAGE}.sc >> /dev/null
     fi
 
@@ -114,28 +121,43 @@ postuninst ()
 
 preupgrade ()
 {
-    # Log Upgrade
-    echo "|| Beginning Package Upgrade - $(date) ||" >> ${INSTALL_LOG}
-
     # Stop the package
     ${SSS} stop > /dev/null
+
+    # DSM6 Upgrade handling
+    if [ "${BUILDNUMBER}" -ge "7321" ] && [ ! -f ${DSM6_UPGRADE} ]; then
+        echo "Deleting legacy user" > ${DSM6_UPGRADE}
+        delgroup ${LEGACY_USER} ${LEGACY_GROUP}
+        deluser ${LEGACY_USER}
+    fi
+
+    # Log Upgrade
+    echo "|| Beginning Package Upgrade - $(date) ||" >> ${INSTALL_LOG}
 
     # Save some stuff
     rm -fr ${TMP_DIR}/${PACKAGE}
     mkdir -p ${TMP_DIR}/${PACKAGE}
-    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
+
+    # Check for legacy var folder and (if found) move contents to .config
+    if [ -d "${INSTALL_DIR}/var" ]; then
+    mv ${INSTALL_DIR}/var/.config ${TMP_DIR}/${PACKAGE}/
+    mv ${INSTALL_DIR}/var/install.log ${TMP_INSTALL_LOG}
+    echo "   [LEGACY] Found var Folder - Moving To .config" >> ${TMP_INSTALL_LOG}
+    else
+    mv ${INSTALL_DIR}/.config ${TMP_DIR}/${PACKAGE}/
+    fi
 
     # Is Installed Radarr Binary Ver. >= SPK Radarr Binary Ver.?
-    CUR_VER=$(${COMMAND} ${RADARR} --? | grep -o "Version.*" | awk '{print $2;exit;}' | tr -d '.')
+    CUR_VER=$(${MONO_PATH}/monodis --assembly ${RADARR} | grep "Version:" | awk '{print $2}')
     echo "   Installed Radarr Binary: ${CUR_VER}" >> ${TMP_INSTALL_LOG}
-    SPK_VER=$(${COMMAND} ${SPK_RADARR} --? | grep -o "Version.*" | awk '{print $2;exit;}' | tr -d '.')
+    SPK_VER=$(${MONO_PATH}/monodis --assembly ${SPK_RADARR} | grep "Version:" | awk '{print $2}')
     echo "   Requested Radarr Binary: ${SPK_VER}" >> ${TMP_INSTALL_LOG}
-    if [ "$CUR_VER" -ge "$SPK_VER" ]; then
-       echo 'KEEP_CUR="yes"' > ${TMP_DIR}/${PACKAGE}/var/KEEP_VAR
+    if [ "${CUR_VER//.}" -ge "${SPK_VER//.}" ]; then
+       echo 'KEEP_CUR="yes"' > ${TMP_DIR}/${PACKAGE}/.config/KEEP_VAR
        echo "   [KEEPING] Installed Radarr Binary - Upgrading Package Only" >> ${TMP_INSTALL_LOG}
        mv ${INSTALL_DIR}/share ${TMP_DIR}/${PACKAGE}/
     else
-       echo 'KEEP_CUR="no"' > ${TMP_DIR}/${PACKAGE}/var/KEEP_VAR
+       echo 'KEEP_CUR="no"' > ${TMP_DIR}/${PACKAGE}/.config/KEEP_VAR
        echo "   [REPLACING] Installed Radarr Binary" >> ${TMP_INSTALL_LOG}
     fi
 
@@ -145,11 +167,11 @@ preupgrade ()
 postupgrade ()
 {
     # Restore some stuff
-    rm -fr ${INSTALL_DIR}/var
-    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
+    rm -fr ${INSTALL_DIR}/.config
+    mv ${TMP_DIR}/${PACKAGE}/.config ${INSTALL_DIR}/
 
     # Restore Current Radarr Binary If Current Ver. >= SPK Ver.
-    . ${INSTALL_DIR}/var/KEEP_VAR
+    . ${INSTALL_DIR}/.config/KEEP_VAR
     if [ "$KEEP_CUR" == "yes" ]; then
        rm -fr ${INSTALL_DIR}/share
        mv ${TMP_DIR}/${PACKAGE}/share ${INSTALL_DIR}/
@@ -157,10 +179,13 @@ postupgrade ()
 
     # Remove Backups & Upgrade Flag
     rm -fr ${TMP_DIR}/${PACKAGE}
-    rm ${INSTALL_DIR}/var/KEEP_VAR
+    rm ${INSTALL_DIR}/.config/KEEP_VAR
 
     # Finish Logging
     echo "|| Package upgraded to $(grep "version" /var/packages/${PACKAGE}/INFO) - $(date) ||" >> ${INSTALL_LOG}
+
+    # Ensure file ownership is correct after upgrade
+    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
 
     exit 0
 }
