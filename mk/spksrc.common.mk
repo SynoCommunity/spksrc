7b758e855778cea43eb4c4b3fd223c4cb68efdca
David Bidorff <david.bidorff@orange.com>
2017-08-25 12:49:03 +0200

    [ADD] Support for rtd1296
    Add Toolchain for rtd1296

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -35,7 +35,8 @@ ARCHS_DUPES = $(filter-out avoton% braswell% broadwell% bromolow% cedarview% gra
 # Available Arches
 ARM5_ARCHES = 88f5281 88f6281
 ARM7_ARCHES = alpine armada370 armada375 armada38x armadaxp comcerto2k monaco
-ARM_ARCHES = $(ARM5_ARCHES) $(ARM7_ARCHES)
+ARM8_ARCHES = rtd1296
+ARM_ARCHES = $(ARM5_ARCHES) $(ARM7_ARCHES) $(ARM8_ARCHES)
 PPC_ARCHES = powerpc ppc824x ppc853x ppc854x qoriq
 x86_ARCHES = evansport
 x64_ARCHES = avoton braswell broadwell bromolow cedarview dockerx64 grantley kvm64 x86 x64 x86_64
