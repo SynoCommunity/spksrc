5750eb5094568672ffa359e23b64d501e0509784
Safihre <safihre@sabnzbd.org>
2017-12-26 14:20:48 +0100

    Force "sc-download" as group and correct SAB-specific permissions

--- spk/sabnzbd/src/service-setup.sh
+++ spk/sabnzbd/src/service-setup.sh
@@ -4,9 +4,34 @@ VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
 SABNZBD="${SYNOPKG_PKGDEST}/share/SABnzbd/SABnzbd.py"
 CFG_FILE="${SYNOPKG_PKGDEST}/var/config.ini"
+UPRGADE_CFG_FILE="${TMP_DIR}/config.ini"
 LANGUAGE="env LANG=en_US.UTF-8"
 
+GROUP="sc-download"
+
 SERVICE_COMMAND="${LANGUAGE} ${PYTHON} ${SABNZBD} -f ${CFG_FILE} --pidfile ${PID_FILE} -d"
+
+# Needed to force correct permissions, during update
+# Extract the right paths from config file
+if [ -r "${UPRGADE_CFG_FILE}" ]; then
+    INCOMPLETE_FOLDER=`grep -Po '(?<=download_dir = ).*' ${UPRGADE_CFG_FILE}`
+    COMPLETE_FOLDER=`grep -Po '(?<=complete_dir = ).*' ${UPRGADE_CFG_FILE}`
+    if [ -n "$(dirname "${INCOMPLETE_FOLDER}")" ]; then
+        SHARE_PATH=$(dirname "${INCOMPLETE_FOLDER}")
+    fi
+fi
+
+set_all_permissions ()
+{
+    # Fix permissions for EFF_USER and GROUP on DIRNAME
+    DIRNAME=$1
+    EFF_USER=$2
+    GROUP=$3
+    echo "Granting '${EFF_USER}' user permissions on ${DIRNAME}" >> ${INST_LOG}
+    synoacltool -add "${DIRNAME}" "user:${EFF_USER}:allow:rwxpdDaARWcC-:fd--" >> ${INST_LOG} 2>&1
+    echo "Granting '${GROUP}' group permissions on ${DIRNAME}" >> ${INST_LOG}
+    synoacltool -add "${DIRNAME}" "group:${GROUP}:allow:rwxpdDaARWcC-:fd--" >> ${INST_LOG} 2>&1
+}
 
 service_postinst ()
 {
@@ -24,10 +49,17 @@ service_postinst ()
         sed -i -e "s|@download_dir@|${wizard_download_dir:=/volume1/downloads}|g" ${CFG_FILE}
     fi
 
+    # Have to make sure our complete/incomplete dirs have right permissions
+    if [ -n "${INCOMPLETE_FOLDER}" ]; then
+        set_all_permissions "${INCOMPLETE_FOLDER}" "${EFF_USER}" "${GROUP}"
+    fi
+    if [ -n "${COMPLETE_FOLDER}" ]; then
+        set_all_permissions "${COMPLETE_FOLDER}" "${EFF_USER}" "${GROUP}"
+    fi
+
     # Discard legacy obsolete busybox user account
     BIN=${SYNOPKG_PKGDEST}/bin
     $BIN/busybox --install $BIN >> ${INST_LOG}
     $BIN/delgroup "${USER}" "users" >> ${INST_LOG}
     $BIN/deluser "${USER}" >> ${INST_LOG}
 }
-
