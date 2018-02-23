14ac6b3a3cea96c631be3d6ad768131c7a682f67
Safihre <safihre@sabnzbd.org>
2018-02-23 14:53:44 +0100

    Headphones - Generic Service approach

---
+++ spk/headphones/src/service-setup.sh
@@ -0,0 +1,30 @@
+PYTHON_DIR="/usr/local/python"
+GIT_DIR="/usr/local/git"
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
+PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+HEADPHONES="${SYNOPKG_PKGDEST}/share/Headphones/Headphones.py"
+CFG_FILE="${SYNOPKG_PKGDEST}/var/config.ini"
+LOG_FILE="${SYNOPKG_PKGDEST}/var/logs/headphones.log"
+
+SERVICE_COMMAND="${PYTHON} ${HEADPHONES} --daemon --pidfile ${PID_FILE} --config ${CFG_FILE} --datadir ${SYNOPKG_PKGDEST}/var/"
+
+GROUP="sc-download"
+LEGACY_GROUP="sc-media"
+
+service_postinst ()
+{
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
+
+    # Create logs directory, otherwise it doesn't start
+    mkdir "$(dirname ${LOG_FILE})" >> ${INST_LOG} 2>&1
+
+    # If nessecary, add user also to the old group
+    syno_user_add_to_legacy_group "${EFF_USER}" "${USER}" "${LEGACY_GROUP}"
+
+    # Remove legacy user
+    # Commands of busybox from spk/python
+    delgroup "${USER}" "users" >> ${INST_LOG}
+    deluser "${USER}" >> ${INST_LOG}
+}
