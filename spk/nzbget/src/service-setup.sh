44caa18b310d7d9bba31e091ecb3219242da67ef
Yves Martin <ymartin1040@gmail.com>
2018-02-23 09:02:31 +0100

    Wizard french translations and permissions handling for NZBGet (#3120)
    * Wizard french translations for NZBGet
    
    * NZBGet fix share path upgrade process
    
    Discard useless redirections already handled in generated scripts
    Fixes for DSM 5
    
    * Correct handeling of NZBGet permissions fixing

--- spk/nzbget/src/service-setup.sh
+++ spk/nzbget/src/service-setup.sh
@@ -2,58 +2,52 @@ PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 NZBGET="${SYNOPKG_PKGDEST}/bin/nzbget"
 CFG_FILE="${SYNOPKG_PKGDEST}/var/nzbget.conf"
 TEMPLATE_CFG_FILE="${SYNOPKG_PKGDEST}/share/nzbget/nzbget.conf"
-UPGRADE_CFG_FILE="${TMP_DIR}/nzbget.conf"
 WEBDIR="${SYNOPKG_PKGDEST}/bin/webui"
-
+NZBGET_INSTALLER="${SYNOPKG_PKGDEST}/var/nzbget.run"
 GROUP="sc-download"
 
 # Force-overwrite the PID-file and WebDir setting
 # These could change depending on previous package settings
 SERVICE_COMMAND="${NZBGET} -c ${CFG_FILE} -o WebDir=${WEBDIR} -o LockFile=${PID_FILE} -D"
 
-# Needed to force correct permissions, during update
-# Extract the right paths from config file
-if [ -r "${UPGRADE_CFG_FILE}" ]; then
-    DOWNLOAD_FOLDER=`grep -Po '(?<=MainDir=).*' ${UPGRADE_CFG_FILE}`
-    if [ -n "$(dirname "${DOWNLOAD_FOLDER}")" ]; then
-        SHARE_PATH=$(dirname "${DOWNLOAD_FOLDER}")
-    fi
-fi
-
 service_postinst ()
 {
     # Download latest NZBGet
     if [ -n "${wizard_stable_release}" ] && [ "${wizard_stable_release}" = true ]; then
-        wget -O "${SYNOPKG_PKGDEST}/nzbget.run" "https://nzbget.net/download/nzbget-latest-bin-linux.run" >> ${INST_LOG} 2>&1
+        wget -O "${NZBGET_INSTALLER}" "https://nzbget.net/download/nzbget-latest-bin-linux.run" >> ${INST_LOG} 2>&1
     fi
     if [ -n "${wizard_testing_release}" ] && [ "${wizard_testing_release}" = true ]; then
-        wget -O "${SYNOPKG_PKGDEST}/nzbget.run" "https://nzbget.net/download/nzbget-latest-testing-bin-linux.run" >> ${INST_LOG} 2>&1
+        wget -O "${NZBGET_INSTALLER}" "https://nzbget.net/download/nzbget-latest-testing-bin-linux.run" >> ${INST_LOG} 2>&1
     fi
 
     # Stop if download failed
-    if [ ! -r "${SYNOPKG_PKGDEST}/nzbget.run" ]; then
-        echo "Failed to download installer, please check the internet connection of your device." >> ${SYNOPKG_TEMP_LOGFILE}
+    if [ ! -r "${NZBGET_INSTALLER}" ]; then
+        echo "Failed to download installer, please check the internet connection of your device."
         exit 1
     fi
 
+    # On DSM5 the lib-dir is not owned by the package-user
+    set_unix_permissions "${SYNOPKG_PKGDEST}/bin"
+
     # Install as nzbget user, for correct permissions
-    sudo -u ${EFF_USER} sh ${SYNOPKG_PKGDEST}/nzbget.run --destdir ${SYNOPKG_PKGDEST}/bin >> ${INST_LOG} 2>&1
+    chmod +x "${NZBGET_INSTALLER}"
+    su ${EFF_USER} -s /bin/sh -c "${NZBGET_INSTALLER} --destdir ${SYNOPKG_PKGDEST}/bin" >> ${INST_LOG} 2>&1
 
     # Make sure installation worked
     if [ ! -r "${NZBGET}" ]; then
-        echo "The installer failed to install NZBGet. Please report the log below to SynoCommunity:" >> ${SYNOPKG_TEMP_LOGFILE}
-        echo "${INST_LOG}" >> ${SYNOPKG_TEMP_LOGFILE}
+        echo "The installer failed to install NZBGet. Please report the log below to SynoCommunity:"
+        echo "${INST_LOG}"
         exit 1
     fi
 
     # Copy the new template config file created by installer
     cp -f ${SYNOPKG_PKGDEST}/bin/nzbget.conf ${TEMPLATE_CFG_FILE}
 
-    # Rempove installer
-    rm -f ${SYNOPKG_PKGDEST}/nzbget.run
+    # Remove installer
+    rm -f "${NZBGET_INSTALLER}"
 
     # Correct options from wizard
-    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+    if [ ! -e "${CFG_FILE}" ]; then
         # Use whatever the installer found best
         # It does optimizations based on the current system
         cp -f ${TEMPLATE_CFG_FILE} ${CFG_FILE}
@@ -71,14 +65,21 @@ service_postinst ()
                ${CFG_FILE}
     fi
 
-    # Have to make sure our download dirs have right permissions
-    if [ -n "${DOWNLOAD_FOLDER}" ] && [ -d "${DOWNLOAD_FOLDER}" ]; then
-        set_syno_permissions "${DOWNLOAD_FOLDER}" "${GROUP}"
-    fi
-
     # Discard legacy obsolete busybox user account
     BIN=${SYNOPKG_PKGDEST}/bin
     $BIN/busybox --install $BIN >> ${INST_LOG}
     $BIN/delgroup "${USER}" "users" >> ${INST_LOG}
     $BIN/deluser "${USER}" >> ${INST_LOG}
+}
+
+service_postupgrade ()
+{
+    # Needed to force correct permissions, during update
+    # Extract the right paths from config file
+    if [ -r "${CFG_FILE}" ]; then
+        MAIN_DIR=`sed -n 's/^MainDir[ ]*=[ ]*//p' ${CFG_FILE}`
+        if [ -n "${MAIN_DIR}" ] && [ -d "${MAIN_DIR}" ]; then
+            set_syno_permissions "${MAIN_DIR}" "${GROUP}"
+        fi
+    fi
 }
