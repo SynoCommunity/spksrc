6535b8cddff4df831301802d726908fff23edb82
cytec <iamcytec@googlemail.com>
2018-03-01 16:07:44 +0100

    added tor spk

---
+++ spk/tor/src/service-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Package
+PACKAGE="tor"
+DNAME="Tor"
+
+# Others
+PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+
+TORBIN="${SYNOPKG_PKGDEST}/bin/tor"
+TORCONF="${SYNOPKG_PKGDEST}/var/torrc"
+
+SERVICE_COMMAND="${TORBIN} -f ${TORCONF} --pidfile ${PID_FILE} --user ${USER}"
+
+service_postinst ()
+{
+
+    #rename configuration depending on install mode
+    if $TOR_EXIT == true ; then
+        mv ${SYNOPKG_PKGDEST}/var/torrc.exit ${SYNOPKG_PKGDEST}/var/torrc
+    elif $TOR_NONEXIT == true ; then
+        mv ${SYNOPKG_PKGDEST}/var/torrc.noexit ${SYNOPKG_PKGDEST}/var/torrc
+    else
+        mv ${SYNOPKG_PKGDEST}/var/torrc.bridge ${SYNOPKG_PKGDEST}/var/torrc
+    fi
+
+    exit 0
+}
