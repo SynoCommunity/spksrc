f9c877204ec9a26f3fe424867547e832a048a81a
Synology <synology@synology.localdomain>
2018-03-20 21:24:35 +0100

    Added new package: Lua v5.3.4

--- spk/lua/src/service-setup.sh
+++ spk/lua/src/service-setup.sh
@@ -6,17 +6,11 @@ DNAME="Lua"
 
 
 
-service_preinst ()
-{
-	exit 0
-}
-
 service_postinst ()
 {
 	echo "Creating startup scripts for Lua executables" >> ${INST_LOG}
 	for executable in ${SYNOPKG_PKGDEST}/bin/lua*; do
-		echo "LD_LIBRARY_PATH=/var/packages/${PACKAGE}/target/lib:\${LD_LIBRARY_PATH} $executable" > /usr/local/bin/$(basename "$executable")
-		chmod 755 /usr/local/bin/$(basename "$executable")
+		ln -s "$executable" /usr/local/bin/$(basename "$executable")
 	done
 
 	exit 0
@@ -29,24 +23,6 @@ service_preuninst ()
 		rm /usr/local/bin/$(basename "$executable")
 	done
 
-	exit 0
-}
-
-service_postuninst ()
-{
-	# Remove permanent config store for this package
-	rm -rf /usr/syno/etc/packages/${PACKAGE}/
-
-	exit 0
-}
-
-service_preupgrade ()
-{
-	exit 0
-}
-
-service_postupgrade ()
-{
 	exit 0
 }
 
