ae6350df97c9a7a5f07aa355dfdb7ae420445ab5
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-04-10 22:34:29 +1000

    Allow user to choose dns servers by name
    Update dnscrypt-proxy to 2.0.9 beta 2

--- spk/dnscrypt-proxy/src/service-setup.sh
+++ spk/dnscrypt-proxy/src/service-setup.sh
@@ -14,11 +14,19 @@ service_postinst ()
         echo "Applying settings from Wizard..." >> "${INST_LOG}"
         cp -f "${TEMPLATE_CFG_FILE}" "${CFG_FILE}" >> "${INST_LOG}"
 
+        # if empty comment out server list
+        wizard_servers=${wizard_servers:-''}
+        if [ -z "${wizard_servers// }" ]; then
+            server_names_enabled='# '
+        fi
+
         listen_addresses=\[${wizard_listen_address:-"'0.0.0.0:10053'"}\]
+        server_names=\[${wizard_servers:-"'scaleway-fr', 'google', 'yandex', 'cloudflare'"}\]
 
         # change default settings
         sed -i -e "s/listen_addresses = .*/listen_addresses = ${listen_addresses}/" \
             -e "s/require_dnssec = .*/require_dnssec = true/" \
+            -e "s/# server_names = .*/${server_names_enabled:-""}server_names = ${server_names}/" \
             -e "s/ipv6_servers = .*/ipv6_servers = ${wizard_ipv6:=false}/" \
             "${CFG_FILE}" >> "${INST_LOG}"
     fi
