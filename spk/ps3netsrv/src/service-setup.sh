9e1f9df70cec15dab0044b1ae68e844db1c5624a
Nikolay Vasilchuk <nikolay.vasilchuk@corp.mail.ru>
2018-04-27 14:48:46 +0300

    Refactored running in background

--- spk/ps3netsrv/src/service-setup.sh
+++ spk/ps3netsrv/src/service-setup.sh
@@ -5,18 +5,14 @@ if [ -r "${CFG_FILE}" ]; then
     . "${CFG_FILE}"
 fi
 
+SERVICE_COMMAND="stdbuf -o L -e L ${BIN} ${PS3_DIR} ${PS3_PORT}"
+SVC_BACKGROUND=y
+SVC_WRITE_PID=y
+
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         sed -i -e "s|@wizard_dir@|${wizard_dir:=/volume1/PS3}|g" ${CFG_FILE}
         sed -i -e "s|@wizard_port@|${wizard_port:=38008}|g" ${CFG_FILE}
     fi
-}
-
-service_prestart ()
-{
-    COMMAND="${BIN} ${PS3_DIR} ${PS3_PORT}"
-    cd ${SYNOPKG_PKGDEST};
-    stdbuf -o L -e L ${COMMAND} >> ${LOG_FILE} 2>&1 &
-    echo "$!" > "${PID_FILE}"
 }
