14b7ab2f31e0ac69da822cffae0c6b5de735771d
Nikolay Vasilchuk <nikolay.vasilchuk@corp.mail.ru>
2018-07-19 16:20:14 +0300

    TG Links

--- spk/mtproxy/src/wizard/install_uifile.sh
+++ spk/mtproxy/src/wizard/install_uifile.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-gen_secret() {
-    echo $(dd if=/dev/urandom bs=16 count=1 2>&1 | od -tx1  | head -n1 | tail -c +9 | tr -d ' ' | tr '[:upper:]' '[:lower:]')
-}
+SECRET="$(dd if=/dev/urandom bs=16 count=1 2>&1 | od -tx1  | head -n1 | tail -c +9 | tr -d ' ' | tr '[:upper:]' '[:lower:]')"
+IP="$(curl -s -4 "https://digitalresistance.dog/myIp")"
 
 WIZARD_CONTENT="$(cat << 'EOF'
 [{
@@ -40,7 +39,6 @@ WIZARD_CONTENT="$(cat << 'EOF'
 }, {
     "step_title": "MTProto configuration",
     "invalid_next_disabled": true,
-    "activate": "{console.log('activate', arguments);}",
     "items": [{
         "type": "textfield",
         "subitems": [{
@@ -72,12 +70,19 @@ WIZARD_CONTENT="$(cat << 'EOF'
     }, {
         "desc": "You can get tag from <a href=\"https://t.me/MTProxybot\" target=\"_blank\">@MTProxybot</a> after registering proxy"
     }]
+}, {
+    "step_title": "Information",
+    "activeate": "{(function(step,ip){if(step.getNext()!=='applyStep'){return}var d=document,$s=d.querySelector('input[name=\"wizard_proxy_secrets\"]'),$p=d.querySelector('input[name=\"wizard_proxy_port\"]'),$l=d.getElementById('wizard_proxy_tg_links'),s=$s.value.split(','),p=$p.value,c='';s.forEach(function(k){var link='tg://proxy?server='+ip+'&port='+p+'&secret='+k;c+='<p><a href=\"'+link+'\" target=\"_blank\">'+link+'</a></p>'});$l.innerHTML=c;}).call(this,arguments[0],'@proxy_ip@');}",
+    "items": [{
+        "desc": "Your proxy links, please save them:"
+    }, {
+        "desc": "<span id=\"wizard_proxy_tg_links\"></span>"
+    }]
 }]
 EOF
 )"
 
-SECRET=$(gen_secret)
-OUTPUT=$(echo $WIZARD_CONTENT | sed "s#@proxy_secret@#${SECRET}#g")
+OUTPUT=$(echo $WIZARD_CONTENT | sed "s#@proxy_secret@#${SECRET}#g" | sed "s#@proxy_ip@#${IP}#g")
 
 echo $OUTPUT > $SYNOPKG_TEMP_LOGFILE
 exit 0
