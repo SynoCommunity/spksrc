f15b71b9055210bee21fa12c831e0044607497bb
Olivier Bourdon <obourdon@opennext.io>
2018-11-23 18:46:27 +0100

    Add tcpdump package

---
+++ spk/tcpdump/src/installer.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# Package
+PACKAGE="tcpdump"
+DNAME="tcpdump"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+PROGS="tcpdump"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    # Links
+    ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
+    for f in $PROGS; do ln -s ${INSTALL_DIR}/bin/$f /usr/local/bin/$f; done
+    
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    # Remove links
+    rm -f ${INSTALL_DIR}
+    for f in $PROGS; do rm -f /usr/local/bin/$f; done
+
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
