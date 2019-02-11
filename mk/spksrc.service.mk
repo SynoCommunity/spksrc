8815ab545b0fd2ac7a6846db92a8460916c87f8e
Stefaan Ghysels <stefaang@gmail.com>
2019-02-11 15:22:02 +0100

    Fix broken json on Ubuntu 18

--- mk/spksrc.service.mk
+++ mk/spksrc.service.mk
@@ -218,7 +218,8 @@ $(STAGING_DIR)/$(DSM_UI_DIR)/config:
 	@echo "    \"title\": \"${DISPLAY_NAME}\"," >> $@
 	@/bin/echo -n "    \"desc\": \"" >> $@
 	@/bin/echo -n "${DESCRIPTION}" | sed -e 's/\\//g' -e 's/"/\\"/g' >> $@
-	@echo "\",\n    \"icon\": \"images/${SPK_NAME}-{0}.png\"," >> $@
+	@echo "\"," >> $@
+	@echo "    \"icon\": \"images/${SPK_NAME}-{0}.png\"," >> $@
 	@echo "    \"type\": \"url\"," >> $@
 	@echo "    \"protocol\": \"${SERVICE_PORT_PROTOCOL}\"," >> $@
 	@echo "    \"port\": \"${SERVICE_PORT}\"," >> $@
