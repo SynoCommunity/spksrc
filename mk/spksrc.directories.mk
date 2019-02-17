5e69ef4efce8d277747af865a77f1a749862ee71
hgy59 <hpgy59@gmail.com>
2019-02-17 01:38:06 +0100

    Revert "framework: accelerate build, cache upstream pip/wheel downloads (#3620)"
    This reverts commit 416f001e72902577074ff9259fd9c610f2156d6c.

--- mk/spksrc.directories.mk
+++ mk/spksrc.directories.mk
@@ -12,9 +12,9 @@
 PWD := $(shell pwd)
 
 DISTRIB_DIR  = $(PWD)/../../distrib
-PIP_DIR = $(DISTRIB_DIR)/pip
-TOOLCHAINS_DIR = $(DISTRIB_DIR)/toolchains
-KERNELS_DIR = $(DISTRIB_DIR)/kernels
+PIP_DIR = $(PWD)/../../distrib/pip
+TOOLCHAINS_DIR = $(PWD)/../../distrib/toolchains
+KERNELS_DIR = $(PWD)/../../distrib/kernels
 PACKAGES_DIR = $(PWD)/../../packages
 
 ifndef WORK_DIR
