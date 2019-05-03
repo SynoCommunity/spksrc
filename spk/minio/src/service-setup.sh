43fa1dd352125f9755735268d0dc0067356d5aca
fgma <fgma@users.noreply.github.com>
2019-05-03 23:57:03 +0200

    minio: optional support for minio auto updates

--- spk/minio/src/service-setup.sh
+++ spk/minio/src/service-setup.sh
@@ -6,15 +6,18 @@ CFG_FILE="${CONFIG_DIR}/options.conf"
 
 service_postinst ()
 {
-	echo "Running service_postinst script" >> "${INST_LOG}"
+    echo "Running service_postinst script" >> "${INST_LOG}"
 
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        sed -i -e "s|@wizard_data_directory@|${wizard_data_directory}|g" ${CFG_FILE}
-        sed -i -e "s|@wizard_access_key@|${wizard_access_key}|g" ${CFG_FILE}
-        sed -i -e "s|@wizard_secret_key@|${wizard_secret_key}|g" ${CFG_FILE}
-	fi
+        sed -i \
+          -e "s|@wizard_data_directory@|${wizard_data_directory}|g" \
+          -e "s|@wizard_access_key@|${wizard_access_key}|g" \
+          -e "s|@wizard_secret_key@|${wizard_secret_key}|g" \
+          -e "s|@wizard_minio_auto_update@|${wizard_minio_auto_update}|g" \
+          ${CFG_FILE}
+    fi
 
-	echo "Install busybox binaries" >> "${INST_LOG}"
+    echo "Install busybox binaries" >> "${INST_LOG}"
     BIN=${SYNOPKG_PKGDEST}/bin
     $BIN/busybox --install $BIN >> ${INST_LOG}
 }
@@ -23,9 +26,15 @@ service_prestart ()
 {    
     if [ -f $CFG_FILE ]
     then
-		. $CFG_FILE
+      . $CFG_FILE
     fi
     
+    # if enabled in install wizard run auto update before starting
+    if [ "$WIZARD_MINIO_AUTO_UPDATE" = 'true' ]
+    then
+      "${SYNOPKG_PKGDEST}/bin/minio" --quiet update
+    fi
+
     SERVICE_OPTIONS="server --quiet --anonymous ${WIZARD_DATA_DIRECTORY}"
     
     export MINIO_ACCESS_KEY=$WIZARD_ACCESS_KEY
