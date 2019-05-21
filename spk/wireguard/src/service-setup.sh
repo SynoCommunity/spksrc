9e5be555c8446b220956262485cf38736e55aeaf
Sebastian Schmidt <publicarray@users.noreply.github.com>
2019-05-21 10:35:47 +1000

    Simplify wireguard scrips by adding bash to the package

--- spk/wireguard/src/service-setup.sh
+++ spk/wireguard/src/service-setup.sh
@@ -4,14 +4,6 @@ NETWORK=172.23.0.1/24 # why 172.23 ? because Synology SRM uses 172.22 and 172.21
 INTERFACE=eth0
 # PID_FILE="${SYNOPKG_PKGDEST}/var/wireguard.pid"
 
-# command is not available on the SRM
-#shellcheck disable=SC2230
-if [ -n "$(which bash)" ]|| [ -n "$(which zsh)" ]; then
-    POSIX=0
-else
-    POSIX=1
-fi
-
 config() {
     # if the config does not exist make one
     if [ ! -f "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" ]; then
@@ -72,49 +64,6 @@ EOF
     fi
 }
 
-config_posix() {
-    if [ ! -f "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" ]; then
-        config
-        sed -i -e '/Address/s/^/#/g' \
-            -e '/SaveConfig/s/^/#/g' \
-            -e '/PostUp/s/^/#/g' \
-            -e '/PostDown/s/^/#/g' \
-            "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
-    fi
-}
-
-start_posix() {
-    # delete and make a new wg0 interface
-    ip link del dev wg0 2>/dev/null || true
-    ip link add dev wg0 type wireguard >> "${LOG_FILE}" 2>&1
-
-    config_posix
-
-    # load config
-    wg setconf wg0 "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
-    # load private key (already set in config file)
-    #wg set wg0 private-key "/var/packages/${SYNOPKG_PKGNAME}/target/var/privatekey"
-    # give clients an address space
-    ip address add dev wg0 ${NETWORK} >> "${LOG_FILE}" 2>&1
-    # set a listening port (already set in config file)
-    #wg set wg0 listen-port $SERVERPORT
-    # start interface
-    ip link set up dev wg0 >> "${LOG_FILE}" 2>&1
-
-    iptables -A SYNO_FORWARD_ACCEPT -i wg0 -j ACCEPT >> "${LOG_FILE}" 2>&1
-    # ip6tables -A SYNO_FORWARD_ACCEPT -i wg0 -j ACCEPT >> "${LOG_FILE}" 2>&1
-    iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE >> "${LOG_FILE}" 2>&1
-    # ip6tables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE >> "${LOG_FILE}" 2>&1
-}
-
-stop_posix() {
-    ip link set down dev wg0 >> "${LOG_FILE}" 2>&1
-    iptables -D SYNO_FORWARD_ACCEPT -i wg0 -j ACCEPT >> "${LOG_FILE}" 2>&1
-    # ip6tables -D SYNO_FORWARD_ACCEPT -i wg0 -j ACCEPT >> "${LOG_FILE}" 2>&1
-    iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE >> "${LOG_FILE}" 2>&1
-    # ip6tables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE >> "${LOG_FILE}" 2>&1
-}
-
 service_postinst () {
     # Link binaries into the PATH
     mkdir -p /usr/local/bin "/var/packages/${SYNOPKG_PKGNAME}/target/etc/" >> "${INST_LOG}" 2>&1
@@ -124,35 +73,22 @@ service_postinst () {
     insmod "/var/packages/${SYNOPKG_PKGNAME}/target/wireguard.ko" >> "${INST_LOG}" 2>&1
     lsmod | grep wireguard >> "${INST_LOG}" 2>&1
 
-    # "command" is not available on the SRM
-    #shellcheck disable=SC2230
-    if [ -z "$(which bash)" ] && [ -n "$(which zsh)" ]; then
-        # requires zsh with modules on the SRM
-        # https://synocommunity.com/package/zsh-static
-        echo "Download custom wg-quick compatable with the zsh shell" >> "${INST_LOG}" 2>&1
-        wget -q https://gist.githubusercontent.com/publicarray/97de680c5e158278de4fed7c67a6e7d7/raw/edb65ebf9bcac5a77293f67a0b965ac323b8bb98/wg-quick.zsh -O "/var/packages/${SYNOPKG_PKGNAME}/target/bin/wg-quick"
+    if [ -x "/bin/bash" ]; then
+        # change shebang to packaged bash
+        sed -i 's/#!\/bin\/bash/#!\/var\/packages\/wireguard\/target\/bin\/bash/' /usr/local/bin/wg-quick
     fi
+
 }
 
 service_prestart() {
     echo "service_prestart" >> "${LOG_FILE}" 2>&1
-    # should be enabled anyway
-    # sysctl net.ipv4.ip_forward=1
-    if [ $POSIX = 1 ]; then
-        start_posix
-    else
-        config
-        wg-quick up "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
-    fi
+    config
+    wg-quick up "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
 }
 service_poststop () {
     echo "service_poststop" >> "${LOG_FILE}" 2>&1
-    if [ $POSIX = 1 ]; then
-        stop_posix
-    else
-        wg-quick down "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
-        chmod 744 "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf"  >> "${LOG_FILE}" 2>&1
-    fi
+    wg-quick down "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf" >> "${LOG_FILE}" 2>&1
+    chmod 744 "/var/packages/${SYNOPKG_PKGNAME}/target/var/wg0.conf"  >> "${LOG_FILE}" 2>&1
 }
 
 service_postuninst () {
