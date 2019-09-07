SynoCommunity/spksrc

commit 3dcf3586f868eceaca3fb949505448ae4eac2f0b

Fix wizard packaging and various package wizards (#3302)
* transmission: fix french wizard

Resolves #3262

* sabnzbd: fix french wizard

* tvheadend: add french wizard translations

* Increase revisions to publish fixes

* framework: restrict wizard files included in package

Prevent inconsistency with signature generated by spkrepo.
Resolves #3262 #3241

mk/spksrc.spk.mk
@@ -277,7 +277,7 @@ wizards:
 ifneq ($(strip $(WIZARDS_DIR)),)
 	@$(MSG) "Preparing DSM Wizards"
 	@mkdir -p $(DSM_WIZARDS_DIR)
-	@find $${SPKSRC_WIZARDS_DIR} -maxdepth 1 -type f -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
+	@find $${SPKSRC_WIZARDS_DIR} -maxdepth 1 -type f -and \( -name "install_uifile" -or -name "install_uifile_???" -or -name "upgrade_uifile" -or -name "upgrade_uifile_???" -or -name "upgrade_uninstall" -or -name "uninstall_uifile_???" \) -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
 	@find $(DSM_WIZARDS_DIR) -maxdepth 1 -type f -print -exec chmod 0644 {} \;
 	$(eval SPK_CONTENT += WIZARD_UIFILES)
 endif

spk/sabnzbd/Makefile
@@ -1,6 +1,6 @@
 SPK_NAME = sabnzbd
 SPK_VERS = 2.3.3
-SPK_REV = 34
+SPK_REV = 35
 SPK_ICON = src/sabnzbd.png
 
 BUILD_DEPENDS = cross/python cross/setuptools cross/pip cross/wheel

spk/sabnzbd/src/wizard/upgrade_uifile_fre
@@ -1,5 +1,5 @@
 [{
-    "step_title": "Attention! Permissions DSM,
+    "step_title": "Attention! Permissions DSM",
     "items": [{
         "desc": "Les permissions de toutes les applications de téléchargement sont gérées par le groupe <b>'sc-download'</b> dans DSM.<br>Le groupe 'users' n'est plus utilisé depuis DSM 6.<br>L'utilisateur spécifique à l'application n'apparaît plus dans la plupart des interfaces de configuration.<br>Merci de lire <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> pour plus de détails."
     },

spk/transmission/Makefile
@@ -1,6 +1,6 @@
 SPK_NAME = transmission
 SPK_VERS = 2.93
-SPK_REV = 14
+SPK_REV = 15
 SPK_ICON = src/transmission.png
 
 DEPENDS = cross/busybox cross/$(SPK_NAME)

spk/transmission/src/wizard/install_uifile_fre
@@ -31,7 +31,7 @@
         }]
     }, {
         "type": "textfield",
-        "desc": "Répertoire de conserver les fichiers jusqu'à ce torrent est terminée. Laisser vide pour désactiver",
+        "desc": "Répertoire où conserver les fichiers de torrent incomplets. Laisser vide pour désactiver",
         "subitems": [{
             "key": "wizard_incomplete_dir",
             "desc": "Répertoire incomplet",
@@ -62,8 +62,8 @@
         }]
     }]
 }, {
-     "Step_title": "Attention! DSM6 Permissions",
-     "articles": [{
-    "desc": "Les permissions de toutes les applications de téléchargement sont gérées par le groupe <b>'sc-download'</b> dans DSM.<br>Le groupe 'users' n'est plus utilisé depuis DSM 6.<br>L'utilisateur spécifique à l'application n'apparaît plus dans la plupart des interfaces de configuration."
+     "step_title": "Attention! DSM6 Permissions",
+     "items": [{
+         "desc": "Les permissions de toutes les applications de téléchargement sont gérées par le groupe <b>'sc-download'</b> dans DSM.<br>Le groupe 'users' n'est plus utilisé depuis DSM 6.<br>L'utilisateur spécifique à l'application n'apparaît plus dans la plupart des interfaces de configuration."
      }]
 }]

spk/transmission/src/wizard/upgrade_uifile_fre
@@ -1,5 +1,5 @@
 [{
-    "step_title": "Attention! Permissions DSM,
+    "step_title": "Attention! Permissions DSM",
     "items": [{
         "desc": "Les permissions de toutes les applications de téléchargement sont gérées par le groupe <b>'sc-download'</b> dans DSM.<br>Le groupe 'users' n'est plus utilisé depuis DSM 6.<br>L'utilisateur spécifique à l'application n'apparaît plus dans la plupart des interfaces de configuration."
     },

spk/tvheadend/src/wizard/install_uifile
@@ -18,6 +18,6 @@
 }, {
     "step_title": "Attention! DSM Permissions",
     "items": [{
-        "desc": "Permissions are managed with the group <b>'sc-media'</b> in DSM.<br>The group 'users' is no longer used as of DSM 6.<br>Package user (= svc-tvheadend on DSM5 and sc-tvheadend on DSM6+) will not appear on most UI settings.<br>Including the following:<br>- Application privilege's permission viewer<br>- FTP's chroot user selector<br>- File Stations's<br>- Change owner<br>- Shared Links Manager -> Enable secure sharing<br><br>The only exceptions are:<br>- Control Panel > Shared Folder > Edit > Permission > System internal user<br>- ACL editor<br>"
+        "desc": "Permissions are managed with the group <b>'sc-media'</b> in DSM.<br>The group 'users' is no longer used as of DSM 6.<br>Package user (= svc-tvheadend on DSM5 and sc-tvheadend on DSM6+) will not appear on most UI settings.<br>Including the following:<br>- Application privilege's permission viewer<br>- FTP's chroot user selector<br>- File Stations's<br>- Change owner<br>- Shared Links Manager -> Enable secure sharing<br><br>The only exceptions are:<br>- Control Panel > Shared Folder > Edit > Permission > System internal user<br>- ACL editor.<br>Please read <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> for details."
     }]
 }]

spk/tvheadend/src/wizard/install_uifile_fre
@@ -1,0 +1,23 @@
+[{
+    "step_title": "Configuration du service",
+    "items": [{
+        "type": "textfield",
+        "desc": "Nom d'utilisateur administrateur. Par défaut, admin",
+        "subitems": [{
+            "key": "wizard_username",
+            "desc": "Nom d'utilisateur"
+        }]
+    }, {
+        "type": "password",
+        "desc": "Mot de passe administrateur. Par défaut, admin",
+        "subitems": [{
+            "key": "wizard_password",
+            "desc": "Mot de passe"
+        }]
+    }]
+}, {
+    "step_title": "Attention! Permissions DSM",
+    "items": [{
+        "desc": "Les permissions sont gérées par le groupe <b>'sc-media'</b> dans DSM.<br>Le groupe 'users' n'est plus utilisé depuis DSM 6.<br>L'utilisateur spécifique à l'application n'apparaît plus dans la plupart des interfaces de configuration.<br>En particulier:<br>- Visionneuse de permission des Privilèges d'Applications<br>- Choix de l'utisateur du dossier root FTP<br>- Les actions de File Stations<br>- Changement de propriétaire<br>- Gestionnaire de liens partagés -> Activer le partage sécurisé<br><br>Les seules exceptions sont:<br>- Panneau de configuration > Dossier partagé > Modifier > Permissions > Utilisateur du système interne<br>- Éditeur de permissions.<br>Merci de lire <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> pour plus de détails."
+    }]
+}]

commit 7313fc748d99b5e04d368f15558c639c6da9388e

Fix packaging of uninstall wizard file
- Fix name of uninstall_uifile
- Without this fix all packages are missing the uninstall wizard
- Bug was introduced by commit 3dcf358 of #3302
- Fixes #3336
- Fixes uninstall of owncloud version 8.1.1-7 (Incorrect MySQL root password)

mk/spksrc.spk.mk
@@ -290,7 +290,7 @@ wizards:
 ifneq ($(strip $(WIZARDS_DIR)),)
 	@$(MSG) "Preparing DSM Wizards"
 	@mkdir -p $(DSM_WIZARDS_DIR)
-	@find $${SPKSRC_WIZARDS_DIR} -maxdepth 1 -type f -and \( -name "install_uifile" -or -name "install_uifile_???" -or -name "install_uifile.sh" -or -name "install_uifile_???.sh" -or -name "upgrade_uifile" -or -name "upgrade_uifile_???" -or -name "upgrade_uifile.sh" -or -name "upgrade_uifile_???.sh" -or -name "upgrade_uninstall" -or -name "uninstall_uifile_???" -or -name "upgrade_uninstall.sh" -or -name "upgrade_uninstall_???.sh" \) -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
+	@find $${SPKSRC_WIZARDS_DIR} -maxdepth 1 -type f -and \( -name "install_uifile" -or -name "install_uifile_???" -or -name "install_uifile.sh" -or -name "install_uifile_???.sh" -or -name "upgrade_uifile" -or -name "upgrade_uifile_???" -or -name "upgrade_uifile.sh" -or -name "upgrade_uifile_???.sh" -or -name "uninstall_uifile" -or -name "uninstall_uifile_???" -or -name "uninstall_uifile.sh" -or -name "uninstall_uifile_???.sh" \) -print -exec cp -f {} $(DSM_WIZARDS_DIR) \;
 	@find $(DSM_WIZARDS_DIR) -maxdepth 1 -type f -not -name "*.sh" -print -exec chmod 0644 {} \;
 	@find $(DSM_WIZARDS_DIR) -maxdepth 1 -type f -name "*.sh" -print -exec chmod 0755 {} \;
 	$(eval SPK_CONTENT += WIZARD_UIFILES)