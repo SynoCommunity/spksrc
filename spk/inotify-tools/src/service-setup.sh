14a1d51ed2029f27931bd57aa133e458d683ccdc
Meinhard Ritscher <unreachable@gmx.net>
2019-11-22 20:39:59 +0100

    Fix library install / uninstall

--- spk/inotify-tools/src/service-setup.sh
+++ spk/inotify-tools/src/service-setup.sh
@@ -16,8 +16,8 @@ service_postinst ()
 
     if [ -e "${SYNOPKG_PKGDEST}/lib/$LIBRARY_WITH_VERSION" ]; then
         ln -s "${SYNOPKG_PKGDEST}/lib/$LIBRARY_WITH_VERSION" "/usr/local/lib/$LIBRARY_WITH_VERSION"
-        ln -s "${SYNOPKG_PKGDEST}/lib/$LIBRARY" "/usr/local/lib/$LIBRARY_WITH_VERSION"
-        ln -s "${SYNOPKG_PKGDEST}/lib/${LIBRARY}.${LIBRARY_MAIN_VERSION}" "/usr/local/lib/$LIBRARY_WITH_VERSION"
+        ln -s "${SYNOPKG_PKGDEST}/lib/$LIBRARY_WITH_VERSION" "/usr/local/lib/$LIBRARY"
+        ln -s "${SYNOPKG_PKGDEST}/lib/$LIBRARY_WITH_VERSION" "/usr/local/lib/${LIBRARY}.${LIBRARY_MAIN_VERSION}"
     fi
 }
 
@@ -27,11 +27,18 @@ service_postuninst ()
     do
         if [ -L "/usr/local/bin/$cmd" ]; then
             rm -f "/usr/local/bin/$cmd"
-            rm "/usr/bin/$cmd"
         fi
     done
 
     if [ -L "/usr/local/lib/$LIBRARY_WITH_VERSION" ]; then
         rm -f "/usr/local/lib/$LIBRARY_WITH_VERSION"
     fi
+
+    if [ -L "/usr/local/lib/$LIBRARY" ]; then
+        rm -f "/usr/local/lib/$LIBRARY"
+    fi 
+
+    if [ -L "/usr/local/lib/${LIBRARY}.${LIBRARY_MAIN_VERSION}" ]; then
+        rm -f "/usr/local/lib/${LIBRARY}.${LIBRARY_MAIN_VERSION}"
+    fi 
 }
