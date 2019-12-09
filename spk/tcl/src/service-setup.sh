de0067cb63c85f91821a74a8b9b9abfcb6a805a4
eelcohn <eelco@huininga.nl>
2019-12-09 22:53:59 +0100

    tcl: update to v8.6.10-4 (#3813)
    * Update Tcl to v8.6.10-4
    * Update spk/tcl/Makefile

--- spk/tcl/src/service-setup.sh
+++ spk/tcl/src/service-setup.sh
@@ -1,9 +1,9 @@
 service_postinst ()
 {
-    ln -s $(find ${SYNOPKG_PKGDEST}/bin/ -name tclsh* -type f -executable) /usr/local/bin/tclsh
+	ln -s $(find "${SYNOPKG_PKGDEST}/bin/" -name "tclsh*" -type f -executable) /usr/local/bin/tclsh
 }
 
 service_postuninst ()
 {
-    rm /usr/local/bin/tclsh
+	rm /usr/local/bin/tclsh
 }
