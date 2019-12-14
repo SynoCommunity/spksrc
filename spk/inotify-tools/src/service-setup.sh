8ed8d9f0a23b5f3d243a2ae64ae51476f57f092c
Meinhard Ritscher <unreachable@gmx.net>
2019-12-14 16:31:28 +0100

    Remove superfluous leftover variables.

--- spk/inotify-tools/src/service-setup.sh
+++ spk/inotify-tools/src/service-setup.sh
@@ -1,9 +1,4 @@
-
 BIN_COMMANDS="inotifywait inotifywatch"
-LIBRARY="libinotifytools.so"
-LIBRARY_MAIN_VERSION="0"
-LIBRARY_SUB_VERSION="4.1"
-LIBRARY_WITH_VERSION="${LIBRARY}.${LIBRARY_MAIN_VERSION}.${LIBRARY_SUB_VERSION}"
 
 service_postinst ()
 {
