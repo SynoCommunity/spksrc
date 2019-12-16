a88e46e8502601244652cdd065ba73653824f6be
heralight <heralight@users.noreply.github.com>
2019-12-16 16:47:43 +0100

    Update duplicity to 0.8.08 with oauth

--- spk/duplicity/src/installer.sh
+++ spk/duplicity/src/installer.sh
@@ -26,6 +26,10 @@ postinst ()
     # Install the wheels
     ${INSTALL_DIR}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${INSTALL_DIR}/share/wheelhouse ${INSTALL_DIR}/share/wheelhouse/*.whl > /dev/null 2>&1
 
+    # fix monotonic python library missing synology ld utils
+    sed -i -e "s/ctypes.util.find_library('c')/'\/lib\/libc.so.6'/" /usr/local/duplicity/env/lib/python2.7/site-packages/monotonic.py
+    sed -i -e "s/ctypes.util.find_library('rt')/'\/lib\/librt.so.1'/" /usr/local/duplicity/env/lib/python2.7/site-packages/monotonic.py
+
     # Add symlink
     mkdir -p /usr/local/bin
     ln -s ${INSTALL_DIR}/env/bin/duplicity /usr/local/bin/duplicity
