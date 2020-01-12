19fe58b99367b42bf6e03014ed4cafabd877141b
hgy59 <hpgy59@gmail.com>
2020-01-12 14:53:46 +0100

    update command line tools
    - define symlinks in Makefiles
    - remove service-setup.sh scripts used for link creation only
    - remove BETA from diyspk packages
    - use variable for optional description in synocli-disk and synocli-net
    - update tmux icon
    - make package icons square (detox, rmlint, testdisk)
    - use unique gnu icon (less, ddrescue)
    - ddrescue: remove pre DSM 6 install scripts (dsm-control.sh, installer.sh)

--- spk/beets/src/service-setup.sh
+++ spk/beets/src/service-setup.sh
@@ -10,18 +10,9 @@ service_postinst ()
     # Install the wheels
     ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
-    # Add symlink
-    mkdir -p /usr/local/bin
-    ln -s ${SYNOPKG_PKGDEST}/env/bin/beet /usr/local/bin/beet
-
     # Extended diagnostic information
     ${SYNOPKG_PKGDEST}/env/bin/beet version >> ${INST_LOG}
     ${SYNOPKG_PKGDEST}/env/bin/beet --version --help >> ${INST_LOG}
     echo -e "\nModules:" >> ${INST_LOG}
     ${SYNOPKG_PKGDEST}/env/bin/pip freeze >> ${INST_LOG}
-}
-
-service_postuninst ()
-{
-    rm /usr/local/bin/beet
 }
