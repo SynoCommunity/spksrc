2b0be216790c3a4220f79125be2ca0d27ef86442
hgy59 <hpgy59@gmail.com>
2020-01-14 00:06:43 +0100

    Revival of old PR #1683
    - update ntopng to version 3.8.1
    - implement DSM 6 service setup (indeed DSM <= 5.2 is not supported anymore)
    - move data dir to {pkg}/var/data
    - move ntopng.conf to {pkg}/etc
    - add new dependencies (lua, libxml2, curl, glib)
    - add nDPI module (from ntop.org too)
    - update rrdtool (add new modules: pango, cairo, pixman; groff, urw-base35-fonts, uchardet)
    - add libmaxminddb
    - add libmysqlclient (mysql-connector-c)
    - add native modules libmysqlclient and ghostscript
    - update libpcap
    - update libsodium
    - update geoip
    - remove unused module cross/termcap from repo
    - remove empty module cross/libcap (maybe we need it later)
    - update ntopng icon
    - add initial GeoLite2 content files (further updates are not available anymore without API key)
    - add patches (by sed) to set synology specific infos into generated code

--- spk/ntopng/src/ntopng.sc
+++
@@ -1,6 +0,0 @@
-[ntopng]
-title="ntopng"
-desc="ntopng"
-port_forward="yes"
-dst.ports="3000/tcp"
-
---
+++ spk/ntopng/src/service-setup.sh
@@ -0,0 +1,5 @@
+# Package specific behaviors
+# Sourced by generic installer and start-stop-status script
+
+CFG_FILE="${SYNOPKG_PKGDEST}/etc/ntopng.conf"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/ntopng ${CFG_FILE}"
