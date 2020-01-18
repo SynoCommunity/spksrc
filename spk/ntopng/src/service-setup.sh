606957bed29f9165bd1cdc5cb950d257bf98141d
hgy59 <hpgy59@gmail.com>
2020-01-18 19:44:23 +0100

    cleanup

--- spk/ntopng/src/service-setup.sh
+++
@@ -1,5 +0,0 @@
-# Package specific behaviors
-# Sourced by generic installer and start-stop-status script
-
-CFG_FILE="${SYNOPKG_PKGDEST}/etc/ntopng.conf"
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/ntopng ${CFG_FILE}"
