218a0dfb699582cb79ba5f91f3bfbbc616240842
hgy59 <hpgy59@gmail.com>
2020-01-22 00:05:48 +0100

    add ser2net to synocli-net
    - move spk/ser2net to diyspk/ser2net
    - add ser2net to spk/synocli-net
    - create description based on included packages

--- diyspk/ser2net/src/service-setup.sh
+++ diyspk/ser2net/src/service-setup.sh
@@ -15,7 +15,7 @@ service_postuninst ()
 {
     for cmd in $COMMANDS
     do
-        if [ -e "/usr/local/sbin/$cmd" ]; then
+        if [ -L "/usr/local/sbin/$cmd" ]; then
             rm -f "/usr/local/sbin/$cmd"
         fi
     done
