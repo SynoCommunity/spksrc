185e0404e32f087a24678217673dc95284583de9
hgy59 <hpgy59@gmail.com>
2020-01-25 12:42:40 +0100

    fix shairport-sync service installation
    - update to DSM6 service configuration
    - fix pid file creation (configure option --with-piddir has no effect, default location is var/run)
    - create start-stop-status functions base on projects systemv template
    - add busybox module usrmng (otherwise busybox install fails for unknown reason)
    - create soft links with busybox install
    - include all documented settings in shaiport-sync.conf
    - TODO: fix DSM5 installation

--- spk/shairport-sync/src/dsm-control.sh
+++ spk/shairport-sync/src/dsm-control.sh
@@ -1,44 +1,60 @@
 #!/bin/sh
 
-# Package
-PACKAGE="shairport-sync"
-DNAME="Shairport Sync"
+NAME="shairport-sync"
 
-# Others
-SHAIRPORT="shairport-sync"
-INSTALL_DIR="/usr/local/${PACKAGE}"
-PATH="${INSTALL_DIR}/bin:${PATH}"
-DAEMON="${INSTALL_DIR}/bin/${SHAIRPORT}"
-PID_FILE="${INSTALL_DIR}/var/${PACKAGE}.pid"
-CONFIG_FILE="${INSTALL_DIR}/var/shairport-sync.conf"
+TARGET_DIR="/usr/local/${NAME}"
+PATH="${TARGET_DIR}/bin:${PATH}"
+DAEMON="${TARGET_DIR}/bin/${NAME}"
+PID_FILE="${TARGET_DIR}/var/run/${NAME}.pid"
+CONFIG_FILE="${TARGET_DIR}/var/${NAME}.conf"
+
 
 start_daemon ()
 {
-    start-stop-daemon -S -q -m -b -p ${PID_FILE} -x ${DAEMON} -- -c ${CONFIG_FILE} 2> /dev/null
+    # Return
+    #   0 if daemon has been started
+    #   2 if daemon could not be started
+
+    # Select daemon mode and define the location of the config file.
+    start-stop-daemon --start --quiet --pidfile ${PID_FILE} --exec ${DAEMON} -- --daemon --configfile=${CONFIG_FILE} &> /dev/null \
+        || return 2
+
+    # Add code here, if necessary, that waits for the process to be ready
+    # to handle requests from services started subsequently which depend
+    # on this one.  As a last resort, sleep for some time.
 }
 
 stop_daemon ()
 {
-    start-stop-daemon -K -q -p ${PID_FILE} -x ${DAEMON}
-    wait_for_status 1 20 || start-stop-daemon -K -s 9 -q -p ${PID_FILE}
+    # Return
+    #   0 if daemon has been stopped
+    #   1 if daemon was already stopped
+    #   2 if daemon could not be stopped
+    #   other if a failure occurred
+    start-stop-daemon --stop --quiet -R=TERM/30/KILL/5 --pidfile ${PID_FILE} --name ${NAME}
+    RETVAL="$?"
+    [ "$RETVAL" = 2 ] && return 2
+
+    # Wait for children to finish too if this is a daemon that forks
+    # and if the daemon is only ever run from this script.
+    # If the above conditions are not satisfied then add some other code
+    # that waits for the process to drop all resources that could be
+    # needed by services started subsequently.
+    # A last resort is to sleep for some time.
+    start-stop-daemon --stop --quiet --oknodo -R=0/30/KILL/5 --exec ${DAEMON}
+    [ "$?" = 2 ] && return 2
+
+    return "$RETVAL"
 }
 
 daemon_status ()
 {
-    start-stop-daemon -K -q -t -p ${PID_FILE}
+    # Return
+    #   0 if daemon is running
+    #   1 if daemon is not running
+    start-stop-daemon --stop --quiet --test --pidfile ${PID_FILE} &> /dev/null
 }
 
-wait_for_status ()
-{
-    counter=$2
-    while [ ${counter} -gt 0 ]; do
-        daemon_status
-        [ $? -eq $1 ] && return
-        let counter=counter-1
-        sleep 1
-    done
-    return 1
-}
 
 case $1 in
     start)
@@ -82,4 +98,3 @@ case $1 in
         exit 1
         ;;
 esac
-
