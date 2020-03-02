cb2cade1efecbf4e3b6b72ee83e972e3b13f6a49
hgy59 <hpgy59@gmail.com>
2020-03-02 18:32:31 +0100

    fix installation on DSM <6
    - sh shell does not support &> redirection
    - sh shell does not support type -t

--- spk/beets/src/service-setup.sh
+++ spk/beets/src/service-setup.sh
@@ -1,19 +1,20 @@
 PYTHON_DIR="/usr/local/python3"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
 VIRTUALENV="${PYTHON_DIR}/bin/python3 -m venv"
+PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env &>> ${INST_LOG}
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env 2>&1 >> ${INST_LOG}
 
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index --upgrade --force-reinstall --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl &>> ${INST_LOG}
+    ${PIP} install --no-deps --no-index --upgrade --force-reinstall --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl 2>&1 >> ${INST_LOG}
 
     # Log installation information
     echo -e "\nInstalled version:" >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/beet version &>> ${INST_LOG}
+    ${SYNOPKG_PKGDEST}/env/bin/beet version 2>&1 >> ${INST_LOG}
     echo -e "\nInstalled python modules:" >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/pip3 freeze >> ${INST_LOG}
+    ${PIP} freeze 2>&1 >> ${INST_LOG}
     echo "" >> ${INST_LOG}
 }
