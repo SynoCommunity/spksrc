30655433ab7bc609125b5e11584f3b08a2a699bb
hgy59 <hpgy59@gmail.com>
2020-03-02 15:38:05 +0100

    - update beets and use python3

--- spk/beets/src/service-setup.sh
+++ spk/beets/src/service-setup.sh
@@ -1,18 +1,19 @@
-PYTHON_DIR="/usr/local/python"
+PYTHON_DIR="/usr/local/python3"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
-VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+VIRTUALENV="${PYTHON_DIR}/bin/python3 -m venv"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env > /dev/null
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env &>> ${INST_LOG}
 
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index --upgrade --force-reinstall --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl &>> ${INST_LOG}
 
-    # Extended diagnostic information
-    ${SYNOPKG_PKGDEST}/env/bin/beet version >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/beet --version --help >> ${INST_LOG}
-    echo -e "\nModules:" >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/pip freeze >> ${INST_LOG}
+    # Log installation information
+    echo -e "\nInstalled version:" >> ${INST_LOG}
+    ${SYNOPKG_PKGDEST}/env/bin/beet version &>> ${INST_LOG}
+    echo -e "\nInstalled python modules:" >> ${INST_LOG}
+    ${SYNOPKG_PKGDEST}/env/bin/pip3 freeze >> ${INST_LOG}
+    echo "" >> ${INST_LOG}
 }
