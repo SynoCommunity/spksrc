a66a85f65182858c921f2ba789f8b0ef5e21c6b1
hgy59 <hpgy59@gmail.com>
2020-03-16 20:05:32 +0100

    duplicity: update to 0.7.19, remove beta=1; itools: update; oscam: retire
    fix saltpad homepage
    use latest version of wheel that works with python 2
    fix linking to rt lib
    
    update itools
    - update dependencies
    - fix ppc-archs
    - fix configure for linking to rt library in cross/usbmuxd
    
    fix usage of DSM_APP_NAME in spk makefiles
    retire oscam package (close #2636)
    Fix pyload/tesseract-ocr for ppc853x-5.2

--- spk/itools/src/service-setup.sh
+++ spk/itools/src/service-setup.sh
@@ -9,24 +9,24 @@ SVC_WRITE_PID=y
 
 service_postinst ()
 {
-    easy_install lockfile
-    ln -s ${SYNOPKG_PKGDEST_VOL} ${VOLUME_DIR}
+    easy_install lockfile  >> ${INST_LOG}
+    ln -s ${SYNOPKG_PKGDEST_VOL} ${VOLUME_DIR}  >> ${INST_LOG}
 }
 
 service_postuninst ()
 {
-    rm -f ${VOLUME_DIR}
+    rm -f ${VOLUME_DIR}  >> ${INST_LOG}
 }
 
 service_prestart ()
 {
-    cp ${SYNOPKG_PKGDEST}/39-libimobiledevice.rules /usr/lib/udev/rules.d/
-    udevadm control --reload-rules
+    cp ${SYNOPKG_PKGDEST}/39-libimobiledevice.rules /usr/lib/udev/rules.d/  >> ${INST_LOG}
+    udevadm control --reload-rules  >> ${INST_LOG}
 }
 
 service_poststop ()
 {
-    ${INSTALL_DIR}/umounting.py
-    rm -f /usr/lib/udev/rules.d/39-libimobiledevice.rules
-    udevadm control --reload-rules
+    ${INSTALL_DIR}/umounting.py  >> ${INST_LOG}
+    rm -f /usr/lib/udev/rules.d/39-libimobiledevice.rules  >> ${INST_LOG}
+    udevadm control --reload-rules  >> ${INST_LOG}
 }
