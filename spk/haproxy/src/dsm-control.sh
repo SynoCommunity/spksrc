2f053d93c2ea8dd9ed1c2bb5b54c60f07662ec7d
hgy59 <hpgy59@gmail.com>
2020-03-18 23:49:40 +0100

    haproxy: fix for DSM 6 - fix DSM 6 compatibility - create certificate by installer (paths used by app for certificate creation does not work on dsm 6)

--- spk/haproxy/src/dsm-control.sh
+++ spk/haproxy/src/dsm-control.sh
@@ -9,21 +9,22 @@ INSTALL_DIR="/usr/local/${PACKAGE}"
 PYTHON_DIR="/usr/local/python"
 PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${PYTHON_DIR}/bin:${PATH}"
 BUILDNUMBER="$(/bin/get_key_value /etc.defaults/VERSION buildnumber)"
+MAJOR_VERSION="$(/bin/get_key_value /etc.defaults/VERSION majorversion)"
 USER="root" # root is needed to listen on ports <1024, haproxy will drop the privileges after startup
 PYTHON="${INSTALL_DIR}/env/bin/python"
 HAPROXY="${INSTALL_DIR}/sbin/haproxy"
 PID_FILE="${INSTALL_DIR}/var/haproxy.pid"
 CFG_FILE="${INSTALL_DIR}/var/haproxy.cfg"
-
+SUDO="$([ "${MAJOR_VERSION}" -ge "6" ] && echo 'sudo -u' || echo 'su' )"
 
 start_daemon ()
 {
-    su ${USER} -s /bin/sh -c "PATH=${PATH} ${HAPROXY} -f ${CFG_FILE} -p ${PID_FILE}"
+    ${SUDO} ${USER} -s /bin/sh -c "PATH=${PATH} ${HAPROXY} -f ${CFG_FILE} -p ${PID_FILE}"
 }
 
 check_config ()
 {
-    su ${USER} -s /bin/sh -c "PATH=${PATH} ${HAPROXY} -c -f ${CFG_FILE}" > /dev/null
+    ${SUDO} ${USER} -s /bin/sh -c "PATH=${PATH} ${HAPROXY} -c -f ${CFG_FILE}" > /dev/null
 }
 
 stop_daemon ()
