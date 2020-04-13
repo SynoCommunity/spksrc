da9a0213390a4a6ccf9e9f93b62239048ecb66dc
hgy59 <hpgy59@gmail.com>
2020-04-13 21:08:34 +0200

    update usbserial Makefiles
    - remove DSM 5.0 specific instructions
    - remove dependencies cross/ftdi_sio, cross/usb-serial as these are taken from the DSM system
    - adjust SPK_REV to continue on version of jadahl.com
    - replace installer script by service-setup.sh
    - use generic link creation (except for bin/lsusb)
    - install link for bin/lsusb only when lsusb is not already installed by the base system
    - use pkg_name as prefix for custom targets

--- spk/usbserial/src/dsm-control.sh
+++ spk/usbserial/src/dsm-control.sh
@@ -2,48 +2,32 @@
 
 # Package
 PACKAGE="UsbSerialDrivers"
-DNAME="UsbSerialDrivers"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
-RUNAS="root"
-    
+
 
 case $1 in
     start)
 
-    insmod /lib/modules/usbserial.ko > /dev/null
-    insmod /lib/modules/ftdi_sio.ko >/dev/null	
-   #insmod ${INSTALL_DIR}/modules/usbserial.ko >/dev/null
-   #insmod ${INSTALL_DIR}/modules/ftdi_sio.ko >/dev/null	
-    insmod ${INSTALL_DIR}/modules/cp210x.ko >/dev/null
-    insmod ${INSTALL_DIR}/modules/pl2303.ko >/dev/null
-    insmod ${INSTALL_DIR}/modules/ch341.ko >/dev/null
-    insmod ${INSTALL_DIR}/modules/ti_usb_3410_5052.ko >/dev/null
+        insmod /lib/modules/usbserial.ko > /dev/null
+        insmod /lib/modules/ftdi_sio.ko >/dev/null	
+        insmod ${INSTALL_DIR}/modules/cp210x.ko >/dev/null
+        insmod ${INSTALL_DIR}/modules/pl2303.ko >/dev/null
+        insmod ${INSTALL_DIR}/modules/ch341.ko >/dev/null
+        insmod ${INSTALL_DIR}/modules/ti_usb_3410_5052.ko >/dev/null
 
-if [ `/bin/get_key_value /etc.defaults/VERSION buildnumber` -ge "5004" ]; then
         # Create udev rules to set permissions to 666 
         # Doing this at package start so it gets done even after DSM upgrade.  
         ln -s ${INSTALL_DIR}/rules.d/60-jadahl.usbserial.rules /lib/udev/rules.d/60-jadahl.usbserial.rules
-	udevadm control --reload-rules
-    else
-        # DSM 5.0 and earlier versions don't dynamically create devices, so create device for everything before build 5004.
-        for NR in 0 1 2 3 4 5 6 7
-        do
-            test -e /dev/ttyUSB${NR} || mknod -m 666 /dev/ttyUSB${NR} c 188 ${NR}
-        done
-
-	for NR in 0 1 2 
-        do
-            test -e /dev/ttyACM${NR} || mknod -m 666 /dev/ttyACM${NR} c 166 ${NR}
-        done
-    
-    fi
+        udevadm control --reload-rules
 
         exit 0
         ;;
     stop)
-    
+        # remove udev rules for USB serial permissions
+        rm -f /lib/udev/rules.d/60-jadahl.usbserial.rules	
+        udevadm control --reload-rules
         exit 0
         ;;
     status)
