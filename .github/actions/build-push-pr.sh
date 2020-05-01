b67058a993ef9c0bcc84c78edcef834be9c57255
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-05-01 11:49:02 +1000

    Dependency tree

--- .github/actions/build-push-pr.sh
+++ .github/actions/build-push-pr.sh
@@ -13,46 +13,76 @@ DEFAULT_TC=6.1
 PARALLEL_MAKE=4
 EOF
 
-# echo "FILES: $FILES"
+
+# get dependency tree
+DEPENDENCY_TREE=
+
+echo "Building dependency tree..."
+for package in $(find spk/ -maxdepth 1 -type d | cut -c 5- | sort)
+do
+    pushd "spk/$package" > /dev/null || exit
+    DEPENDENCY_TREE+=$(make dependency-tree)$'\n'
+    popd > /dev/null || exit
+done
+
 
 # filter for changes made in the cross and spk directories
 GH_FILES=$(echo "$GH_FILES" | grep -oE "(spk.*)|(cross.*)")
 
+echo "FILES: $GH_FILES"
+
 # create array of potential packages where files have changed
 GH_PACKAGES_ARR=()
-for GH_FILE in $GH_FILES
+for file in $GH_FILES
 do
-    # remove leading spk and cross from string
-    GH_FILE=${GH_FILE#spk/}
-    if [[ "$GH_FILE" == cross/* ]]; then
-        make dependency-tree
-        GH_FILE=${GH_FILE#cross/}
+    # Add spk
+    # remove leading spk from string
+    if [[ "$file" == spk/* ]]; then
+        package=${file#spk/}
+        # get package name / folder name
+        package=$(echo "$package" | grep -oE "^[^\/]*")
+        GH_PACKAGES_ARR+=("$package")
     fi
 
+    # Find dependencies
+    # remove leading spk/cross/native from string
+    file=${file#spk/}
+    file=${file#cross/}
+    file=${file#native/}
     # get package name / folder name
-    GH_PACKAGE=$(echo "$GH_FILE" | grep -oE "^[^\/]*")
-    # echo "PACKAGE: $PACKAGE"
-    GH_PACKAGES_ARR+=("$GH_PACKAGE")
+    package=$(echo "$file" | grep -oE "^[^\/]*")
+    echo "package: $package"
+    packages=$(echo "$DEPENDENCY_TREE" \
+        | awk -v package="$package" \
+        'NF == 2 {x=$2} $2 == package {print x}' \
+         | sort -u)
+
+    echo "awk: $packages"
+
+    for package in $packages
+    do
+        GH_PACKAGES_ARR+=("$package")
+    done
+
 done
 
 # de-duplicate packages
-GH_PACKAGES=$(printf %s "${GH_PACKAGES_ARR[*]}" | tr ' ' '\n' | sort -u)
+packages=$(printf %s "${GH_PACKAGES_ARR[*]}" | tr ' ' '\n' | sort -u)
 
-if [ -z "$GH_PACKAGES" ]; then
+if [ -z "$packages" ]; then
     echo "no package built. Empty PACKAGES var"
     exit 0
 fi
 
-echo "===> PACKAGES to Build: $GH_PACKAGES"
+echo "===> PACKAGES to Build: $packages"
 
-for GH_PACKAGE in $GH_PACKAGES
+# Build
+for package in $packages
 do
     # make sure that the package exists
-    if [ -d "spk/$GH_PACKAGE" ]; then
-        cd spk/"$GH_PACKAGE" && make "$GH_ARCH"
+    if [ -d "spk/$package" ]; then
+        cd spk/"$package" && make "$GH_ARCH"
     else
-        # must be from cross/
-        echo "$GH_PACKAGE is not a spk PACKAGE" # TODO: maybe find depended packages
-        make dependency-tree
+        echo "$package is not found"
     fi
 done
