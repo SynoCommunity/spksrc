ec5f4fe7ac7ece16e545a85f03f3b68c3b6ee848
josef109 <josefglaze@gmail.com>
2020-05-03 17:04:10 +0200

    Urbackup improvements

--- spk/urbackup/src/service-setup.sh
+++ spk/urbackup/src/service-setup.sh
@@ -6,14 +6,13 @@ service_postinst ()
     mkdir -p "${SHARE_PATH}"
     chown ${PRIV_PREFIX}${USER}: "${SHARE_PATH}"
     chmod 770 "${SHARE_PATH}"
+    echo "tank/images" > ${SYNOPKG_PKGDEST}/var/urbackup/dataset  
+    echo "${SHARE_PATH}" > ${SYNOPKG_PKGDEST}/var/urbackup/backupfolder 
 }
 
 service_prestart ()
 {
     CONFIG_DIR="${SYNOPKG_PKGDEST}/var"
-#    URBACKUP_OPTIONS="run -d -v error -u sc-urbackup"
-
-#    SERVICE_OPTIONS=$URBACKUP_OPTIONS
 
     # Required: start-stop-daemon do not set environment variables
     HOME=${SYNOPKG_PKGDEST}/var
