d368e6bb9b874bd76b3243b5912017062b7f7c58
hgy59 <hpgy59@gmail.com>
2020-05-04 02:02:13 +0200

    fix dependencies

--- spk/fishnet/src/service-setup.sh
+++ spk/fishnet/src/service-setup.sh
@@ -10,8 +10,14 @@ service_postinst ()
     # Install the wheels
     ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG}
     
+
+    # get target file independent of python3 version
+    target=$(${INSTALL_DIR}/env/bin/python -m site | grep -o "[^']*${SYNOPKG_PKGNAME}/env[^']*")/fishnet.py  >> ${INST_LOG}
+    
+    # Fix shebang
+    sed -i -e "s|^#!.*$|#!${INSTALL_DIR}/env/bin/python3|g" ${target}  >> ${INST_LOG}
+    
     # make script executable and create a link to ${INSTALL_DIR}/bin/fishnet
-    echo "target=$(${INSTALL_DIR}/env/bin/python -m site | grep -o "[^']*${SYNOPKG_PKGNAME}/env[^']*")/fishnet.py"  >> ${INST_LOG}
     chmod +x ${target}   >> ${INST_LOG}
     ln -s ${target} ${INSTALL_DIR}/bin/fishnet   >> ${INST_LOG}
 }
