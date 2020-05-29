74d8a1eb3ca9da97596eba31257750f61d265c10
Vincent Fortier <th0ma7@users.noreply.github.com>
2020-05-29 01:25:52 -0400

    ffmpeg: update to 4.2.3 including dependencies (#3965)
    * ffmpeg: update to version 4.2.3
    * gmmlib Intel 20Q1 update version 20.1.1
    * libva (2.7.1) and libva-utils (2.7.1) update
    * Intel Media Driver 20Q1 update v20.1.1
    * Intel Media SDK 20Q1 update version 20.1.1
    * x264 update to latest stable and new download url
    * x265 update to latest version 3.3
    * libogg update to version 1.3.4
    * freetype: update to version 2.10.2
    * fribidi update to version 1.0.9
    * libblueray update to version 1.2.0
    * util-linux update to version 2.35
    * intel-media-driver update to version 19.4.1
    * SVT-AV1 version 0.8.3
    * ffmpeg: Add SVT-AV1 experimental support
    * SVT-HEVC version 1.4.3 experimental support ffmpeg
    * intel-media-driver 20.1.1: patch for issue #907
    * Add setuid to vainfo binary
    * libaom: update to version 2.0.0-rc1

--- spk/ffmpeg/src/service-setup.sh
+++ spk/ffmpeg/src/service-setup.sh
@@ -5,10 +5,12 @@ service_postinst ()
 {
     # setuid for proper vaapi access
     chmod u+s /var/packages/ffmpeg/target/bin/ffmpeg
+    chmod u+s /var/packages/ffmpeg/target/bin/vainfo
 }
 
 service_postupgrade ()
 {
     # setuid for proper vaapi access
     chmod u+s /var/packages/ffmpeg/target/bin/ffmpeg
+    chmod u+s /var/packages/ffmpeg/target/bin/vainfo
 }
