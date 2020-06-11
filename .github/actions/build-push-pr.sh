c8cc5bc08b8dab9e93971d6fb1a95108ec714d78
hgy59 <hpgy59@gmail.com>
2020-06-11 20:28:50 +0200

    improve github actions
    - separate build of noarch and arch specific packages
    - add Build Status job to report and fail on build errors

--- .github/actions/build-push-pr.sh
+++ .github/actions/build-push-pr.sh
@@ -18,34 +18,66 @@ do
 done
 
 # search for dependent spk packages
-for package in $DEPENDENT_PACKAGES
+for package in ${DEPENDENT_PACKAGES}
 do
     echo "===> Searching for dependent package: $package"
     packages=$(echo "${DEPENDENCY_LIST}" | grep " ${package} " | grep -o ".*:" | tr ':' ' ' | sort -u | tr '\n' ' ')
     echo "===> Found: $packages"
-    SPK_TO_BUILD+=$packages
+    SPK_TO_BUILD+=${packages}
 done
 
 # remove duplicate packages
 packages=$(printf %s "${SPK_TO_BUILD}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 
-if [ -z "$packages" ]; then
+# find all noarch packages
+all_noarch=$(find spk/ -maxdepth 2 -mindepth 2 -name "Makefile" -exec grep -Ho "override ARCH" {} \; | grep -Po ".*spk/\K[^/]*" | sort | tr '\n' ' ')
+
+# separate noarch and arch specific packages
+arch_packages=
+noarch_packages=
+for package in ${packages}
+do
+    if [ "$(echo ${all_noarch} | grep -ow ${package})" = "" ]; then
+        arch_packages+="${package} "
+    else
+        noarch_packages+="${package} "
+    fi
+done
+
+echo "===> ARCH   packages: ${arch_packages}"
+echo "===> NOARCH packages: ${noarch_packages}"
+
+if [ "${GH_ARCH}" = "noarch" ]; then
+    build_packages=${noarch_packages}
+else
+    build_packages=${arch_packages}
+fi
+
+if [ -z "${build_packages}" ]; then
     echo "===> No packages built <==="
     exit 0
 fi
 
 echo ""
-echo "===> PACKAGES to Build: $packages"
+echo "===> PACKAGES to Build: ${build_packages}"
 
 # Build
-for package in $packages
+for package in ${build_packages}
 do
     echo "----------------------------------------"
     # make sure that the package exists
     if [ -f "/github/workspace/spk/$package/Makefile" ]; then
         # use TCVERSION and ARCH parameters to get real exit code.
-        make TCVERSION=${GH_ARCH##*-} ARCH=${GH_ARCH%%-*} -C /github/workspace/spk/"$package"
+        make TCVERSION=${GH_ARCH##*-} ARCH=${GH_ARCH%%-*} -C /github/workspace/spk/${package}
+        result=$?
+        
+        if [ $result -eq 0 ];
+        then
+            echo "$(date --date=now +"%Y.%m.%d %H:%M:%S") - $package ($GH_ARCH) DONE"               >> ${BUILD_SUCCESS_FILE}
+        else
+            echo "$(date --date=now +"%Y.%m.%d %H:%M:%S") - $package ($GH_ARCH) FAILED ($result)"   >> ${BUILD_ERROR_FILE}
+        fi
     else
-        echo "/github/workspace/spk/$package/Makefile not found"
+        echo "/github/workspace/spk/${package}/Makefile not found"
     fi
 done
---
+++ .github/actions/build_status.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# We do not terminate the build on errors as we want to build all packages.
+#
+# Therfore failed builds are logged in the build error file defined by the 
+# env variable $BUILD_ERROR_FILE
+# If this file exists and contains at least one line, we show here the content 
+# of the file and exit with error.
+# Otherwise we show the other file containing the sucessfully built packages.
+
+if [ -f "${BUILD_ERROR_FILE}" ]; then
+    if [ $(cat "${BUILD_ERROR_FILE}" | wc -l) -gt 0 ]; then
+        cat "${GH_BUILD_ERROR_FILE}"
+        echo ""
+        echo "Please analyze the log file of the build job."
+        exit 1
+    fi
+fi
+if [ -f "${BUILD_SUCCESS_FILE}" ]; then
+    cat "${BUILD_SUCCESS_FILE}"
+fi
