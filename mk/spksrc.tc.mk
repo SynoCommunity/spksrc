65b45c6c9d12d1eefbffd7b2043ad116aca76cd2
Vincent Fortier <th0ma7@gmail.com>
2020-07-29 13:10:27 +0000

    linuxtv: Use kernel version for blacklisting modules
    - Fix TC_FIRMWARE for 6.2.2-24922
    - Add TC_KERNEL to track kernel version
    - Use TC_KERNEL to exclude modules from BLACKLIST

--- mk/spksrc.tc.mk
+++ mk/spksrc.tc.mk
@@ -84,6 +84,7 @@ tc_vars: patch
 	@echo CXXFLAGS := $(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)
 	@echo LDFLAGS := $(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)
 	@echo TC_FIRMWARE := $(TC_FIRMWARE)
+	@echo TC_KERNEL := $(TC_KERNEL)
 	@echo TC_OS_MIN_VER := $(TC_OS_MIN_VER)
 	@echo TC_ARCH := $(TC_ARCH)
 
