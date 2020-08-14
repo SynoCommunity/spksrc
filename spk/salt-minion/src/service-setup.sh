0da1a95ad5c189209ed35fb0a60a204352b11379
Kuko Armas <kuko@canarytek.com>
2020-08-14 19:25:51 +0100

    Adapted to service framework

---
+++ spk/salt-minion/src/service-setup.sh
@@ -0,0 +1,22 @@
+PYTHON_DIR="/usr/local/python3"
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
+VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
+LANGUAGE="env LANG=en_US.UTF-8"
+SALT_MINION="${SYNOPKG_PKGDEST}/env/bin/salt-minion"
+PID_FILE="${SYNOPKG_PKGDEST}/var/run/salt-minion.pid"
+
+SERVICE_COMMAND="${SALT_MINION} -c ${SYNOPKG_PKGDEST}/etc/salt -d"
+
+service_postinst ()
+{
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
+
+    # Install wheels
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG}
+
+    # Copy fixed rsax931.py file
+    cp ${SYNOPKG_PKGDEST}/share/rsax931.py ${SYNOPKG_PKGDEST}/env/lib/python3.7/site-packages/salt/utils/rsax931.py
+
+}
