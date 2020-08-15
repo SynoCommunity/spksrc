19e26bc54c05e15c73bd94519fd1e58dcd3f2c98
Kuko Armas <kuko@canarytek.com>
2020-08-15 22:49:45 +0100

    include busybox utils and fix rsax931.py with patch

--- spk/salt-minion/src/service-setup.sh
+++ spk/salt-minion/src/service-setup.sh
@@ -16,7 +16,11 @@ service_postinst ()
     # Install wheels
     ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG}
 
-    # Copy fixed rsax931.py file
-    cp ${SYNOPKG_PKGDEST}/share/rsax931.py ${SYNOPKG_PKGDEST}/env/lib/python3.7/site-packages/salt/utils/rsax931.py
+    # Install busybox stuff
+    ${SYNOPKG_PKGDEST}/bin/busybox --install ${SYNOPKG_PKGDEST}/bin
+
+    # Patch rsax931.py file to find libcrypto lib
+    ${SYNOPKG_PKGDEST}/bin/patch ${SYNOPKG_PKGDEST}/env/lib/python3.7/site-packages/salt/utils/rsax931.py < ${SYNOPKG_PKGDEST}/share/rsax931.py.patch >> ${INST_LOG} 2>&1
 
 }
+
