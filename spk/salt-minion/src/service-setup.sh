6262291e3ec651ced282175c90e0575251d18cf6
Kuko Armas <kuko@canarytek.com>
2020-08-20 01:20:26 +0100

    Store config in /var/salt/etc so it's not lost in upgrades

--- spk/salt-minion/src/service-setup.sh
+++ spk/salt-minion/src/service-setup.sh
@@ -1,12 +1,14 @@
 PYTHON_DIR="/usr/local/python3"
+SALT_PKG_DIR="/usr/local/salt-minion"
 PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
-PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
+PYTHON="${SALT_PKG_DIR=}/env/bin/python"
 LANGUAGE="env LANG=en_US.UTF-8"
-SALT_MINION="${SYNOPKG_PKGDEST}/env/bin/salt-minion"
-PID_FILE="${SYNOPKG_PKGDEST}/var/run/salt-minion.pid"
+SALT_MINION="${SALT_PKG_DIR}/env/bin/salt-minion"
+SALT_CONF_DIR="/var/salt/etc"
+PID_FILE="${SALT_PKG_DIR}/var/salt-minion.pid"
 
-SERVICE_COMMAND="${SALT_MINION} -c ${SYNOPKG_PKGDEST}/etc/salt -d"
+SERVICE_COMMAND="${SALT_MINION} -c /var/salt/etc -d"
 
 service_postinst ()
 {
@@ -20,5 +22,14 @@ service_postinst ()
     # (Rely on patch util bundled with python3's busybox)
     ${PYTHON_DIR}/bin/patch ${SYNOPKG_PKGDEST}/env/lib/python3.7/site-packages/salt/utils/rsax931.py < ${SYNOPKG_PKGDEST}/share/rsax931.py.patch >> ${INST_LOG} 2>&1
 
+    # Prepare salt-minion config in /var/salt
+    install -m 755 -d ${SALT_CONF_DIR}
+    install -m 755 -d ${SALT_CONF_DIR}/minion.d
+    install -m 644 ${SYNOPKG_PKGDEST}/share/minion.conf ${SALT_CONF_DIR}/
+    echo "pki_dir: ${SALT_CONF_DIR}/pki/minion" > ${SALT_CONF_DIR}/minion.d/01-pki-dir.conf
+    echo "pidfile: ${SYNOPKG_PKGDEST}/var/salt-minion.pid" > ${SALT_CONF_DIR}/minion.d/02_pidfile.conf
+    # Populate salt master address and minion_id only if file don't already exist
+    test -f ${SALT_CONF_DIR}/minion.d/99-master-address.conf || echo "master: salt" > ${SALT_CONF_DIR}/minion.d/99-master-address.conf
+    test -f ${SALT_CONF_DIR}/minion.d/98-minion-id.conf || echo "id: myname" > ${SALT_CONF_DIR}/minion.d/98-minion-id.conf
 }
 
