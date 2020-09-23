23cd49b49714cbf0d7fa4e7ee935c6bc0bd9f940
Vincent Fortier <th0ma7@gmail.com>
2020-09-23 23:55:08 +0000

    toolchain: Revert to previous TC_PREFIX TC_TARGET behaviour

--- mk/spksrc.tc.mk
+++ mk/spksrc.tc.mk
@@ -25,7 +25,7 @@ TC_LOCAL_VARS_MK = $(WORK_DIR)/tc_vars.mk
 
 #####
 
-RUN = cd $(WORK_DIR)/$(TC_PREFIX) && env $(ENV)
+RUN = cd $(WORK_DIR)/$(TC_TARGET) && env $(ENV)
 MSG = echo "===>   "
 
 include ../../mk/spksrc.download.mk
@@ -61,15 +61,16 @@ tc_vars: fix
 	do \
 	  target=`echo $${tool} | sed 's/\(.*\):\(.*\)/\1/'` ; \
 	  source=`echo $${tool} | sed 's/\(.*\):\(.*\)/\2/'` ; \
-	  echo TC_ENV += `echo $${target} | tr [:lower:] [:upper:] `=\"$(WORK_DIR)/$(TC_PREFIX)/bin/$(TC_PREFIX)-$${source}\" ; \
+	  echo TC_ENV += `echo $${target} | tr [:lower:] [:upper:] `=\"$(WORK_DIR)/$(TC_TARGET)/bin/$(TC_TARGET)-$${source}\" ; \
 	done
 	@echo TC_ENV += CFLAGS=\"$(CFLAGS) $$\(ADDITIONAL_CFLAGS\)\"
 	@echo TC_ENV += CPPFLAGS=\"$(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)\"
 	@echo TC_ENV += CXXFLAGS=\"$(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)\"
 	@echo TC_ENV += LDFLAGS=\"$(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)\"
-	@echo TC_CONFIGURE_ARGS := --host=$(TC_PREFIX) --build=i686-pc-linux
+	@echo TC_CONFIGURE_ARGS := --host=$(TC_TARGET) --build=i686-pc-linux
+	@echo TC_TARGET := $(TC_TARGET)
 	@echo TC_PREFIX := $(TC_PREFIX)
-	@echo TC_PATH := $(WORK_DIR)/$(TC_PREFIX)/bin/
+	@echo TC_PATH := $(WORK_DIR)/$(TC_TARGET)/bin/
 	@echo CFLAGS := $(CFLAGS) $$\(ADDITIONAL_CFLAGS\)
 	@echo CPPFLAGS := $(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)
 	@echo CXXFLAGS := $(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)
