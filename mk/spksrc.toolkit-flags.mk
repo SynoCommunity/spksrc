f974d29a9fb0b8b923250565afb7842fd3b6761b
Vincent Fortier <th0ma7@gmail.com>
2020-09-24 16:54:58 +0000

    toolkit: better manage exception for empty vars
    TOOLKIT_BASE_DIR and TOOLKIT_SYSROOT may require to be empty.
    As such allow using 'nop' value to force empty field for theses
    two variables when needed.

--- mk/spksrc.toolkit-flags.mk
+++ mk/spksrc.toolkit-flags.mk
@@ -26,6 +26,14 @@ ifeq ($(strip $(TOOLKIT_STRIP)),)
 TOOLKIT_STRIP = 6
 endif
 
+ifeq ($(strip $(TOOLKIT_BASE_DIR)),)
+TOOLKIT_BASE_DIR = $(TOOLKIT_TARGET)
+else ifeq ($(strip $(TOOLKIT_BASE_DIR)),nop)
+TOOLKIT_BASE_DIR = 
+endif
+
 ifeq ($(strip $(TOOLKIT_SYSROOT)),)
-TOOLKIT_SYSROOT = $(TOOLKIT_TARGET)/sys-root/usr
+TOOLKIT_SYSROOT ?= $(TOOLKIT_BASE_DIR)/sys-root/usr
+else ifeq ($(strip $(TOOLKIT_SYSROOT)),nop)
+TOOLKIT_SYSROOT = 
 endif
