b6377db4477175fb1776f3b1ad68c31cd2bb4d8a
Nyaran <nyayukko@gmail.com>
2020-10-12 01:29:08 +0200

    sickchill: Apply comments

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -3,38 +3,50 @@ GIT_DIR="/usr/local/git"
 PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
 HOME="${SYNOPKG_PKGDEST}/var"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
-GIT="${GIT_DIR}/bin/git"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
-SICKCHILL="${SYNOPKG_PKGDEST}/var/SickChill/SickChill.py"
+SC_INSTALL_DIR="${SYNOPKG_PKGDEST}/share/SickChill"
+SC_BINARY="${SC_INSTALL_DIR}/SickChill.py"
 SC_DATA_DIR="${SYNOPKG_PKGDEST}/var/data"
 SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
+SC_CONFIG_GIT_BRANCH=master
+SC_CONFIG_GIT_COMMIT_HASH=2190ef19c06b27911c01e04d5310220bcd6ac929
+SC_CONFIG_GIT_COMMIT_BRANCH=v2020.09.28-2
 
 GROUP="sc-download"
 
-SERVICE_COMMAND="${PYTHON} ${SICKCHILL} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
+SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
 
-
-service_postinst ()
-{
-    # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
-
-    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        # Clone the repository
-        ${GIT} clone --depth 10 --recursive -q -b master git://github.com/SickChill/SickChill.git ${SYNOPKG_PKGDEST}/var/SickChill > /dev/null 2>&1
-
-      if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
-        if [ -f "${SC_CFG_FILE}" ]; then
-          sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
-          sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
-        else
-          mkdir -p ${SC_DATA_DIR}
-          cat << EOF > ${SC_CFG_FILE}
+set_config() {
+    if [ -f "${SC_CFG_FILE}" ]; then
+        if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
+            sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
+            sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
+        fi
+        sed -i "/^\s*branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_BRANCH}/" ${SC_CFG_FILE}
+        sed -i "/^\s*cur_commit_hash\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_HASH}/" ${SC_CFG_FILE}
+        sed -i "/^\s*cur_commit_branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_BRANCH}/" ${SC_CFG_FILE}
+    else
+        mkdir -p ${SC_DATA_DIR}
+        cat << EOF > ${SC_CFG_FILE}
 [General]
 web_username = ${wizard_username}
 web_password = ${wizard_password}
+branch = ${SC_CONFIG_GIT_BRANCH}
+cur_commit_hash = ${SC_CONFIG_GIT_COMMIT_HASH}
+cur_commit_branch = ${SC_CONFIG_GIT_COMMIT_BRANCH}
 EOF
-        fi
-      fi
     fi
+}
+
+service_postinst() {
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >>${INST_LOG}
+
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        set_config
+    fi
+}
+
+service_postupgrade() {
+    set_config
 }
