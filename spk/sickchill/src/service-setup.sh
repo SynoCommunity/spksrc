0066c21849534f83c60c1e9b8d8a76840fcfb2dc
Nyaran <nyayukko@gmail.com>
2020-11-14 22:23:01 +0100

    sickchill: Read git data from file generated on install. sickchill: Use wheels sickchill: Update to sickchill v2020.10.22-2

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -1,22 +1,22 @@
 PYTHON_DIR="/usr/local/python3"
-GIT_DIR="/usr/local/git"
-PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
+PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
 HOME="${SYNOPKG_PKGDEST}/var"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
-PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
+PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
 SC_INSTALL_DIR="${SYNOPKG_PKGDEST}/share/SickChill"
 SC_BINARY="${SC_INSTALL_DIR}/SickChill.py"
 SC_DATA_DIR="${SYNOPKG_PKGDEST}/var/data"
 SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
-SC_CONFIG_GIT_BRANCH=master
-SC_CONFIG_GIT_COMMIT_HASH=2190ef19c06b27911c01e04d5310220bcd6ac929
-SC_CONFIG_GIT_COMMIT_BRANCH=v2020.09.28-2
+
 
 GROUP="sc-download"
 
 SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
 
 set_config() {
+    . ${SYNOPKG_PKGDEST}/share/git_data
+
     if [ -f "${SC_CFG_FILE}" ]; then
         if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
             sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
@@ -31,9 +31,9 @@ set_config() {
 [General]
 web_username = ${wizard_username}
 web_password = ${wizard_password}
-branch = ${SC_CONFIG_GIT_BRANCH}
-cur_commit_hash = ${SC_CONFIG_GIT_COMMIT_HASH}
-cur_commit_branch = ${SC_CONFIG_GIT_COMMIT_BRANCH}
+branch = ${SC_GIT_BRANCH}
+cur_commit_hash = ${SC_GIT_COMMIT_HASH}
+cur_commit_branch = ${SC_GIT_COMMIT_BRANCH}
 EOF
     fi
 }
@@ -41,6 +41,9 @@ EOF
 service_postinst() {
     # Create a Python virtualenv
     ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >>${INST_LOG}
+
+    # Install the wheels
+    ${PIP} install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         set_config
