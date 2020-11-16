6fcc2352520a37a75a94995be9fc2621be02f69b
hgy59 <hpgy59@gmail.com>
2020-11-16 08:30:15 +0100

    add urbackup-client
    - add cross/urbackup-client
    - provide urbackup-server and urbackup-client with spk/urbackup
    - remove obsolete shared folder creation in service-setup.sh
    - adjust wizard files

--- spk/urbackup/src/service-setup.sh
+++ spk/urbackup/src/service-setup.sh
@@ -4,9 +4,6 @@ PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 service_postinst ()
 {
    	mkdir -p ${SYNOPKG_PKGDEST}/var/urbackup
-    mkdir -p "${SHARE_PATH}"
-    chown ${PRIV_PREFIX}${USER}: "${SHARE_PATH}"
-    chmod 770 "${SHARE_PATH}"
     echo "tank/images" > ${SYNOPKG_PKGDEST}/var/urbackup/dataset  
     echo "${SHARE_PATH}" > ${SYNOPKG_PKGDEST}/var/urbackup/backupfolder 
 }
