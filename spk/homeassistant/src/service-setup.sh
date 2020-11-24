5db0e605b9fbde6bb8206a4dcaecba59e0af70b4
hgy59 <hpgy59@gmail.com>
2020-11-24 23:51:51 +0100

    revert tmpdir for service

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -2,25 +2,24 @@ PYTHON_DIR="/usr/local/python3"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}/bin:${PATH}"
 
-CONFIG_DIR=${SYNOPKG_PKGDEST}/var/config
+CONFIG_DIR="${SYNOPKG_PKGDEST}/var/config"
 
-# use alternate TMPDIR (as /tmp might have <300MB free space for 'pip install')
-# TMPDIR is supported by pip https://github.com/pypa/pip/issues/4462
-TMPDIR=${SYNOPKG_PKGDEST}/tmp
-
-SERVICE_COMMAND="TMPDIR=${TMPDIR} ${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --pid-file ${PID_FILE} --log-file ${LOG_FILE} --daemon"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --pid-file ${PID_FILE} --log-file ${LOG_FILE} --daemon"
 SVC_CWD="${SYNOPKG_PKGDEST}/var/"
 HOME="${SYNOPKG_PKGDEST}/var/"
 
 service_postinst ()
 {
+    # use alternate TMPDIR (as /tmp might have <300MB free space for 'pip install')
+    # TMPDIR is supported by pip https://github.com/pypa/pip/issues/4462
+    TMPDIR=${SYNOPKG_PKGDEST}/tmp
     mkdir -p "${TMPDIR}"
 
     # Create a Python virtualenv
     ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
 
     # Install the wheels
-    TMPDIR=${TMPDIR} ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    TMPDIR=${SYNOPKG_PKGDEST}/tmp ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
     mkdir -p "${CONFIG_DIR}"
     # For pip to install pure python module (others have to be provided as wheels in requirement.txt)
