d1a0921c409c024ee1675c9de38fc737be0f93bf
hgy59 <hpgy59@gmail.com>
2020-12-12 09:06:03 +0100

    change package icon size
    - change icon size from 72x72 to 64x64 for DSM 7

--- mk/spksrc.spk.mk
+++ mk/spksrc.spk.mk
@@ -279,10 +279,14 @@ icons:
 ifneq ($(strip $(SPK_ICON)),)
 	$(create_target_dir)
 	@$(MSG) "Creating PACKAGE_ICON.PNG for $(SPK_NAME)"
+ifneq ($(shell expr "$(TCVERSION)" \>= 7.0),1)
 	(convert $(SPK_ICON) -thumbnail 72x72 -strip - > $(WORK_DIR)/PACKAGE_ICON.PNG)
+else
+	(convert $(SPK_ICON) -thumbnail 64x64 -strip - > $(WORK_DIR)/PACKAGE_ICON.PNG)
+endif  
 	@$(MSG) "Creating PACKAGE_ICON_256.PNG for $(SPK_NAME)"
 	(convert $(SPK_ICON) -thumbnail 256x256 -strip - > $(WORK_DIR)/PACKAGE_ICON_256.PNG)
-	$(eval SPK_CONTENT +=  PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG)
+	$(eval SPK_CONTENT += PACKAGE_ICON.PNG PACKAGE_ICON_256.PNG)
 endif
 
 .PHONY: info-checksum
