e7c0cf5f1bee6ddfa8294b1d8552d64ab2a5ab3b
hgy59 <hpgy59@gmail.com>
2020-12-19 22:49:47 +0100

    update all Makefiles for updated ARCHS definitions
    - include spksrc.archs.mk instead of spksrc.common.mk for arch definitions
    - replace *_ARCHES by *_ARCHS
    - use OLD_PCC_ARCHS for unsupported archs where applicable

--- mk/spksrc.archs.mk
+++ mk/spksrc.archs.mk
@@ -8,32 +8,29 @@ SRM_ARM7_ARCHS = northstarplus ipq806x dakota
 # DSM - all ARMv7 except SRM specific archs
 DSM_ARM7_ARCHS = alpine armada370 armada375 armada38x armadaxp comcerto2k monaco
 
-ARM5_ARCHS = 88f6281
-ARM7_ARCHS = $(DSM_ARM7_ARCHS) $(SRM_ARM7_ARCHS)
-# hi3535 is not supported by generic ARMv7 arch
-ARM7L_ARCHS = hi3535
-ARM8_ARCHS = rtd1296 armada37xx
-ARM_ARCHS = $(ARM5_ARCHS) $(ARM7_ARCHS) $(ARM8_ARCHS)
-
-PPC_ARCHS = powerpc ppc824x ppc853x ppc854x qoriq
-
-i686_ARCHS = evansport
-x64_ARCHS = apollolake avoton braswell broadwell broadwellnk bromolow cedarview denverton dockerx64 geminilake grantley purley kvmx64 v1000 x86_64
-
-# outdated unsupported archs
-OBSOLETE_ARCHS = powerpc ppc824x ppc854x x86
-
 # Generic archs used for packages supporting multiple archs (where applicable)
 GENERIC_ARMv7_ARCH = armv7
 GENERIC_ARM64_ARCH = aarch64
 GENERIC_x64_ARCH = x64
-ARCHS_WITH_GENERIC_SUPPORT = $(ARM7_ARCHS) $(ARM8_ARCHS) $(x64_ARCHS)
+GENERIC_ARCHS = $(GENERIC_ARMv7_ARCH) $(GENERIC_ARM64_ARCH) $(GENERIC_x64_ARCH)
 
-# Archs including generic archs
-ALL_ARM7_ARCHS = $(GENERIC_ARMv7_ARCH) $(ARM7_ARCHS) $(ARM7L_ARCHS)
-ALL_ARM8_ARCHS = $(GENERIC_ARM64_ARCH) $(ARM8_ARCHS)
-ALL_x64_ARCHS = $(GENERIC_x64_ARCH) $(x64_ARCHS)
+ARM5_ARCHS = 88f6281
+ARM7_ARCHS = $(GENERIC_ARMv7_ARCH) $(DSM_ARM7_ARCHS) $(SRM_ARM7_ARCHS)
+# hi3535 is not supported by generic ARMv7 arch
+ARM7L_ARCHS = hi3535
+ARM8_ARCHS = $(GENERIC_ARM64_ARCH) rtd1296 armada37xx
+ARM_ARCHS = $(ARM5_ARCHS) $(ARM7_ARCHS) $(ARM7L_ARCHS) $(ARM8_ARCHS)
+
+PPC_ARCHS = powerpc ppc824x ppc853x ppc854x qoriq
+
+i686_ARCHS = evansport
+x64_ARCHS = $(GENERIC_x64_ARCH) apollolake avoton braswell broadwell broadwellnk bromolow cedarview denverton dockerx64 geminilake grantley purley kvmx64 v1000 x86_64
 
 # Arch groups
-ALL_ARM_ARCHS = $(ARM5_ARCHS) $(ALL_ARM7_ARCHS) $(ALL_ARM8_ARCHS)
-ALL_ARCHS = $(ALL_x64_ARCHS) $(i686_ARCHS) $(PPC_ARCHS) $(ALL_ARM_ARCHS)
+ALL_ARCHS = $(x64_ARCHS) $(i686_ARCHS) $(PPC_ARCHS) $(ARM_ARCHS)
+ARCHS_WITH_GENERIC_SUPPORT = $(filter-out $(GENERIC_ARCHS), $(ARM7_ARCHS) $(ARM8_ARCHS) $(x64_ARCHS)))
+# PPC_ARCHS except qoriq
+OLD_PPC_ARCHS = powerpc ppc824x ppc853x ppc854x
+
+# outdated unsupported archs
+OBSOLETE_ARCHS = powerpc ppc824x ppc854x x86
