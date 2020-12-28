adfe3ffc31240f9a845c8d67ff78e6501e3466ca
Vincent Fortier <th0ma7@gmail.com>
2020-12-28 15:59:55 +0000

    meson: Rename MESON_CFG to MESON_CFG_DIR

--- mk/spksrc.cross-meson-env.mk
+++ mk/spksrc.cross-meson-env.mk
@@ -1,5 +1,5 @@
 # meson cross-compilation definitions
-MESON_CFG=$(WORK_DIR)/../../../mk/meson
+MESON_CFG_DIR=$(WORK_DIR)/../../../mk/meson
 
 # Set default build directory
 ifeq ($(strip $(MESON_BUILD_DIR)),)
@@ -11,23 +11,23 @@ CONFIGURE_ARGS += -Dbuildtype=release
 
 # Define per arch specific common options
 ifeq ($(findstring $(ARCH),$(ARM5_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/armv5.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv5.cfg
 endif
 ifeq ($(findstring $(ARCH),$(ARM7_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/armv7.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv7.cfg
 endif
 ifeq ($(findstring $(ARCH),hi3535),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/armv7l.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv7l.cfg
 endif
 ifeq ($(findstring $(ARCH),$(ARM8_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/armv8.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv8.cfg
 endif
 ifeq ($(findstring $(ARCH), $(PPC_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/ppc.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/ppc.cfg
 endif
 ifeq ($(findstring $(ARCH),$(x86_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/i686.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/i686.cfg
 endif
 ifeq ($(findstring $(ARCH),$(x64_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG)/x86_64.cfg
+  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/x86_64.cfg
 endif
