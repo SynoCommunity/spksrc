4d89923f88f8180e56574c29f03467f35471608a
hgy59 <hpgy59@gmail.com>
2020-12-30 12:20:29 +0100

    [homeassistant] fix integrations (#4341)
    * fix integrations
    - add dependencies for xiaomi integrations
    - fix playstation 4 integration
    - use always alternate temp dir.
    - update tlslite-ng to v0.7.6 (closes #4324)

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -1,3 +1,7 @@
+# no shebang, this file is included by following scripts
+# - installer
+# - start-stop-status
+
 PYTHON_DIR="/usr/local/python3"
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}/bin:${PATH}"
@@ -10,18 +14,24 @@ HOME="${SYNOPKG_PKGDEST}/var/"
 
 service_postinst ()
 {
-    # use alternate TMPDIR (as /tmp might have <400MB free space for 'pip install')
-    # TMPDIR is supported by pip https://github.com/pypa/pip/issues/4462 ('--build dir' is not supported anymore)
-    TMPDIR=${SYNOPKG_PKGDEST}/tmp
-    mkdir -p "${TMPDIR}"
-
     # Create a Python virtualenv
     ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
 
     # Install the wheels
-    TMPDIR=${SYNOPKG_PKGDEST}/tmp ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --upgrade --no-index --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --upgrade --no-index --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
 
     mkdir -p "${CONFIG_DIR}"
     # For pip to install pure python module (others have to be provided as wheels in requirement.txt)
     chown -R ${EFF_USER} ${SYNOPKG_PKGDEST}/env
 }
+
+
+# use alternate TMPDIR for service_postinst and start_daemon (start-stop-status)
+# /tmp might have <400MB free space
+# TMPDIR is supported by pip https://github.com/pypa/pip/issues/4462 ('--build dir' is not supported anymore)
+TMPDIR=${SYNOPKG_PKGDEST}/tmp
+if [[ ! -e "${TMPDIR}" ]]; then
+    mkdir -p "${TMPDIR}"
+    chown ${EFF_USER} "${TMPDIR}"
+fi
+export TMPDIR=${SYNOPKG_PKGDEST}/tmp
