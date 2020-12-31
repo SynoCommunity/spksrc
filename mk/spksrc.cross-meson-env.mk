003d76ab2bda2be9684c8f3b522b9b5f07571af6
hgy59 <hpgy59@gmail.com>
2020-12-31 15:42:33 +0100

    fix meson build
    - followup of #4320

--- mk/spksrc.cross-meson-env.mk
+++ mk/spksrc.cross-meson-env.mk
@@ -1,5 +1,4 @@
 # meson cross-compilation definitions
-MESON_CFG_DIR=$(WORK_DIR)/../../../mk/meson
 
 # Set default build directory
 ifeq ($(strip $(MESON_BUILD_DIR)),)
@@ -9,25 +8,38 @@ endif
 # Set other build options
 CONFIGURE_ARGS += -Dbuildtype=release
 
-# Define per arch specific common options
+# Use arch specific configuration file
+MESON_CFG_DIR=$(realpath $(WORK_DIR)/../../../mk/meson)
+MESON_CFG_FILE =
+
 ifeq ($(findstring $(ARCH),$(ARMv5_ARCHS)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv5.cfg
+  MESON_CFG_FILE = armv5.cfg
 endif
-ifeq ($(findstring $(ARCH),$(ARMv7_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv7.cfg
+ifeq ($(findstring $(ARCH),$(ARMv7_ARCHS)),$(ARCH))
+  MESON_CFG_FILE = armv7.cfg
 endif
 ifeq ($(findstring $(ARCH),$(ARMv7L_ARCHS)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/armv7l.cfg
+  MESON_CFG_FILE = armv7l.cfg
 endif
-ifeq ($(findstring $(ARCH),$(ARMv8_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/aarch64.cfg
+ifeq ($(findstring $(ARCH),$(ARMv8_ARCHS)),$(ARCH))
+  MESON_CFG_FILE = aarch64.cfg
 endif
-ifeq ($(findstring $(ARCH), $(PPC_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/ppc.cfg
+ifeq ($(findstring $(ARCH), $(PPC_ARCHS)),$(ARCH))
+  MESON_CFG_FILE = ppc.cfg
 endif
 ifeq ($(findstring $(ARCH),$(i686_ARCHS)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/i686.cfg
+  MESON_CFG_FILE = i686.cfg
 endif
-ifeq ($(findstring $(ARCH),$(x64_ARCHES)),$(ARCH))
-  CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/x86_64.cfg
+ifeq ($(findstring $(ARCH),$(x64_ARCHS)),$(ARCH))
+  MESON_CFG_FILE = x86_64.cfg
 endif
+
+ifeq ($(strip $(MESON_CFG_FILE)),)
+  $(error No meson config file defined for $(ARCH))
+else
+  ifeq ($(wildcard $(MESON_CFG_DIR)/$(MESON_CFG_FILE)),)
+    $(error meson config file not found: $(MESON_CFG_DIR)/$(MESON_CFG_FILE))
+  endif
+endif
+
+CONFIGURE_ARGS += --cross-file $(MESON_CFG_DIR)/$(MESON_CFG_FILE)
