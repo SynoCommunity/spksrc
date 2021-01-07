ec789700a9de20df3160dbf6fa9b772b24bcd595
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-01-08 10:20:21 +1100

    [Adminer] fix DSM6 regression

--- spk/adminer/src/installer.sh
+++ spk/adminer/src/installer.sh
@@ -6,6 +6,12 @@ DNAME="Adminer"
 
 HTACCESS_FILE=/var/services/web_packages/adminer/.htaccess
 
+if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+INSTALL_DIR=${SYNOPKG_PKGDEST}/web
+DSM6_WEB_DIR="/var/services/web"
+HTACCESS_FILE=${INSTALL_DIR}/.htaccess
+fi
+
 preinst ()
 {
     exit 0
@@ -16,6 +22,10 @@ postinst ()
     # Edit .htaccess according to the wizard
     sed -i -e "s|@@_wizard_htaccess_allowed_from_@@|${wizard_htaccess_allowed_from}|g" ${HTACCESS_FILE}
 
+    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+        # Install the web interface
+        cp -pR ${INSTALL_DIR} ${DSM6_WEB_DIR}/adminer
+    fi
     exit 0
 }
 
@@ -26,6 +36,10 @@ preuninst ()
 
 postuninst ()
 {
+    # Remove the web interface
+    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+        rm -rf ${DSM6_WEB_DIR}/adminer
+    fi
     exit 0
 }
 
