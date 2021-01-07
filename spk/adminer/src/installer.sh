4258ee756e42194200d092cadd942ab771e74e47
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-01-07 23:32:28 +1100

    [Adminer] install via resource worker
    Update to apache2.4 and php7.4
    
    [Adminer] fix DSM6 regression

--- spk/adminer/src/installer.sh
+++ spk/adminer/src/installer.sh
@@ -4,11 +4,13 @@
 PACKAGE="adminer"
 DNAME="Adminer"
 
-# Others
-INSTALL_DIR=${SYNOPKG_PKGDEST}/web
-WEB_DIR="/var/services/web"
+HTACCESS_FILE=/var/services/web_packages/adminer/.htaccess
 
+if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+INSTALL_DIR=${SYNOPKG_PKGDEST}/web
+DSM6_WEB_DIR="/var/services/web"
 HTACCESS_FILE=${INSTALL_DIR}/.htaccess
+fi
 
 preinst ()
 {
@@ -20,9 +22,10 @@ postinst ()
     # Edit .htaccess according to the wizard
     sed -i -e "s|@@_wizard_htaccess_allowed_from_@@|${wizard_htaccess_allowed_from}|g" ${HTACCESS_FILE}
 
-    # Install the web interface
-    cp -pR ${INSTALL_DIR} ${WEB_DIR}/adminer
-
+    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+        # Install the web interface
+        cp -pR ${INSTALL_DIR} ${DSM6_WEB_DIR}/adminer
+    fi
     exit 0
 }
 
@@ -34,8 +37,9 @@ preuninst ()
 postuninst ()
 {
     # Remove the web interface
-    rm -rf ${WEB_DIR}/adminer
-
+    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+        rm -rf ${DSM6_WEB_DIR}/adminer
+    fi
     exit 0
 }
 
@@ -48,6 +52,6 @@ postupgrade ()
 {
     # Edit .htaccess according to the wizard
     sed -i -e "s|@@_wizard_htaccess_allowed_from_@@|${wizard_htaccess_allowed_from}|g" ${HTACCESS_FILE}
-    
+
     exit 0
 }
