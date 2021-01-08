4748558e926c6407ce3f0dfbc244f62ae94b7e49
hgy59 <hpgy59@gmail.com>
2021-01-08 23:41:11 +0100

    fix ejabberd service configuration and startup

--- spk/ejabberd/src/service-setup.sh
+++ spk/ejabberd/src/service-setup.sh
@@ -1,33 +1,18 @@
 
 
+# service ctl file
+EJABBERD_CTL="${SYNOPKG_PKGDEST}/sbin/ejabberdctl"
+
+
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        sed -i -e "s#{hosts, \[\"localhost\"\]}.#{hosts, \[\"${wizard_ejabberd_hostname}\"\]}.#g" \
-               -e "s#\%\%{acl, admin, {user, \"ermine\", \"example.org\"}}.#{acl, admin, {user, \"${wizard_ejabberd_admin_username}\", \"${wizard_ejabberd_hostname}\"}}.#g" \
-               -e "s#{access_createnode, pubsub_createnode},#{access_createnode, pubsub_createnode},\n\t\t  {max_items_node, 1000000},#g" \
-               ${SYNOPKG_PKGDEST}/var/ejabberd.cfg
-        ${SYNOPKG_PKGDEST}/sbin/ejabberdctl start > /dev/null
-        ${SYNOPKG_PKGDEST}/sbin/ejabberdctl register ${wizard_ejabberd_admin_username} ${wizard_ejabberd_hostname} ${wizard_ejabberd_admin_password}
-        ${SYNOPKG_PKGDEST}/sbin/ejabberdctl stop > /dev/null
+        ${EJABBERD_CTL} start       >> ${INST_LOG} 2>&1
+        ${EJABBERD_CTL} started     >> ${INST_LOG} 2>&1
+        
+        ${EJABBERD_CTL} register ${wizard_ejabberd_admin_username} ${wizard_ejabberd_hostname} ${wizard_ejabberd_admin_password}  >> ${INST_LOG} 2>&1
+        
+        ${EJABBERD_CTL} stop        >> ${INST_LOG} 2>&1
+        ${EJABBERD_CTL} stopped     >> ${INST_LOG} 2>&1
     fi
 }
-
-
-###   service_preupgrade ()
-###   {
-###       # Save the configuration file
-###       rm -fr ${TMP_DIR}/${PACKAGE}
-###       mkdir -p ${TMP_DIR}/${PACKAGE}/etc/ejabberd
-###       mkdir -p ${TMP_DIR}/${PACKAGE}/var/lib/ejabberd
-###       mv ${SYNOPKG_PKGDEST}/etc/ejabberd/* ${TMP_DIR}/${PACKAGE}/etc/ejabberd
-###       mv ${SYNOPKG_PKGDEST}/var/lib/ejabberd/* ${TMP_DIR}/${PACKAGE}/var/lib/ejabberd
-###   }
-###   
-###   service_postupgrade ()
-###   {
-###       # Restore the configuration file
-###       mv ${TMP_DIR}/${PACKAGE}/etc/ejabberd/* ${SYNOPKG_PKGDEST}/etc/ejabberd
-###       mv ${TMP_DIR}/${PACKAGE}/var/lib/ejabberd/* ${SYNOPKG_PKGDEST}/var/lib/ejabberd
-###       rm -fr ${TMP_DIR}/${PACKAGE}
-###   }
