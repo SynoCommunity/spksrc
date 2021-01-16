3996d1606f4ffc6e0debc2822a861894fb9af7ca
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-01-16 16:15:01 +1100

    jackett simplify service script

--- spk/jackett/src/service-setup.sh
+++ spk/jackett/src/service-setup.sh
@@ -1,20 +1,7 @@
 
-# Package specific behaviours
-# Sourced script by generic installer and start-stop-status scripts
+PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+JACKETT="${SYNOPKG_PKGDEST}/share/jackett"
+HOME_DIR="${SYNOPKG_PKGDEST}/var"
 
-service_prestart ()
-{
-    # Replace generic service startup
-    PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
-    JACKETT="${SYNOPKG_PKGDEST}/share/jackett"
-    HOME_DIR="${SYNOPKG_PKGDEST}/var"
-
-    echo "Starting Jackett as user ${EFF_USER}" >> ${LOG_FILE}
-    COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${JACKETT} --PIDFile ${PID_FILE}"
-
-    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 6 ]; then
-        su ${EFF_USER} -s /bin/sh -c "${COMMAND}" >> ${LOG_FILE} 2>&1 &
-    else
-        ${COMMAND} >> ${LOG_FILE} 2>&1 &
-    fi
-}
+SERVICE_COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${JACKETT} --PIDFile ${PID_FILE}"
+SVC_BACKGROUND=y
