f1bff7d11f04510eb0170e283cd835072ef03b4c
Vincent Fortier <th0ma7@gmail.com>
2021-01-26 00:19:01 +0000

    kernel.mk: Address all kernel types using build version

--- mk/spksrc.kernel-flags.mk
+++ mk/spksrc.kernel-flags.mk
@@ -10,8 +10,8 @@ ifeq ($(strip $(KERNEL_DIST_FILE)),)
 KERNEL_DIST_FILE = $(KERNEL_ARCH)-$(KERNEL_DIST).$(KERNEL_EXT)
 endif
 
-# For DSM version >= 6.x
-ifeq ($(shell expr "$(word 1,$(subst ., ,$(TC_VERS)))" \>= 6),1)
+# For DSM version >= 6.1
+ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 15152),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
 KERNEL_DIST_NAME = $(KERNEL_DIST).$(KERNEL_EXT)
 endif
@@ -28,10 +28,28 @@ ifeq ($(strip $(KERNEL_STRIP)),)
 KERNEL_STRIP = 0
 endif
 
-# For DSM version <= 5.x
-else
+# For DSM version = 5.2
+else ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 5565),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
-KERNEL_DIST_NAME = $(KERNEL_ARCH)-source.txz
+KERNEL_DIST_NAME = $(KERNEL_ARCH)-source.$(KERNEL_EXT)
+endif
+
+ifeq ($(strip $(KERNEL_URL_DIR)),)
+KERNEL_URL_DIR = /
+endif
+
+ifeq ($(strip $(KERNEL_PREFIX)),)
+KERNEL_PREFIX = source/$(KERNEL_DIST)
+endif
+
+ifeq ($(strip $(KERNEL_STRIP)),)
+KERNEL_STRIP = 1
+endif
+
+# For DSM version <= 5.1
+else ifeq ($(shell expr "$(KERNEL_BUILD)" \<= 5004),1)
+ifeq ($(strip $(KERNEL_DIST_NAME)),)
+KERNEL_DIST_NAME = synogpl-$(KERNEL_BUILD)-$(KERNEL_ARCH).$(KERNEL_EXT)
 endif
 
 ifeq ($(strip $(KERNEL_URL_DIR)),)
