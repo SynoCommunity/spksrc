0de80cc38a48f80fc03e492e64067fb14c835fda
hgy59 <hpgy59@gmail.com>
2021-01-31 20:16:13 +0100

    update znc and libicu, prepare for DSM7 (#4393)
    - update libicu from v67.1 to v68.2
    - update znc from v1.8.1 to v1.8.2
    - remove cross/busybox dependency and DSM6 migration to delete DSM5 user and group (#4373)
    - fix libicu to build with DSM7 (gcc 7.3 needs use of C99 compatible floating point math)

--- spk/znc/src/service-setup.sh
+++ spk/znc/src/service-setup.sh
@@ -5,8 +5,8 @@
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 ZNC="${SYNOPKG_PKGDEST}/bin/znc"
 CERT_FILE="${SYNOPKG_PKGDEST}/var/znc.pem"
-PYTHON3_PATH="/var/packages/python3/target"
-SERVICE_COMMAND="env LD_LIBRARY_PATH=${PYTHON3_PATH}/lib ${ZNC} -d ${SYNOPKG_PKGDEST}/var"
+PYTHON3_LIB_PATH="/var/packages/python3/target/lib"
+SERVICE_COMMAND="env LD_LIBRARY_PATH=${PYTHON3_LIB_PATH} ${ZNC} -d ${SYNOPKG_PKGDEST}/var"
 SVC_BACKGROUND=yes
 
 service_postinst ()
@@ -17,12 +17,6 @@ service_postinst ()
     sed -i -e "s,@username@,${wizard_username:=admin},g" ${SYNOPKG_PKGDEST}/var/configs/znc.conf
     sed -i -e "s,@password@,${wizard_password:=admin},g" ${SYNOPKG_PKGDEST}/var/configs/znc.conf
     sed -i -e "s,@zncuser@,${EFF_USER},g" ${SYNOPKG_PKGDEST}/var/configs/oidentd.conf
-
-    # Discard legacy obsolete busybox user account
-    BIN=${SYNOPKG_PKGDEST}/bin
-    $BIN/busybox --install $BIN
-    $BIN/delgroup "${USER}" "users" >> ${INST_LOG}
-    $BIN/deluser "${USER}" >> ${INST_LOG}
 }
 
 service_prestart ()
@@ -33,5 +27,7 @@ service_prestart ()
     else
         echo "Generating initial certificate file" >> ${LOG_FILE}
         ${ZNC} -d ${SYNOPKG_PKGDEST}/var -p >> ${LOG_FILE}
+        echo "Certificate file created. Starting..." >> ${LOG_FILE}
     fi
 }
+
