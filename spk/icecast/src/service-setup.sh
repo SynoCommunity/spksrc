3675a6f9a08c0171635f040499e40627de4a6f4a
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-02-04 12:35:00 +1100

    Fix Icecast for DSM 7.0
    icecast upgrade from old config file

--- spk/icecast/src/service-setup.sh
+++ spk/icecast/src/service-setup.sh
@@ -10,6 +10,14 @@ service_postinst ()
     # Edit the configuration according to the wizard
     sed -i -e "s/@username@/${wizard_ic_username:=admin}/g" ${CFG_FILE}
     sed -i -e "s/@password@/${wizard_ic_password:=changepassword}/g" ${CFG_FILE}
+    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ]; then
+        sed -i -e "s|/var/packages/icecast/var|/var/packages/icecast/target/var|g" ${CFG_FILE}
+    fi
 }
 
+
+service_postupgrade ()
+{
+    sed -i -e "s|/usr/local/icecast|/var/packages/icecast|g" ${CFG_FILE}
+}
 
