20436f2ed67b13853fc1fd4f0b62edfbfe2bc8eb
sadmin <sadmin@KaliStation.home>
2021-02-09 00:53:14 +0100

    Erlang v23.2

---
+++ spk/erlang/src/installer.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Package
+PACKAGE="erlang"
+DNAME="Erlang"
+
+# Others
+INSTALL_DIR="/var/packages/${PACKAGE}/target"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
