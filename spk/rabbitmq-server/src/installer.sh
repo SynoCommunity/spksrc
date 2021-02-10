f5b2c82468d7ad0d8c77d520c752e8253a9be807
sadmin <sadmin@KaliStation.home>
2021-02-10 21:32:19 +0100

    Tentative to cross compile RabbitMQ Server

---
+++ spk/rabbitmq-server/src/installer.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Package
+PACKAGE="rabbitmq-server"
+DNAME="RabbitMQ Server"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}/target"
+
+
+preinst ()
+{
+    exit 0
+}
+
+postinst ()
+{
+    exit 0
+}
+
+preuninst ()
+{
+    exit 0
+}
+
+postuninst ()
+{
+    exit 0
+}
+
+preupgrade ()
+{
+    exit 0
+}
+
+postupgrade ()
+{
+    exit 0
+}
