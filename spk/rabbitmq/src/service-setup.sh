85d5bd0f5bb35ab9fb37d56217e7a521479e97d4
sadmin <sadmin@KaliStation.home>
2021-02-19 17:27:30 +0100

    RabbitMQ : plugins management installed during postinstall + no more patches needed for script

--- spk/rabbitmq/src/service-setup.sh
+++ spk/rabbitmq/src/service-setup.sh
@@ -3,11 +3,19 @@ SVC_CWD="${SYNOPKG_PKGDEST}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
+# HOME useful at installation time
+export HOME=/var/packages/rabbitmq/home
+
+MQ_BIN=/var/packages/rabbitmq/target/lib/rabbitmq_server-3.8.12/sbin
 
 service_postinst ()
 {
 
-    sed -i 's/SYS_PREFIX\=/SYS_PREFIX\=\/var\/packages\/rabbitmq\/target\//g' /var/packages/rabbitmq/target/lib/rabbitmq_server-3.8.12/sbin/rabbitmq-defaults
+    sed -i 's/SYS_PREFIX\=/SYS_PREFIX\=\/var\/packages\/rabbitmq\/target\//g' ${MQ_BIN}/rabbitmq-d
+efaults
+
+    # install plugins for rabbitmq management
+    ${MQ_BIN}/rabbitmq-plugins enable rabbitmq_management
 
 }
 
