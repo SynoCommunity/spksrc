5dc0a75d099152865250e149c16036c1a8aa4450
Vincent Fortier <th0ma7@gmail.com>
2021-02-22 01:50:39 +0000

    linuxtv: Optimize service-setup.sh to reduce code duplication

--- spk/synokernel-linuxtv/src/service-setup.sh
+++ spk/synokernel-linuxtv/src/service-setup.sh
@@ -1,24 +1,29 @@
-CFG_FILE="/usr/local/${SYNOPKG_PKGNAME}/etc/${SYNOPKG_PKGNAME}.ini"
-UDEV_RULE=60-${SYNOPKG_PKGNAME}.rules
+INI="/usr/local/${SYNOPKG_PKGNAME}/etc/${SYNOPKG_PKGNAME}.ini"
+CFG="/usr/local/${SYNOPKG_PKGNAME}/etc/${SYNOPKG_PKGNAME}.cfg"
+UDEV=60-${SYNOPKG_PKGNAME}.rules
 
 write_config() {
-    # Drop variables into a configuration file
-    echo "default=true"                                       > ${CFG_FILE}
-    echo "HAUPPAUGE_WINTV_DUALHD=${HAUPPAUGE_WINTV_DUALHD}"  >> ${CFG_FILE}
-    echo "MYGICA_T230A=${MYGICA_T230A}"                      >> ${CFG_FILE}
+   # Drop variables based on .cfg file to
+   # generate the .ini configuration file
+   # based on user option selection
+   echo "default=true" > ${INI}
+   for option in $(cat ${CFG}); do
+      var=${option%%=*}
+      echo "${var}=${!var}" >> ${INI}
+   done
 }
 
 service_postinst() {
-    [ ! -f ${CFG_FILE} ] && mkdir -p /usr/local/${SYNOPKG_PKGNAME}/etc
+    [ ! -f ${INI} ] && mkdir -p /usr/local/${SYNOPKG_PKGNAME}/etc
     write_config
 }
 
 service_preupgrade() {
-    [ -f ${CFG_FILE} ] && mv ${CFG_FILE} /tmp/${SYNOPKG_PKGNAME}.ini
+    [ -f ${INI} ] && mv ${INI} /tmp/${SYNOPKG_PKGNAME}.ini
 }
 
 service_postupgrade() {
-    if [ ! -f ${CFG_FILE} ]; then
+    if [ ! -f ${INI} ]; then
         mkdir -p /usr/local/${SYNOPKG_PKGNAME}/etc
         mv /tmp/${SYNOPKG_PKGNAME}.ini /usr/local/${SYNOPKG_PKGNAME}/etc
     fi
@@ -28,6 +33,6 @@ service_postupgrade() {
 service_postuninst ()
 {
     # ensure to remove rules for USB serial permissions, created at service start
-    rm -f /lib/udev/rules.d/${UDEV_RULE}   >> "${INST_LOG}"
+    rm -f /lib/udev/rules.d/${UDEV} >> "${INST_LOG}"
     exit 0
 }
