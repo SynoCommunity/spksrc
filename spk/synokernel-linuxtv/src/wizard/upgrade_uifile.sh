25fb010685ab3544d62b2a16deb7ba460879db5e
Vincent Fortier <th0ma7@gmail.com>
2021-03-02 03:07:00 +0000

    linuxtv: Fix managing future new entries properly

--- spk/synokernel-linuxtv/src/wizard/upgrade_uifile.sh
+++ spk/synokernel-linuxtv/src/wizard/upgrade_uifile.sh
@@ -4,6 +4,18 @@ set +x
 CFG_FILE="/usr/local/${SYNOPKG_PKGNAME}/etc/${SYNOPKG_PKGNAME}.ini"
 . ${CFG_FILE}
 
+# Ensure to add any new entries in the $OPTIONS
+# variable below so it always get a value
+# even though it didn't exist previously in the
+# .ini configuration file
+OPTIONS=( PCTV461E HAUPPAUGE_WINTV_DUALHD MYGICA_T230 SMS_SIANO_MDTV )
+
+for option in "${OPTIONS[@]}"; do
+   var=${option%%=*}
+   [ -z "${!var}" ] \
+      && eval ${var}=false
+done
+
 FIRST=`/bin/cat<<EOF
 {
     "step_title": "SynoKernel USB Serial kernel module configuration",
@@ -18,22 +30,22 @@ FIRST=`/bin/cat<<EOF
         {
             "key": "PCTV461E",
             "desc": "Allegro A8293, Empia EM28178, Montage M88DS3103, Montage M88TS2022",
-            "defaultValue": "$PCTV461E"
+            "defaultValue": $PCTV461E
         },
         {
             "key": "HAUPPAUGE_WINTV_DUALHD",
             "desc": "Hauppauge WinTV Dual-HD",
-            "defaultValue": "$HAUPPAUGE_WINTV_DUALHD"
+            "defaultValue": $HAUPPAUGE_WINTV_DUALHD
         },
         {
             "key": "MYGICA_T230",
             "desc": "MyGica T230A, MyGica T230C, MyGica T230C2, MyGica T230C2 Lite",
-            "defaultValue": "$MYGICA_T230"
+            "defaultValue": $MYGICA_T230
         },
         {
             "key": "SMS_SIANO_MDTV",
             "desc": "Siano Mobile Digital MDTV Receiver",
-            "defaultValue": "$SMS_SIANO_MDTV"
+            "defaultValue": $SMS_SIANO_MDTV
         }]
     }]
 }
