18fb435fcc8cac5bd9f4fa6b79027792555ef7e6
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-03-14 09:44:26 +1100

    jackett fix mono start script

--- spk/jackett/src/service-setup-mono.sh
+++ spk/jackett/src/service-setup-mono.sh
@@ -2,10 +2,12 @@ MONO_PATH="/var/packages/mono/target/bin"
 PATH="${SYNOPKG_PKGDEST}/bin:${MONO_PATH}:${PATH}"
 MONO="${MONO_PATH}/mono"
 JACKETT="${SYNOPKG_PKGDEST}/share/JackettConsole.exe"
+HOME_DIR="${SYNOPKG_PKGDEST}/var"
 
 # workaround for mono bug with armv5 (https://github.com/mono/mono/issues/12537)
 if [ "$SYNOPKG_DSM_ARCH" == "88f6281" -o "$SYNOPKG_DSM_ARCH" == "88f6282" ]; then
     MONO="MONO_ENV_OPTIONS='-O=-aot,-float32' ${MONO_PATH}/mono"
 fi
 
-COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${MONO} ${JACKETT} --PIDFile ${PID_FILE}"
+SERVICE_COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${MONO} ${JACKETT} --PIDFile ${PID_FILE}"
+SVC_BACKGROUND=y
