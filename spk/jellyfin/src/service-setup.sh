3dab73b9573a5a9b7b3571344cba0136b050f4d5
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-03-14 15:23:17 +1100

    Jellyfin: fixes

--- spk/jellyfin/src/service-setup.sh
+++ spk/jellyfin/src/service-setup.sh
@@ -1,7 +1,7 @@
 # https://github.com/dotnet/core/issues/4011
 [ -z "$DOTNET_BUNDLE_EXTRACT_BASE_DIR" ] && export DOTNET_BUNDLE_EXTRACT_BASE_DIR="${XDG_CACHE_HOME:-"/var/packages/jellyfin/target/var/"}/dotnet_bundle_extract"
 
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/share/jellyfin --noautorunwebapp --package-name synology -d ${SYNOPKG_PKGDEST}/var/data -C ${SYNOPKG_PKGDEST}/var/cache -c ${SYNOPKG_PKGDEST}/var/config -l ${SYNOPKG_PKGDEST}/var/log -w ${SYNOPKG_PKGDEST}/web"
+SERVICE_COMMAND="env LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${SYNOPKG_PKGDEST}/share/jellyfin --package-name synology -d ${SYNOPKG_PKGDEST}/var/data -C ${SYNOPKG_PKGDEST}/var/cache -c ${SYNOPKG_PKGDEST}/var/config -l ${SYNOPKG_PKGDEST}/var/log -w ${SYNOPKG_PKGDEST}/web"
 
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
