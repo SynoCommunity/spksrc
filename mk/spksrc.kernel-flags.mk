526724f2d4da9ab307d48da0f07178aeecd0f668
Vincent Fortier <th0ma7@users.noreply.github.com>
2021-03-29 19:30:59 -0400

    kernel.mk - Add kernel module automated building feature (#4526)
    * kernel: Fix SRM kernels using proper flags
    
    * kernel.mk: Migrate to using version* functions
    
    * kernel: Add + extraversion for >= 4.4 to simplify later calls
    
    * kernel.mk: Add REQUIRE_KERNEL_MODULE support
    
    * kernel: Add PLIST.auto feature for kernel objects
    
    * plist.mk: Consider kernel as PLIST.auto by default

--- mk/spksrc.kernel-flags.mk
+++ mk/spksrc.kernel-flags.mk
@@ -1,3 +1,9 @@
+# if TC_TYPE is unset then it is being
+# called directly from spksrc/kernel directory
+ifeq ($(strip $(TC_TYPE)),)
+include $(WORK_DIR)/tc_vars.mk
+endif
+
 ifeq ($(strip $(KERNEL_NAME)),)
 KERNEL_NAME = syno-$(KERNEL_ARCH)-$(KERNEL_VERS)
 endif
@@ -10,7 +16,22 @@ ifeq ($(strip $(KERNEL_DIST_FILE)),)
 KERNEL_DIST_FILE = $(KERNEL_ARCH)-$(KERNEL_DIST).$(KERNEL_EXT)
 endif
 
+ifeq ($(strip $(KERNEL_CONFIG)),)
+KERNEL_CONFIG = synoconfigs/$(KERNEL_ARCH)
+endif
+
+#
+# For DSM
+#
+ifeq ($(strip $(TC_TYPE)),DSM)
+
+ifeq ($(strip $(KERNEL_DIST_SITE)),)
+KERNEL_DIST_SITE = https://sourceforge.net/projects/dsgpl/files/Synology%20NAS%20GPL%20Source/$(KERNEL_BUILD)branch/$(KERNEL_URL_DIR)
+endif
+
+#
 # For DSM version >= 6.1
+#
 ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 15152),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
 KERNEL_DIST_NAME = $(KERNEL_DIST).$(KERNEL_EXT)
@@ -28,7 +49,9 @@ ifeq ($(strip $(KERNEL_STRIP)),)
 KERNEL_STRIP = 0
 endif
 
+#
 # For DSM version = 5.2
+#
 else ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 5565),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
 KERNEL_DIST_NAME = $(KERNEL_ARCH)-source.$(KERNEL_EXT)
@@ -46,7 +69,9 @@ ifeq ($(strip $(KERNEL_STRIP)),)
 KERNEL_STRIP = 1
 endif
 
+#
 # For DSM version <= 5.1
+#
 else ifeq ($(shell expr "$(KERNEL_BUILD)" \<= 5004),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
 KERNEL_DIST_NAME = synogpl-$(KERNEL_BUILD)-$(KERNEL_ARCH).$(KERNEL_EXT)
@@ -65,10 +90,28 @@ KERNEL_STRIP = 1
 endif
 endif
 
+#
+# SRM
+#
+else ifeq ($(strip $(TC_TYPE)),SRM)
+
 ifeq ($(strip $(KERNEL_DIST_SITE)),)
-KERNEL_DIST_SITE = https://sourceforge.net/projects/dsgpl/files/Synology%20NAS%20GPL%20Source/$(KERNEL_BUILD)branch/$(KERNEL_URL_DIR)
+KERNEL_DIST_SITE = https://sourceforge.net/projects/dsgpl/files/Synology%20Router%20GPL%20Source/$(KERNEL_BUILD)branch/$(KERNEL_URL_DIR)
 endif
 
-ifeq ($(strip $(KERNEL_CONFIG)),)
-KERNEL_CONFIG = synoconfigs/$(KERNEL_ARCH)
+ifeq ($(strip $(KERNEL_DIST_NAME)),)
+KERNEL_DIST_NAME = synogpl-$(KERNEL_BUILD)-$(KERNEL_ARCH).$(KERNEL_EXT)
+endif
+
+ifeq ($(strip $(KERNEL_URL_DIR)),)
+KERNEL_URL_DIR = /
+endif
+
+ifeq ($(strip $(KERNEL_PREFIX)),)
+KERNEL_PREFIX = source/$(KERNEL_DIST)
+endif
+
+ifeq ($(strip $(KERNEL_STRIP)),)
+KERNEL_STRIP = 1
+endif
 endif
--- mk/spksrc.kernel.mk
+++ mk/spksrc.kernel.mk
@@ -9,7 +9,8 @@ include ../../mk/spksrc.kernel-flags.mk
 NAME          = $(KERNEL_NAME)
 COOKIE_PREFIX = linux-
 URLS          = $(KERNEL_DIST_SITE)/$(KERNEL_DIST_NAME)
-PKG_DIR       = linux
+PKG_NAME      = linux
+PKG_DIR       = $(PKG_NAME)
 ifneq ($(KERNEL_DIST_FILE),)
 LOCAL_FILE    = $(KERNEL_DIST_FILE)
 # download.mk uses PKG_DIST_FILE
@@ -28,8 +29,16 @@ EXTRACT_CMD   = $(EXTRACT_CMD.$(KERNEL_EXT)) --skip-old-files --strip-components
 PRE_CONFIGURE_TARGET = kernel_pre_configure_target
 CONFIGURE_TARGET     = kernel_configure_target
 PRE_COMPILE_TARGET   = kernel_module_prepare_target
+ifeq ($(strip $(REQUIRE_KERNEL_MODULE)),)
 COMPILE_TARGET       = nop
-COPY_TARGET          = nop
+else
+COMPILE_TARGET       = kernel_module_compile_target
+endif
+# spksrc.install.mk called for PRE_INSTALL_PLIST
+# in order to generate a work*/linux.plist.auto
+# later used by spksr.plist.mk to generate the
+# diff based on .ko kernel objects
+INSTALL_TARGET       = nop
 
 #####
 
@@ -56,10 +65,16 @@ include ../../mk/spksrc.configure.mk
 compile: configure
 include ../../mk/spksrc.compile.mk
 
+install: compile
+include ../../mk/spksrc.install.mk
+
+plist: install
+include ../../mk/spksrc.plist.mk
+
 clean:
 	rm -fr work work-*
 
-all: compile
+all: install plist
 
 ### For make digests
 include ../../mk/spksrc.generate-digests.mk
@@ -75,8 +90,8 @@ kernel_configure_target:
 	@$(MSG) "Updating kernel Makefile"
 	$(RUN) sed -i -r 's,^CROSS_COMPILE\s*.+,CROSS_COMPILE\t= $(TC_PATH)$(TC_PREFIX),' Makefile
 	$(RUN) sed -i -r 's,^ARCH\s*.+,ARCH\t= $(KERNEL_ARCH),' Makefile
-# Add "+" to EXTRAVERSION for kernels version >= 4.x
-ifeq ($(shell expr "$(word 1,$(subst ., ,$(TC_KERNEL)))" \>= 4),1)
+# Add "+" to EXTRAVERSION for kernels version >= 4.4
+ifeq ($(call version_ge, ${TC_KERNEL}, 4.4),1)
 	$(RUN) sed -i -r -e 's,^EXTRAVERSION\s*.+,&+,' -e 's,=\+,= \+,' Makefile
 endif
 	test -e $(WORK_DIR)/arch/$(KERNEL_ARCH) || $(RUN) ln -sf $(KERNEL_BASE_ARCH) arch/$(KERNEL_ARCH)
@@ -85,9 +100,9 @@ endif
 	@$(MSG) "Applying $(KERNEL_CONFIG) configuration"
 	$(RUN) cp $(KERNEL_CONFIG) .config
 	@$(MSG) "Set any new symbols to their default value"
-# olddefconfig is not available <= 3.2
-ifeq ($(shell printf '%s\n' "$(TC_KERNEL)" "3.3" | sort -V | head -1),$(TC_KERNEL))
-	@$(MSG) "oldconfig OLD style... $(TC_KERNEL) <= 3.2"
+# olddefconfig is not available < 3.8
+ifeq ($(call version_lt, ${TC_KERNEL}, 3.8),1)
+	@$(MSG) "oldconfig OLD style... $(TC_KERNEL) < 3.8"
 	$(RUN) yes "" | $(MAKE) oldconfig
 else
 	$(RUN) $(MAKE) olddefconfig
@@ -99,7 +114,8 @@ kernel_module_prepare_target:
 	@$(MSG) "DISTRIB_DIR = $(DISTRIB_DIR)"
 	@$(MSG) "Prepare kernel source for module build"
 	$(RUN) $(MAKE) modules_prepare
-ifeq ($(shell expr "$(word 1,$(subst ., ,$(TC_KERNEL)))" \>= 3),1)
+# Call to make kernelversion is not available for kernel <= 3.0
+ifeq ($(call version_ge, ${TC_KERNEL}, 3),1)
 	@$(MSG) "Get kernel version"
 	$(RUN) $(MAKE) kernelversion
 endif
@@ -107,4 +123,14 @@ endif
 .PHONY: kernel_module_compile_target
 
 kernel_module_compile_target:
-	$(RUN) $(MAKE) modules
+	@for module in $(REQUIRE_KERNEL_MODULE); \
+	do \
+	  $(MAKE) kernel_module_build module=$$module ; \
+	done
+
+kernel_module_build:
+	@$(MSG) Building kernel module module=$(module)
+	$(RUN) LDFLAGS="" $(MAKE) -C $(WORK_DIR)/linux INSTALL_MOD_PATH=$(STAGING_INSTALL_PREFIX) modules M=$(word 2,$(subst :, ,$(module))) $(firstword $(subst :, ,$(module)))=m $(lastword $(subst :, ,$(module))).ko
+	$(RUN) cat $(word 2,$(subst :, ,$(module)))/modules.order >> $(WORK_DIR)/linux/modules.order
+	$(RUN) mkdir -p $(STAGING_INSTALL_PREFIX)/lib/modules/$(TC_KERNEL)/kernel/$(word 2,$(subst :, ,$(module)))
+	install -m 644 $(WORK_DIR)/linux/$(word 2,$(subst :, ,$(module)))/$(lastword $(subst :, ,$(module))).ko $(STAGING_INSTALL_PREFIX)/lib/modules/$(TC_KERNEL)/kernel/$(word 2,$(subst :, ,$(module)))
