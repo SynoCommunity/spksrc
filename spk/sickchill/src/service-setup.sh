95b285aec30a1addc9d87aad3db3d7b6aeff761f
miigotu <miigotu@gmail.com>
2021-04-02 13:15:18 -0400

    sickchill: fix upgrade and remove cross, set update_frequency to 24 hours
    Signed-off-by: miigotu <miigotu@gmail.com>

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -1,39 +1,45 @@
 PYTHON_DIR="/usr/local/python3"
-PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
-PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
-HOME="${SYNOPKG_PKGDEST}/var"
+GIT_DIR="/usr/local/git"
+
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${GIT_DIR}/bin:${PATH}"
+
 VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
+PIP="${SYNOPKG_PKGDEST}/env/bin/pip3"
+GIT="${GIT_DIR}/bin/git"
+
 SC_INSTALL_DIR="${SYNOPKG_PKGDEST}/share/SickChill"
 SC_BINARY="${SC_INSTALL_DIR}/SickChill.py"
 SC_DATA_DIR="${SYNOPKG_PKGDEST}/var/data"
 SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
 
-
 GROUP="sc-download"
 
-SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
+SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR} --port 8081"
 
 set_config() {
-    . ${SYNOPKG_PKGDEST}/share/git_data
-
     if [ -f "${SC_CFG_FILE}" ]; then
-        if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
+        if [ -n "${wizard_username}" ]; then
             sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
+        fi
+        if [ -n "${wizard_password}" ]; then
             sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
         fi
-        sed -i "/^\s*branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_BRANCH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_hash\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_HASH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_BRANCH}/" ${SC_CFG_FILE}
+
+        sed -i "/^branch/d" ${SC_CFG_FILE}
+        sed -i "/^cur_commit_hash/d" ${SC_CFG_FILE}
+        sed -i "/^cur_commit_branch/d" ${SC_CFG_FILE}
+
+        # Make sure we always update the port in their restored config
+        sed -i "s/\(web_port *= *\).*/\18081/" ${SC_CFG_FILE}
     else
         mkdir -p ${SC_DATA_DIR}
         cat << EOF > ${SC_CFG_FILE}
 [General]
 web_username = ${wizard_username}
 web_password = ${wizard_password}
-branch = ${SC_GIT_BRANCH}
-cur_commit_hash = ${SC_GIT_COMMIT_HASH}
-cur_commit_branch = ${SC_GIT_COMMIT_BRANCH}
+web_port = 8081
+update_frequency = 24
 EOF
     fi
 }
@@ -47,6 +53,8 @@ service_postinst() {
 
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         set_config
+
+        ${GIT} clone --depth 10 --recursive -q -b ${wizard_branch} git://github.com/SickChill/SickChill.git ${SC_INSTALL_DIR} > /dev/null 2>&1
     fi
 }
 
