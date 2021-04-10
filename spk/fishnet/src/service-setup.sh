15ab9804171595a50a32d8df46b9f6192f4bce5d
hgy59 <hpgy59@gmail.com>
2021-04-10 23:00:07 +0200

    update fishnet

--- spk/fishnet/src/service-setup.sh
+++ spk/fishnet/src/service-setup.sh
@@ -1,23 +1,25 @@
-INSTALL_DIR="/usr/local/${SYNOPKG_PKGNAME}"
-PYTHON_DIR="/usr/local/python3"
-VIRTUALENV="${PYTHON_DIR}/bin/python3 -m venv"
+
+PYTHON_DIR="/var/packages/python3/target/bin"
+VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
 
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG}
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index --upgrade --force-reinstall --find-links ${wheelhouse} ${wheelhouse}/*.whl
     
 
     # get target file independent of python3 version
-    target=$(${INSTALL_DIR}/env/bin/python -m site | grep -o "[^']*${SYNOPKG_PKGNAME}/env[^']*")/fishnet.py  >> ${INST_LOG}
+    target=$(${SYNOPKG_PKGDEST}/env/bin/python3 -m site | grep -o "[^']*${SYNOPKG_PKGNAME}/env[^']*")/fishnet.py
     
     # Fix shebang
-    sed -i -e "s|^#!.*$|#!${INSTALL_DIR}/env/bin/python3|g" ${target}  >> ${INST_LOG}
+    sed -i -e "s|^#!.*$|#!${SYNOPKG_PKGDEST}/env/bin/python3|g" ${target}
     
-    # make script executable and create a link to ${INSTALL_DIR}/bin/fishnet
-    chmod +x ${target}   >> ${INST_LOG}
-    ln -s ${target} ${INSTALL_DIR}/bin/fishnet   >> ${INST_LOG}
+    # make script executable and create a link to ${SYNOPKG_PKGDEST}/bin/fishnet
+    chmod +x ${target}
+    ln -s ${target} ${SYNOPKG_PKGDEST}/bin/fishnet
 }
+
