da75c625d46f41abf4840fd38bf792d0230b47c1
hgy59 <hpgy59@gmail.com>
2021-04-10 22:40:44 +0200

    update python3 and python38

--- spk/python3/src/service-setup.sh
+++ spk/python3/src/service-setup.sh
@@ -1,21 +1,27 @@
+
 PATH="${SYNOPKG_PKGDEST}/bin:$PATH"
 
 service_postinst ()
 {
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${SYNOPKG_PKGDEST}/bin/pip install --no-deps --force-reinstall --no-index --find-links ${wheelhouse} ${wheelhouse}/*.whl
 
     # Log installation informations
-    ${SYNOPKG_PKGDEST}/bin/python3 --version >> ${INST_LOG} 2>&1
-    echo -e "\nSystem installed modules:" >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/bin/pip freeze >> ${INST_LOG}
+    echo "Installed version: $( ${SYNOPKG_PKGDEST}/bin/python3 --version 2>&1 )"
+    
+    echo "Installed modules:"
+    ${SYNOPKG_PKGDEST}/bin/pip freeze
 
     # Install busybox stuff
     # So pure-Python spk's can use the functions
+    echo "Install busybox."
     ${SYNOPKG_PKGDEST}/bin/busybox --install ${SYNOPKG_PKGDEST}/bin
 
     # Byte-compile in background
+    echo "Start background module compilation."    
     PYTHON_SHORT_VER=$(${SYNOPKG_PKGDEST}/bin/python3 -c 'import sys; print("{0}.{1}".format(*sys.version_info[:2]))')
-    ${SYNOPKG_PKGDEST}/bin/python3 -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} >> ${INST_LOG} &
-    ${SYNOPKG_PKGDEST}/bin/python3 -OO -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} >> ${INST_LOG} &
+    ${SYNOPKG_PKGDEST}/bin/python3 -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} </dev/null &>/dev/null &
+    ${SYNOPKG_PKGDEST}/bin/python3 -OO -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} </dev/null &>/dev/null &
 }
+
--- spk/python38/src/service-setup.sh
+++ spk/python38/src/service-setup.sh
@@ -1,17 +1,21 @@
+
 PATH="${SYNOPKG_PKGDEST}/bin:$PATH"
 
 service_postinst ()
 {
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${SYNOPKG_PKGDEST}/bin/pip install --no-deps --force-reinstall --no-index --find-links ${wheelhouse} ${wheelhouse}/*.whl
 
     # Log installation informations
-    ${SYNOPKG_PKGDEST}/bin/python3 --version >> ${INST_LOG} 2>&1
-    echo -e "\nSystem installed modules:" >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/bin/pip freeze >> ${INST_LOG}
+    echo "Installed version: $( ${SYNOPKG_PKGDEST}/bin/python3 --version 2>&1 )"
+    
+    echo "Installed modules:"
+    ${SYNOPKG_PKGDEST}/bin/pip freeze
 
     # Byte-compile in background
     PYTHON_SHORT_VER=$(${SYNOPKG_PKGDEST}/bin/python3 -c 'import sys; print("{0}.{1}".format(*sys.version_info[:2]))')
-    ${SYNOPKG_PKGDEST}/bin/python3 -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} >> ${INST_LOG} &
-    ${SYNOPKG_PKGDEST}/bin/python3 -OO -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} >> ${INST_LOG} &
+    ${SYNOPKG_PKGDEST}/bin/python3 -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} </dev/null &>/dev/null &
+    ${SYNOPKG_PKGDEST}/bin/python3 -OO -m compileall -q -f ${SYNOPKG_PKGDEST}/lib/python${PYTHON_SHORT_VER} </dev/null &>/dev/null &
 }
+
