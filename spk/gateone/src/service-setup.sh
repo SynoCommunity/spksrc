04f62e1dda7c5e924ffece290afd527d9416e574
hgy59 <hpgy59@gmail.com>
2021-04-10 23:01:13 +0200

    update gateone
    - usevalidate_preupgrade

--- spk/gateone/src/service-setup.sh
+++ spk/gateone/src/service-setup.sh
@@ -1,7 +1,7 @@
-PYTHON_DIR="/usr/local/python"
-PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
+PYTHON_DIR="/var/packages/python/target/bin"
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}:${PATH}"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
-VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
+VIRTUALENV="${PYTHON_DIR}/virtualenv"
 GATEONE="${SYNOPKG_PKGDEST}/env/bin/gateone"
 SETTINGS_DIR="${SYNOPKG_PKGDEST}/var/conf.d"
 CONF_FILE="${SYNOPKG_PKGDEST}/var/conf.d/90custom.conf"
@@ -24,19 +24,20 @@ SVC_BACKGROUND=yes
 service_postinst ()
 {
     # Create a Python virtualenv
-    echo "Creaing Python virtualenv..." >> ${INST_LOG}
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG} 2>&1
+    echo "Creaing Python virtualenv..."
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
 
     # Install the wheels
-    echo "Installing wheels..." >> ${INST_LOG}
-    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    echo "Installing wheels..."
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index -U --force-reinstall -f ${wheelhouse} ${wheelhouse}/*.whl
 
     # Install GateOne
-    echo "Installing Gateone" >> ${INST_LOG}
-    ${PYTHON} ${SYNOPKG_PKGDEST}/share/gateone/setup.py install --prefix=${SYNOPKG_PKGDEST}/env --skip_init_scripts >> ${INST_LOG}
+    echo "Installing Gateone"
+    ${PYTHON} ${SYNOPKG_PKGDEST}/share/gateone/setup.py install --prefix=${SYNOPKG_PKGDEST}/env --skip_init_scripts
 
     # Install initial certificates
-    echo "Installing initial certificates..." >> ${INST_LOG}
+    echo "Installing initial certificates..."
     $CP "${CERTPATH}${CERTIFICATE}" "${KEYPATH}${KEYFILE}" ${SSL_DIR}
     sed -i -e "s,@certificate@,${SSL_DIR}${CERTIFICATE},g" ${CONF_FILE}
     sed -i -e "s,@keyfile@,${SSL_DIR}${KEYFILE},g" ${CONF_FILE}
@@ -48,15 +49,16 @@ service_postinst ()
     # Discard legacy obsolete busybox user account
     BIN=${SYNOPKG_PKGDEST}/bin
     $BIN/busybox --install $BIN
-    $BIN/delgroup "${USER}" "users" >> ${INST_LOG}
-    $BIN/deluser "${USER}" >> ${INST_LOG}
+    $BIN/delgroup "${USER}" "users"
+    $BIN/deluser "${USER}"
 }
 
-service_preupgrade ()
+validate_preupgrade ()
 {
     # Revision 5 introduces backward incompatible changes
-    if [ `echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/"` -le 4 ]; then
+    if [ $(echo ${SYNOPKG_OLD_PKGVER} | sed -r "s/^.*-([0-9]+)$/\1/") -le 4 ]; then
         echo "Please uninstall previous version, no update possible.<br>Remember to save your ${INSTALL_DIR}/var/server.conf file before uninstalling.<br>You will need to manually port old configuration settings to the new configuration files."
         exit 1
     fi
 }
+
