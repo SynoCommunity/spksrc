be62103476b0a10acfb1cb351cdf8535a06ecffb
hgy59 <hpgy59@gmail.com>
2021-04-10 22:42:20 +0200

    update synocli-monitor

--- spk/synocli-monitor/src/service-setup.sh
+++ spk/synocli-monitor/src/service-setup.sh
@@ -1,6 +1,7 @@
 
 service_postinst ()
 {
-    echo "Install busybox" >> "${INST_LOG}"
-    ${SYNOPKG_PKGDEST}/bin/busybox --install -s ${SYNOPKG_PKGDEST}/bin >> ${INST_LOG}
+    echo "Install busybox"
+    ${SYNOPKG_PKGDEST}/bin/busybox --install -s ${SYNOPKG_PKGDEST}/bin
 }
+
