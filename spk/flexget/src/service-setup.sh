9fa87b135e771714e61fe20436ba024c55d71fb7
hgy59 <hpgy59@gmail.com>
2021-04-21 22:18:39 +0200

    update flexget
    - update flexget to fix security issues

--- spk/flexget/src/service-setup.sh
+++ spk/flexget/src/service-setup.sh
@@ -2,13 +2,13 @@ PYTHON_DIR="/var/packages/python3/target/bin"
 VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
-CONFIG_FILE="${SYNOPKG_PKGDEST}/var/config.yml"
+CONFIG_FILE="${SYNOPKG_PKGVAR}/config.yml"
 
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/flexget -c ${CONFIG_FILE} --logfile ${LOG_FILE} daemon start"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
-SVC_CWD="${SYNOPKG_PKGDEST}/var/"
-HOME="${SYNOPKG_PKGDEST}/var/"
+SVC_CWD="${SYNOPKG_PKGVAR}/"
+HOME="${SYNOPKG_PKGVAR}/"
 
 service_postinst ()
 {
@@ -20,7 +20,7 @@ service_postinst ()
     ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index --force-reinstall --find-links ${wheelhouse} ${wheelhouse}/*.whl
 
     # Copying "config.yml" file to the "var/" folder
-    install -m 755 -d ${SYNOPKG_PKGDEST}/var
-    install -m 644 ${SYNOPKG_PKGDEST}/share/config.yml ${SYNOPKG_PKGDEST}/var
+    install -m 755 -d ${SYNOPKG_PKGVAR}
+    install -m 644 ${SYNOPKG_PKGDEST}/share/config.yml ${SYNOPKG_PKGVAR}
 }
 
