00f81626e4c572c2afad553c672a71b982af2650
hgy59 <hpgy59@gmail.com>
2021-04-22 18:09:53 +0200

    workaround to avoid double log entries

--- spk/flexget/src/service-setup.sh
+++ spk/flexget/src/service-setup.sh
@@ -4,7 +4,14 @@ PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
 CONFIG_FILE="${SYNOPKG_PKGVAR}/config.yml"
 
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/flexget -c ${CONFIG_FILE} --logfile ${LOG_FILE} daemon start"
+# flexget always writes the logfile flexget.log in the folder of the config file.
+# this is the same file as defined by the variable LOG_FILE.
+# if the parameter --logfile is not used or specifies the same logfile, then
+# all log file entries are doubled (seems to be an old bug in flexget daemon mode).
+# with "--logfile ${SYNOPKG_PKGVAR}/daemon.log", logs are written once to both files (flexget.log and daemon.log).
+# we could use "--logfile /dev/null" to avoid double log entries, but with this we might loose 
+# logs that are only written to the file specified with --logfile.
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/flexget -c ${CONFIG_FILE} --logfile ${SYNOPKG_PKGVAR}/daemon.log daemon start"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 SVC_CWD="${SYNOPKG_PKGVAR}/"
