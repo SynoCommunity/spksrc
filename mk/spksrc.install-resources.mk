a0c56922be79661a5c9452ddae2780a1ed8a164d
hgy59 <hpgy59@gmail.com>
2021-04-29 17:34:10 +0200

    generic solution for multifile digests (followup of #4566) (#4575)
    * generic solution for multifile digests (followup of #4566)
    - enable "make digests" for packages with multiple source files
    - use mk file for digests generation in spksrc.install-resources.mk
    - use spksrc.install-resources.mk for packages that do not compile sources
    * fix install-resources.mk for specific archs
    * incorporate code review
    - rename PKG_DIST_ARCHS to PKG_DIST_ARCH_LIST

--- mk/spksrc.install-resources.mk
+++ mk/spksrc.install-resources.mk
@@ -21,6 +21,12 @@ endif
 DIST_FILE     = $(DISTRIB_DIR)/$(LOCAL_FILE)
 DIST_EXT      = $(PKG_EXT)
 
+ifneq ($(ARCH),)
+ARCH_SUFFIX = -$(ARCH)-$(TCVERSION)
+TC = syno$(ARCH_SUFFIX)
+endif
+
+
 #####
 
 ifneq ($(REQUIRE_KERNEL),)
@@ -75,22 +81,8 @@ clean:
 
 all: install
 
-sha1sum := $(shell which sha1sum 2>/dev/null || which gsha1sum 2>/dev/null)
-sha256sum := $(shell which sha256sum 2>/dev/null || which gsha256sum 2>/dev/null)
-md5sum := $(shell which md5sum 2>/dev/null || which gmd5sum 2>/dev/null || which md5 2>/dev/null)
-
-.PHONY: $(DIGESTS_FILE)
-$(DIGESTS_FILE): download
-	@$(MSG) "Generating digests for $(PKG_NAME)"
-	@rm -f $@ && touch -f $@
-	@for type in SHA1 SHA256 MD5; do \
-	  case $$type in \
-	    SHA1)     tool=${sha1sum} ;; \
-	    SHA256)	  tool=${sha256sum} ;; \
-	    MD5)      tool=${md5sum} ;; \
-	  esac ; \
-	  echo "$(LOCAL_FILE) $$type `$$tool $(DIST_FILE) | cut -d\" \" -f1`" >> $@ ; \
-	done
+### For make digests
+include ../../mk/spksrc.generate-digests.mk
 
 ### For make dependency-tree
 include ../../mk/spksrc.dependency-tree.mk
