f7e7e226337452b4f93d7b24bbbdaa3f4b6f1c6d
hgy59 <hpgy59@gmail.com>
2021-05-01 21:30:20 +0200

    update duplicity to v0.8.19
    - update requirements
    - update instructions on how to update requirements.txt
    - update changelog and list all included backends as bullet list
    - adjust cross dependencies
    - fix service-setup.sh for redesigned installer logs

--- spk/duplicity/src/service-setup.sh
+++ spk/duplicity/src/service-setup.sh
@@ -8,12 +8,12 @@ PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >> ${INST_LOG}
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
 
     # Install the wheels
-    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --upgrade --no-index --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG}
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --upgrade --no-index --find-links ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
 
     # create link in bin folder
-    ln -sf ${SYNOPKG_PKGDEST}/env/bin/duplicity ${SYNOPKG_PKGDEST}/bin/duplicity >> ${INST_LOG}
+    ln -sf ${SYNOPKG_PKGDEST}/env/bin/duplicity ${SYNOPKG_PKGDEST}/bin/duplicity
 }
 
