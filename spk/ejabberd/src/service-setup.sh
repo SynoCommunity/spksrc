2dfa74f98dd1b71ea5ccd51b0207b2d6185e86ea
hgy59 <hpgy59@gmail.com>
2021-05-02 23:14:25 +0200

    adjust ejabberd for DSM7
    - use different privilege files for DSM 7 and DSM < 7
    - adjust installer logs (#4539)
    - link ejabberdctl to target/bin

--- spk/ejabberd/src/service-setup.sh
+++ spk/ejabberd/src/service-setup.sh
@@ -1,17 +1,19 @@
 
 # service ctl file
-EJABBERD_CTL="${SYNOPKG_PKGDEST}/sbin/ejabberdctl"
+EJABBERD_CTL="${SYNOPKG_PKGDEST}/bin/ejabberdctl"
+# HOME to place the erlang cookie into
+export HOME=${SYNOPKG_PKGDEST}
 
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        ${EJABBERD_CTL} start       >> ${INST_LOG} 2>&1
-        ${EJABBERD_CTL} started     >> ${INST_LOG} 2>&1
+        ${EJABBERD_CTL} start
+        ${EJABBERD_CTL} started
         
-        ${EJABBERD_CTL} register ${wizard_ejabberd_admin_username} ${wizard_ejabberd_hostname} ${wizard_ejabberd_admin_password}  >> ${INST_LOG} 2>&1
+        ${EJABBERD_CTL} register ${wizard_ejabberd_admin_username} ${wizard_ejabberd_hostname} ${wizard_ejabberd_admin_password}
         
-        ${EJABBERD_CTL} stop        >> ${INST_LOG} 2>&1
-        ${EJABBERD_CTL} stopped     >> ${INST_LOG} 2>&1
+        ${EJABBERD_CTL} stop
+        ${EJABBERD_CTL} stopped
     fi
 }
 
