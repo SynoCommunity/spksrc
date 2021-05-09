abb0b951e32feace07a6d09b8a19bb454bb067e5
hgy59 <hpgy59@gmail.com>
2021-05-09 09:52:34 +0200

    update mercurial to v5.7.1 and python 3.8 (#4591)

---
+++ spk/mercurial/src/service-setup.sh
@@ -0,0 +1,15 @@
+
+PYTHON_DIR="/var/packages/python38/target/bin"
+VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
+PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
+
+service_postinst ()
+{
+    # Create a Python virtualenv
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
+
+    # Install the wheels
+    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --upgrade --no-index --find-links ${wheelhouse} ${wheelhouse}/*.whl
+}
+
