cec18f418886ac93665d266ea7b8ed6ed403770d
hgy59 <hpgy59@gmail.com>
2021-05-25 06:55:24 +0200

    rabbitmq: update to v3.8.16

--- spk/rabbitmq/src/service-setup.sh
+++ spk/rabbitmq/src/service-setup.sh
@@ -1,5 +1,5 @@
 
-RABBITMQ_SBIN=${SYNOPKG_PKGDEST}/lib/rabbitmq_server-3.8.12/sbin
+RABBITMQ_SBIN=${SYNOPKG_PKGDEST}/lib/rabbitmq_server-3.8.16/sbin
 SERVICE_COMMAND="${RABBITMQ_SBIN}/rabbitmq-server"
 SVC_CWD="${SYNOPKG_PKGDEST}"
 SVC_BACKGROUND=y
