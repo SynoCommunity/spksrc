74301e330bc6a93a2a413f7b1db158188d52699c
fgma <fgma@users.noreply.github.com>
2021-06-15 23:18:13 +0200

    minio: update to latest release

--- spk/minio/src/service-setup.sh
+++ spk/minio/src/service-setup.sh
@@ -10,23 +10,17 @@ service_postinst ()
     echo "WIZARD_DATA_DIRECTORY=${wizard_data_directory}" >> ${INST_VARIABLES}
     echo "WIZARD_ACCESS_KEY=${wizard_access_key}" >> ${INST_VARIABLES}
     echo "WIZARD_SECRET_KEY=${wizard_secret_key}" >> ${INST_VARIABLES}
-
-    echo "Install busybox binaries"
-    BIN=${SYNOPKG_PKGDEST}/bin
-    $BIN/busybox --install $BIN
 }
 
-service_prestart ()
-{
-    if [ -f ${INST_VARIABLES} ]; then
-      . ${INST_VARIABLES}
-    fi
+if [ -f ${INST_VARIABLES} ]; then
+  . ${INST_VARIABLES}
+fi
 
-    SERVICE_OPTIONS="server --quiet --anonymous ${WIZARD_DATA_DIRECTORY}"
-    
-    export MINIO_ACCESS_KEY=$WIZARD_ACCESS_KEY
-    export MINIO_SECRET_KEY=$WIZARD_SECRET_KEY
+export MINIO_ROOT_USER=$WIZARD_ACCESS_KEY
+export MINIO_ROOT_PASSWORD=$WIZARD_SECRET_KEY
+export HOME=${SYNOPKG_PKGVAR}
 
-    # Required: start-stop-daemon do not set environment variables
-    export HOME=${SYNOPKG_PKGVAR}
-}
+MINIO="${SYNOPKG_PKGDEST}/bin/minio"
+SERVICE_COMMAND="${MINIO} server --quiet --anonymous ${WIZARD_DATA_DIRECTORY}"
+SVC_BACKGROUND=y
+SVC_WRITE_PID=y
