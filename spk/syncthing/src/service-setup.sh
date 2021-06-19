ed9c78d901938c3069a132c862ec9d19a3e9e996
hgy59 <hpgy59@gmail.com>
2021-06-19 19:19:14 +0200

    syncthing: update to v1.17.0 (#4685)

--- spk/syncthing/src/service-setup.sh
+++ spk/syncthing/src/service-setup.sh
@@ -1,3 +1,4 @@
+# syncthing service definition
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/syncthing -home=${SYNOPKG_PKGVAR}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
@@ -6,7 +7,7 @@ GROUP="sc-syncthing"
 
 service_prestart ()
 {
-    # Read additional startup options from /usr/local/syncthing/var/options.conf
+    # Read additional startup options from var/options.conf
     if [ -f ${SYNOPKG_PKGVAR}/options.conf ]; then
         . ${SYNOPKG_PKGVAR}/options.conf
         SERVICE_COMMAND="${SERVICE_COMMAND} ${SYNCTHING_OPTIONS}"
