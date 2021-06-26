7fab267fc948bf44740290bdd4084b0e8bead7d1
jaloji <83598824+jaloji@users.noreply.github.com>
2021-06-26 13:05:47 +0200

    Update clientbuffer.cpp
    Add information about source code origin in the header

--- cross/znc/modules/clientbuffer.cpp
+++ cross/znc/modules/clientbuffer.cpp
@@ -13,6 +13,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Information about this module can be found here: https://wiki.znc.in/Clientbuffer
+ * Source code of this module can be found here: https://github.com/CyberShadow/znc-clientbuffer
  */
 
 #include <znc/Modules.h>
