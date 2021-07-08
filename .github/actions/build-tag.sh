50e4fcb5fc5ea849d3fa337127e6a2ae48f0682f
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-07-08 15:01:55 +1000

    Add spaces for local.mk

--- .github/actions/build-tag.sh
+++ .github/actions/build-tag.sh
@@ -2,7 +2,7 @@
 
 make setup-synocommunity
 sed -i -e "s|#PARALLEL_MAKE=.*|PARALLEL_MAKE=4|" \
-    -e "s|PUBLISH_API_KEY=.*|PUBLISH_API_KEY=$API_KEY|" \
+    -e "s|PUBLISH_API_KEY =.*|PUBLISH_API_KEY = $API_KEY|" \
     local.mk
 
 # PACKAGE=$(echo "refs/tags/dnscrypt-proxy-2.0.42" | grep -oE "([0-9a-zA-Z]*-)*")
