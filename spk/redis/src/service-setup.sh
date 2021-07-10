885967a9b1dd05db04d07bcc7c978dd83dca53ac
hgy59 <hpgy59@gmail.com>
2021-07-10 23:43:22 +0200

    redis: update and DSM7 compatibility (#4727)
    - update redis to v5.0.12
    - adjust redis.conf for DSM7
    - update installer for DSM5 and DSM6 to create link for DSM7 var folder

--- spk/redis/src/service-setup.sh
+++ spk/redis/src/service-setup.sh
@@ -1,9 +1,9 @@
-CFG_FILE="${SYNOPKG_PKGDEST}/var/redis.conf"
-PATH="${SYNOPKG_PKGDEST}:${PATH}"
+
+# redis service setup
+CFG_FILE="${SYNOPKG_PKGVAR}/redis.conf"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/redis-server ${CFG_FILE}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
-
 
 service_postinst ()
 {
@@ -13,5 +13,4 @@ service_postinst ()
     # Set the maximum memory to use in configuration file
     sed -i -e "s/@maxmemory@/${MEMORY}mb/g" ${CFG_FILE}
 }
-
 
