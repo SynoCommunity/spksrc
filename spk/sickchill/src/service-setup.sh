66514973444ce4b766b511dbf7f5bb55f3cbaaeb
miigotu <miigotu@gmail.com>
2021-07-17 00:47:02 -0400

    Try to switch sickchill to pip install and work for dsm7
    Signed-off-by: miigotu <miigotu@gmail.com>
    
    Do not override arch, so that dependancies are cross compiled
    
    Signed-off-by: miigotu <miigotu@gmail.com>

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -1,53 +1,61 @@
-PYTHON_DIR="/usr/local/python3"
-PIP=${SYNOPKG_PKGDEST}/env/bin/pip3
-PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}/bin:${PATH}"
-HOME="${SYNOPKG_PKGDEST}/var"
-VIRTUALENV="${PYTHON_DIR}/bin/virtualenv"
-PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
-SC_INSTALL_DIR="${SYNOPKG_PKGDEST}/share/SickChill"
-SC_BINARY="${SC_INSTALL_DIR}/SickChill.py"
-SC_DATA_DIR="${SYNOPKG_PKGDEST}/var/data"
-SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
+PYTHON_BIN="/var/packages/python38/target/bin"
+VIRTUALENV="${PYTHON_BIN}/virtualenv"
 
+VENV="${SYNOPKG_PKGHOME}/.venv"
+PIP="${VENV}/bin/pip3"
+PYTHON="${VENV}/bin/python3"
+
+PATH="${VENV}/bin:${PATH}:${SYNOPKG_PKGDEST}/bin:${PYTHON_BIN}:${PATH}"
+
+SICKCHILL="${VENV}/bin/SickChill.py"
 
 GROUP="sc-download"
 
-SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
+PIDFILE="${SYNOPKG_PKGTMP}/${SYNOPKG_PKGNAME}.pid"
+
+SERVICE_COMMAND="LANG=en_US.UTF-8 ${PYTHON} ${SICKCHILL} --daemon --nolaunch --pidfile=${PIDFILE} --datadir=${SYNOPKG_PKGVAR} --port=${SYNOPKG_PKGPORT}"
+
+SVC_CWD="${SYNOPKG_PKGDEST}/"
+HOME="${SYNOPKG_PKGDEST}/"
+
 
 set_config() {
-    . ${SYNOPKG_PKGDEST}/share/git_data
+    CFG_FILE="${SYNOPKG_PKGVAR}/config.ini"
 
-    if [ -f "${SC_CFG_FILE}" ]; then
+    if [ -f "${CFG_FILE}" ]; then
         if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
-            sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
-            sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
+            sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${CFG_FILE}
+            sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${CFG_FILE}
         fi
-        sed -i "/^\s*branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_BRANCH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_hash\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_HASH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_branch\s*=/s/\s*=\s*.*/ = ${SC_CONFIG_GIT_COMMIT_BRANCH}/" ${SC_CFG_FILE}
     else
-        mkdir -p ${SC_DATA_DIR}
-        cat << EOF > ${SC_CFG_FILE}
+        mkdir -p ${SYNOPKG_PKGVAR}
+        cat << EOF > ${CFG_FILE}
 [General]
 web_username = ${wizard_username}
 web_password = ${wizard_password}
-branch = ${SC_GIT_BRANCH}
-cur_commit_hash = ${SC_GIT_COMMIT_HASH}
-cur_commit_branch = ${SC_GIT_COMMIT_BRANCH}
 EOF
     fi
 }
 
 service_postinst() {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env >>${INST_LOG}
+    ${VIRTUALENV} --system-site-packages ${VENV}
+
+    # If sickchill is not installed via a wheel, site-packages cannot be included due to shadowing.
+    # ${VIRTUALENV}  ${VENV}
 
     # Install the wheels
-    ${PIP} install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl >> ${INST_LOG} 2>&1
+    ${PIP} install --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
+
+    # If package inscludes all deps, need --no-deps here
+     ${PIP} install --no-deps --no-index -U --force-reinstall -f ${SYNOPKG_PKGDEST}/share/wheelhouse ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
 
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         set_config
     fi
+
+    set_unix_permissions "${SYNOPKG_PKGHOME}"
+    set_unix_permissions "${SYNOPKG_PKGVAR}"
 }
 
 service_postupgrade() {
