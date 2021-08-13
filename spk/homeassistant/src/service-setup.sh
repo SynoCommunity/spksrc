b8eb321b1311fbe2dcbba8d2a4f108cd360e086b
hgy59 <hpgy59@gmail.com>
2021-08-13 08:33:20 +0200

    homeassistant: update to v2021.8.6
    - update numpy
    - update homeassistant, home-assistant-frontend, hass-nabucasa and dependencies

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -1,5 +1,5 @@
 
-# no shebang, this file is included by following scripts
+# service setup: content is included by following scripts
 # - installer
 # - start-stop-status
 
@@ -21,29 +21,13 @@ service_postinst ()
     # ensure current pip (>= 20.3)
     # older versions with old dependency resolver will complain about double dependencies while
     # install is done with local *.whl files and requirements from the index.
-    ${SYNOPKG_PKGDEST}/env/bin/python3 -m pip install --upgrade pip
 
     echo "Install packages from wheels"
     wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
     ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --no-index ${wheelhouse}/*.whl
     
     echo "Install packages from index"
-    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --requirement ${SYNOPKG_PKGVAR}/postinst_requirements.txt
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-input --requirement ${SYNOPKG_PKGDEST}/share/postinst_requirements.txt
 
     mkdir -p "${CONFIG_DIR}"
 }
-
-service_preupgrade ()
-{
-    # migrate configuration
-    # move config folder from DSM<7 to DSM7 destination
-    OLD_CONFIG_DIR="${SYNOPKG_PKGDEST}/var/config"
-    if [ -d "${OLD_CONFIG_DIR}" ]; then
-        # on DSM<7 "${SYNOPKG_PKGDEST}/var" is the same folder as "${SYNOPKG_PKGVAR}"
-        if [ "$(realpath ${OLD_CONFIG_DIR})" != "$(realpath ${CONFIG_DIR})" ]; then
-            echo "Move configuration from ${OLD_CONFIG_DIR} to ${CONFIG_DIR}"
-            mv -f "${OLD_CONFIG_DIR}" "${SYNOPKG_PKGVAR}"
-        fi
-    fi
-}
-
