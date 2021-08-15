7a3e8ecb770597817c3895a0e08bbe1d389ef879
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-08-15 23:37:28 +1000

    Parallel cmake

--- mk/spksrc.cross-cmake.mk
+++ mk/spksrc.cross-cmake.mk
@@ -38,14 +38,14 @@ cmake_configure_target:
 	@$(MSG)    - Path BUILD_DIR = $(CMAKE_BUILD_DIR)
 	$(RUN) rm -rf CMakeCache.txt CMakeFiles
 	$(RUN) mkdir --parents $(CMAKE_BUILD_DIR)
-	cd $(CMAKE_BUILD_DIR) && env $(ENV) cmake $(CMAKE_ARGS) $(WORK_DIR)/$(PKG_DIR)
+	cd $(CMAKE_BUILD_DIR) && env $(ENV) cmake -j$(NCPUS) $(CMAKE_ARGS) $(WORK_DIR)/$(PKG_DIR)
 
 .PHONY: cmake_compile_target
 
 # default compile:
 cmake_compile_target:
 	@$(MSG) - CMake compile
-	cd $(CMAKE_BUILD_DIR) && env $(ENV) $(MAKE)
+	cd $(CMAKE_BUILD_DIR) && env $(ENV) $(MAKE) -j$(NCPUS)
 
 .PHONY: cmake_install_target
 
