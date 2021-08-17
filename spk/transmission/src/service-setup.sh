504d53be23792397aa02361000e506c8895984ed
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-08-17 11:10:55 +1000

    transmission: attempt to create folders

--- spk/transmission/src/service-setup.sh
+++ spk/transmission/src/service-setup.sh
@@ -9,16 +9,16 @@ GROUP="sc-download"
 
 SERVICE_COMMAND="${TRANSMISSION} -g ${SYNOPKG_PKGVAR} -x ${PID_FILE} -e ${LOG_FILE}"
 
-validate_preinst ()
+validate_preinst()
 {
-    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ] && [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
         # If chosen, they need to exist
         if [ -n "${wizard_watch_dir}" ] && [ ! -d "${wizard_watch_dir}" ]; then
-            echo "Watch directory ${wizard_watch_dir} does not exist."
+            echo "Watch directory ${wizard_watch_dir} does not exist. And the package could not create it."
             exit 1
         fi
         if [ -n "${wizard_incomplete_dir}" ] && [ ! -d "${wizard_incomplete_dir}" ]; then
-            echo "Incomplete directory ${wizard_incomplete_dir} does not exist."
+            echo "Incomplete directory ${wizard_incomplete_dir} does not exist. And the package could not create it."
             exit 1
         fi
     fi
@@ -29,6 +29,12 @@ validate_preinst ()
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        # Attempt to create the folders after the user/group/shared folder has been created by the package (DSM7+)
+        if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
+            mkdir -p "${wizard_watch_dir}"
+            mkdir -p "${wizard_incomplete_dir}"
+        fi
+
         # Edit the configuration according to the wizard
         sed -i -e "s|@download_dir@|${wizard_volume:=/volume1}/${wizard_download_dir:=/downloads}|g" ${CFG_FILE}
         sed -i -e "s|@username@|${wizard_username:=admin}|g" ${CFG_FILE}
@@ -56,6 +62,26 @@ service_postinst ()
             set_syno_permissions "${wizard_incomplete_dir}" "${GROUP}"
         fi
     fi
+}
+
+validate_postinst()
+{
+    # Need to make sure the user/group has been created by the package
+    # before checking that the folder exists (chicken egg problem)
+    # https://github.com/SynoCommunity/spksrc/issues/4766#issuecomment-899535272
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        # If chosen, they need to exist
+        if [ -n "${wizard_watch_dir}" ] && [ ! -d "${wizard_watch_dir}" ]; then
+            echo "<p style=\"color:red\">Watch directory ${wizard_watch_dir} does not exist. And the package could not create it. <br> Please uninstall the package, check permissions and try again.</p>"
+            exit 1
+        fi
+        if [ -n "${wizard_incomplete_dir}" ] && [ ! -d "${wizard_incomplete_dir}" ]; then
+            echo "<p style=\"color:red\">Incomplete directory ${wizard_incomplete_dir} does not exist. And the package could not create it. <br> Please uninstall the package, check permissions and try again.</p>"
+            exit 1
+        fi
+    fi
+
+    exit 0
 }
 
 
