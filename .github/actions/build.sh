fb14aa6899e17b9fb08271450b382b549562c0ce
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-08-26 20:39:19 +1000

    CI: fix parallel builds. Add manual builds print more details on error

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 # github action to build the packages depending on changed files
-# 
+#
 # Functions:
 # - Build all packages depending on files defined in ${GH_FILES}.
 # - Build for arch defined by ${GH_ARCH} (e.g. x64-6.1, noarch, ...).
 # - Successfully built packages are logged to $BUILD_SUCCESS_FILE.
 # - Failed builds are logged to ${BUILD_ERROR_FILE} and annotated as error.
 # - The build output is structured into log groups by package.
-# - As the disk space in the workflow environment is limitted, we clean the 
+# - As the disk space in the workflow environment is limitted, we clean the
 #   work folder of each package after build. At 2020.06 this limit is 14GB.
 # - ffmpeg is not cleaned to be available for dependents.
 # - Therefore ffmpeg is built first if triggered by its own or a dependent.
 
 echo "::group:: ---- initialize build"
 make setup-synocommunity
-sed -i -e "s|#PARALLEL_MAKE=.*|PARALLEL_MAKE=4|" local.mk
+sed -i -e "s|#PARALLEL_MAKE\s*=.*|PARALLEL_MAKE=max|" local.mk
 echo "::endgroup::"
 
 echo "::group:: ---- find dependent packages"
 
 # filter for changes made in the spk directories and take unique package name (without spk folder)
-SPK_TO_BUILD=$(echo "${GH_FILES}" | tr ' ' '\n' | grep -oP "(spk)/\K[^\/]*" | sort -u | tr '\n' ' ')
+SPK_TO_BUILD+=$(echo "${GH_FILES}" | tr ' ' '\n' | grep -oP "(spk)/\K[^\/]*" | sort -u | tr '\n' ' ')
 
 # filter for changes made in the cross and native directories and take unique package name (including cross or native folder)
 DEPENDENT_PACKAGES=$(echo "${GH_FILES}" | tr ' ' '\n' | grep -oP "(cross|native)/[^\/]*" | sort -u | tr '\n' ' ')
@@ -59,7 +59,7 @@ packages=$(printf %s "${SPK_TO_BUILD}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
 # find all packages that depend on spk/ffmpeg is built before.
 all_ffmpeg_packages=$(find spk/ -maxdepth 2 -mindepth 2 -name "Makefile" -exec grep -Ho "export FFMPEG_DIR" {} \; | grep -Po ".*spk/\K[^/]*" | sort | tr '\n' ' ')
 
-# if ffmpeg or one of its dependents is to build, ensure 
+# if ffmpeg or one of its dependents is to build, ensure
 # ffmpeg is first package in the list of packages to build.
 for package in ${packages}
 do
@@ -74,7 +74,7 @@ done
 # find all noarch packages
 all_noarch=$(find spk/ -maxdepth 2 -mindepth 2 -name "Makefile" -exec grep -Ho "override ARCH" {} \; | grep -Po ".*spk/\K[^/]*" | sort | tr '\n' ' ')
 
-# separate noarch and arch specific packages 
+# separate noarch and arch specific packages
 # and filter out packages that are removed or do not exist (e.g. nzbdrone)
 arch_packages=
 noarch_packages=
@@ -117,14 +117,16 @@ do
 
     if [ "${GH_ARCH%%-*}" != "noarch" ]; then
         # use TCVERSION and ARCH to get real exit codes.
-        make TCVERSION=${GH_ARCH##*-} ARCH=${GH_ARCH%%-*} -C ./spk/${package}
+        echo "make TCVERSION=${GH_ARCH##*-} ARCH=${GH_ARCH%%-*} -C ./spk/${package}" >>build.log
+        make TCVERSION=${GH_ARCH##*-} ARCH=${GH_ARCH%%-*} -C ./spk/${package} |& tee >(tail -10 >>build.log)
     else
         if [ "${GH_ARCH}" = "noarch" ]; then
             TCVERSION=
         else
             TCVERSION=${GH_ARCH##*-}
         fi
-        make TCVERSION=${TCVERSION} ARCH= -C ./spk/${package}
+        echo "make TCVERSION=${TCVERSION} ARCH= -C ./spk/${package}" >>build.log
+        make TCVERSION=${TCVERSION} ARCH= -C ./spk/${package} |& tee >(tail -10 >>build.log)
     fi
     result=$?
 
@@ -132,6 +134,8 @@ do
     then
         echo "$(date --date=now +"%Y.%m.%d %H:%M:%S") - ${package}: (${GH_ARCH}) DONE"   >> ${BUILD_SUCCESS_FILE}
     else
+        cat build.log >> ${BUILD_ERROR_LOGFILE}
+        printf "\n\n" >build.log
         echo "$(date --date=now +"%Y.%m.%d %H:%M:%S") - ${package}: (${GH_ARCH}) FAILED" >> ${BUILD_ERROR_FILE}
     fi
 
