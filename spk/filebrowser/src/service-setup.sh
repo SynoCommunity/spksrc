fd337187a1dc8a8e227a72b57a63321fb004db08
Sebastian Schmidt <publicarray@users.noreply.github.com>
2021-08-27 13:20:48 +1000

    Fix xec: "getent": executable file not found in $PATH
    by setting HOME variable

--- spk/filebrowser/src/service-setup.sh
+++ spk/filebrowser/src/service-setup.sh
@@ -1,3 +1,8 @@
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/filebrowser -a 0.0.0.0 -p ${SERVICE_PORT} --noauth -r /"
+# https://help.synology.com/developer-guide/integrate_dsm/fhs.html
+if [ -z "$SYNOPKG_PKGHOME" ]; then
+    SYNOPKG_PKGHOME=$SYNOPKG_PKGVAR
+fi
+export HOME=${SYNOPKG_PKGHOME}
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/filebrowser -a 0.0.0.0 -p ${SERVICE_PORT} --noauth -r / -l ${LOG_FILE} -d ${SYNOPKG_PKGHOME}/filebrowser.db"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
