38f53cd4e455f7ad24675f9862c3914f34f2f79f
hgy59 <hpgy59@gmail.com>
2021-08-28 13:11:23 +0200

    make service setup independent of SPK_VERS

--- spk/rabbitmq/src/service-setup.sh
+++ spk/rabbitmq/src/service-setup.sh
@@ -1,5 +1,8 @@
 
-RABBITMQ_SBIN=${SYNOPKG_PKGDEST}/lib/rabbitmq_server-3.8.16/sbin
+# evaluate version dependent path
+PATTERN=( ${SYNOPKG_PKGDEST}/lib/rabbitmq_server-*/sbin )
+RABBITMQ_SBIN=${PATTERN[0]}
+
 SERVICE_COMMAND="${RABBITMQ_SBIN}/rabbitmq-server"
 SVC_CWD="${SYNOPKG_PKGDEST}"
 SVC_BACKGROUND=y
@@ -10,6 +13,7 @@ export HOME=${SYNOPKG_PKGDEST}
 
 service_postinst ()
 {
+    echo "Set SYS_PREFIX=${SYNOPKG_PKGDEST} in ${RABBITMQ_SBIN}/rabbitmq-defaults"
     sed -i "s%SYS_PREFIX=%SYS_PREFIX=${SYNOPKG_PKGDEST}%g" ${RABBITMQ_SBIN}/rabbitmq-defaults
 }
 
