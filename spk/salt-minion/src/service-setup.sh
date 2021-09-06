5478f65f7791c6bd03b7017670d09ce759cf5065
Bjarne Saltbaek <arnebjarne72@hotmail.com>
2021-09-06 21:32:11 +0200

    Moved config to permanent config storage

--- spk/salt-minion/src/service-setup.sh
+++ spk/salt-minion/src/service-setup.sh
@@ -1,33 +1,33 @@
-PYTHON_DIR="/bin"
-PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
-VIRTUALENV="${PYTHON_DIR}/python -m venv"
-PYTHON="/bin/python3"
+PYTHON_DIR="/var/packages/python38/target/bin"
+PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}:${PATH}"
+VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
+PYTHON="${PYTHON_DIR}/python3"
 LANGUAGE="env LANG=en_US.UTF-8"
-SALT_MINION="${SYNOPKG_PKGDEST}/bin/salt-minion"
-PID_FILE="${SYNOPKG_PKGDEST}/var/run/salt-minion.pid"
+SALT_MINION="${SYNOPKG_PKGDEST}/env/bin/salt-minion"
+PID_FILE="${SYNOPKG_PKGDEST}/env/var/run/salt-minion.pid"
 
-SERVICE_COMMAND="${SALT_MINION} -c ${SYNOPKG_PKGDEST}/etc/salt -d"
+SERVICE_COMMAND="${SALT_MINION} -c ${SYNOPKG_PKGDEST}/env/etc/salt -d"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}
+    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
 
     # Install the wheels
     wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
-    ${SYNOPKG_PKGDEST}/bin/pip install --no-deps --no-index -U --force-reinstall -f ${wheelhouse} ${wheelhouse}/*.whl
+    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --no-index --upgrade --force-reinstall -f ${wheelhouse} ${wheelhouse}/*.whl
 
     # Patch rsax931.py file to find libcrypto lib
     # (Rely on patch util bundled with python3's busybox)
-    ${SYNOPKG_PKGDEST}/bin/patch ${SYNOPKG_PKGDEST}/lib/python*/site-packages/salt/utils/rsax931.py -i ${SYNOPKG_PKGDEST}/share/rsax931.py.patch
+    ${SYNOPKG_PKGDEST}/bin/patch ${SYNOPKG_PKGDEST}/env/lib/python3.8/site-packages/salt/utils/rsax931.py -i ${SYNOPKG_PKGDEST}/share/rsax931.py.patch
 
     # Prepare salt-minion config in /var/packages/salt-minion/
-    install -m 755 -d ${SYNOPKG_PKGDEST}/etc/salt/minion.d
-    install -m 644 ${SYNOPKG_PKGDEST}/share/minion.conf ${SYNOPKG_PKGDEST}/etc/salt/minion
-    echo "pidfile: ${PID_FILE}" > ${SYNOPKG_PKGDEST}/etc/salt/minion.d/02_pidfile.conf
-    echo "root_dir: ${SYNOPKG_PKGDEST}" > ${SYNOPKG_PKGDEST}/etc/salt/minion.d/01_rootdir.conf
+    ln -s /var/packages/${SYNOPKG_PKGNAME}/etc ${SYNOPKG_PKGDEST}/env/etc
+    test -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d || install -m 755 -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion || install -m 644 ${SYNOPKG_PKGDEST}/share/minion.conf ${SYNOPKG_PKGDEST}/env/etc/salt/minion
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/02_pidfile.conf || echo "pidfile: ${PID_FILE}" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/02_pidfile.conf
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/01_rootdir.conf || echo "root_dir: ${SYNOPKG_PKGDEST}/env" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/01_rootdir.conf
     # Populate salt master address and minion_id only if file don't already exist
-    test -f ${SYNOPKG_PKGDEST}/etc/salt/minion.d/99-master-address.conf || echo "master: salt" > ${SYNOPKG_PKGDEST}/etc/salt/minion.d/99-master-address.conf
-    test -f ${SYNOPKG_PKGDEST}/etc/salt/minion.d/98-minion-id.conf || echo "id: myname" > ${SYNOPKG_PKGDEST}/etc/salt/minion.d/98-minion-id.conf
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/99-master-address.conf || echo "master: salt" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/99-master-address.conf
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/98-minion-id.conf || echo -n "id: " > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/98-minion-id.conf && hostname -s >> ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/98-minion-id.conf
 }
-
