dfde9c382d41e3ace3e210981de59b17f448f875
fgma <fgma@users.noreply.github.com>
2021-09-11 12:27:50 +0200

    minio: update wizard

--- spk/minio/src/service-setup.sh
+++ spk/minio/src/service-setup.sh
@@ -7,20 +7,21 @@ INST_VARIABLES="${INST_ETC}/installer-variables"
 
 service_postinst ()
 {
+    echo "WIZARD_DATA_VOLUME=${wizard_data_volume}" >> ${INST_VARIABLES}
     echo "WIZARD_DATA_DIRECTORY=${wizard_data_directory}" >> ${INST_VARIABLES}
-    echo "WIZARD_ACCESS_KEY=${wizard_access_key}" >> ${INST_VARIABLES}
-    echo "WIZARD_SECRET_KEY=${wizard_secret_key}" >> ${INST_VARIABLES}
+    echo "WIZARD_ROOT_USER=${wizard_root_user}" >> ${INST_VARIABLES}
+    echo "WIZARD_ROOT_PASSWORD=${wizard_root_password}" >> ${INST_VARIABLES}
 }
 
 if [ -f ${INST_VARIABLES} ]; then
   . ${INST_VARIABLES}
 fi
 
-export MINIO_ROOT_USER=$WIZARD_ACCESS_KEY
-export MINIO_ROOT_PASSWORD=$WIZARD_SECRET_KEY
+export MINIO_ROOT_USER=$WIZARD_ROOT_USER
+export MINIO_ROOT_PASSWORD=$WIZARD_ROOT_PASSWORD
 export HOME=${SYNOPKG_PKGVAR}
 
 MINIO="${SYNOPKG_PKGDEST}/bin/minio"
-SERVICE_COMMAND="${MINIO} server --quiet --anonymous ${WIZARD_DATA_DIRECTORY}"
+SERVICE_COMMAND="${MINIO} server --quiet --anonymous ${WIZARD_DATA_VOLUME}/${WIZARD_DATA_DIRECTORY}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
--- spk/minio/src/wizard/upgrade_uifile.sh
+++ spk/minio/src/wizard/upgrade_uifile.sh
@@ -13,6 +13,108 @@ cat <<EOF > $SYNOPKG_TEMP_LOGFILE
 	{
 		"step_title": "MinIO configuration",
 		"items": [
+			{
+                "type": "combobox",
+                "desc": "Please select a volume to use for the data folder",
+                "subitems": [
+                    {
+                        "key": "wizard_data_volume",
+                        "desc": "volume name",
+						"defaultValue": "${WIZARD_DATA_VOLUME}",
+                        "displayField": "display_name",
+                        "valueField": "volume_path",
+                        "editable": false,
+                        "mode": "remote",
+                        "api_store": {
+                            "api": "SYNO.Core.Storage.Volume",
+                            "method": "list",
+                            "version": 1,
+                            "baseParams": {
+                                "limit": -1,
+                                "offset": 0,
+                                "location": "internal"
+                            },
+                            "root": "volumes",
+                            "idProperty": "volume_path",
+                            "fields": [
+                                "display_name",
+                                "volume_path"
+                            ]
+                        },
+                        "validator": {
+                            "fn": "{console.log(arguments);return true;}"
+                        }
+                    }
+                ]
+            },
+			{
+                "type": "textfield",
+                "desc": "data shared folder (using the volume chosen above)",
+                "subitems": [
+                    {
+                        "key": "wizard_data_directory",
+                        "desc": "data shared folder",
+						"defaultValue": "${WIZARD_DATA_DIRECTORY}",
+                        "validator": {
+                            "allowBlank": false,
+                            "regex": {
+                                "expr": "/^[\\w _-]+$/",
+                                "errorText": "Subdirectories are not supported."
+                            }
+                        }
+                    }
+                ]
+            },
+			{
+				"type": "textfield",
+				"subitems": [
+					{
+						"key": "wizard_root_user",
+						"desc": "MinIO root user",
+						"defaultValue": "${WIZARD_ROOT_USER}",
+						"validator": {
+							"allowBlank": false,
+							"minLength": 3,
+							"regex": {
+								"expr": "/^[^<>:*/?\"|]*$/",
+								"errorText": "Not allowed character in username"
+							}
+						}
+					},
+					{
+						"key": "wizard_root_password",
+						"desc": "MinIO root password",
+						"defaultValue": "${WIZARD_ROOT_PASSWORD}",
+						"validator": {
+							"allowBlank": false,
+							"minLength": 8,
+							"regex": {
+								"expr": "/^[^\"|]*$/",
+								"errorText": "Not allowed character in password"
+							}
+						}
+					}
+				]
+			}
+		]
+	},
+	{
+        "step_title": "DSM Permissions",
+        "items": [
+            {
+                "desc": "Please read <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> for details."
+            }
+        ]
+    }
+]
+
+
+
+
+[
+	{
+		"step_title": "MinIO configuration",
+		"items": [
 		  {
 			"type": "textfield",
 			"subitems": [
