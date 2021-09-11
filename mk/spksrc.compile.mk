943ecf5e080eaf85f3b2808ff42ae53d31306e80
Vincent Fortier <th0ma7@gmail.com>
2021-09-11 12:54:47 +0000

    compile.mk: Fix warning: -j2 forced in makefile

--- mk/spksrc.compile.mk
+++ mk/spksrc.compile.mk
@@ -6,15 +6,6 @@
 #  compile_target       (override with COMPILE_TARGET)
 #  post_compile_target  (override with POST_COMPILE_TARGET)
 
-# Set parallel options in caller
-ifneq ($(PARALLEL_MAKE),nop)
-ifeq ($(call version_ge, ${MAKELEVEL}, 2),1)
-ifneq ($(filter -j%,$(MAKEFLAGS)),-j$(NCPUS))
-MAKEFLAGS += -j$(NCPUS)
-endif
-endif
-endif
-
 COMPILE_COOKIE = $(WORK_DIR)/.$(COOKIE_PREFIX)compile_done
 
 ifeq ($(strip $(PRE_COMPILE_TARGET)),)
@@ -45,7 +36,7 @@ endif
 pre_compile_target: compile_msg
 
 compile_target:  $(PRE_COMPILE_TARGET)
-	@$(RUN) $(PSTAT_TIME) $(MAKE) $(COMPILE_MAKE_OPTIONS)
+	@$(RUN) $(PSTAT_TIME) $(MAKE) -j$(NCPUS) $(COMPILE_MAKE_OPTIONS)
 
 post_compile_target: $(COMPILE_TARGET)
 
