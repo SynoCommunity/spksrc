8f10f47cc284cb124133441c2dc8baed598ba235
Vincent Fortier <th0ma7@gmail.com>
2021-09-14 01:27:26 +0000

    copy.mk: Fix conditional copying

--- mk/spksrc.copy.mk
+++ mk/spksrc.copy.mk
@@ -47,10 +47,9 @@ copy_target: $(PRE_COPY_TARGET) $(INSTALL_PLIST)
 	(cd $(INSTALL_DIR)/$(INSTALL_PREFIX) && tar cpf - `cat $(INSTALL_PLIST) | cut -d':' -f2`) | \
 	  tar xpf - -C $(STAGING_DIR)
 	# Copy non-target to STAGING_SPK* if directory exists
-	@if [ -d $(INSTALL_DIR)/$(INSTALL_PREFIX_VAR) ] ;
-	then \
+	@if [ -d $(INSTALL_DIR)/$(INSTALL_PREFIX_VAR) ] ; then \
 	  (cd $(INSTALL_DIR)/$(INSTALL_PREFIX_VAR) && find . -mindepth 1 -maxdepth 1 | tar cpf - --files-from=/dev/stdin) | \
-	    tar xpf - -C $(STAGING_SPKVAR) ;
+	    tar xpf - -C $(STAGING_SPKVAR) ; \
 	fi
 
 post_copy_target: $(COPY_TARGET)
