07b4e668a7a1c6e693d2dc4d17d28b02e4ae0f46
Vincent Fortier <th0ma7@gmail.com>
2021-09-16 23:32:49 +0000

    common.mk: diyspk folder must be handled like spk (thnx @hgy59)

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -95,7 +95,7 @@ endif
 
 # Always send PSTAT output to proper log file
 # independantly from active Makefile location
-ifeq ($(filter cross spk,$(shell basename $(dir $(abspath $(dir $$PWD))))),)
+ifeq ($(filter cross diyspk spk,$(shell basename $(dir $(abspath $(dir $$PWD))))),)
 PSTAT_LOG = $(shell pwdx $$(ps -o ppid= $$(echo $$PPID)) | cut -f2 -d:)/build.stats.log
 else ifneq ($(wildcard $(WORK_DIR)),)
 PSTAT_LOG = $(WORK_DIR)/../build.stats.log
