7d99bff4c0d4995d9399d2cd43ac6fad46dc47c7
Bjarne Saltbaek <arnebjarne72@hotmail.com>
2021-09-21 11:33:51 +0200

    Update spk/salt-minion/src/service-setup.sh
    Ditto previous commit.
    
    Co-authored-by: Sebastian Schmidt <publicarray@users.noreply.github.com>

--- spk/salt-minion/src/service-setup.sh
+++ spk/salt-minion/src/service-setup.sh
@@ -19,9 +19,9 @@ service_postinst ()
 
     # Prepare salt-minion config in /var/packages/salt-minion/
     test -L ${SYNOPKG_PKGDEST}/env/etc || ln -s /var/packages/${SYNOPKG_PKGNAME}/etc ${SYNOPKG_PKGDEST}/env/etc
-    test -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d || install -m 755 -o sc-${SYNOPKG_PKGNAME} -g ${SYNOPKG_PKGNAME} -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d
-    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion || install -m 644 -o sc-${SYNOPKG_PKGNAME} -g ${SYNOPKG_PKGNAME} ${SYNOPKG_PKGDEST}/share/minion ${SYNOPKG_PKGDEST}/env/etc/salt/minion
-    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/proxy || install -m 644 -o sc-${SYNOPKG_PKGNAME} -g ${SYNOPKG_PKGNAME} ${SYNOPKG_PKGDEST}/share/proxy ${SYNOPKG_PKGDEST}/env/etc/salt/proxy
+    test -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d || install -m 755 -d ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion || install -m 644 ${SYNOPKG_PKGDEST}/share/minion ${SYNOPKG_PKGDEST}/env/etc/salt/minion
+    test -f ${SYNOPKG_PKGDEST}/env/etc/salt/proxy || install -m 644 ${SYNOPKG_PKGDEST}/share/proxy ${SYNOPKG_PKGDEST}/env/etc/salt/proxy
     test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/02_pidfile.conf || echo "pidfile: ${PID_FILE}" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/02_pidfile.conf
     test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/01_rootdir.conf || echo "root_dir: ${SYNOPKG_PKGDEST}/env" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/01_rootdir.conf
     test -f ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/03_logging.conf || echo "log_file: udp://localhost:10514" > ${SYNOPKG_PKGDEST}/env/etc/salt/minion.d/03_logging.conf
