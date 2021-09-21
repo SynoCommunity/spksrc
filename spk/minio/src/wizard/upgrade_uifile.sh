4170fce0eefe1e29bd256be11cbd722a80da2a97
fgma <30936930+fgma@users.noreply.github.com>
2021-09-21 20:45:59 +0200

    Update minio to 2021-09-09T21-37-07Z (#4683)
    * minio: fix SPK_VERS for DSM7
    * minor fixes
    - adjust license
    - derive tag from PKG_VERS
    - increment SPK_REV
    * Fix spk/minio/src/wizard/upgrade_uifile.sh
    * minio: updated to 2021-09-18T18-09-59Z
    * minio: update wizard to select volume from combobox
    * minio: upgrade to latest release and change verion number scheme
    * minio: fix PKG_VERS
    * minio: add license file to package
    Co-authored-by: fgma <fgma@users.noreply.github.com>
    Co-authored-by: hgy59 <hpgy59@gmail.com>

--- spk/minio/src/wizard/upgrade_uifile.sh
+++ spk/minio/src/wizard/upgrade_uifile.sh
@@ -13,57 +13,104 @@ cat <<EOF > $SYNOPKG_TEMP_LOGFILE
 	{
 		"step_title": "MinIO configuration",
 		"items": [
-		  {
-			"type": "textfield",
-			"subitems": [
-			  {
-				"key": "wizard_data_directory",
-				"desc": "Data directory location",
-				"defaultValue": "${WIZARD_DATA_DIRECTORY}",
-				"validator": {
-				  "allowBlank": false,
-				  "regex": {
-					"expr": "/^\\\/volume[0-9]{1,2}\\\/[^<>: */?\"]*/",
-					"errorText": "Path should begin with /volume?/ where ? is volume number (1-99)"
-				  }
-				}
-			  },
-			  {
-				"key": "wizard_access_key",
-				"desc": "MinIO access key",
-				"defaultValue": "${WIZARD_ACCESS_KEY}",
-				"validator": {
-				  "allowBlank": false,
-				  "minLength": 3,
-				  "regex": {
-					"expr": "/^[^<>:*/?\"|]*$/",
-					"errorText": "Not allowed character in key"
-				  }
-				}
-			  },
-			  {
-				"key": "wizard_secret_key",
-				"desc": "MinIO secret key",
-				"defaultValue": "${WIZARD_SECRET_KEY}",
-				"validator": {
-				  "allowBlank": false,
-				  "minLength": 8,
-				  "regex": {
-					"expr": "/^[^\"|]*$/",
-					"errorText": "Not allowed character in key"
-				  }
-				}
-			  }
-			]
-		  }
+			{
+                "type": "combobox",
+                "desc": "Please select a volume to use for the data folder",
+                "subitems": [
+                    {
+                        "key": "wizard_data_volume",
+                        "desc": "Volume name",
+						"defaultValue": "${WIZARD_DATA_VOLUME}",
+                        "displayField": "display_name",
+                        "valueField": "volume_path",
+                        "editable": false,
+                        "mode": "remote",
+                        "api_store": {
+                            "api": "SYNO.Core.Storage.Volume",
+                            "method": "list",
+                            "version": 1,
+                            "baseParams": {
+                                "limit": -1,
+                                "offset": 0,
+                                "location": "internal"
+                            },
+                            "root": "volumes",
+                            "idProperty": "volume_path",
+                            "fields": [
+                                "display_name",
+                                "volume_path"
+                            ]
+                        },
+                        "validator": {
+                            "fn": "{console.log(arguments);return true;}"
+                        }
+                    }
+                ]
+            },
+			{
+                "type": "textfield",
+                "desc": "Data shared folder (using the volume chosen above)",
+                "subitems": [
+                    {
+                        "key": "wizard_data_directory",
+                        "desc": "Data shared folder",
+                        "defaultValue": "${WIZARD_DATA_DIRECTORY}",
+                        "validator": {
+                            "allowBlank": false,
+                            "regex": {
+                                "expr": "/^[\\\\w _-]+$/",
+                                "errorText": "Subdirectories are not supported."
+                            }
+                        }
+                    }
+                ]
+            },
+			{
+				"type": "textfield",
+				"subitems": [
+					{
+						"key": "wizard_root_user",
+						"desc": "MinIO root user",
+						"defaultValue": "${WIZARD_ROOT_USER}",
+						"validator": {
+							"allowBlank": false,
+							"minLength": 3,
+							"regex": {
+								"expr": "/^[^<>:*/?\"|]*$/",
+								"errorText": "Not allowed character in username"
+							}
+						}
+					}
+                ]
+            },
+            {
+				"type": "textfield",
+				"subitems": [
+					{
+						"key": "wizard_root_password",
+						"desc": "MinIO root password",
+						"defaultValue": "${WIZARD_ROOT_PASSWORD}",
+						"validator": {
+							"allowBlank": false,
+							"minLength": 8,
+							"regex": {
+								"expr": "/^[^\"|]*$/",
+								"errorText": "Not allowed character in password"
+							}
+						}
+					}
+				]
+			}
 		]
 	},
 	{
-		"step_title": "Attention! DSM6 Permissions",
-		"items": [{
-			"desc": "Permissions for this package are handled by the <b>'sc-minio'</b> group. <br>Using File Station, add this group to every folder minio should be allowed to access.<br>Please read <a <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> for details."
-		}]
-	}
+        "step_title": "DSM Permissions",
+        "items": [
+            {
+                "desc": "Please read <a target=\"_blank\" href=\"https://github.com/SynoCommunity/spksrc/wiki/Permission-Management\">Permission Management</a> for details."
+            }
+        ]
+    }
 ]
 EOF
 exit 0
