5ace92bef622cccf606bddeac278d193b8f5bdf2
BKSteve <stephen.rieger@gmail.com>
2021-11-05 17:04:37 +0700

    Sickchill: update with Py3.8.12 and DSM7 support (#4920)
    * Sickchill rebuild inc DSM6&7 with new py3.8.12
    
    * Sickchill cross and requirements updates
    
    * SickChill 6 7 with Py3.8.12-6 tested working!
    
    * Update wizard to warn users that the first start can take longer.
    
    Co-authored-by: Sebastian Schmidt <publicarray@users.noreply.github.com>
    
    * Fix regression in python wheel cross build.  Reverts `mk/spksrc.common.mk`, `mk/spksrc.wheel.mk` from 98dc6b42372350376c7455f33c3a6843b9de4c2f
    
    * SickChill 6 7 update requirements* and makefile
    
    Co-authored-by: Sebastian Schmidt <publicarray@users.noreply.github.com>
    Co-authored-by: Vincent Fortier <th0ma7@gmail.com>

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -17,8 +17,8 @@ RUN = cd $(WORK_DIR)/$(PKG_DIR) && env $(ENV)
 PIP ?= pip
 # Why ask for the same thing twice? Always cache downloads
 PIP_CACHE_OPT ?= --cache-dir $(PIP_DIR)
-PIP_ARGS = --no-binary :all: $(PIP_CACHE_OPT) --no-deps
-PIP_WHEEL = $(PIP) wheel $(PIP_ARGS) --global-option="build_ext" --global-option="-j$(NCPUS)" --wheel-dir $(WHEELHOUSE)
+PIP_WHEEL_ARGS = wheel --no-binary :all: $(PIP_CACHE_OPT) --no-deps --wheel-dir $(WHEELHOUSE)
+PIP_WHEEL = $(PIP) $(PIP_WHEEL_ARGS)
 PIP_DOWNLOAD = $(PIP) download $(PIP_ARGS) --dest $(BASE_DISTRIB_DIR)
 
 # Available languages
