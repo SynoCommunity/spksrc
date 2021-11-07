36de32ff14177ebe1ce77ccfff7c3519a033a046
hgy59 <hpgy59@gmail.com>
2021-11-07 23:57:56 +0100

    cleanup makefile

--- spk/icecast/src/service-setup.sh
+++ spk/icecast/src/service-setup.sh
@@ -1,6 +1,6 @@
 
+# icecast service setup
 CFG_FILE="${SYNOPKG_PKGVAR}/icecast.xml"
-PATH="${SYNOPKG_PKGDEST}:${PATH}"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/icecast -c ${CFG_FILE}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
@@ -12,4 +12,3 @@ service_postinst ()
     sed -i -e "s/@username@/${wizard_ic_username:=admin}/g" ${CFG_FILE}
     sed -i -e "s/@password@/${wizard_ic_password:=changepassword}/g" ${CFG_FILE}
 }
-
