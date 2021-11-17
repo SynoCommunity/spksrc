e7e2a7577a69223a55a803c83606c602603d429c
André Colomb <src@andre.colomb.de>
2021-11-17 22:11:21 +0100

    syncthing: Ask for Web GUI credentials during installation.
    Use the "syncthing generate" subcommand to apply the given username /
    password combination to the existing config file.

--- spk/syncthing/src/service-setup.sh
+++ spk/syncthing/src/service-setup.sh
@@ -5,6 +5,25 @@ SVC_WRITE_PID=y
 
 GROUP="sc-syncthing"
 
+set_credentials() {
+    if [ -n "${wizard_username}" -a -n "${wizard_password}" ]; then
+        # Password needs to be hashed for config entry
+        ${SYNOPKG_PKGDEST}/bin/syncthing generate --home=${SYNOPKG_PKGVAR} \
+            --gui-user="${wizard_username}" --gui-password="${wizard_password}"
+        fi
+    fi
+}
+
+service_postinst() {
+    # Required: set $HOME environment variable
+    HOME=${SYNOPKG_PKGVAR}
+    export HOME
+
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        set_credentials
+    fi
+}
+
 service_prestart ()
 {
     # Read additional startup options from var/options.conf
