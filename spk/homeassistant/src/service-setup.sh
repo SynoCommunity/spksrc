ffc8208726b56fd95ca623fd0f3fe7eb7515de26
Vincent Fortier <th0ma7@gmail.com>
2021-11-18 22:39:28 +0000

    homeassistant: Rough 1st pass to migrate to python310

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -3,7 +3,7 @@
 # - installer
 # - start-stop-status
 
-PYTHON_DIR="/var/packages/python38/target/bin"
+PYTHON_DIR="/var/packages/python310/target/bin"
 VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
@@ -11,17 +11,6 @@ CONFIG_DIR="${SYNOPKG_PKGVAR}/config"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --pid-file ${PID_FILE} --log-file ${LOG_FILE} --daemon"
 SVC_CWD="${SYNOPKG_PKGVAR}"
 HOME="${SYNOPKG_PKGVAR}"
-
-
-rename_file ()
-{
-    _from=$1
-    _to=$2
-    if [ ! "${_from}" -ef "${_to}" ]; then
-        echo "- rename ${_from##*/}  to  ${_to##*/}"
-        mv -f ${_from} ${_to}
-    fi
-}
 
 
 service_postinst ()
@@ -36,19 +25,6 @@ service_postinst ()
     
     separator="===================================================="
     wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
-
-    echo ${separator}
-    echo "Rename arch specific wheels"
-    uname_m=$(uname -m)
-    for wheel_file in ${wheelhouse}/pycryptodome*-none-any.whl ; do
-        new_wheel_file=$(echo ${wheel_file} | sed "s|cp35-none-any|cp35-abi3-linux_${uname_m}|g")
-        rename_file "${wheel_file}" "${new_wheel_file}"
-    done
-    none_name="-cp38-none-any.whl"
-    arch_name="-cp38-cp38-linux_${uname_m}.whl"
-    for wheel_file in ${wheelhouse}/*${none_name} ; do
-        rename_file "${wheel_file}" "${wheel_file%${none_name}}${arch_name}"
-    done
 
     echo ${separator}
     echo "Install packages from wheels"
