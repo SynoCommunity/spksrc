368e94b2133f2cdf4e35052e45d09779d34a7af3
hgy59 <hpgy59@gmail.com>
2021-12-05 23:33:11 +0100

    Add kiwix (#4627)
    * add kiwix
    - add spk for kiwix
    - add dependencies
      o kiwix-tools
      o kiwix-lib
      o libzim
    * rename kiwix-lib to libkiwix
    - kiwix-lib repo was renamed to libkiwix
    * run kiwix-serve as service
    - add service configuration
    - run with library file
    - initialize empty library file
    * add wizard to create share
    * finalize wizard for DSM 7 compatible shared folder
    - update wizard and merge to one configuration page
    - add config file to make shared folder available
    - update libzim
    * validate unsupported archs

---
+++ spk/kiwix/src/service-setup.sh
@@ -0,0 +1,32 @@
+
+PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+KIWIX_SERVE="${SYNOPKG_PKGDEST}/bin/kiwix-serve"
+CONFIG_FILE="${SYNOPKG_PKGVAR}/kiwix.cfg"
+SVC_WRITE_PID=y
+SVC_BACKGROUND=y
+
+SHARED_ZIM_FOLDER=${SYNOPKG_PKGVAR}
+if [ -r "${CONFIG_FILE}" ]; then
+    . "${CONFIG_FILE}"
+fi
+
+LIBRARY_FILE="${SHARED_ZIM_FOLDER}/library.xml"
+SERVICE_COMMAND="${KIWIX_SERVE} --port=${SERVICE_PORT} --library ${LIBRARY_FILE}"
+
+service_prestart ()
+{
+    if [ ! -d "${SHARED_ZIM_FOLDER}" ]; then
+        echo "Error: Missing shared folder for zim content [${SHARED_ZIM_FOLDER}]."
+        exit 1
+    fi
+
+    if [ ! -f "${LIBRARY_FILE}" ]; then
+        cp ${SYNOPKG_PKGDEST}/var/empty_library.xml "${LIBRARY_FILE}"
+    fi
+}
+
+service_postinst ()
+{
+    sed -e "s|@@_wizard_shared_folder_@@|${wizard_data_volume}/${wizard_data_folder}|g" -i "${CONFIG_FILE}"
+}
+
