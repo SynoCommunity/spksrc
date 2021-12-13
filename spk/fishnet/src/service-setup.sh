bfe70fb418a15849e8c559a6aec60865d6a31713
Vincent Fortier <th0ma7@gmail.com>
2021-12-13 01:05:36 +0000

    fishnet: Migrate to Python 3.10

--- spk/fishnet/src/service-setup.sh
+++ spk/fishnet/src/service-setup.sh
@@ -1,16 +1,15 @@
-
-PYTHON_DIR="/var/packages/python3/target/bin"
-VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
+# Define python310 binary path
+PYTHON_DIR="/var/packages/python310/target/bin"
+# Add local bin, virtualenv along with python310 to the default PATH
+PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
+    install_python_virtualenv
 
     # Install the wheels
-    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
-    ${SYNOPKG_PKGDEST}/env/bin/pip3 install --no-deps --no-index --upgrade --force-reinstall --find-links ${wheelhouse} ${wheelhouse}/*.whl
-    
+    install_python_wheels
 
     # get target file independent of python3 version
     target=$(${SYNOPKG_PKGDEST}/env/bin/python3 -m site | grep -o "[^']*${SYNOPKG_PKGNAME}/env[^']*")/fishnet.py
