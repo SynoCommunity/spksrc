78cb6dcbb5b9c116012224cec0709facb7e24458
André Colomb <src@andre.colomb.de>
2021-12-19 03:45:29 +0100

    syncthing: Ask for Web GUI credentials in installation wizard (#4995)
    * syncthing: Normalize UI files.
    Run each through the following command to apply consistent style:
    jq --indent 4 .
    * syncthing: Explicitly use "serve" subcommand in service-setup.
    * syncthing: Ask for Web GUI credentials during installation.
    Use the "syncthing generate" subcommand to apply the given username /
    password combination to the existing config file.
    * syncthing: Incorporate upstream release v1.18.5.
    Only package version and hashes updated.  Built successfully for
    all-supported.
    Note that the previous package version should have already
    self-updated an installed Syncthing binary when the service was
    running long enough.
    * syncthing: Describe wizard addition in the CHANGELOG.
    * syncthing: Fix syntax error in service-setup.sh.

--- spk/syncthing/src/service-setup.sh
+++ spk/syncthing/src/service-setup.sh
@@ -1,9 +1,27 @@
 # syncthing service definition
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/syncthing -home=${SYNOPKG_PKGVAR}"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/syncthing serve --home=${SYNOPKG_PKGVAR}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
 GROUP="sc-syncthing"
+
+set_credentials() {
+    if [ -n "${wizard_username}" -a -n "${wizard_password}" ]; then
+        # Password needs to be hashed for config entry
+        ${SYNOPKG_PKGDEST}/bin/syncthing generate --home=${SYNOPKG_PKGVAR} \
+            --gui-user="${wizard_username}" --gui-password="${wizard_password}"
+    fi
+}
+
+service_postinst() {
+    # Required: set $HOME environment variable
+    HOME=${SYNOPKG_PKGVAR}
+    export HOME
+
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+        set_credentials
+    fi
+}
 
 service_prestart ()
 {
