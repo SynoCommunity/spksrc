550199dcc5b40ffbd5fadcc91beec91f371b9399
hgy59 <hpgy59@gmail.com>
2021-12-23 17:57:01 +0100

    update rsnapshot
    - update rsnapshot to v1.4.4
    - add logs for installation steps
    - update icon

--- spk/rsnapshot/src/service-setup.sh
+++ spk/rsnapshot/src/service-setup.sh
@@ -1,27 +1,27 @@
 
 service_postinst ()
 {
-    # Copy default config file
     if [ ! -e ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf ]; then
-        cp ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf.default ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
+        echo "Create rsnapshot.conf from rsnapshot.conf.default"
+        $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf.default ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
     fi
 }
 
 service_save ()
 {
-    # Save configuration
-    $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf "${TMP_DIR}"/rsnapshot.conf
+    echo "Save rsnapshot.conf to ${TMP_DIR}"
+    $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf "${TMP_DIR}/rsnapshot.conf"
 }
 
 service_restore ()
 {
-    # Restore configuration
-    $MV "${TMP_DIR}"/rsnapshot.conf ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
+    echo "Restore rsnapshot.conf from ${TMP_DIR}"
+    $MV "${TMP_DIR}/rsnapshot.conf" ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
 }
 
 service_postupgrade ()
 {
-    # Upgrade configuration file if needed
+    echo "Provide configuration file upgrade"
     ${SYNOPKG_PKGDEST}/bin/rsnapshot upgrade-config-file
 }
 
