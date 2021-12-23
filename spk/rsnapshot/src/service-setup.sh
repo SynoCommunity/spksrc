f46b8eb8ed6ac4854c5c72f59f70a999f9bd6740
hgy59 <hpgy59@gmail.com>
2021-12-23 19:45:46 +0100

    use var folder for config file
    - migrate existing config from etc folder

--- spk/rsnapshot/src/service-setup.sh
+++ spk/rsnapshot/src/service-setup.sh
@@ -1,23 +1,31 @@
 
 service_postinst ()
 {
-    if [ ! -e ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf ]; then
+    if [ ! -e ${SYNOPKG_PKGVAR}/rsnapshot.conf ]; then
         echo "Create rsnapshot.conf from rsnapshot.conf.default"
-        $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf.default ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
+        $CP ${SYNOPKG_PKGVAR}/rsnapshot.conf.default ${SYNOPKG_PKGVAR}/rsnapshot.conf
     fi
 }
 
-service_save ()
-{
-    echo "Save rsnapshot.conf to ${TMP_DIR}"
-    $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf "${TMP_DIR}/rsnapshot.conf"
-}
+if [ "${SYNOPKG_PKG_STATUS}" != "INSTALL" ] && [ "$(echo ${SYNOPKG_OLD_PKGVER} | sed -r 's/^.*-([0-9]+)$/\1/')" -le 3 ]; then
 
-service_restore ()
-{
-    echo "Restore rsnapshot.conf from ${TMP_DIR}"
-    $MV "${TMP_DIR}/rsnapshot.conf" ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf
-}
+    # custom save/restore for former packages with config in etc folder
+    service_save ()
+    {
+        if [ -e ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf ]; then
+            echo "Save rsnapshot.conf to ${TMP_DIR}"
+            $CP ${SYNOPKG_PKGDEST}/etc/rsnapshot.conf "${TMP_DIR}/rsnapshot.conf"
+        fi
+    }
+
+    service_restore ()
+    {
+        if [ -e "${TMP_DIR}/rsnapshot.conf" ]; then
+            echo "Restore rsnapshot.conf from ${TMP_DIR}"
+            $MV "${TMP_DIR}/rsnapshot.conf" ${SYNOPKG_PKGVAR}/rsnapshot.conf
+        fi
+    }
+fi
 
 service_postupgrade ()
 {
