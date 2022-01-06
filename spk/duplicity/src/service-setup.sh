331601f6ac418d6fdb6ca8757dae6c02fee03b62
Vincent Fortier <th0ma7@gmail.com>
2022-01-06 23:46:18 +0000

    duplicity: Adjust version at postinst

--- spk/duplicity/src/service-setup.sh
+++ spk/duplicity/src/service-setup.sh
@@ -3,6 +3,8 @@
 
 PYTHON_DIR="/var/packages/python310/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
+DUPLICITY_VERSION="0.8.21"
+DUPLICITY_INIT="${SYNOPKG_PKGDEST}/env/lib/python3.10/site-packages/duplicity/__init__.py"
 
 service_postinst ()
 {
@@ -11,5 +13,7 @@ service_postinst ()
 
     # Install the wheels
     install_python_wheels
+
+    sed -i -e "/^__version__ =/s/=.*/= u\x27${DUPLICITY_VERSION}\x27/" ${DUPLICITY_INIT}
 }
 
