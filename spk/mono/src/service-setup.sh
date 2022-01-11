c6b33624344c92168699b00a5a7be6a7f1c55962
Sebastian Schmidt <publicarray@users.noreply.github.com>
2022-01-11 22:32:40 +1100

    Use home folder instead of /usr/share/.mono

--- spk/mono/src/service-setup.sh
+++ spk/mono/src/service-setup.sh
@@ -4,6 +4,6 @@
 service_postinst ()
 {
     # Sync ca certificates
-    curl -Lko ${SYNOPKG_PKGDEST}/ca-certificates.crt https://curl.se/ca/cacert.pem
-    ${SYNOPKG_PKGDEST}/bin/cert-sync ${SYNOPKG_PKGDEST}/ca-certificates.crt
+    mkdir -p ${SYNOPKG_PKGVAR}/.config ${SYNOPKG_PKGVAR}/.mono
+    HOME="${SYNOPKG_PKGVAR}" ${SYNOPKG_PKGDEST}/bin/cert-sync --user /etc/ssl/certs/ca-certificates.crt
 }
