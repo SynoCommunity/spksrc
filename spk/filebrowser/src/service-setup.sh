b40c3d08821c5362d6d467d8ec1e41fc59425ed8
hgy59 <hpgy59@gmail.com>
2022-01-22 13:24:00 +0100

    small fine-tuning
    - use BUILD_DEPENDS for native packages
    - update icon

--- spk/filebrowser/src/service-setup.sh
+++ spk/filebrowser/src/service-setup.sh
@@ -1,7 +1,9 @@
+
 # https://help.synology.com/developer-guide/integrate_dsm/fhs.html
-if [ -z "$SYNOPKG_PKGHOME" ]; then
-    SYNOPKG_PKGHOME="$SYNOPKG_PKGVAR"
+if [ -z "${SYNOPKG_PKGHOME}" ]; then
+    SYNOPKG_PKGHOME="${SYNOPKG_PKGVAR}"
 fi
+
 export HOME="${SYNOPKG_PKGHOME}"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/filebrowser -a 0.0.0.0 -p ${SERVICE_PORT} -r / -l ${LOG_FILE} -d ${SYNOPKG_PKGHOME}/filebrowser.db"
 SVC_BACKGROUND=y
