ce5cade628904cdccd486c55f7243a7c172dfdb3
Sebastian Schmidt <publicarray@users.noreply.github.com>
2022-01-23 15:25:39 +1100

    mono

--- spk/mono/src/service-setup.sh
+++ spk/mono/src/service-setup.sh
@@ -4,6 +4,5 @@
 service_postinst ()
 {
     # Sync ca certificates
-    mkdir -p ${SYNOPKG_PKGVAR}/.config ${SYNOPKG_PKGVAR}/.mono
-    HOME="${SYNOPKG_PKGVAR}" ${SYNOPKG_PKGDEST}/bin/cert-sync --user /etc/ssl/certs/ca-certificates.crt
+    ${SYNOPKG_PKGDEST}/bin/cert-sync /etc/ssl/certs/ca-certificates.crt
 }
