a4c5cf2e7ae344e98511451ff9833ff5b816c315
Vincent Fortier <th0ma7@gmail.com>
2022-01-25 00:52:35 +0000

    tc.mk: Auto-manage gcc version handling

--- mk/spksrc.tc.mk
+++ mk/spksrc.tc.mk
@@ -56,44 +56,46 @@ all: fix $(TC_LOCAL_VARS_MK)
 $(TC_LOCAL_VARS_MK): fix
 	env $(MAKE) --no-print-directory tc_vars > $@ 2>/dev/null;
 
-.PHONY:
+.PHONY: tc_vars
 tc_vars:
-	@echo TC_ENV :=
-	@for tool in $(TOOLS) ; \
+	@echo TC_ENV := ; \
+	for tool in $(TOOLS) ; \
 	do \
 	  target=`echo $${tool} | sed 's/\(.*\):\(.*\)/\1/'` ; \
 	  source=`echo $${tool} | sed 's/\(.*\):\(.*\)/\2/'` ; \
 	  echo TC_ENV += `echo $${target} | tr [:lower:] [:upper:] `=\"$(WORK_DIR)/$(TC_TARGET)/bin/$(TC_PREFIX)$${source}\" ; \
-	done
-	@echo TC_ENV += CFLAGS=\"$(CFLAGS) $$\(ADDITIONAL_CFLAGS\)\"
-	@echo TC_ENV += CPPFLAGS=\"$(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)\"
-	@echo TC_ENV += CXXFLAGS=\"$(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)\"
-	@echo TC_ENV += LDFLAGS=\"$(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)\"
-	@echo TC_CONFIGURE_ARGS := --host=$(TC_TARGET) --build=i686-pc-linux
-	@echo TC_TYPE := $(TC_TYPE)
-	@echo TC_TARGET := $(TC_TARGET)
-	@echo TC_PREFIX := $(TC_PREFIX)
-	@echo TC_PATH := $(WORK_DIR)/$(TC_TARGET)/bin/
-	@echo CFLAGS := $(CFLAGS) $$\(ADDITIONAL_CFLAGS\)
-	@echo CPPFLAGS := $(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)
-	@echo CXXFLAGS := $(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)
-	@echo LDFLAGS := $(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)
-	@echo TC_LIBRARY := $(TC_LIBRARY)
-	@echo TC_INCLUDE := $(TC_INCLUDE)
-	@echo TC_EXTRA_CFLAGS := $(TC_EXTRA_CFLAGS)
-	@echo TC_VERS := $(TC_VERS)
-	@echo TC_BUILD := $(TC_BUILD)
-	@echo TC_OS_MIN_VER := $(TC_OS_MIN_VER)
-	@echo TC_ARCH := $(TC_ARCH)
+	  if [ "$${target}" = "cc" ] ; then \
+	    gcc_version=`eval $$(echo $(WORK_DIR)/$(TC_TARGET)/bin/$(TC_PREFIX)$${source} -dumpversion)` ; \
+	  fi ; \
+	done ; \
+	echo TC_ENV += CFLAGS=\"$(CFLAGS) $$\(ADDITIONAL_CFLAGS\)\" ; \
+	echo TC_ENV += CPPFLAGS=\"$(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)\" ; \
+	echo TC_ENV += CXXFLAGS=\"$(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)\" ; \
+	echo TC_ENV += LDFLAGS=\"$(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)\" ; \
+	echo TC_CONFIGURE_ARGS := --host=$(TC_TARGET) --build=i686-pc-linux ; \
+	echo TC_TYPE := $(TC_TYPE) ; \
+	echo TC_TARGET := $(TC_TARGET) ; \
+	echo TC_PREFIX := $(TC_PREFIX) ; \
+	echo TC_PATH := $(WORK_DIR)/$(TC_TARGET)/bin/ ; \
+	echo CFLAGS := $(CFLAGS) $$\(ADDITIONAL_CFLAGS\) ; \
+	echo CPPFLAGS := $(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\) ; \
+	echo CXXFLAGS := $(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\) ; \
+	echo LDFLAGS := $(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\) ; \
+	echo TC_LIBRARY := $(TC_LIBRARY) ; \
+	echo TC_INCLUDE := $(TC_INCLUDE) ; \
+	echo TC_EXTRA_CFLAGS := $(TC_EXTRA_CFLAGS) ; \
+	echo TC_VERS := $(TC_VERS) ; \
+	echo TC_BUILD := $(TC_BUILD) ; \
+	echo TC_OS_MIN_VER := $(TC_OS_MIN_VER) ; \
+	echo TC_ARCH := $(TC_ARCH) ; \
+	echo TC_GCC := $${gcc_version} ; \
+	echo TC_GLIBC := $(TC_GLIBC)
 # Add "+" to EXTRAVERSION for kernels version >= 4.4
 ifeq ($(call version_ge, ${TC_KERNEL}, 4.4),1)
 	@echo TC_KERNEL := $(TC_KERNEL)+
 else
 	@echo TC_KERNEL := $(TC_KERNEL)
 endif
-	@echo TC_GCC := $(TC_GCC)
-	@echo TC_GLIBC := $(TC_GLIBC)
-
 
 ### Clean rules
 clean:
