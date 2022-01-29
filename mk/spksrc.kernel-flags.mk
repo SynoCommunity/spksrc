1cd1b7174e6827bc448bfd533a64f5a08481b319
Kerma Gérald <gandalf@gk2.net>
2022-01-29 21:14:13 +0100

    DSM7.0: add kernel and toolchain for armada37xx
    add DSM >= 7.0 in mk/spksrc.kernel-flags.mk
    fix URL for DSM 7.0 kernel and toolchain
    add kernel for syno-armada37xx-7.0
    add toolchain for syno-armada37xx-7.0
    
    Signed-off-by: Kerma Gérald <gandalf@gk2.net>

--- mk/spksrc.kernel-flags.mk
+++ mk/spksrc.kernel-flags.mk
@@ -30,9 +30,33 @@ KERNEL_DIST_SITE = https://sourceforge.net/projects/dsgpl/files/Synology%20NAS%2
 endif
 
 #
+# For DSM version >= 7.0
+#
+ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 41890),1)
+
+KERNEL_DIST_SITE = https://global.download.synology.com/download/ToolChain/Synology%20NAS%20GPL%20Source/$(TC_VERS)-$(KERNEL_BUILD)/$(KERNEL_URL_DIR)
+
+ifeq ($(strip $(KERNEL_DIST_NAME)),)
+KERNEL_DIST_NAME = $(KERNEL_DIST).$(KERNEL_EXT)
+endif
+
+ifeq ($(strip $(KERNEL_URL_DIR)),)
+KERNEL_URL_DIR = $(KERNEL_ARCH)
+endif
+
+ifeq ($(strip $(KERNEL_PREFIX)),)
+KERNEL_PREFIX = $(KERNEL_DIST)
+endif
+
+ifeq ($(strip $(KERNEL_STRIP)),)
+KERNEL_STRIP = 0
+endif
+
+#
+#
 # For DSM version >= 6.1
 #
-ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 15152),1)
+else ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 15152),1)
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
 KERNEL_DIST_NAME = $(KERNEL_DIST).$(KERNEL_EXT)
 endif
