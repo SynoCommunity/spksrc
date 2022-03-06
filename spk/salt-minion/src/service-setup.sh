a5c601e24414c2f7d2035e5f161dfe32b309f580
Vincent Fortier <th0ma7@users.noreply.github.com>
2022-03-06 05:57:58 -0800

    Update salt-minion to 3004 and python310 (#5017)
    * salt-minion: Initial import from PR #4845
    
    * salt-minion: Move log+cache+pki in var
    
    * salt-minion: Use patch.py to fis path for libcrypto.so
    
    * Update spk/salt-minion/Makefile
    
    Co-authored-by: hgy59 <hpgy59@gmail.com>
    
    * Update spk/salt-minion/Makefile
    
    Co-authored-by: hgy59 <hpgy59@gmail.com>
    
    * salt-minion: Fix pid & socket directories
    
    * salt-minion: Use default start-stop-status PID_FILE variable
    
    * salt-minion: Change log level back to info
    
    * salt-minion: Move configurations to @appconf
    
    Co-authored-by: Bjarne Saltbaek <arnebjarne72@hotmail.com>
    
    * salt-master: Migrate to python310
    
    Co-Author:Bjarne Saltbaek <arnebjarne72@hotmail.com>
    
    * salt-master: Fix service install
    
    * salt-*: Fix typo
    
    * salt-*: Enable using root_dir: configuration option
    
    Co-authored-by: hgy59 <hpgy59@gmail.com>
    Co-authored-by: Bjarne Saltbaek <arnebjarne72@hotmail.com>

--- spk/salt-minion/src/service-setup.sh
+++ spk/salt-minion/src/service-setup.sh
@@ -1,33 +1,39 @@
-PYTHON_DIR="/var/packages/python3/target/bin"
-PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}:${PATH}"
-VIRTUALENV="${PYTHON_DIR}/python3 -m venv"
-PYTHON="${SYNOPKG_PKGDEST=}/env/bin/python"
+# Define python310 binary path
+PYTHON_DIR="/var/packages/python310/target/bin"
+# Add local bin, virtualenv along with python310 to the default PATH
+PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 LANGUAGE="env LANG=en_US.UTF-8"
-SALT_MINION="${SYNOPKG_PKGDEST}/env/bin/salt-minion"
-PID_FILE="${SYNOPKG_PKGDEST}/var/run/salt-minion.pid"
+SYNOPKG_PKGETC=/var/packages/${SYNOPKG_PKGNAME}/etc
 
-SERVICE_COMMAND="${SALT_MINION} -c ${SYNOPKG_PKGDEST}/var -d"
+SERVICE_COMMAND="salt-minion --pid-file ${PID_FILE} -c ${SYNOPKG_PKGETC} -d"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
+    install_python_virtualenv
 
     # Install wheels
-    wheelhouse=${SYNOPKG_PKGDEST}/share/wheelhouse
-    ${SYNOPKG_PKGDEST}/env/bin/pip install --no-deps --force-reinstall --no-index --find-links ${wheelhouse} ${wheelhouse}/*.whl
+    install_python_wheels
 
-    # Patch rsax931.py file to find libcrypto lib
-    # (Rely on patch util bundled with python3's busybox)
-    ${PYTHON_DIR}/bin/patch ${SYNOPKG_PKGDEST}/env/lib/python3.7/site-packages/salt/utils/rsax931.py < ${SYNOPKG_PKGDEST}/share/rsax931.py.patch
+    # patch rsax931.py file to find libcrypto lib provided by python310
+    # (rely on patch==1.16 included in requirements-pure.txt)
+    python ${SYNOPKG_PKGDEST}/env/lib/python3.10/site-packages/patch.py \
+           --directory=${SYNOPKG_PKGDEST}/env/lib/python3.10/site-packages/salt/utils \
+           ${SYNOPKG_PKGDEST}/share/rsax931.py.patch
 
-    # Prepare salt-minion config in /var/salt
-    install -m 755 -d ${SYNOPKG_PKGDEST}/var
-    install -m 755 -d ${SYNOPKG_PKGDEST}/var/minion.d
-    install -m 644 ${SYNOPKG_PKGDEST}/share/minion.conf ${SYNOPKG_PKGDEST}/var
-    echo "pidfile: ${PID_FILE}" > ${SYNOPKG_PKGDEST}/var/minion.d/02_pidfile.conf
+    # Prepare salt-minion config in /var/packages/salt-minion/target/etc
+    test -d ${SYNOPKG_PKGETC}/minion.d || install -m 755 -d ${SYNOPKG_PKGETC}/minion.d
+    test -f ${SYNOPKG_PKGETC}/minion || install -m 644 ${SYNOPKG_PKGDEST}/share/minion ${SYNOPKG_PKGETC}/minion
+    test -f ${SYNOPKG_PKGETC}/proxy || install -m 644 ${SYNOPKG_PKGDEST}/share/proxy ${SYNOPKG_PKGETC}/proxy
+    test -f ${SYNOPKG_PKGETC}/minion.d/01_pidfile.conf || echo "pidfile: run" > ${SYNOPKG_PKGETC}/minion.d/01_pidfile.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/02_sockdir.conf || echo "sock_dir: run/minion" > ${SYNOPKG_PKGETC}/minion.d/02_sockdir.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/03_cachedir.conf || echo "cachedir: cache" > ${SYNOPKG_PKGETC}/minion.d/03_cachedir.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/04_logging.conf || echo "log_file: ${SYNOPKG_PKGNAME}.log" > ${SYNOPKG_PKGETC}/minion.d/04_logging.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/05_loglevel.conf || echo "log_level_logfile: info" > ${SYNOPKG_PKGETC}/minion.d/05_loglevel.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/06_pkidir.conf || echo "pki_dir: pki/minion" > ${SYNOPKG_PKGETC}/minion.d/06_pkidir.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/07_rootdir.conf || echo "root_dir: ${SYNOPKG_PKGVAR}" > ${SYNOPKG_PKGETC}/minion.d/07_rootdir.conf
+
     # Populate salt master address and minion_id only if file don't already exist
-    test -f ${SYNOPKG_PKGDEST}/var/minion.d/99-master-address.conf || echo "master: salt" > ${SYNOPKG_PKGDEST}/var/minion.d/99-master-address.conf
-    test -f ${SYNOPKG_PKGDEST}/var/minion.d/98-minion-id.conf || echo "id: myname" > ${SYNOPKG_PKGDEST}/var/minion.d/98-minion-id.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/99-master-address.conf || echo "master: localhost" > ${SYNOPKG_PKGETC}/minion.d/99-master-address.conf
+    test -f ${SYNOPKG_PKGETC}/minion.d/98-minion-id.conf || echo -n "id: $(hostname -s)" > ${SYNOPKG_PKGETC}/minion.d/98-minion-id.conf
 }
-
