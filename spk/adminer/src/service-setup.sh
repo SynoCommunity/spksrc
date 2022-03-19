f9b6ac1a44a0afa56bd12dbb3aea8e3f9ad07fd0
hgy59 <hpgy59@gmail.com>
2022-03-19 22:57:31 +0100

    Adminer: update to v4.8.1 (#5160)
    - minor adjustments
      o create var folder for log file
      o migrate installer.sh to service-setup.sh

--- spk/adminer/src/service-setup.sh
+++ spk/adminer/src/service-setup.sh
@@ -1,57 +1,33 @@
-#!/bin/sh
-
-# Package
-PACKAGE="adminer"
-DNAME="Adminer"
 
 HTACCESS_FILE=/var/services/web_packages/adminer/.htaccess
 
 if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
 INSTALL_DIR=${SYNOPKG_PKGDEST}/web
-DSM6_WEB_DIR="/var/services/web"
+DSM6_WEB_DIR="/var/services/web/adminer"
 HTACCESS_FILE=${INSTALL_DIR}/.htaccess
 fi
 
-preinst ()
-{
-    exit 0
-}
-
-postinst ()
+service_postinst ()
 {
     # Edit .htaccess according to the wizard
     sed -i -e "s|@@_wizard_htaccess_allowed_from_@@|${wizard_htaccess_allowed_from}|g" ${HTACCESS_FILE}
 
-    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ];then
         # Install the web interface
-        cp -pR ${INSTALL_DIR} ${DSM6_WEB_DIR}/adminer
+        cp -pR ${INSTALL_DIR} ${DSM6_WEB_DIR}
     fi
-    exit 0
 }
 
-preuninst ()
-{
-    exit 0
-}
-
-postuninst ()
+service_postuninst ()
 {
     # Remove the web interface
-    if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ];then
-        rm -rf ${DSM6_WEB_DIR}/adminer
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ];then
+        rm -rf ${DSM6_WEB_DIR}
     fi
-    exit 0
 }
 
-preupgrade ()
-{
-    exit 0
-}
-
-postupgrade ()
+service_postupgrade ()
 {
     # Edit .htaccess according to the wizard
     sed -i -e "s|@@_wizard_htaccess_allowed_from_@@|${wizard_htaccess_allowed_from}|g" ${HTACCESS_FILE}
-
-    exit 0
 }
