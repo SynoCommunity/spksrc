9e4f0127340f38d38229c60fcf6bf4dcc75dff52
Vincent Fortier <th0ma7@gmail.com>
2022-03-24 17:58:58 +0000

    kernel: Use new 6.2.4 kernels

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -33,7 +33,7 @@ AVAILABLE_TCVERSIONS = $(sort $(foreach arch,$(AVAILABLE_TOOLCHAINS),$(shell ech
 # Available toolchains formatted as '{ARCH}-{TC}'
 AVAILABLE_KERNEL = $(subst syno-,,$(sort $(notdir $(wildcard ../../kernel/syno-*))))
 AVAILABLE_KERNEL_VERSIONS = $(sort $(foreach arch,$(AVAILABLE_KERNEL),$(shell echo ${arch} | cut -f2 -d'-')))
-SUPPORTED_KERNEL_VERSIONS = 6.1 6.2.3 7.0
+SUPPORTED_KERNEL_VERSIONS = 6.1 6.2.4 7.0
 
 # Global arch definitions
 include ../../mk/spksrc.archs.mk
