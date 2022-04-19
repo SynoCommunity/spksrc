bf829a51a8dcc18b62839336ec7c1cec9a05c5a6
hgy59 <hpgy59@gmail.com>
2022-04-19 18:21:56 +0200

    update server and client
    - update urbackup-server to v2.4.15
    - update urbackup-client to v2.4.11
    - create link from sbin to bin as for DSM 7 compatibility

--- spk/urbackup/src/service-setup.sh
+++ spk/urbackup/src/service-setup.sh
@@ -1,18 +1,21 @@
 # Setup environment
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 
+SERVER=${SYNOPKG_PKGDEST}/bin/urbackupsrv
+
+SERVICE_COMMAND="${SERVER} run --daemon --user ${EFF_USER} --http-port ${SERVICE_PORT} --pidfile ${PID_FILE} --loglevel info --logfile ${LOG_FILE}"
+
 service_postinst ()
 {
-   	mkdir -p ${SYNOPKG_PKGDEST}/var/urbackup
-    echo "tank/images" > ${SYNOPKG_PKGDEST}/var/urbackup/dataset  
-    echo "${SHARE_PATH}" > ${SYNOPKG_PKGDEST}/var/urbackup/backupfolder 
+    mkdir -p ${SYNOPKG_PKGVAR}/urbackup
+    echo "tank/images" > ${SYNOPKG_PKGVAR}/urbackup/dataset  
+    echo "${SHARE_PATH}" > ${SYNOPKG_PKGVAR}/urbackup/backupfolder 
 }
 
 service_prestart ()
 {
-    CONFIG_DIR="${SYNOPKG_PKGDEST}/var"
+    CONFIG_DIR="${SYNOPKG_PKGVAR}"
 
-    # Required: start-stop-daemon do not set environment variables
-    HOME=${SYNOPKG_PKGDEST}/var
-    export HOME
+    # Required: start-stop-daemon does not set environment variables
+    export HOME=${SYNOPKG_PKGVAR}
 }
