d79166c1d9463bb5f702a811a277de79cd67b9ce
Vincent Fortier <th0ma7@gmail.com>
2022-04-29 19:59:41 +0000

    synocli-kernelmodule.sh: First reliable iteration

--- spk/synocli-kernel/src/synocli-kernelmodule.sh
+++ spk/synocli-kernel/src/synocli-kernelmodule.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #########################################################################
 # Written by: th0ma7@gmail.com
@@ -18,93 +18,45 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #########################################################################
 
+#------------------------------------------------
 # Make sure an argument was passed 
+#------------------------------------------------
 usage ()
 {
-printf '%10s %s\n' "Usage :" "$0 [-m <path> ] [-a <insmod|rmmod|reload|status>] module1.ko module2.ko ..." 1>&2
-printf '%10s %s\n' "Optional :" "[-f <path>] [-k <version> ] [-n <name>]" 1>&2
-echo 1>&2
-printf '%40s %s\n' "[-m <path> ] : " "Kernel module base path (OPTIONAL if -n <package> is provided)" 1>&2
-printf '%40s %s\n' "[-f <path> ] : " "Additional firmware base path (OPTIONAL)" 1>&2
-printf '%40s %s\n' "[-k <path> ] : " "Kernel version (OPTIONAL: uses \`uname -r\` if not provided)" 1>&2
-printf '%40s %s\n' "[-n <package> ] : " "SynoCommunity package name containing kernel modules" 1>&2
-printf '%40s %s\n' "[-a <insmod|rmmod|reload|status> ] : " "Action to be performed" 1>&2
-printf '%40s %s\n' "[-h] : " "Print this help" 1>&2
-printf '%40s %s\n' "[-v] : " "Verbose mode" 1>&2
-echo 1>&2
-printf '%10s %s\n' "" "Examples :" 1>&2
-printf '%20s %s\n' "" "$0 -a status -n synokernel-cdrom cdrom sr_mod" 1>&2
-echo 1>&2
-printf '%20s %s\n' "" "$0 -a status -k 4.4.59+ \\" 1>&2
-printf '%30s %s\n' "" "-m /var/packages/synokernel-cdrom/target/lib/modules \\" 1>&2
-printf '%30s %s\n' "" "drivers/cdrom/cdrom.ko \\" 1>&2
-printf '%30s %s\n' "" "drivers/scsi/sr_mod.ko" 1>&2
+echo
+printf '%10s %s\n' "Usage :" "$0 [-s|--spk <package>] [<insmod|rmmod|reload|status>] module1.ko module2.ko ..."
+printf '%20s %s\n' "Optional :" "[-c|--config <file>:<option>]"
+echo
+printf '%40s %s\n' "[-s|--spk <package>] : " "SynoCommunity package name containing kernel modules"
+printf '%40s %s\n' "[<insmod|rmmod|reload|status>] : " "Action to be performed"
+printf '%40s %s\n' "[-h|--help] : " "Print this help"
+printf '%40s %s\n' "[-v|--verbose] : " "Verbose mode"
+echo
+printf '%10s %s\n' "" "Examples :"
+printf '%20s %s\n' "" "$0 --spk synokernel-cdrom cdrom sr_mod --verbose status"
 echo 1>&2
 }
-[ $# -eq 0 ] && usage && exit 1
 
-# Get basic options
-while getopts ":h:m:f:k:n:a:v:" arg; do
-  case $arg in
-    m) MPATH=${OPTARG};;
-    f) FPATH=${OPTARG};;
-    k) KVER=${OPTARG};;
-    n) PKGNAME=${OPTARG};;
-    a) ACTION=${OPTARG};;
-    h) usage && exit 0;;
-    v) VERBOSE="TRUE";;
-  esac
-done
-
-# Gather the remaining kernel modules passed as parameters
-shift $((OPTIND-1))
-KO=$@
-
-# Set system module firmware path file index
-SYS_FIRMWARE_PATH=/sys/module/firmware_class/parameters/path
-
-# Set LOG output
-SYNOLOG=/tmp/synocli-kernelmodule.log
-[ -n "${PKGNAME}" ] && SYNOLOG=/tmp/synocli-kernelmodule-${PKGNAME}.log
-
-exec >> $SYNOLOG
-
-# Set default kernel version
-[ -z "${KVER}" ] && KVER=$(uname -r)
-
-# If neither the module path or name is being provided, exit
-if [ -z "${MPATH}" ]; then
-   [ -z "{PKGNAME}" ] \
-      && usage \
-	  || MPATH=/var/packages/${PKGNAME}/target/lib/modules
-fi
-
-# If module path does not exists, exit
-if [ ! -d ${MPATH} ]; then
-   echo "ERROR: Module path [${MPATH}] does not exist or inaccessible..." 1>&2
-   echo 1>&2
-   usage
-fi
-
-# Assign default firmware path if not set
-if [ -z "${FPATH}" ]; then
-   FPATH=/var/packages/${PKGNAME}/target/lib/firmware
-fi
-
-# Get ARCH
-[ -z "${ARCH}" ] && ARCH=$(uname -a | awk '{print $NF}' | cut -f2 -d_)
-
-# Get DSM DSM_VERSION
-[ -z "${DSM_VERSION}" ] && DSM_VERSION=$(sed -n 's/^productversion=\(.*\)/\1/p' /etc/VERSION | sed -e 's/"//g')
-
-# Set kernel module .ko object base path
-# If does not exists, exit
-KPATH=${MPATH}/${ARCH}-${DSM_VERSION}/${KVER}
-if [ ! -d ${KPATH} ]; then
-   echo "ERROR: Kernel modules base path [${KPATH}] does not exist or inaccessible..." 1>&2
-   echo 1>&2
-   usage
-fi
+#------------------------------------------------
+# Print detailed information for debugging
+#------------------------------------------------
+verbose ()
+{
+printf '%20s %s\n' "" "$0 (verbose)"
+printf '%60s %s\n' "SynoCommunity kernel driver package name (SPK)" "[${SPK}]"
+printf '%60s %s\n' "SynoCommunity configuration file (SPK_CFG)" "[${SPK_CFG}]"
+printf '%60s %s\n' "SynoCommunity configuration path (SPK_CFG_PATH)" "[${SPK_CFG_PATH}]"
+printf '%60s %s\n' "SynoCommunity configuration option (SPK_CFG_OPT)" "[${SPK_CFG_OPT}]"
+printf '%60s %s\n' "Synology NAS arch (ARCH)" "[${ARCH}]"
+printf '%60s %s\n' "Synology DSM version (DSM_VERSION)" "[${DSM_VERSION}]"
+printf '%60s %s\n' "Running kernel version (KVER)" "[${KVER}]"
+printf '%60s %s\n' "Module action insmod|rmmod|reload|status (ACTION)" "[${ACTION}]"
+printf '%60s %s\n' "Kernel modules path (MPATH)" "[${MPATH}]"
+printf '%60s %s\n' "Full kernel modules path (KPATH)" "[${KPATH}]"
+printf '%60s %s\n' "Device firmware path (FPATH)" "[${FPATH}]"
+printf '%60s %s\n' "Kernel objects list (KO_LIST)" "[${KO_LIST}]"
+printf '%60s %s\n' "Kernel objects found (KO_FOUND)" "[${KO_FOUND}]"
+}
 
 
 #------------------------------------------------
@@ -114,10 +66,9 @@ fi
 #   /<path>/<module>*
 # The following find the right module as needed
 #------------------------------------------------
-fix_ko_path ()
+ko_path_match ()
 {
-   KO_TMP=""
-   for ko in $KO
+   for ko in $KO_LIST
    do
       # first check if the name
 	  # matches to a path
@@ -127,13 +78,116 @@ fix_ko_path ()
 		 # Replace any '-' or '_' by '[-_]
 		 ko=$(echo ${ko} | sed -r 's/[-_]/[-_]/g')
          # Find full module kernel object path
-         ko=drivers/$(find $KPATH -name $ko | awk -F'drivers/' '{print $2}')
+         ko=$(find $KPATH -name $ko | awk -F'drivers/' '{print $2}')
       fi
-      KO_TMP="$KO_TMP $ko"
+      KO_FOUND+="$ko "
    done
 
-   KO="$KO_TMP"
+   KO_FOUND=$(echo $KO_FOUND | xargs)
+
+   if [ ! $(echo $KO_LIST | wc -w) -eq $(echo $KO_FOUND | wc -w) ]; then
+	  verbose && usage
+      echo "ERROR: Some kernel modules where not found..." 1>&2
+      echo 1>&2
+	  exit 1
+   fi
 }
+
+
+[ $# -eq 0 ] && usage && exit 1
+
+###
+### Global variables
+###
+SPK=""                                                                 # SynoCommunity kernel driver package name
+SPK_CFG=""                                                             # SynoCommunity configuration file
+SPK_CFG_OPT=""                                                         # SynoCommunity configuration option
+ACTION=""                                                              # Module action insmod|rmmod|reload|status
+VERBOSE="FALSE"                                                        # Set verbose mode
+HELP="FALSE"                                                           # Print help
+
+while [ $# -gt 0 ] 
+do
+   case $1 in
+                          -s|--spk ) shift 1
+						             SPK=$1;;
+                       -c|--config ) shift 1
+						             SPK_CFG=$(echo $1 | cut -f1 -d:)
+						             SPK_CFG_OPT=$(echo $1 | cut -f2 -d:);;
+                         -h|--help ) HELP="TRUE";;
+        insmod|rmmod|reload|status ) ACTION=$1;;
+                      -v|--verbose ) VERBOSE="TRUE";;
+                                 * ) KO_LIST+="$1 ";;
+   esac
+   shift 1;
+done
+
+###
+### Other global variables
+###
+ARCH=$(uname -a | awk '{print $NF}' | cut -f2 -d_)                     # Synology NAS arch
+DSM_VERSION=$(sed -n 's/^productversion="\(.*\)"/\1/p' /etc/VERSION)   # Synology DSM version
+SPK_CFG_PATH="/var/packages/${SPK}/target/etc"                         # SynoCommunity configuration path
+MPATH="/var/packages/${SPK}/target/lib/modules"                        # Kernel modules path
+FPATH="/var/packages/${SPK}/target/lib/firmware"                       # Device firmware path
+FPATH_SYS="/sys/module/firmware_class/parameters/path"                 # System module firmware path file index
+KVER=$(uname -r)                                                       # Running kernel version
+KPATH="${MPATH}/${ARCH}-${DSM_VERSION}/${KVER}/drivers"                # Full kernel modules path
+KO_LIST=$(echo ${KO_LIST} | xargs)                                     # List of kernel objects to enable|disable
+KO_LIST_CFG=""                                                         # List of kernel objects in configuration
+KO_FOUND=""                                                            # List of found kernel objects
+SYNOLOG=/tmp/synocli-kernelmodule.log                                  # Default log output file
+
+# Set LOG output using SPK
+[ -n "${SPK}" ] && SYNOLOG=/tmp/synocli-kernelmodule-${SPK}.log
+
+# All output to SYNOLOG, STDOUT to the screen
+exec > >(tee -a ${SYNOLOG}) 2> >(tee -a ${SYNOLOG} >/dev/null)
+
+# If module path does not exists, exit
+if [ ! -d ${MPATH} ]; then
+   usage
+   echo -ne "\nERROR: Module path [${MPATH}] does not exist or inaccessible...\n\n"
+   exit 1
+fi
+
+# Set kernel module .ko object base path
+# If does not exists, exit
+if [ ! -d ${KPATH} ]; then
+   usage
+   echo -ne "\nERROR: Kernel modules base path [${KPATH}] does not exist or inaccessible...\n\n"
+   exit 1
+fi
+
+if [ ${SPK_CFG} ]; then
+   # Check that configuration exists (if requested)
+   if [ ! -f ${SPK_CFG_PATH}/${SPK_CFG} ]; then
+      usage
+      echo -ne "\nERROR: Configuration file [${SPK_CFG_PATH}/${SPK_CFG}] does not exist or inaccessible...\n\n"
+      exit 1
+   fi	
+   # Check that configuration option exists
+   KO_LIST_CFG=$(sed -n "s/^${SPK_CFG_OPT}:\(.*\)/\1/p" ${SPK_CFG_PATH}/${SPK_CFG})
+   if [ "${KO_LIST_CFG}" ]; then
+      KO_LIST=$(echo ${KO_LIST} ${KO_LIST_CFG} | xargs -n1 | sort -u | xargs)
+   else
+      usage
+      echo -ne "\nERROR: Configuration option [${SPK_CFG_OPT}:] no found in file [${SPK_CFG_PATH}/${SPK_CFG}]...\n\n"
+      exit 1
+   fi	
+fi
+
+echo 179 $(sed -n "s/^${SPK_CFG_OPT}:\(.*\)/\1/p" ${SPK_CFG_PATH}/${SPK_CFG})
+
+# Find resulting kernel object path
+# and confirm that KO_LIST and KO_FOUND
+# do match
+ko_path_match
+
+# If verbose is set print default arguments
+[ ${VERBOSE} = "TRUE" ] && verbose
+
+[ ${HELP} = "TRUE" ] && usage && exit 0
 
 # load the requested modules
 load ()
@@ -144,7 +198,7 @@ load ()
    if [ -n "${FPATH}" ]; then
       echo -ne "\tAdd optional firmware path...\n"
 	  printf '%65s' "[${FPATH}]"
-      echo "${FPATH}" > ${SYS_FIRMWARE_PATH}
+      echo "${FPATH}" > ${FPATH_SYS}
 
       if [ $? -eq 0 ]; then
          echo -ne " OK\n"
@@ -155,7 +209,7 @@ load ()
    fi
 
    echo -ne "\t[insmod] kernel modules...\n"
-   for ko in $KO
+   for ko in $KO_FOUND
    do
       module=$(echo "${ko}" | sed -e 's/.*\///' -e 's/-/_/' -e 's/\.ko//')
       printf '%40s %-25s' $(basename $ko) "[$module]"
@@ -185,7 +239,7 @@ unload ()
 
    # Unload drivers in reverse order
    echo -ne "\t[rmmod] kernel modules...\n"
-   for item in $KO; do echo $item; done | tac | while read ko
+   for item in $KO_FOUND; do echo $item; done | tac | while read ko
    do
       module=$(echo "${ko}" | sed -e 's/.*\///' -e 's/-/_/g' -e 's/\.ko//')
       printf '%40s %-25s' $(basename $ko) "[$module]"
@@ -203,7 +257,7 @@ unload ()
    # Remove firmware path to running kernel
    if [ -n "${FPATH}" ]; then
       echo -ne "\tUnloading of optional firmware path...\n"
-      echo "" > ${SYS_FIRMWARE_PATH}
+      echo "" > ${FPATH_SYS}
       error=$?
    fi
 
@@ -217,7 +271,7 @@ status ()
    error=0
 
    echo -ne "\t[status] kernel modules...\n"
-   for ko in $KO
+   for ko in $KO_FOUND
    do
       module=$(echo "${ko}" | sed -e 's/.*\///' -e 's/-/_/g' -e 's/\.ko//')
       printf '%40s %-25s' $(basename $ko) "[$module]"
@@ -236,7 +290,7 @@ status ()
       echo -ne "\tStatus of optional firmware path...\n"
 	  printf '%65s' "[${FPATH}]"
 
-      grep -q ${FPATH} ${SYS_FIRMWARE_PATH}
+      grep -q ${FPATH} ${FPATH_SYS}
       if [ $? -eq 0 ]; then
          echo -ne " OK\n"
       else
@@ -248,58 +302,41 @@ status ()
    return $error
 }
 
-# Fis Kernel object path in order to allow
-# receiving full path kernel module or
-# kernel object name only
-fix_ko_path
-
-# Print details if in debug mode
-if [ ${VERBOSE} = "TRUE" ]; then
-   printf '%20s %s\n' "" "$0 (verbose)" 1>&2
-   printf '%60s %s\n' "SynoCommunity kernel driver package name (PKGNAME)" "[${PKGNAME}]" 1>&2
-   printf '%60s %s\n' "Synology NAS arch (ARCH)" "[${ARCH}]" 1>&2
-   printf '%60s %s\n' "Synology DSM version (DSM_VERSION)" "[${DSM_VERSION}]" 1>&2
-   printf '%60s %s\n' "Module action insmod|rmmod|reload|status (ACTION)" "[${ACTION}]" 1>&2
-   printf '%60s %s\n' "Kernel modules path (MPATH)" "[${MPATH}]" 1>&2
-   printf '%60s %s\n' "Device firmware path (FPATH)" "[${FPATH}]" 1>&2
-   printf '%60s %s\n' "Running kernel version (KVER)" "[${KVER}]" 1>&2
-   printf '%60s %s\n' "Kernel objects (KO)" "[${KO}]" 1>&2
-fi
 
 case $ACTION in
     insmod)
        if status; then
-           echo ${PKGNAME} is already running
+           echo ${SPK} is already running
            exit 0
        else
-           echo Starting ${PKGNAME} ...
+           echo Starting ${SPK} ...
            load
            exit $?
        fi
        ;;
     rmmod)
        if status; then
-           echo Stopping ${PKGNAME} ...
+           echo Stopping ${SPK} ...
            unload
            exit $?
        else
-           echo ${PKGNAME} is not running
+           echo ${SPK} is not running
            exit 0
        fi
        ;;
     reload)
-       echo -ne "---\nReloading ${PKGNAME} package...\n"
+       echo -ne "---\nReloading ${SPK} package...\n"
        unload
        load
        exit $?
        ;;
     status)
-       echo -ne "---\nStatus of ${PKGNAME} package...\n"
+       echo -ne "---\nStatus of ${SPK} package...\n"
        if status; then
-           echo ${PKGNAME} is running
+           echo ${SPK} is running
            exit 0
        else
-           echo ${PKGNAME} is not running
+           echo ${SPK} is not running
            exit 1
        fi
        ;;
