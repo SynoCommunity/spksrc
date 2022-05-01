318bcf3c04ac8f54e80b06599297f12ab56073da
Smaarn <antoine.malliarakis@gmail.com>
2022-05-01 19:31:59 +0200

    Fixed setup to handle configuration file

--- spk/memcached/src/service-setup.sh
+++ spk/memcached/src/service-setup.sh
@@ -6,10 +6,10 @@ WEB_DIR="/var/services/web"
 PATH="${SYNOPKG_PKGDEST}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
 MEMCACHED="${SYNOPKG_PKGDEST}/bin/memcached"
 SERVICE_COMMAND="${MEMCACHED} -d -m `awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo` -P ${PID_FILE}"
+CONFIG_FILE="${WEB_DIR}/phpMemcachedAdmin/Config/Memcache.php"
 
 service_postinst ()
 {
-
     if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
 
       # Install the web interface
@@ -18,6 +18,18 @@ service_postinst ()
       chown http "${WEB_DIR}/phpMemcachedAdmin/Temp/"
   
       chown http "${WEB_DIR}/phpMemcachedAdmin/Config/"
+    fi
+
+    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+      cp "${WEB_DIR}/phpMemcachedAdmin/Config/Memcache.sample.php" "${CONFIG_FILE}"
+      chgrp http "${CONFIG_FILE}"
+      chmod g+w "${CONFIG_FILE}"
+    elif [ -f "${TMP_DIR}/Config/Memcache.php" ]; then
+      cp -p "${TMP_DIR}/Config/Memcache.php" "${CONFIG_FILE}"
+    else
+      cp "${WEB_DIR}/phpMemcachedAdmin/Config/Memcache.sample.php" "${CONFIG_FILE}"
+      chgrp http "${CONFIG_FILE}"
+      chmod g+w "${CONFIG_FILE}"
     fi
 
     return 0
@@ -38,7 +50,17 @@ service_preupgrade ()
     if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
       # Remove the web interface
       rm -fr "${WEB_DIR}/phpMemcachedAdmin"
+    else
+      if [ -f "${WEB_DIR}/phpMemcachedAdmin/Config/Memcache.php" ]; then
+        mkdir -p "${TMP_DIR}/Config/"
+        cp -p "${WEB_DIR}/phpMemcachedAdmin/Config/Memcache.php" "${TMP_DIR}/Config/Memcache.php"
+      fi
     fi
 
     return 0
+}
+
+service_postupgrade ()
+{
+  
 }
