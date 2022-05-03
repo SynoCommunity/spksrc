189b190e5813fb6de6cfc5e7f6ebdbf184b787b4
André Colomb <src@andre.colomb.de>
2022-05-03 22:21:23 +0200

    syncthing: Avoid overwriting newer binary from internal updater (fixes #5142) (#5181)
    * syncthing: Define shortcut variable for binary location.
    * syncthing: Simplify HOME setting in service-setup.
    The variable is needed for any function that tries to call the
    syncthing binary.  So define and export it once at the top instead of
    in every function that needs it.
    * syncthing: Retain installed binary if newer than the packaged one.
    Check the version strings from the installed and packaged binaries
    respectively during the preupgrade step.  Keep a backup in the
    SYNOPKG_TEMP_UPGRADE_FOLDER if necessary and restore that during the
    postupgrade step.
    * Incorporate latest upstream release v1.20.0.
    * syncthing: Bump dependency to Go 1.18 and remove native/go_1.17.

--- spk/syncthing/src/service-setup.sh
+++ spk/syncthing/src/service-setup.sh
@@ -1,23 +1,25 @@
 # syncthing service definition
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/syncthing serve --home=${SYNOPKG_PKGVAR}"
+SYNCTHING="${SYNOPKG_PKGDEST}/bin/syncthing"
+SERVICE_COMMAND="${SYNCTHING} serve --home=${SYNOPKG_PKGVAR}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
 GROUP="sc-syncthing"
 
+# Required to run any syncthing command: set $HOME environment variable
+HOME=${SYNOPKG_PKGVAR}
+export HOME
+
+
 set_credentials() {
     if [ -n "${wizard_username}" -a -n "${wizard_password}" ]; then
         # Password needs to be hashed for config entry
-        ${SYNOPKG_PKGDEST}/bin/syncthing generate --home=${SYNOPKG_PKGVAR} \
+        ${SYNCTHING} generate --home=${SYNOPKG_PKGVAR} \
             --gui-user="${wizard_username}" --gui-password="${wizard_password}"
     fi
 }
 
 service_postinst() {
-    # Required: set $HOME environment variable
-    HOME=${SYNOPKG_PKGVAR}
-    export HOME
-
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         set_credentials
     fi
@@ -37,8 +39,33 @@ service_prestart ()
         ln -sf ${cert_dir}/cert.pem ${SYNOPKG_PKGVAR}/https-cert.pem
         ln -sf ${cert_dir}/privkey.pem ${SYNOPKG_PKGVAR}/https-key.pem
     fi
+}
 
-    # Required: set $HOME environment variable
-    HOME=${SYNOPKG_PKGVAR}
-    export HOME
+version_le()
+{
+    if printf '%s\n' "$1" "$2" | sort -VC ; then
+        return 0 # true since we are returning an error code
+    fi
+    return 255 # everything else is false
+}
+
+service_preupgrade()
+{
+    CUR_VER=$(${SYNCTHING} --version | awk '{print $2}' | awk --field-separator=- '{print $1}')
+    PKG_VER=$(${SYNOPKG_PKGINST_TEMP_DIR}/bin/syncthing --version | awk '{print $2}' | awk --field-separator=- '{print $1}')
+    if version_le $CUR_VER $PKG_VER; then
+	install_log "Package ${PKG_VER} is newer than or same as installed binary ${CUR_VER}"
+    else
+	install_log "Installed binary ${CUR_VER} is newer than package ${PKG_VER}, preserving a backup"
+	mkdir ${SYNOPKG_TEMP_UPGRADE_FOLDER}/bin
+        cp -p ${SYNCTHING} ${SYNOPKG_TEMP_UPGRADE_FOLDER}/bin/syncthing
+    fi
+}
+
+service_postupgrade()
+{
+    if [ -x ${SYNOPKG_TEMP_UPGRADE_FOLDER}/bin/syncthing ]; then
+	install_log "Restoring preserved binary backup"
+	mv -f ${SYNOPKG_TEMP_UPGRADE_FOLDER}/bin/syncthing ${SYNCTHING}
+    fi
 }
