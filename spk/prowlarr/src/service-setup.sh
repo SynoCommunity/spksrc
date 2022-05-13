6c375758c98c490d10bf154a067494df601193ea
hgy59 <hpgy59@gmail.com>
2022-05-13 12:04:50 +0200

    fix prowlarr
    - fix prowlarr PID_FILE
    - use date for package version since internal updater is used
    - update prowlarr to v0.2.0.1678

--- spk/prowlarr/src/service-setup.sh
+++ spk/prowlarr/src/service-setup.sh
@@ -1,15 +1,14 @@
 
 # Prowlarr service setup
-
 PROWLARR="${SYNOPKG_PKGDEST}/share/Prowlarr/bin/Prowlarr"
 
 # Prowlarr uses custom Config and PID directories
 HOME_DIR="${SYNOPKG_PKGVAR}"
-CONFIG_DIR="${SYNOPKG_PKGVAR}/.config"
+CONFIG_DIR="${HOME_DIR}/.config"
+PID_FILE="${CONFIG_DIR}/Prowlarr/prowlarr.pid"
 
 SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${PROWLARR}"
 SVC_BACKGROUND=y
-SVC_WRITE_PID=y
 
 service_postinst ()
 {
