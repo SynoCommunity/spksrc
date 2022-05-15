01e03e9ef3eaf3dda8bdc0433aa0d36e2f57c99f
hgy59 <hpgy59@gmail.com>
2022-05-15 17:24:46 +0200

    radarr: prevent downgrade on package update (#5273)
    * radarr: prevent downgrade on package update
    - backup and restore existing radarr distribution to prevent downgrade
    - adjust upgrade wizard (fre still needs translation)
    * update french update wizard
    * radarr: use explicit data folder

--- spk/radarr/src/service-setup.sh
+++ spk/radarr/src/service-setup.sh
@@ -5,7 +5,8 @@ RADARR="${SYNOPKG_PKGDEST}/share/Radarr/bin/Radarr"
 # Radarr uses custom Config and PID directories
 HOME_DIR="${SYNOPKG_PKGVAR}"
 CONFIG_DIR="${HOME_DIR}/.config"
-PID_FILE="${CONFIG_DIR}/Radarr/radarr.pid"
+RADARR_CONFIG_DIR="${CONFIG_DIR}/Radarr"
+PID_FILE="${RADARR_CONFIG_DIR}/radarr.pid"
 
 # SPK_REV 15 has it in the wrong place for DSM 7
 LEGACY_CONFIG_DIR="${SYNOPKG_PKGDEST}/var/.config"
@@ -13,14 +14,14 @@ LEGACY_CONFIG_DIR="${SYNOPKG_PKGDEST}/var/.config"
 GROUP="sc-download"
 LEGACY_GROUP="sc-media"
 
-SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${RADARR}"
+SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${RADARR} -nobrowser -data=${RADARR_CONFIG_DIR}"
 SVC_BACKGROUND=y
 
 service_postinst ()
 {
     # Move config.xml to .config
-    mkdir -p ${CONFIG_DIR}/Radarr
-    mv ${SYNOPKG_PKGDEST}/app/config.xml ${CONFIG_DIR}/Radarr/config.xml
+    mkdir -p ${RADARR_CONFIG_DIR}
+    mv ${SYNOPKG_PKGDEST}/app/config.xml ${RADARR_CONFIG_DIR}/config.xml
     
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
         set_unix_permissions "${CONFIG_DIR}"
@@ -42,13 +43,28 @@ service_preupgrade ()
             fi
         fi
     fi
+    
+    ## never update Radarr distribution, use internal updater only
+    [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup ] && rm -rf ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup
+    echo "Backup existing distribution to ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+    mkdir -p ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup 2>&1
+    rsync -aX ${SYNOPKG_PKGDEST}/share ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/ 2>&1
 }
 
 service_postupgrade ()
 {
-    # Make Radarr do an update check on start to avoid possible Radarr
-    # downgrade when synocommunity package is updated
-    touch ${CONFIG_DIR}/Radarr/update_required
+    ## restore Radarr distribution
+    if [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share ]; then
+        echo "Restore previous distribution from ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+        rm -rf ${SYNOPKG_PKGDEST}/share/Radarr/bin 2>&1
+        # prevent overwrite of updated package_info
+        rsync -aX --exclude=package_info ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/ ${SYNOPKG_PKGDEST}/share 2>&1
+    else
+        echo "Set update required"
+        # Make Radarr do an update check on start to avoid possible Radarr
+        # downgrade when synocommunity package is updated
+        touch ${RADARR_CONFIG_DIR}/update_required 2>&1
+    fi
 
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
         set_unix_permissions "${CONFIG_DIR}"
