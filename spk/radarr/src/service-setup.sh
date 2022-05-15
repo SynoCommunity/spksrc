125947ba62e1ef3a1875a7449086fc66e5385402
hgy59 <hpgy59@gmail.com>
2022-05-15 23:19:08 +0200

    Radarr: update service setup and permission info (#5274)
    * update radarr service setup
    - create update_required even current version does not handle it
    - remove old sc-media group migration
    * adjust permission information in wizards
    - DSM 7 does not support sc-download group
    - wiki page explains permission management

--- spk/radarr/src/service-setup.sh
+++ spk/radarr/src/service-setup.sh
@@ -11,8 +11,8 @@ PID_FILE="${RADARR_CONFIG_DIR}/radarr.pid"
 # SPK_REV 15 has it in the wrong place for DSM 7
 LEGACY_CONFIG_DIR="${SYNOPKG_PKGDEST}/var/.config"
 
+# for DSM < 7 only:
 GROUP="sc-download"
-LEGACY_GROUP="sc-media"
 
 SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${RADARR} -nobrowser -data=${RADARR_CONFIG_DIR}"
 SVC_BACKGROUND=y
@@ -22,13 +22,14 @@ service_postinst ()
     # Move config.xml to .config
     mkdir -p ${RADARR_CONFIG_DIR}
     mv ${SYNOPKG_PKGDEST}/app/config.xml ${RADARR_CONFIG_DIR}/config.xml
-    
+
+    echo "Set update required"
+    # Make Radarr do an update check on start to avoid possible Radarr
+    # downgrade when synocommunity package is updated
+    touch ${RADARR_CONFIG_DIR}/update_required 2>&1
+
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
         set_unix_permissions "${CONFIG_DIR}"
-
-        # If nessecary, add user also to the old group before removing it
-        syno_user_add_to_legacy_group "${EFF_USER}" "${USER}" "${LEGACY_GROUP}"
-        syno_user_add_to_legacy_group "${EFF_USER}" "${USER}" "users"
     fi
 }
 
@@ -43,7 +44,7 @@ service_preupgrade ()
             fi
         fi
     fi
-    
+
     ## never update Radarr distribution, use internal updater only
     [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup ] && rm -rf ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup
     echo "Backup existing distribution to ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
@@ -59,14 +60,5 @@ service_postupgrade ()
         rm -rf ${SYNOPKG_PKGDEST}/share/Radarr/bin 2>&1
         # prevent overwrite of updated package_info
         rsync -aX --exclude=package_info ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/ ${SYNOPKG_PKGDEST}/share 2>&1
-    else
-        echo "Set update required"
-        # Make Radarr do an update check on start to avoid possible Radarr
-        # downgrade when synocommunity package is updated
-        touch ${RADARR_CONFIG_DIR}/update_required 2>&1
-    fi
-
-    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
-        set_unix_permissions "${CONFIG_DIR}"
     fi
 }
