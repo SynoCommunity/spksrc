6fb84e67bc0ccee1a54f5acece6ef4821d79dec9
Vincent Fortier <th0ma7@gmail.com>
2022-05-19 18:25:28 +0000

    archs.mk: ARMv7L was missing from 32-bit variable

--- mk/spksrc.archs.mk
+++ mk/spksrc.archs.mk
@@ -28,7 +28,7 @@ PPC_ARCHS = powerpc ppc824x ppc853x ppc854x qoriq
 i686_ARCHS = evansport
 x64_ARCHS = $(GENERIC_x64_ARCH) apollolake avoton braswell broadwell broadwellnk bromolow cedarview denverton dockerx64 geminilake grantley purley kvmx64 v1000 x86 x86_64
 
-32bit_ARCHS = $(ARMv5_ARCHS) $(ARMv7_ARCHS) $(i686_ARCHS) $(PPC_ARCHS)
+32bit_ARCHS = $(ARMv5_ARCHS) $(ARMv7_ARCHS) $(ARMv7L_ARCHS) $(i686_ARCHS) $(PPC_ARCHS)
 64bit_ARCHS = $(ARMv8_ARCHS) $(x64_ARCHS)
 
 # Arch groups
