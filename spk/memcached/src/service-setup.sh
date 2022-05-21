fd543451fb5b447e6d57c9df94b3334d573cd23f
hgy59 <hpgy59@gmail.com>
2022-05-21 15:59:54 +0200

    memcached: rework (2)
    - use custom app/config file to link with phpMemcachdAdmin
    - update icon
    - update config backup/restore and limit to DSM < 7
    - set owner of config file on DSM < 7

--- spk/memcached/src/service-setup.sh
+++ spk/memcached/src/service-setup.sh
@@ -1,57 +1,57 @@
-#!/bin/sh
 
-# Others
-# Only used for DSM 6
-DSM6_WEB_DIR="/var/services/web"
-WEB_DIR="/var/services/web_packages"
 if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
-WEB_DIR="${DSM6_WEB_DIR}"
+    # Only used for DSM 6
+    WEB_DIR="/var/services/web"
+    CONFIG_DIR="${WEB_DIR}/phpMemcachedAdmin/Config"
+    CONFIG_FILE="${CONFIG_DIR}/Memcache.php"
+    CONFIG_BACKUP="${TMP_DIR}/Config"
 fi
 
-PATH="${SYNOPKG_PKGDEST}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 MEMCACHED="${SYNOPKG_PKGDEST}/bin/memcached"
-SERVICE_COMMAND="${MEMCACHED} -d -m `awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo` -P ${PID_FILE}"
-CONFIG_DIR="${WEB_DIR}/phpMemcachedAdmin/Config"
-CONFIG_BACKUP="${TMP_DIR}/Config"
+MEMORY=$(awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo)
+SERVICE_COMMAND="${MEMCACHED} -d -m ${MEMORY} -P ${PID_FILE}"
 
 service_postinst ()
 {
     if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
+    
+        # Install the web interface
+        cp -Rpv "${SYNOPKG_PKGDEST}/share/phpMemcachedAdmin" "${WEB_DIR}"
+    
+        if [ -d "${CONFIG_BACKUP}" ]; then
+            tar -cf - -C "${CONFIG_BACKUP}" --exclude="Memcache.sample.php" . | tar -xvf - -C "${CONFIG_DIR}"
+        fi
 
-      # Install the web interface
-      cp -Rpv "${SYNOPKG_PKGDEST}/share/phpMemcachedAdmin" "${WEB_DIR}"
-  
-      chown http "${WEB_DIR}/phpMemcachedAdmin/Temp/"
-  
-      chown http "${WEB_DIR}/phpMemcachedAdmin/Config/"
+        chown http "${WEB_DIR}/phpMemcachedAdmin/Temp/"
+        chown http "${CONFIG_DIR}"
+        chown http "${CONFIG_FILE}"
+    fi
+}
+
+service_preuninst ()
+{
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
+        if [ "${SYNOPKG_PKG_STATUS}" == "UPGRADE" ]; then
+            if [ -d "${TMP_DIR}/Config/" ]; then
+                cp -pRv "${TMP_DIR}/Config" "${TMP_DIR}"
+            fi
+        fi
     fi
 }
 
 service_postuninst ()
 {
     if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
-      # Remove the web interface
-      rm -fr "${WEB_DIR}/phpMemcachedAdmin"
-    fi
-}
-
-service_restore () 
-{
-    if [ -d "${CONFIG_BACKUP}" ]; then 
-      tar -cf - -C "${CONFIG_BACKUP}" . | tar -xf - -C "${CONFIG_DIR}"
-      rm -rvf "${CONFIG_BACKUP}"
+        # Remove the web interface
+        rm -fr "${WEB_DIR}/phpMemcachedAdmin"
     fi
 }
 
 service_preupgrade ()
 {
-    if [ -d "${CONFIG_DIR}" ]; then
-      mkdir -p "${CONFIG_BACKUP}"
-      tar -cf - -C "${CONFIG_DIR}" --exclude="Memcache.sample.php" . | tar -xf - -C "${CONFIG_BACKUP}"
-    fi
     if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
-      # Remove the web interface
-      rm -fr "${WEB_DIR}/phpMemcachedAdmin"
+        # Remove the web interface
+        rm -fr "${WEB_DIR}/phpMemcachedAdmin"
     fi
 }
-
