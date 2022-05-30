a88af71c766344adbb5f839274a4a8ff0825ab16
hgy59 <hpgy59@gmail.com>
2022-05-30 06:41:24 +0200

    icecast: update dependencies and update for DSM7 (#4628)
    * update icecast dependencies and update for DSM7
    - adjust package paths in icecast.xml (avoid use of /usr/local/{package})
    - update ezstream to v1.0.2 (and add cross/libcheck for build)
    - update taglib to v1.12
    - install favicon.ico
    - add ezstream cli tools to path (SPK_COMMANDS)
    * install and configure custom mime.types file
    - provide mime.types for file serving
    - use dsm 7 compatible var folder
    * optimize code
    - optimize code of icecast and ezstream
    - update changelog as openssl is updated too
    * build only shared libraries of cross/speex
    - without the build of static libraries the binaries are not built and libogg is not required
    * disable build of static libraries
    - static libraries are obsolete here
    * update cross/libshout
    * icecast: add service port
    - add service port to create firewall rules
    - define DSM UI url for admin page

--- spk/icecast/src/service-setup.sh
+++ spk/icecast/src/service-setup.sh
@@ -1,5 +1,6 @@
-CFG_FILE="${SYNOPKG_PKGDEST}/var/icecast.xml"
-PATH="${SYNOPKG_PKGDEST}:${PATH}"
+
+# icecast service setup
+CFG_FILE="${SYNOPKG_PKGVAR}/icecast.xml"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/icecast -c ${CFG_FILE}"
 SVC_BACKGROUND=y
 SVC_WRITE_PID=y
@@ -11,5 +12,3 @@ service_postinst ()
     sed -i -e "s/@username@/${wizard_ic_username:=admin}/g" ${CFG_FILE}
     sed -i -e "s/@password@/${wizard_ic_password:=changepassword}/g" ${CFG_FILE}
 }
-
-
