1fbddfe29fd1278ddbfa6761876da1507ef0b532
Sebastian Schmidt <publicarray@users.noreply.github.com>
2022-06-13 01:40:09 +0000

    Jellyfin 10.8 and Jackett with .net 6.0 (#5311)
    * update jellyfin to 10.8
    
    * update dotnet
    
    dotnet 5.0 is not supported anymore
    https://dotnet.microsoft.com/en-us/download/dotnet
    
    * update node and npm
    
    * update jackett
    
    * add firewall manually
    
    Previously the framework would just add the necessary json
    
    * Fix jackett loosing configuration
    migrate folder to appdata
    
    * update change logs
    
    * Jackett 0.20.1043 Update using .NET 6.0 and adding alternate tmpdir
    
    * New maintainer: stevenliuit
    
    Co-authored-by: Tim Potter <musikgoat@gmail.com>

--- spk/jackett/src/service-setup.sh
+++ spk/jackett/src/service-setup.sh
@@ -1,7 +1,22 @@
 
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 JACKETT="${SYNOPKG_PKGDEST}/share/jackett"
-HOME_DIR="${SYNOPKG_PKGDEST}/var"
+HOME_DIR="${SYNOPKG_PKGVAR}"
 
 SERVICE_COMMAND="env HOME=${HOME_DIR} PATH=${PATH} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${JACKETT} --PIDFile ${PID_FILE}"
 SVC_BACKGROUND=y
+
+service_preupgrade ()
+{
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -ge 7 ]; then
+        CONFIG_DIR="${HOME_DIR}/.config"
+        LEGACY_CONFIG_DIR="${SYNOPKG_PKGDEST}/var/.config"
+        # ensure user data is in @appdata folder
+        if [ -d "${LEGACY_CONFIG_DIR}" ]; then
+            if [ "$(realpath ${LEGACY_CONFIG_DIR})" != "$(realpath ${CONFIG_DIR})" ]; then
+                echo "Move ${LEGACY_CONFIG_DIR} to ${CONFIG_DIR}"
+                mv ${LEGACY_CONFIG_DIR} ${CONFIG_DIR} 2>&1
+            fi
+        fi
+    fi
+}
