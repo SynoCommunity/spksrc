6bc6453a5159e181abad78129ed6ca416fd5ba89
hgy59 <hpgy59@gmail.com>
2022-06-14 23:54:39 +0200

    adjust config file on update
    - install new config file as *.new on package upgrade on DSM < 7
    - add upgrade wizard when updating from 1.x to 2.x to inform about manual config file migration

--- spk/mosquitto/src/wizard/upgrade_uifile.sh
+++ spk/mosquitto/src/wizard/upgrade_uifile.sh
@@ -9,9 +9,11 @@ cat <<EOF > $SYNOPKG_TEMP_LOGFILE
 [{
     "step_title": "Update Configuration from ${SYNOPKG_OLD_PKGVER}",
     "items": [{
-        "desc": "The configuration file for Mosquitto v2 will be installed as <strong>/var/packages/mosquitto/var/mosquitto.conf.new</strong>. Please make a copy of your current mosquitto.conf file and use the new configuration file as template, apply your custom settings and save it as mosquitto.conf."
+        "desc": "The configuration file for Mosquitto v2 will be installed as <code>/var/packages/mosquitto/var/mosquitto.conf.new</code>."
       }, {
-        "desc": "Note: the new default configuration is for anonymous access and restricted to localhost (127.0.0.1)."
+        "desc": "After the package is updated, please make a copy of your current <code>mosquitto.conf</code> file and use the new configuration file as a template, apply your custom settings and save it as <code>mosquitto.conf</code>. Finally restart mosquitto to apply the updated configuration."
+      }, {
+        "desc": "<b>Note:</b> The new default configuration is for anonymous access and restricted to localhost (127.0.0.1)."
       }
     ]
   }
