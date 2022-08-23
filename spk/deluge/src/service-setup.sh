438173487b41ebf41cf33246aad6edace137f586
Vincent Fortier <th0ma7@gmail.com>
2022-08-23 20:21:07 +0000

    deluge: Enable multi-daemon in non-background mode

--- spk/deluge/src/service-setup.sh
+++ spk/deluge/src/service-setup.sh
@@ -1,22 +1,43 @@
 PYTHON_DIR="/var/packages/python310/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
-CORE_CFG_FILE="${SYNOPKG_PKGVAR}/core.conf"
-WATCH_CFG_FILE="${SYNOPKG_PKGVAR}/autoadd.conf"
-
-# Required variables to start both processes
-# We use Generic Service variables for main deamon
-DELUGED="${SYNOPKG_PKGDEST}/env/bin/deluged"
-DELUGE_WEB="${SYNOPKG_PKGDEST}/env/bin/deluge-web"
-CFG_DIR="${SYNOPKG_PKGVAR}"
+#
+CFG_PATH="${SYNOPKG_PKGVAR}"
+CFG_CORE="${SYNOPKG_PKGVAR}/core.conf"
+CFG_WATCH="${SYNOPKG_PKGVAR}/autoadd.conf"
+#
 PYTHON_EGG_CACHE="${SYNOPKG_PKGDEST}/env/cache"
-#DELUGE_WEB_PID="${SYNOPKG_PKGDEST}/var/deluge-web.pid"
-DELUGE_WEB_LOG="${SYNOPKG_PKGDEST}/var/deluge-web.log"
-
-DAEMON_DELUGED="${DELUGED} --config ${CFG_DIR} --logfile ${LOG_FILE} --loglevel info --pidfile ${PID_FILE}"
-#DAEMON_DELUGE_WEB="${DELUGE_WEB} --config ${CFG_DIR} --logfile ${DELUGE_WEB_LOG} --loglevel info --pidfile ${DELUGE_WEB_PID}"
-DAEMON_DELUGE_WEB="${DELUGE_WEB} --config ${CFG_DIR} --logfile ${DELUGE_WEB_LOG} --loglevel info --pidfile ${PID_FILE}"
-SERVICE_COMMAND[0]="${DAEMON_DELUGED}"
-SERVICE_COMMAND[1]="${DAEMON_DELUGE_WEB}"
+#
+# deluded & deluge-web options:
+# -c --config
+# -l --logfile
+# -L --loglevel
+# -d --do-not-daemonize
+# -P --pidfile
+#
+DELUGED="${SYNOPKG_PKGDEST}/env/bin/deluged"
+DELUGED_LOG="${SYNOPKG_PKGDEST}/var/deluged.log"
+DELUGED_PID="${SYNOPKG_PKGDEST}/var/deluged.pid"
+#DELUGED_DAEMON="${DELUGED} --config ${CFG_PATH} --logfile ${DELUGED_LOG} --loglevel info --logrotate --pidfile ${DELUGED_PID}"
+#DELUGED_DAEMON="${DELUGED} --config ${CFG_PATH} --logfile ${DELUGED_LOG} --loglevel info --logrotate --pidfile ${PID_FILE}"
+#DELUGED_DAEMON="${DELUGED} --config ${CFG_PATH} --logfile ${DELUGED_LOG} --loglevel info --logrotate"
+#DELUGED_DAEMON="${DELUGED} --config ${CFG_PATH} --logfile ${LOG_FILE} --loglevel info --pidfile ${PID_FILE}"
+#
+DELUGEWEB="${SYNOPKG_PKGDEST}/env/bin/deluge-web"
+DELUGEWEB_LOG="${SYNOPKG_PKGDEST}/var/deluge-web.log"
+DELUGEWEB_PID="${SYNOPKG_PKGDEST}/var/deluge-web.pid"
+#DELUGEWEB_DAEMON="${DELUGEWEB} --config ${CFG_PATH} --logfile ${DELUGEWEB_LOG} --loglevel info --logrotate --pidfile ${DELUGEWEB_PID}"
+#DELUGEWEB_DAEMON="${DELUGEWEB} --config ${CFG_PATH} --logfile ${DELUGEWEB_LOG} --loglevel info --logrotate --pidfile ${PID_FILE}"
+#DELUGEWEB_DAEMON="${DELUGEWEB} --config ${CFG_PATH} --logfile ${DELUGEWEB_LOG} --loglevel info --logrotate"
+#DELUGEWEB_DAEMON="${DELUGEWEB} --config ${CFG_PATH} --logfile ${LOG_FILE} --loglevel info --pidfile ${PID_FILE}"
+#
+# Startup OPTION 1:
+SVC_BACKGROUND=yes
+SVC_WRITE_PID=yes
+DELUGEWEB_DAEMON="${DELUGEWEB} -c ${CFG_PATH} ${DELUGE_ARGS} -l ${DELUGEWEB_LOG} -L info --logrotate -P ${DELUGEWEB_PID} -d"
+DELUGED_DAEMON="${DELUGED} -c ${CFG_PATH} ${DELUGE_ARGS} -l ${DELUGED_LOG} -L info --logrotate -P ${DELUGED_PID} --d"
+#
+SERVICE_COMMAND[0]="${DELUGED_DAEMON}"
+SERVICE_COMMAND[1]="${DELUGEWEB_DAEMON}"
 
 GROUP="sc-download"
 
@@ -53,7 +74,7 @@ service_postinst ()
 
     # Edit the configuration files according to the wizard
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        for cfg_file in "${CORE_CFG_FILE} ${WATCH_CFG_FILE}"; do
+        for cfg_file in "${CFG_CORE} ${CFG_WATCH}"; do
             sed -i -e "s|@download_dir@|${wizard_download_dir:=/volume1/downloads}|g" ${cfg_file}
             if [ -d "${wizard_complete_dir}" ]; then
                 sed -i -e "s|@complete_dir_enabled@|true|g" ${cfg_file}
@@ -64,10 +85,10 @@ service_postinst ()
             fi
         done
         if [ -d "${wizard_watch_dir}" ]; then
-            sed -i -e "s|\"enabled_plugins\": \[\],|\"enabled_plugins\": \[\n        \"AutoAdd\"\n    \], \n|g" ${CORE_CFG_FILE}
-            sed -i -e "s|@watch_dir@|${wizard_watch_dir}|g" ${WATCH_CFG_FILE}
+            sed -i -e "s|\"enabled_plugins\": \[\],|\"enabled_plugins\": \[\n        \"AutoAdd\"\n    \], \n|g" ${CFG_CORE}
+            sed -i -e "s|@watch_dir@|${wizard_watch_dir}|g" ${CFG_WATCH}
         else
-            sed -i -e "/@watch_dir@/d" ${WATCH_CFG_FILE}
+            sed -i -e "/@watch_dir@/d" ${CFG_WATCH}
         fi
     fi
 
@@ -80,20 +101,20 @@ service_postupgrade ()
 {
     # Needed to force correct permissions, during update from prior version
     # Extract the right paths from config file
-    if [ -r "${CORE_CFG_FILE}" ]; then
-        DOWNLOAD_DIR=`sed -n 's/.*"download_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CORE_CFG_FILE}`
-        COMPLETE_DIR=`sed -n 's/.*"move_completed_path"[ ]*:[ ]*"\(.*\)",/\1/p' ${CORE_CFG_FILE}`
-        WATCHED_DIR=`sed -n 's/.*"autoadd_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CORE_CFG_FILE}`
+    if [ -r "${CFG_CORE}" ]; then
+        download=`sed -n 's/.*"download_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_CORE}`
+        complete=`sed -n 's/.*"move_completed_path"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_CORE}`
+        watch=`sed -n 's/.*"autoadd_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_CORE}`
 
         # Apply permissions
-        if [ -n "${DOWNLOAD_DIR}" ] && [ -d "${DOWNLOAD_DIR}" ]; then
-            set_syno_permissions "${DOWNLOAD_DIR}" "${GROUP}"
+        if [ -n "${download}" ] && [ -d "${download}" ]; then
+            set_syno_permissions "${download}" "${GROUP}"
         fi
-        if [ -n "${COMPLETE_DIR}" ] && [ -d "${COMPLETE_DIR}" ]; then
-            set_syno_permissions "${COMPLETE_DIR}" "${GROUP}"
+        if [ -n "${complete}" ] && [ -d "${complete}" ]; then
+            set_syno_permissions "${complete}" "${GROUP}"
         fi
-        if [ -n "${WATCHED_DIR}" ] && [ -d "${WATCHED_DIR}" ]; then
-            set_syno_permissions "${WATCHED_DIR}" "${GROUP}"
+        if [ -n "${watch}" ] && [ -d "${watch}" ]; then
+            set_syno_permissions "${watch}" "${GROUP}"
         fi
     fi
 }
