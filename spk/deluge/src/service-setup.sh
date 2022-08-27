18f071236b32e58a1748bbca9eb498ff2ff68287
Vincent Fortier <th0ma7@gmail.com>
2022-08-27 21:10:48 +0000

    deluge: Refining install+upgrade scripts and wizard

--- spk/deluge/src/service-setup.sh
+++ spk/deluge/src/service-setup.sh
@@ -35,6 +35,33 @@ if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
     GROUP="sc-download"
 fi
 
+deluge_default_install ()
+{
+    # Edit the configuration according to the wizard
+    shared_folder="${wizard_volume:=/volume1}/${wizard_download_dir:=downloads}"
+    sed -i -e "s|@shared_folder@|${shared_folder}|g" ${CFG_FILE}
+    sed -i -e "s|@script_dir@|${SYNOPKG_PKGVAR}/scripts|g" ${CFG_FILE}
+
+    # Create download directories
+    mkdir -p -m 0775 "${shared_folder}/incomplete"
+    mkdir -p -m 0775 "${shared_folder}/complete"
+    mkdir -p -m 0775 "${shared_folder}/watch"
+
+    # Edit the configuration files according to the wizard
+    for cfg_file in "${CFG_FILE} ${CFG_WATCH}"; do
+        # Default download directory
+        sed -i -e "s|@download_dir@|${shared_folder}/incomplete|g" ${cfg_file}
+        # Complete directory enabled
+        sed -i -e "s|@complete_dir_enabled@|true|g" ${cfg_file}
+        sed -i -e "s|@complete_dir@|${shared_folder}/complete|g" ${cfg_file}
+    done
+    # Watch directory
+    sed -i -e "s|\"enabled_plugins\": \[\],|\"enabled_plugins\": \[\n        \"AutoAdd\"\n    \], \n|g" ${CFG_FILE}
+    sed -i -e "s|@watch_dir@|${shared_folder}/watch|g" ${CFG_WATCH}
+    # plugins directory
+    sed -i -e "s|@plugins_dir@|${SYNOPKG_PKGVAR}/plugins|g" ${CFG_FILE}
+}
+
 service_postinst ()
 {
     # Create a Python virtualenv
@@ -44,45 +71,17 @@ service_postinst ()
     install_python_wheels
 
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        # Edit the configuration according to the wizard
-        shared_folder="${wizard_volume:=/volume1}/${wizard_download_dir:=downloads}"
-        sed -i -e "s|@shared_folder@|${shared_folder}|g" ${CFG_FILE}
-        sed -i -e "s|@script_dir@|${SYNOPKG_PKGVAR}/scripts|g" ${CFG_FILE}
-
-        if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
-            sed -i -e "s|permissions\s*=.*|permissions = ""|g" ${CFG_FILE}
-        fi
-
-        # Create logs directory
-        mkdir -p "${DELUGE_LOGS}"
-
-        # Create download directories
-        mkdir -p -m 0775 "${shared_folder}/incomplete"
-        mkdir -p -m 0775 "${shared_folder}/complete"
-        mkdir -p -m 0775 "${shared_folder}/watch"
-
-        # Edit the configuration files according to the wizard
-        for cfg_file in "${CFG_FILE} ${CFG_WATCH}"; do
-            # Default download directory
-            sed -i -e "s|@download_dir@|${shared_folder}/incomplete|g" ${cfg_file}
-            # Complete directory enabled
-            sed -i -e "s|@complete_dir_enabled@|true|g" ${cfg_file}
-            sed -i -e "s|@complete_dir@|${shared_folder}/complete|g" ${cfg_file}
-        done
-        # Watch directory
-        sed -i -e "s|\"enabled_plugins\": \[\],|\"enabled_plugins\": \[\n        \"AutoAdd\"\n    \], \n|g" ${CFG_FILE}
-        sed -i -e "s|@watch_dir@|${shared_folder}/watch|g" ${CFG_WATCH}
-        # plugins directory
-        sed -i -e "s|@plugins_dir@|${SYNOPKG_PKGVAR}/plugins|g" ${CFG_FILE}
+        deluge_default_install
     fi
 }
 
 
 service_postupgrade ()
 {
+    # Adjust permissions on new path for DSM <= 6
     # Needed to force correct permissions, during update from prior version
     # Extract the right paths from config file
-    if [ -r "${CFG_FILE}" ]; then
+    if [ -r "${CFG_FILE}" -a "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
         OLD_INCOMPLETE_FOLDER=`sed -n 's/.*"download_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_FILE}`
         OLD_COMPLETE_FOLDER=`sed -n 's/.*"move_completed_path"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_FILE}`
         OLD_WATCH_FOLDER=`sed -n 's/.*"autoadd_location"[ ]*:[ ]*"\(.*\)",/\1/p' ${CFG_FILE}`
@@ -91,18 +90,28 @@ service_postupgrade ()
         NEW_COMPLETE_FOLDER="${wizard_volume:=/volume1}/${wizard_download_dir:=/downloads}/complete"
         NEW_WATCH_FOLDER="${wizard_volume:=/volume1}/${wizard_download_dir:=/downloads}/watch"
 
-        # Adjust permissions on new path for DSM <= 6
-        if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -le 6 ]; then
-            # add group (DSM6)
-            if [ -n "${NEW_INCOMPLETE_FOLDER}" ] && [ -d "${NEW_INCOMPLETE_FOLDER}" ]; then
-                set_syno_permissions "${NEW_INCOMPLETE_FOLDER}" "${GROUP}"
-            fi
-            if [ -n "${NEW_COMPLETE_FOLDER}" ] && [ -d "${NEW_COMPLETE_FOLDER}" ]; then
-                set_syno_permissions "${NEW_COMPLETE_FOLDER}" "${GROUP}"
-            fi
-            if [ -n "${NEW_WATCHED_FOLDER}" ] && [ -d "${NEW_WATCHED_FOLDER}" ]; then
-                set_syno_permissions "${NEW_WATCHED_FOLDER}" "${GROUP}"
-            fi
+        # Older versions of Deluge on DSM <= 6 must
+        # be updated using a newer configuration
+
+        # Backup current old version of core.conf and autoadd.conf
+        cp -p ${CFG_FILE} ${CFG_FILE}-backup.$(date +%Y%m%d-%H%M)
+        cp -p ${CFG_WATCH} ${CFG_WATCH}-backup.$(date +%Y%m%d-%H%M)
+
+        # Copy new "default" version of core.conf
+        cp -p ${CFG_FILE}.new ${CFG_FILE}
+        cp -p ${CFG_WATCH}.new ${CFG_WATCH}
+        # Reset to default installation
+        deluge_default_install
+
+        # add group (DSM6)
+        if [ -n "${NEW_INCOMPLETE_FOLDER}" ] && [ -d "${NEW_INCOMPLETE_FOLDER}" ]; then
+            set_syno_permissions "${NEW_INCOMPLETE_FOLDER}" "${GROUP}"
+        fi
+        if [ -n "${NEW_COMPLETE_FOLDER}" ] && [ -d "${NEW_COMPLETE_FOLDER}" ]; then
+            set_syno_permissions "${NEW_COMPLETE_FOLDER}" "${GROUP}"
+        fi
+        if [ -n "${NEW_WATCHED_FOLDER}" ] && [ -d "${NEW_WATCHED_FOLDER}" ]; then
+            set_syno_permissions "${NEW_WATCHED_FOLDER}" "${GROUP}"
         fi
 
         # Migrate data to the new download paths except
@@ -110,30 +119,14 @@ service_postupgrade ()
         shopt -s dotglob # copy hidden folder/files too
         if [ -n "${OLD_INCOMPLETE_FOLDER}" ] &&  [ "$OLD_INCOMPLETE_FOLDER" != "$NEW_INCOMPLETE_FOLDER" ]; then
             mkdir -p "$NEW_INCOMPLETE_FOLDER"
-            mv -nv "$OLD_INCOMPLETE_FOLDER"/* "$NEW_INCOMPLETE_FOLDER/"
+            echo "mv -nv $OLD_INCOMPLETE_FOLDER/* $NEW_INCOMPLETE_FOLDER/"
+            #mv -nv "$OLD_INCOMPLETE_FOLDER"/* "$NEW_INCOMPLETE_FOLDER/"
         fi
         if [ -n "${OLD_WATCH_FOLDER}" ] &&  [ "$OLD_WATCH_FOLDER" != "$NEW_WATCH_FOLDER" ]; then
             mkdir -p "$NEW_WATCH_FOLDER"
-            mv -nv "$OLD_WATCH_FOLDER"/* "$NEW_WATCH_FOLDER/"
+            echo "mv -nv $OLD_WATCH_FOLDER/* $NEW_WATCH_FOLDER"
+            #mv -nv "$OLD_WATCH_FOLDER"/* "$NEW_WATCH_FOLDER/"
         fi
-        shopt -d dotglob
-
-        #if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
-            ## Edit the configuration files according to the wizard
-            #for cfg_file in "${CFG_FILE} ${CFG_WATCH}"; do
-            #    # Default download directory
-            #    sed -i -e "s|@download_dir@|${NEW_INCOMPLETE_FOLDER}|g" ${cfg_file}
-            #    # Complete directory enabled
-            #    sed -i -e "s|@complete_dir_enabled@|true|g" ${cfg_file}
-            #    sed -i -e "s|@complete_dir@|${NEW_COMPLETE_FOLDER}|g" ${cfg_file}
-            #done
-            ## Watch directory
-            #sed -i -e "s|\"enabled_plugins\": \[\],|\"enabled_plugins\": \[\n        \"AutoAdd\"\n    \], \n|g" ${CFG_FILE}
-            #sed -i -e "s|@watch_dir@|${NEW_WATCH_FOLDER}|g" ${CFG_WATCH}
-
-            #sed -i -e "s|complete_dir\s*=.*|complete_dir = ${NEW_COMPLETE_FOLDER}|g" ${CFG_FILE}
-            #sed -i -e "s|download_dir\s*=.*|download_dir = ${NEW_INCOMPLETE_FOLDER}|g" ${CFG_FILE}
-            #sed -i -e "s|dirscan_dir\s*=.*|dirscan_dir = ${NEW_WATCH_FOLDER}|g" ${CFG_FILE}
-        #fi
+        shopt -u dotglob
     fi
 }
