d2edd4b2d909e484f6f64ce060270757c8ea06cd
miigotu <miigotu@gmail.com>
2022-09-14 18:19:47 -0400

    Switch sickchill to use a pypi based install to match other distributions
    Signed-off-by: miigotu <miigotu@gmail.com>

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -3,14 +3,14 @@ PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 HOME="${SYNOPKG_PKGVAR}"
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
 SC_INSTALL_DIR="${SYNOPKG_PKGDEST}/share/SickChill"
-SC_BINARY="${SC_INSTALL_DIR}/SickChill.py"
+SC_BINARY="${SYNOPKG_PKGDEST}/env/bin/SickChill"
 SC_DATA_DIR="${SYNOPKG_PKGVAR}/data"
 SC_CFG_FILE="${SC_DATA_DIR}/config.ini"
 
 
 GROUP="sc-download"
 
-SERVICE_COMMAND="${PYTHON} ${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
+SERVICE_COMMAND="${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
 
 set_config() {
     . ${SYNOPKG_PKGDEST}/share/git_data
@@ -20,29 +20,24 @@ set_config() {
             sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
             sed -i "/^\s*web_password\s*=/s/\s*=\s*.*/ = ${wizard_password}/" ${SC_CFG_FILE}
         fi
-        # update git commit to sickchill updater
-        sed -i "/^\s*branch\s*=/s/\s*=\s*.*/ = ${SC_GIT_BRANCH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_hash\s*=/s/\s*=\s*.*/ = ${SC_GIT_COMMIT_HASH}/" ${SC_CFG_FILE}
-        sed -i "/^\s*cur_commit_branch\s*=/s/\s*=\s*.*/ = ${SC_GIT_COMMIT_BRANCH}/" ${SC_CFG_FILE}
     else
         mkdir -p ${SC_DATA_DIR}
         cat << EOF > ${SC_CFG_FILE}
 [General]
 web_username = ${wizard_username}
 web_password = ${wizard_password}
-branch = ${SC_GIT_BRANCH}
-cur_commit_hash = ${SC_GIT_COMMIT_HASH}
-cur_commit_branch = ${SC_GIT_COMMIT_BRANCH}
 EOF
     fi
 }
 
 service_postinst() {
-    # Create a Python virtualenv
+    separator="===================================================="
+
+    echo ${separator}
     install_python_virtualenv
-    
-    # Install the wheels
-    install_python_wheels --extra-index-url https://wheel-index.linuxserver.io/ubuntu/
+
+    echo ${separator}
+    install_python_wheels
     
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
         set_config
