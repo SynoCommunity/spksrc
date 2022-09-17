ffaec3eeb2235713be3e624fdcb071492553d5c8
miigotu <miigotu@gmail.com>
2022-09-17 00:38:16 -0400

    Update service-setup.sh

--- spk/sickchill/src/service-setup.sh
+++ spk/sickchill/src/service-setup.sh
@@ -13,8 +13,6 @@ GROUP="sc-download"
 SERVICE_COMMAND="${SC_BINARY} --daemon --nolaunch --pidfile ${PID_FILE} --config ${SC_CFG_FILE} --datadir ${SC_DATA_DIR}"
 
 set_config() {
-    . ${SYNOPKG_PKGDEST}/share/git_data
-
     if [ -f "${SC_CFG_FILE}" ]; then
         if [ -n "${wizard_username}" ] && [ -n "${wizard_password}" ]; then
             sed -i "/^\s*web_username\s*=/s/\s*=\s*.*/ = ${wizard_username}/" ${SC_CFG_FILE}
