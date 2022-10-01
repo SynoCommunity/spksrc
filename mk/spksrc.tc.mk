7e4b2de31c11e531594446377563f4dedb1dc661
Vincent Fortier <th0ma7@gmail.com>
2022-10-01 01:15:18 +0000

    tc.mk: Move cargo & rustup under /opt

--- mk/spksrc.tc.mk
+++ mk/spksrc.tc.mk
@@ -123,12 +123,11 @@ tc_vars:
 	echo TC_ENV += CPPFLAGS=\"$(CPPFLAGS) $$\(ADDITIONAL_CPPFLAGS\)\" ; \
 	echo TC_ENV += CXXFLAGS=\"$(CXXFLAGS) $$\(ADDITIONAL_CXXFLAGS\)\" ; \
 	echo TC_ENV += LDFLAGS=\"$(LDFLAGS) $$\(ADDITIONAL_LDFLAGS\)\" ; \
-	echo TC_ENV += CARGO_HOME=\"/usr/local\" ; \
-	echo TC_ENV += CARGO=\"/usr/local/bin/cargo\" ; \
-	echo TC_ENV += RUSTC=\"/usr/local/bin/rustc\" ; \
-	echo TC_ENV += CARGO_TARGET=\"$(RUST_TARGET)\" ; \
-	echo TC_ENV += CARGO_TARGET_$(shell echo $(RUST_TARGET) | tr - _ | tr a-z A-Z)_AR=\"$(WORK_DIR)/$(TC_TARGET)/bin/$(TC_PREFIX)ar\" ; \
+	echo TC_ENV += CARGO_HOME=\"/opt/cargo\" ; \
+	echo TC_ENV += RUSTUP_HOME=\"/opt/rustup\" ; \
+	echo TC_ENV += CARGO_BUILD_TARGET=\"$(RUST_TARGET)\" ; \
 	echo TC_ENV += CARGO_TARGET_$(shell echo $(RUST_TARGET) | tr - _ | tr a-z A-Z)_LINKER=\"$(WORK_DIR)/$(TC_TARGET)/bin/$(TC_PREFIX)gcc\" ; \
+#	echo TC_ENV += RUSTFLAGS=\"--target=$(RUST_TARGET) -Clinker=$(WORK_DIR)/$(TC_TARGET)/bin/$(TC_PREFIX)gcc\" ; \
 	echo TC_CONFIGURE_ARGS := --host=$(TC_TARGET) --build=i686-pc-linux ; \
 	echo TC_TYPE := $(TC_TYPE) ; \
 	echo TC_TARGET := $(TC_TARGET) ; \
