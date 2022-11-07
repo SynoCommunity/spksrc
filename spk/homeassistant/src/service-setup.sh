17e84989839157a9bf49c121093d292bf27f984b
hgy59 <hpgy59@gmail.com>
2022-11-07 23:10:01 +0100

    homeassistant update
    - update dependencies
    - adjust service-setup for command line parameters that are not supported anymore (--pid-file, --daemon)
    - avoid use of install_python_wheels function, as we want to install whl files from wheelhouse
    - abort installation when default_config installation failed (fail fast)
    - increase expected installation time up to 60 minutes (it took 55 min. on a DS115j)

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -7,7 +7,9 @@ PYTHON_DIR="/var/packages/python310/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
 CONFIG_DIR="${SYNOPKG_PKGVAR}/config"
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --pid-file ${PID_FILE} --log-file ${LOG_FILE} --daemon"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --log-file ${LOG_FILE}"
+SVC_WRITE_PID=y
+SVC_BACKGROUND=y
 SVC_CWD="${SYNOPKG_PKGVAR}"
 HOME="${SYNOPKG_PKGVAR}"
 
@@ -20,13 +22,23 @@ service_postinst ()
     install_python_virtualenv
 
     echo ${separator}
-    install_python_wheels
+    echo "Install packages from wheels"
+    pip install --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
+
+    echo ${separator}
+    echo "Install pure python packages from index"
+    pip install --no-deps --no-input \
+                --requirement ${SYNOPKG_PKGDEST}/share/wheelhouse/requirements-pure.txt
 
     echo ${separator}
     echo "Install packages for default_config from index"
     pip install --no-deps --no-input \
                 --cache-dir ${SYNOPKG_PKGVAR}/pip-cache \
                 --requirement ${SYNOPKG_PKGDEST}/share/postinst_default_config_requirements.txt
+    if [ $? -ne 0 ]; then
+        echo "ERROR: default_config installation failed";
+        return;
+    fi
 
     echo ${separator}
     echo "Install packages for homeassistant.components from index"
