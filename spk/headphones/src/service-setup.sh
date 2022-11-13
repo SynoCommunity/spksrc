cfe5f45c83838265d5d13f5dc2c4575dd1d2c5c6
hgy59 <hpgy59@gmail.com>
2022-11-13 17:42:25 +0100

    headphones: update to Python 3.10
    - update to Python 3.10
    - update for DSM 7 compatibility

--- spk/headphones/src/service-setup.sh
+++ spk/headphones/src/service-setup.sh
@@ -1,27 +1,16 @@
-PYTHON_DIR="/var/packages/python/target/bin"
+PYTHON_DIR="/var/packages/python310/target/bin"
 GIT_DIR="/var/packages/git/target/bin"
 PATH="${SYNOPKG_PKGDEST}/bin:${SYNOPKG_PKGDEST}/env/bin:${PYTHON_DIR}:${GIT_DIR}:${PATH}"
-PYTHON="${SYNOPKG_PKGDEST}/env/bin/python"
-VIRTUALENV="${PYTHON_DIR}/virtualenv"
+PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
 HEADPHONES="${SYNOPKG_PKGDEST}/share/Headphones/Headphones.py"
-CFG_FILE="${SYNOPKG_PKGDEST}/var/config.ini"
+CFG_FILE="${SYNOPKG_PKGVAR}/config.ini"
 
-SERVICE_COMMAND="${PYTHON} ${HEADPHONES} --daemon --pidfile ${PID_FILE} --config ${CFG_FILE} --datadir ${SYNOPKG_PKGDEST}/var/"
+SERVICE_COMMAND="${PYTHON} ${HEADPHONES} --daemon --pidfile ${PID_FILE} --config ${CFG_FILE} --datadir ${SYNOPKG_PKGVAR}/"
 
 GROUP="sc-download"
-LEGACY_GROUP="sc-media"
 
 service_postinst ()
 {
     # Create a Python virtualenv
-    ${VIRTUALENV} --system-site-packages ${SYNOPKG_PKGDEST}/env
-
-    # If nessecary, add user also to the old group
-    syno_user_add_to_legacy_group "${EFF_USER}" "${USER}" "${LEGACY_GROUP}"
-
-    # Remove legacy user
-    # Commands of busybox from spk/python
-    delgroup "${USER}" "users"
-    deluser "${USER}"
+    install_python_virtualenv
 }
-
