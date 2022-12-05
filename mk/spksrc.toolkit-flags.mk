c6768c1a6bb4d594129ad31cf426f4c38b381f2e
hgy59 <hpgy59@gmail.com>
2022-12-05 21:26:02 +0100

    revert changes in spksrc.toolkit-flags.mk

--- mk/spksrc.toolkit-flags.mk
+++ mk/spksrc.toolkit-flags.mk
@@ -1,5 +1,3 @@
-TOOLKIT_ROOT=$(WORK_DIR)/../../../toolkit/syno-$(ARCH)-$(TCVERSION)/work
-
 ifeq ($(strip $(TOOLKIT_NAME)),)
 TOOLKIT_NAME = syno-$(TOOLKIT_ARCH)
 endif
@@ -38,44 +36,4 @@ ifeq ($(strip $(TOOLKIT_SYSROOT)),)
 TOOLKIT_SYSROOT ?= $(TOOLKIT_BASE_DIR)/sys-root/usr
 else ifeq ($(strip $(TOOLKIT_SYSROOT)),nop)
 TOOLKIT_SYSROOT = 
-endif
-
-TOOLKIT_CFLAGS = -I$(TOOLKIT_ROOT)/include
-
-# Specic TOOLKIT to add to include search path
-ifeq ($(findstring 'scsi',$(TOOLKIT)),'scsi')
-TOOLKIT_CFLAGS += -I$(TOOLKIT_ROOT)/include/scsi
-endif
-ifeq ($(findstring 'mtd',$(TOOLKIT)),'mtd')
-TOOLKIT_CFLAGS += -I$(TOOLKIT_ROOT)/include/mtd
-endif
-ifeq ($(findstring 'glib',$(TOOLKIT)),'glib')
-TOOLKIT_CFLAGS += -I$(TOOLKIT_ROOT)/include/glib-2.0
-endif
-TOOLKIT_CPPFLAGS = -I$(TOOLKIT_ROOT)/include
-TOOLKIT_CXXFLAGS = -I$(TOOLKIT_ROOT)/include
-#
-TOOLKIT_LDFLAGS = -L$(TOOLKIT_ROOT)/lib
-TOOLKIT_LDFLAGS += -Wl,-rpath-link,$(TOOLKIT_ROOT)/lib
-TOOLKIT_LDFLAGS += -Wl,-rpath,$(TOOLKIT_ROOT)/lib
-
-include ../../mk/spksrc.common.mk
-
-# Add lib64 for x86_64 archs
-ifeq ($(findstring $(ARCH),$(x64_ARCHES)),$(ARCH))
-TOOLKIT_LDFLAGS += -L$(TOOLKIT_ROOT)/lib64
-TOOLKIT_LDFLAGS += -Wl,-rpath-link,$(TOOLKIT_ROOT)/lib64
-TOOLKIT_LDFLAGS += -Wl,-rpath,$(TOOLKIT_ROOT)/lib64
-TOOLKIT_PKG_CONFIG_PATH += $(TOOLKIT_ROOT)/lib/pkgconfig:$(TOOLKIT_ROOT)/lib64/pkgconfig
-else
-TOOLKIT_PKG_CONFIG_PATH = $(TOOLKIT_ROOT)/lib/pkgconfig
-endif
-
-# Add native hardware acceleration libraries and include files for evansport
-ifeq ($(findstring $(ARCH),'evansport'),$(ARCH))
-TOOLKIT_CFLAGS += -I$(TOOLKIT_ROOT)/include/intelce-utilities
-TOOLKIT_CFLAGS += -I$(TOOLKIT_ROOT)/include/intelce-utilities/linux_user
-TOOLKIT_LDFLAGS += -L$(TOOLKIT_ROOT)/lib/intelce-utilities
-TOOLKIT_LDFLAGS += -Wl,-rpath-link,$(TOOLKIT_ROOT)/lib/intelce-utilities
-TOOLKIT_LDFLAGS += -Wl,-rpath,$(TOOLKIT_ROOT)/lib/intelce-utilities
 endif
