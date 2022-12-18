904efa0a07c851eb239f22f5ba4f767ffaccdec0
hgy59 <hpgy59@gmail.com>
2022-12-18 21:13:08 +0100

    redis: update to v7.0.7

--- spk/redis/src/service-setup.sh
+++ spk/redis/src/service-setup.sh
@@ -8,7 +8,7 @@ SVC_WRITE_PID=y
 service_postinst ()
 {
     # Use 15% of total physical memory with maximum of 64MB
-    MEMORY=`awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo`
+    MEMORY=$(awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo)
 
     # Set the maximum memory to use in configuration file
     sed -i -e "s/@maxmemory@/${MEMORY}mb/g" ${CFG_FILE}
