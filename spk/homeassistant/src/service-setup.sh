6fa53a6c12bd0aa203b1fd2e32432e6ecace7bfb
hgy59 <hpgy59@gmail.com>
2022-12-20 01:10:20 +0100

    adjust requirements
    - add hacs integration (from archive file)
    - add pycryptodomex (requirements-abi3.txt)
    - save/restore pip-cache on package update
    - fix dependencies

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -10,30 +10,64 @@ SVC_CWD="${SYNOPKG_PKGVAR}"
 HOME="${SYNOPKG_PKGVAR}"
 
 
+# save and restore the pip-cache on package update
+# ------------------------------------------------
+# Avoid the use of ${SYNOPKG_PKGVAR} folder for the pip cache.
+# Under DSM<7 this folder's ownership will be changed to sc-homeassistant,
+# but the installer runs under root and the cache must be owned by root.
+PIP_CACHE_BACKUP_DIR=${TMP_DIR}/pip-cache
+PIP_CACHE_DIR=${SYNOPKG_PKGDEST}/pip-cache
+export PIP_DOWNLOAD_CACHE=${PIP_CACHE_DIR}
+# avoid installation to user specific site-packages folder
+export PYTHONNOUSERSIZE=1
+
+service_save ()
+{
+    if [ -d "${PIP_CACHE_DIR}" ]; then 
+        echo "Save pip cache to ${PIP_CACHE_BACKUP_DIR}"
+        $MKDIR ${TMP_DIR}/pip-cache
+        $CP ${PIP_CACHE_DIR}/. ${PIP_CACHE_BACKUP_DIR}
+    fi
+}
+
+
 service_postinst ()
 {
     separator="===================================================="
 
     echo ${separator}
-    echo "Create folder: ${CONFIG_DIR}"
-    mkdir -p "${CONFIG_DIR}"
-
+    if [ "${SYNOPKG_PKG_STATUS}" == "UPGRADE" -a -d "${PIP_CACHE_BACKUP_DIR}" ]; then 
+        echo "Restore pip cache from ${PIP_CACHE_BACKUP_DIR}"
+        $MV ${PIP_CACHE_BACKUP_DIR} ${PIP_CACHE_DIR}/
+        # ensure current user is owner of pip-cache
+        chown -R $(id -u):$(id -g) ${PIP_CACHE_DIR}/
+    else
+        echo "Create pip cache directory: ${PIP_CACHE_DIR}"
+        $MKDIR ${PIP_CACHE_DIR}
+    fi
+    
     echo ${separator}
     echo "Install Python virtual environment"
     install_python_virtualenv
+    
+    echo ${separator}
+    echo "Downgrade pip for homeassistant compatibility"
+    pip install --disable-pip-version-check pip==22.2.2
+
+    echo ${separator}
+    echo "Install HACS into: ${CONFIG_DIR}/custom_components/hacs"
+    mkdir -p "${CONFIG_DIR}/custom_components/hacs"
+    tar -xzf ${SYNOPKG_PKGDEST}/share/hacs.tar.gz -C ${CONFIG_DIR}/custom_components/hacs
 
     echo ${separator}
     echo "Install packages from wheels"
-    pip install --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
+    pip install --disable-pip-version-check --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
 
     echo ${separator}
     echo "Install pure python packages from index"
-    pip install --no-deps --no-input \
-                --requirement ${SYNOPKG_PKGDEST}/share/wheelhouse/requirements-pure.txt
+    pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/wheelhouse/requirements-pure.txt
 
     echo ${separator}
     echo "Install packages for homeassistant.components from index"
-    pip install --no-deps --no-input \
-                --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
-
+    pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
 }
