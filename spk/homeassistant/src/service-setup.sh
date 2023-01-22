661ac4792281d07efc936fb7f9e4d89f8914c588
hgy59 <hpgy59@gmail.com>
2023-01-22 19:11:46 +0100

    homeassistant: update to v2022.10.5 (#5478)
    * homeassistant: update to v2022.10.5
    - initial commit, wheel for orjson deactivated, as it does not build yet
    - requirements of official requirement files updated
    - additional requirements not validated yet
    
    * fix build of orjson wheel
    - include native/python310 bin folder in PATH to find maturin (and avoid use of python3, pip, wheel.. of dev environment)
    - declare ARMv5 and OLD_PPC_ARCHS as unsupported (C++11 required for numpy)
    
    * fix botocore version
    
    * homeassistant update
    - update dependencies
    - adjust service-setup for command line parameters that are not supported anymore (--pid-file, --daemon)
    - avoid use of install_python_wheels function, as we want to install whl files from wheelhouse
    - abort installation when default_config installation failed (fail fast)
    - increase expected installation time up to 60 minutes (it took 55 min. on a DS115j)
    
    * initial requirements update
    - update requirements (crossenv and pure, remove abi3)
    - move requirements of default_configuration to requirements-pure.txt
    - homeassistant core, web and default_config are working without errors
    - some integrations work already (synology dsm/srm, hue, brother, ipp)
    - update package icon with higher resolution
    
    * fix psutil wheel for qoriq
    
    * adjust requirements
    - add hacs integration (from archive file)
    - add pycryptodomex (requirements-abi3.txt)
    - save/restore pip-cache on package update
    - fix dependencies
    - fix psutil for qoriq (again)
    - remove duplicate requirements
    
    * adjust multidict dependency
    - multidict<6.0.0,>=5.1.0 required
    
    * fix python virtualenv creation for DSM<7
    - package user needs access to created virtual environment
    * fix python wheels installation for DSM<7
    - package user needs access to installed site-packages
    * fix permissions to site-packages for DSM<7
    
    * Add requirements-custom.txt
    - Add requirements-custom.txt for custom requirements that will be reinstalled on package update
    - add websockets to crossenv reqirements
    - add pycryptodome
    
    * fix custom requirements installation

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -1,15 +1,34 @@
 
-# service setup: content is included by following scripts
-# - installer
-# - start-stop-status
-
 PYTHON_DIR="/var/packages/python310/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
 
 CONFIG_DIR="${SYNOPKG_PKGVAR}/config"
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --pid-file ${PID_FILE} --log-file ${LOG_FILE} --daemon"
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --log-file ${LOG_FILE}"
+SVC_WRITE_PID=y
+SVC_BACKGROUND=y
 SVC_CWD="${SYNOPKG_PKGVAR}"
 HOME="${SYNOPKG_PKGVAR}"
+
+
+# save and restore the pip-cache on package update
+# ------------------------------------------------
+# Avoid the use of ${SYNOPKG_PKGVAR} folder for the pip cache.
+# Under DSM<7 this folder's ownership will be changed to sc-homeassistant,
+# but the installer runs under root and the cache must be owned by root.
+PIP_CACHE_BACKUP_DIR=${TMP_DIR}/pip-cache
+PIP_CACHE_DIR=${SYNOPKG_PKGDEST}/pip-cache
+export PIP_DOWNLOAD_CACHE=${PIP_CACHE_DIR}
+# avoid installation to user specific site-packages folder
+export PYTHONNOUSERSIZE=1
+
+service_save ()
+{
+    if [ -d "${PIP_CACHE_DIR}" ]; then 
+        echo "Save pip cache to ${PIP_CACHE_BACKUP_DIR}"
+        $MKDIR ${TMP_DIR}/pip-cache
+        $CP ${PIP_CACHE_DIR}/. ${PIP_CACHE_BACKUP_DIR}
+    fi
+}
 
 
 service_postinst ()
@@ -17,22 +36,60 @@ service_postinst ()
     separator="===================================================="
 
     echo ${separator}
+    if [ "${SYNOPKG_PKG_STATUS}" == "UPGRADE" -a -d "${PIP_CACHE_BACKUP_DIR}" ]; then 
+        echo "Restore pip cache from ${PIP_CACHE_BACKUP_DIR}"
+        $MV ${PIP_CACHE_BACKUP_DIR} ${PIP_CACHE_DIR}/
+        # ensure current user is owner of pip-cache
+        chown -R $(id -u):$(id -g) ${PIP_CACHE_DIR}/
+    else
+        echo "Create pip cache directory: ${PIP_CACHE_DIR}"
+        $MKDIR ${PIP_CACHE_DIR}
+    fi
+
+    echo ${separator}
+    echo "Install Python virtual environment"
     install_python_virtualenv
+    
+    echo ${separator}
+    echo "Downgrade pip for homeassistant compatibility"
+    pip install --disable-pip-version-check pip==22.2.2
 
     echo ${separator}
-    install_python_wheels
+    echo "Install HACS into: ${CONFIG_DIR}/custom_components/hacs"
+    mkdir -p "${CONFIG_DIR}/custom_components/hacs"
+    tar -xzf ${SYNOPKG_PKGDEST}/share/hacs.tar.gz -C ${CONFIG_DIR}/custom_components/hacs
 
     echo ${separator}
-    echo "Install packages for default_config from index"
-    pip install --no-deps --no-input \
-                --cache-dir ${SYNOPKG_PKGVAR}/pip-cache \
-                --requirement ${SYNOPKG_PKGDEST}/share/postinst_default_config_requirements.txt
+    echo "Install packages from wheels"
+    pip install --disable-pip-version-check --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
+
+    echo ${separator}
+    echo "Install pure python packages from index"
+    pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/wheelhouse/requirements-pure.txt
 
     echo ${separator}
     echo "Install packages for homeassistant.components from index"
-    pip install --no-deps --no-input \
-                --cache-dir ${SYNOPKG_PKGVAR}/pip-cache \
-                --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
+    pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/postinst_components_requirements.txt
 
-    mkdir -p "${CONFIG_DIR}"
+
+    if [ "${SYNOPKG_PKG_STATUS}" == "UPGRADE" ]; then
+        if [ "$SYNOPKG_DSM_VERSION_MAJOR" -lt 7 ]; then
+            # restore custom requirements file
+            if [ -f ${TMP_DIR}/requirements-custom.txt ]; then
+                $CP ${TMP_DIR}/requirements-custom.txt ${SYNOPKG_PKGVAR}/requirements-custom.txt
+            fi
+        fi
+        if [ -e ${SYNOPKG_PKGVAR}/requirements-custom.txt ]; then
+            echo ${separator}
+            echo "Install custom packages from index"
+            pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGVAR}/requirements-custom.txt
+        fi
+    else
+        $MV ${SYNOPKG_PKGVAR}/requirements-custom.txt.new ${SYNOPKG_PKGVAR}/requirements-custom.txt
+    fi
+
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+        # ensure package user has access to the virtual env packages, installed as root
+        set_unix_permissions ${SYNOPKG_PKGDEST}/env
+    fi
 }
