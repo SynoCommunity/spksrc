728ba2b04e3f7fb49eff43dc6fbd06002e51aa78
Michael Reid <michael_reid@email.com>
2023-02-03 10:38:18 -0400

    Use current package update logic

--- spk/readarr/src/service-setup.sh
+++ spk/readarr/src/service-setup.sh
@@ -9,17 +9,20 @@ READARR_CONFIG_DIR="${CONFIG_DIR}/Readarr"
 PID_FILE="${READARR_CONFIG_DIR}/readarr.pid"
 CMD_ARGS="-nobrowser -data=${READARR_CONFIG_DIR}"
 
-GROUP="sc-download"
+if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+    GROUP="sc-download"
+fi
 
 SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${READARR} ${CMD_ARGS}"
 SVC_BACKGROUND=y
+SVC_WAIT_TIMEOUT=90
 
 service_postinst ()
 {
-    # Move config.xml to .config
-    mkdir -p ${READARR_CONFIG_DIR}
-    mv ${SYNOPKG_PKGDEST}/app/config.xml ${READARR_CONFIG_DIR}/config.xml
-    
+    echo "Set update required"
+    # Make Readarr do an update check on start
+    touch ${READARR_CONFIG_DIR}/update_required 2>&1
+
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
         set_unix_permissions "${CONFIG_DIR}"
     fi
@@ -42,11 +45,6 @@ service_postupgrade ()
         rm -rf ${SYNOPKG_PKGDEST}/share/Readarr/bin 2>&1
         # prevent overwrite of updated package_info
         rsync -aX --exclude=package_info ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/ ${SYNOPKG_PKGDEST}/share 2>&1
-    else
-        echo "Set update required"
-        # Make Readarr do an update check on start to avoid possible Readarr
-        # downgrade when synocommunity package is updated
-        touch ${READARR_CONFIG_DIR}/update_required 2>&1
     fi
 
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
