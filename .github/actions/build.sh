078a67399364a336c9528172d3ae20ee1126efff
hgy59 <hpgy59@gmail.com>
2023-02-11 11:36:51 +0100

    adjust github build action
    - suppress error output when checking *.spk files are created

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -76,7 +76,7 @@ do
     result=$?
 
     # For a build to succeed a <package>_<arch>-<version>.spk must also be generated
-    if [ ${result} -eq 0 -a "$(ls -1 ./packages/$(sed -n -e '/^SPK_NAME/ s/.*= *//p' spk/${package}/Makefile)_*.spk)" ]; then
+    if [ ${result} -eq 0 -a "$(ls -1 ./packages/$(sed -n -e '/^SPK_NAME/ s/.*= *//p' spk/${package}/Makefile)_*.spk 2> /dev/null)" ]; then
         echo "$(date --date=now +"%Y.%m.%d %H:%M:%S") - ${package}: (${GH_ARCH}) DONE"   >> ${BUILD_SUCCESS_FILE}
     # Ensure it's not a false-positive due to pre-check
     elif tail -15 build.log | grep -viq 'spksrc.pre-check.mk'; then
