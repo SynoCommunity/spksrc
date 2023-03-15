a12e2a95fd43fead79a48aa6417be2e6b5e3e41d
fgma <fgma@users.noreply.github.com>
2023-03-15 22:25:44 +0100

    restic-rest-server: add support for custom args

--- spk/restic-rest-server/src/service-setup.sh
+++ spk/restic-rest-server/src/service-setup.sh
@@ -10,6 +10,8 @@ fi
 
 load_variables_from_file "${INST_VARIABLES}"
 
+REST_SERVER_CUSTOM_ARGS_FILE="${WIZARD_DATA_VOLUME}/${WIZARD_DATA_DIRECTORY}/restic_rest_server_custom_args.txt"
+
 service_prestart ()
 {
     ARGS=""
@@ -27,6 +29,10 @@ service_prestart ()
             ARGS="${ARGS} --prometheus-no-auth"
         fi
     fi
+    if [ -f "$REST_SERVER_CUSTOM_ARGS_FILE" -a -r "$REST_SERVER_CUSTOM_ARGS_FILE" ]; then
+        CUSTOM_ARGS=`cat ${REST_SERVER_CUSTOM_ARGS_FILE}`
+        ARGS="${ARGS} ${CUSTOM_ARGS}"
+    fi
 
     SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/restic-rest-server --listen ":${SERVICE_PORT}" --path ${WIZARD_DATA_VOLUME}/${WIZARD_DATA_DIRECTORY} ${ARGS}"
     SVC_BACKGROUND=y
@@ -36,6 +42,7 @@ service_prestart ()
 service_postinst ()
 {
     touch ${wizard_data_volume}/${wizard_data_directory}/.htpasswd
+    touch ${REST_SERVER_CUSTOM_ARGS_FILE}
     echo WIZARD_DATA_VOLUME="${wizard_data_volume}"                 >> ${INST_VARIABLES}
     echo WIZARD_DATA_DIRECTORY="${wizard_data_directory}"           >> ${INST_VARIABLES}
     echo WIZARD_APPEND_ONLY="${wizard_append_only}"                 >> ${INST_VARIABLES}
