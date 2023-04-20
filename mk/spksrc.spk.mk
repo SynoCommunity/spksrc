815f410ee3ba31e3cb86069f78ad55dea7cb65a2
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2023-04-20 09:25:54 -0400

    Add publish validation check

--- mk/spksrc.spk.mk
+++ mk/spksrc.spk.mk
@@ -435,7 +435,16 @@ endif
 ifeq ($(PUBLISH_API_KEY),)
 	$(error Set PUBLISH_API_KEY in local.mk)
 endif
-	http --verify=no --ignore-stdin --auth $(PUBLISH_API_KEY): POST $(PUBLISH_URL)/packages @$(SPK_FILE_NAME)
+	@response=$$(http --verbose --verify=no --ignore-stdin --auth $(PUBLISH_API_KEY): POST $(PUBLISH_URL)/packages @$(SPK_FILE_NAME) 2>&1); \
+	status=$$?; \
+	response_code=$$(echo "$$response" | grep -Fi "HTTP/1.1" | awk '{print $$2}'); \
+	if [ "$$response_code" = "201" ]; then \
+		output=$$(echo "$$response" | grep -vFi "HTTP/1.1"); \
+		echo "Package published successfully: $$output"; \
+	else \
+		echo "Error: Failed to publish package - HTTP response code $$response_code"; \
+		exit 1; \
+	fi
 
 
 ### Clean rules
