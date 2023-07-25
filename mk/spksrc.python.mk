0f715859a851195d1e9e78177e1ed7412f569fd4
hgy59 <hpgy59@gmail.com>
2023-07-25 20:43:44 +0200

    fix spksrc.python.mk
    - add Makefile variable PYTHON_PACKAGE to define the python package to reuse as dependency
    - take python variables from python-cc.mk
    - link python-cc.mk into package folder used by build_wheel_target in spksrc.wheel.mk

--- mk/spksrc.python.mk
+++ mk/spksrc.python.mk
@@ -1,26 +1,25 @@
 ###
 ### Reuse Python libraries
 ###
+# Variables:
+#  PYTHON_PACKAGE       Must be set to the python spk folder (python310, python311, ...)
 
-# Default python version to use
-ifeq ($(strip $(PYTHON_VERSION)),)
-export PYTHON_VERSION = 311
-endif
 
 # set default spk/python* path to use
-export PYTHON_DIR = $(realpath $(shell pwd)/../python$(PYTHON_VERSION)/work-$(ARCH)-$(TCVERSION)/install/var/packages/python$(PYTHON_VERSION)/target)
+PYTHON_PACKAGE_ROOT = $(realpath $(shell pwd)/../$(PYTHON_PACKAGE)/work-$(ARCH)-$(TCVERSION))
+export PYTHON_DIR = $(realpath $(PYTHON_PACKAGE_ROOT)/install/var/packages/$(PYTHON_PACKAGE)/target)
+
+# get PYTHON_VERSION and other variables
+-include $(PYTHON_PACKAGE_ROOT)/python-cc.mk
 
 # set PYTHONPATH for spksrc.python-module.mk
-PYTHONPATH = $(PYTHON_SITE_PACKAGES_NATIVE):$(PYTHON_LIB_NATIVE):$(PYTHON_DIR)/lib/python$(shell echo $${PYTHON_VERSION%??}).$(shell echo $${PYTHON_VERSION#?})/site-packages/
-
-# always define SPK_DEPENDS
-SPK_DEPENDS := "python$(PYTHON_VERSION)>=$(shell sed -n 's/^SPK_VERS = \(.*\)/\1/p' $(shell pwd)/../python$(PYTHON_VERSION)/Makefile)-$(shell sed -n 's/^SPK_REV = \(.*\)/\1/p' $(shell pwd)/../python$(PYTHON_VERSION)/Makefile)"
+PYTHONPATH = $(PYTHON_SITE_PACKAGES_NATIVE):$(PYTHON_LIB_NATIVE):$(PYTHON_DIR)/lib/python$(PYTHON_VERSION)/site-packages/
 
 ifneq ($(wildcard $(PYTHON_DIR)),)
-export ADDITIONAL_LDFLAGS = -Wl,--rpath-link,$(PYTHON_DIR)/lib -Wl,--rpath,/var/packages/python$(PYHTON_VERSION)/target/lib
+export ADDITIONAL_LDFLAGS = -Wl,--rpath-link,$(PYTHON_DIR)/lib -Wl,--rpath,/var/packages/$(PYTHON_PACKAGE)/target/lib
 PRE_DEPEND_TARGET = python_pre_depend
 else
-BUILD_DEPENDS += cross/python$(PYTHON_VERSION)
+BUILD_DEPENDS += cross/$(PYTHON_PACKAGE)
 endif
 
 # minimal set of libraries to use
@@ -36,8 +35,8 @@ PYTHON_LIBS += ncurses++w.pc
 PYTHON_LIBS += ncursesw.pc
 PYTHON_LIBS += openssl.pc
 PYTHON_LIBS += panelw.pc
-PYTHON_LIBS += python-$(shell echo $${PYTHON_VERSION%??}).$(shell echo $${PYTHON_VERSION#?})-embed.pc
-PYTHON_LIBS += python-$(shell echo $${PYTHON_VERSION%??}).$(shell echo $${PYTHON_VERSION#?}).pc
+PYTHON_LIBS += python-$(PYTHON_VERSION)-embed.pc
+PYTHON_LIBS += python-$(PYTHON_VERSION).pc
 PYTHON_LIBS += python$(shell echo $${PYTHON_VERSION%??})-embed.pc
 PYTHON_LIBS += python$(shell echo $${PYTHON_VERSION%??}).pc
 PYTHON_LIBS += readline.pc
@@ -49,6 +48,8 @@ include ../../mk/spksrc.spk.mk
 
 .PHONY: python_pre_depend
 python_pre_depend:
+	@$(MSG) Use existing python in $(PYTHON_PACKAGE_ROOT)
 	@mkdir -p $(STAGING_INSTALL_PREFIX)/lib/pkgconfig/
 	@$(foreach lib,$(PYTHON_LIBS),ln -sf $(PYTHON_DIR)/lib/pkgconfig/$(lib) $(STAGING_INSTALL_PREFIX)/lib/pkgconfig/ ;)
-	@ln -sf $(realpath $(shell pwd)/../python$(PYTHON_VERSION)/work-$(ARCH)-$(TCVERSION)/crossenv) $(STAGING_INSTALL_PREFIX)/crossenv
+	@ln -sf $(PYTHON_PACKAGE_ROOT)/crossenv $(STAGING_INSTALL_PREFIX)/crossenv
+	@ln -sf $(PYTHON_PACKAGE_ROOT)/python-cc.mk $(WORK_DIR)/python-cc.mk
