e0e7f9714016f25d9d0629bdde4f6f6aa9dd6439
Vincent Fortier <th0ma7@gmail.com>
2023-07-31 22:32:59 +0000

    python-wheel.mk: Fix when used from spksrc.python.mk
    In order for rust wheels to be built properly one must change
    STAGING_INSTALL_PREFIX to point towards the spk/python311 which
    is set by PYTHON_DIR variable
    
    Also, only set PYTHONPATH if not already set previous such
    as by spksrc.python.mk

--- mk/spksrc.python-wheel.mk
+++ mk/spksrc.python-wheel.mk
@@ -22,8 +22,17 @@ include ../../mk/spksrc.cross-cc.mk
 # Fetch python variables
 -include $(WORK_DIR)/python-cc.mk
 
+# If using spk.python.mk with PYTHON_DIR defined
+# then redirect STAGING_INSTALL_PREFIX so rust
+# wheels can find openssl and other libraries
+ifneq ($(strip $(PYTHON_DIR)),)
+STAGING_INSTALL_PREFIX := $(PYTHON_DIR)
+endif
+
 # Python module variables
+ifeq ($(strip $(PYTHONPATH)),)
 PYTHONPATH = $(PYTHON_LIB_NATIVE):$(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_DIR)/site-packages/
+endif
 
 ## python wheel specific configurations
 include ../../mk/spksrc.wheel-env.mk
