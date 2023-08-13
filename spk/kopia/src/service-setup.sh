a87138938c5e8f0cbb219ddca8319d2076a5d9bc
hgy59 <hpgy59@gmail.com>
2023-08-13 13:27:35 +0200

    add kopia
    - this is an initial commit with hardcoded password kopia

---
+++ spk/kopia/src/service-setup.sh
@@ -0,0 +1,6 @@
+export KOPIA_LOG_DIR=${SYNOPKG_PKGVAR}
+export KOPIA_CACHE_DIR=${SYNOPKG_PKGVAR}
+
+#PASSWORD=${wizard_password}
+PASSWORD=kopia
+SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/kopia server start --ui --insecure --address=http://0.0.0.0:${SERVICE_PORT} --server-password=${PASSWORD} --legacy-api --grpc --control-api"
