985f2db6b885201f8de0cd8425cdfc2fd5def1f4
hgy59 <hpgy59@gmail.com>
2023-08-13 14:48:11 +0200

    fix package installation

--- spk/kopia/src/service-setup.sh
+++ spk/kopia/src/service-setup.sh
@@ -4,3 +4,5 @@ export KOPIA_CACHE_DIR=${SYNOPKG_PKGVAR}
 #PASSWORD=${wizard_password}
 PASSWORD=kopia
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/kopia server start --ui --insecure --address=http://0.0.0.0:${SERVICE_PORT} --server-password=${PASSWORD} --legacy-api --grpc --control-api"
+SVC_BACKGROUND=yes
+SVC_WRITE_PID=yes
