dc86e436fe12f020d1104ab1c5e2e6caefdc2515
hgy59 <hpgy59@gmail.com>
2023-09-09 02:04:33 +0200

    fix transmission (#5685) (#5865)
    * fix transmission (#5685)
    - fix empty blocklist by patch
    - update icon
    - use TMP_DIR to avoid permission error logs for session files
    - remove DSM 6 portions in service-setup.sh
    * Remove pre-DSM7 config
    * adjust patch to fix empty blocklists
    - avoid log of error 22 (EINV) in fallback to user-space copy
    * transmission: remove python path
    * transmisison: fix file locking on ARMv7_ARCHS
    - force use of flock (since fnctl with F_OFD_SETLK does not work)
    
    ---------
    
    Co-authored-by: Michael Reid <michael_reid@email.com>

--- spk/transmission/src/service-setup.sh
+++ spk/transmission/src/service-setup.sh
@@ -1,25 +1,16 @@
-# Add python to path
-# This gives tranmission the power to execute python scripts on completion (like TorrentToMedia).
-if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
-    # use system python for DSM7
-    PYTHON_BIN_PATHS=""
-else
-    GROUP="sc-download"
-    PYTHON_BIN_PATHS="/var/packages/python310/target/bin:/var/packages/python38/target/bin:/var/packages/python3/target/bin:"
-fi
 
-PATH="${SYNOPKG_PKGDEST}/bin:${PYTHON_BIN_PATHS}${PATH}"
 CFG_FILE="${SYNOPKG_PKGVAR}/settings.json"
 TRANSMISSION="${SYNOPKG_PKGDEST}/bin/transmission-daemon"
+export TMP_DIR=${SYNOPKG_PKGTMP}
 
-SERVICE_COMMAND="${TRANSMISSION} -g ${SYNOPKG_PKGVAR} -x ${PID_FILE} -e ${LOG_FILE}"
+SERVICE_COMMAND="${TRANSMISSION} --config-dir ${SYNOPKG_PKGVAR} --pid-file ${PID_FILE} --logfile ${LOG_FILE}"
 
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" = "INSTALL" ]; then
         # Capture wizard variable
         TXN_DNLOAD=${wizard_download_dir:=volume1/downloads}
-        # Check that the path exists, if not use path in package shares
+        # Check that the path exists, if not, use path in package shares
         if [ ! -d "${TXN_DNLOAD}" ]; then
             TXN_DNLOAD=$(realpath "/var/packages/${SYNOPKG_PKGNAME}/shares/${wizard_download_share}")
         fi
@@ -30,12 +21,7 @@ service_postinst ()
         # Create the managed folders
         for item in "${TXN_FOLDRS[@]}"; do
             folder="$TXN_DNLOAD/$item"
-            if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -ge 7 ]; then
-                mkdir -p "$folder"
-            else
-                /bin/su "${EFF_USER}" -s /bin/sh -c "mkdir -p $folder"
-                set_syno_permissions "$folder" "${GROUP}"
-            fi
+            mkdir -p "$folder"
             TXN_PATHS+=("$folder")
         done
 
@@ -43,6 +29,7 @@ service_postinst ()
         sed -e "s|@username@|${wizard_username:=admin}|g" \
             -e "s|@password@|${wizard_password:=admin}|g" \
             -i "${CFG_FILE}"
+
         i=0
         while [ $i -lt ${#TXN_FUNCTS[@]} ]; do
             if [ -d "${TXN_PATHS[$i]}" ]; then
