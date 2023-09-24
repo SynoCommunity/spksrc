cb160650b7856667fb05f45e94a037e9d00838e0
brakenium <22982712+brakenium@users.noreply.github.com>
2023-09-24 18:39:36 +0000

    Change port, disable telemetry and add wizard

--- spk/influxdb/src/service-setup.sh
+++ spk/influxdb/src/service-setup.sh
@@ -2,3 +2,14 @@ SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/influxd"
 SVC_BACKGROUND=yes
 SVC_WRITE_PID=yes
 SVC_CWD="${SYNOPKG_PKGVAR}"
+
+
+service_preinst ()
+{
+    # Set the port to 8085 in a config.yaml file
+    # This is the port that the web interface will be available on
+    cat << EOF > "${SYNOPKG_PKGVAR}"/config.yaml
+    http-bind-address: ":8085"
+    reporting-disabled: true
+EOF
+}
