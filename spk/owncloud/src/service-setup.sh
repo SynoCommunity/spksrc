677ef1886ecc86de4a5b2e8d8915634c984e599f
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2023-10-27 08:38:26 -0400

    Move app call to variable

--- spk/owncloud/src/service-setup.sh
+++ spk/owncloud/src/service-setup.sh
@@ -13,6 +13,7 @@ fi
 OCROOT="${WEB_DIR}/${SYNOPKG_PKGNAME}"
 JQ="/bin/jq"
 SED="/bin/sed"
+TAR="/bin/tar"
 SYNOSVC="/usr/syno/sbin/synoservice"
 
 if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
@@ -209,7 +210,7 @@ service_postinst ()
                 # Extract archive to temp folder
                 TEMPDIR="${SYNOPKG_PKGTMP}/${SYNOPKG_PKGNAME}"
                 ${MKDIR} "${TEMPDIR}"
-                /bin/tar -xzf "${wizard_backup_file}" -C "${TEMPDIR}" 2>&1
+                ${TAR} -xzf "${wizard_backup_file}" -C "${TEMPDIR}" 2>&1
 
                 # Restore configuration files and directories
                 rsync -aX --update -I "${TEMPDIR}/configs/root/.user.ini" "${TEMPDIR}/configs/root/.htaccess" "${OCROOT}/" 2>&1
@@ -291,7 +292,7 @@ service_preuninst ()
             # Create backup archive
             archive_name="$(basename "$TEMPDIR").tar.gz"
             echo "Creating compressed archive of ownCloud data in file $archive_name"
-            /bin/tar -C "$TEMPDIR" -czf "${SYNOPKG_PKGTMP}/$archive_name" . 2>&1
+            ${TAR} -C "$TEMPDIR" -czf "${SYNOPKG_PKGTMP}/$archive_name" . 2>&1
 
             # Move archive to export directory
             ${MKDIR} "${wizard_export_path}"
