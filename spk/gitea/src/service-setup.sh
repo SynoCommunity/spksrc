d23a127986b683f2c2f3f62e0eec6098acfe519c
wiktor.kobiela <wuch.mail@gmail.com>
2023-11-25 23:43:26 +0100

    Update package

--- spk/gitea/src/service-setup.sh
+++ spk/gitea/src/service-setup.sh
@@ -5,39 +5,42 @@ PATH="/var/packages/git/target/bin:${PATH}"
 if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 7 ]; then
     SYNOPKG_PKGHOME="${SYNOPKG_PKGDEST}"
 fi
+
+if [ $SYNOPKG_DSM_VERSION_MAJOR -lt 6 ]; then
+    SYNOPKG_PKGHOME="${SYNOPKG_PKGVAR}"
+fi
+
 ENV="PATH=${PATH} HOME=${SYNOPKG_PKGHOME}"
 
 SERVICE_COMMAND="env ${ENV} ${GITEA} web --port ${SERVICE_PORT} --pid ${PID_FILE}"
 SVC_BACKGROUND=y
 
-service_preinst ()
-{
-    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ] && [ $SYNOPKG_DSM_VERSION_MAJOR -lt 6 ]; then
-        SHARED_FOLDER="${wizard_shared_folder_name}"
-        if [ ! -d "${SHARED_FOLDER}" ]; then
-            mkdir -p "${SHARED_FOLDER}" || {
-                echo "Failed to create directory \"${SHARED_FOLDER}\"."
-                exit 1
-            }
-        fi
-        set_syno_permissions "${SHARED_FOLDER}" "${EFF_USER}"
-    fi
-}
-
 service_postinst ()
 {
     if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
-        SHARED_FOLDER="${wizard_shared_folder_name}"
         IP=$(ip route get 1 | awk '{print $(NF);exit}')
-        # Default configuration with shared folder
-        {
-            echo "[repository]"
-            echo "ROOT = ${SHARED_FOLDER}/gitea-repositories"
-            echo "[server]"
-            echo "LFS_CONTENT_PATH = ${SHARED_FOLDER}/lfs"
-            echo "SSH_DOMAIN = ${IP:=localhost}"
-            echo "DOMAIN = ${IP:=localhost}"
-            echo "ROOT_URL = http://${IP:=localhost}:${SERVICE_PORT}/"
-        } > "$CONF_FILE"
+    fi
+}
+
+service_preupgrade ()
+{
+    #Backup existing config on DSM6
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 6 ]; then
+        if [-f "${SYNOPKG_PKGHOME}/conf.ini" ]; then
+            echo "Backup old config on DSM6"  
+            mv ${SYNOPKG_PKGHOME}/conf.ini ${SYNOPKG_PKGHOME}/conf.ini.bck 2>&1
+        fi
+    fi
+}
+
+service_postupgrade ()
+{
+    #Restore existing config on DSM6
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 6 ]; then
+        if [-f "${SYNOPKG_PKGHOME}/conf.ini.bkc" ]; then
+            echo "Restore old config on DSM6" 
+            rm -f ${SYNOPKG_PKGHOME}/conf.ini
+            mv ${SYNOPKG_PKGHOME}/conf.ini.bck ${SYNOPKG_PKGHOME}/conf.ini 2>&1
+        fi
     fi
 }
