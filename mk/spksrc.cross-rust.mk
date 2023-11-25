8942371c0c1c88c1840d7f9c567e8e1d73b3c4ec
hgy59 <hpgy59@gmail.com>
2023-11-25 07:34:08 +0100

    synocli-file: Add lsd and update tools
    - Add lsd v1.0.0
    - Update bat to v0.24.0
    - Update eza to v0.16.1
    - Update fd to v8.7.0
    - Update fzf to v0.44.1
    - Update micro to v2.0.13
    - Update sd to v1.0.0
    - update zlib-ng to v2.1.4

--- mk/spksrc.cross-rust.mk
+++ mk/spksrc.cross-rust.mk
@@ -53,9 +53,9 @@ endif
 CARGO_INSTALL_ARGS += --path $(RUST_SRC_DIR)
 CARGO_INSTALL_ARGS += --root $(STAGING_INSTALL_PREFIX)
 
-# Default build with rust and installation with cargo
+# Default build with rust and install with cargo
 rust_install_target:
-	@echo "  ==> Cargo install rust package $(PKG_NAME)"
+	@echo "  ==> Cargo install rust package $(PKG_NAME) ($(shell rustc --version); $(RUST_TOOLCHAIN))"
 	@$(RUN) cargo +$(RUST_TOOLCHAIN) install $(CARGO_INSTALL_ARGS)
 
 
