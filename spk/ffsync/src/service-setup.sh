4e8141c3580f1f6a98ddb0ed4f60bda4d6a4fefd
hgy59 <hpgy59@gmail.com>
2023-12-01 22:28:15 +0100

    ffsync: improve package installation
    - define DBUSER for package db user (SPK_NAME is not defined)
    - use installed diesel binary
    - fix config file location
    - use a single sed call to patch config file

--- spk/ffsync/src/service-setup.sh
+++ spk/ffsync/src/service-setup.sh
@@ -1,15 +1,18 @@
 PYTHON_DIR="/var/packages/python311/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
+
 MARIADB_10_INSTALL_DIRECTORY="/var/packages/MariaDB10"
 MARIADB_10_BIN_DIRECTORY="${MARIADB_10_INSTALL_DIRECTORY}/target/usr/local/mariadb10/bin"
 MYSQL="${MARIADB_10_BIN_DIRECTORY}/mysql"
 
 PYTHON="${SYNOPKG_PKGDEST}/env/bin/python3"
-SYNCSERVER="${HOME}/bin/syncserver"
-HOME="${SYNOPKG_PKGDEST}/share/${SPK_NAME}"
-CFG_FILE="${HOME}/config/local.toml"
+SYNCSERVER="${SYNOPKG_PKGDEST}/bin/syncserver"
+DIESEL="${SYNOPKG_PKGDEST}/bin/diesel"
+CFG_FILE="${SYNOPKG_PKGVAR}/local.toml"
 
 SERVICE_COMMAND="${SYNCSERVER} --config=${CFG_FILE}"
+
+DBUSER=ffsync
 
 validate_preinst ()
 {
@@ -20,15 +23,15 @@ validate_preinst ()
                 echo "Incorrect MySQL root password"
                 exit 1
             fi
-            if ${MYSQL} -u root -p"${wizard_mysql_password_root}" mysql -e "SELECT User FROM user" | grep ^${SPK_NAME}$ > /dev/null 2>&1; then
-                echo "MySQL user ${SPK_NAME} already exists"
+            if ${MYSQL} -u root -p"${wizard_mysql_password_root}" mysql -e "SELECT User FROM user" | grep ^${DBUSER}$ > /dev/null 2>&1; then
+                echo "MySQL user ${DBUSER} already exists"
                 exit 1
             fi
             if ${MYSQL} -u root -p"${wizard_mysql_password_root}" -e "SHOW DATABASES" | grep -E '^(syncstorage_rs|tokenserver_rs)$' > /dev/null 2>&1; then
-                echo "MySQL database(s) for ${SPK_NAME} already exist(s)"
+                echo "MySQL database(s) for ${DBUSER} already exist(s)"
                 exit 1
             fi
-        fi   
+        fi
     fi
 }
 
@@ -44,23 +47,23 @@ service_postinst ()
         # login as root sql user using whatever creds you set up for that
         # this sets up a user for sync storage and sets up the databases
         ${MYSQL} -u root -p ${wizard_mysql_password_root} <<EOF
-CREATE USER "${SPK_NAME}"@"localhost" IDENTIFIED BY "${wizard_password_ffsync}";
+CREATE USER "${DBUSER}"@"localhost" IDENTIFIED BY "${wizard_password_ffsync}";
 CREATE DATABASE syncstorage_rs;
 CREATE DATABASE tokenserver_rs;
-GRANT ALL PRIVILEGES on syncstorage_rs.* to ${SPK_NAME}@localhost;
-GRANT ALL PRIVILEGES on tokenserver_rs.* to ${SPK_NAME}@localhost;
+GRANT ALL PRIVILEGES on syncstorage_rs.* to ${DBUSER}@localhost;
+GRANT ALL PRIVILEGES on tokenserver_rs.* to ${DBUSER}@localhost;
 EOF
 
         # Run initial database migrations
         # syncstorage db initialization
-        $HOME/.cargo/bin/diesel --database-url "mysql://${SPK_NAME}:${wizard_password_ffsync}@localhost/syncstorage_rs" \
+        ${DIESEL} --database-url "mysql://${DBUSER}:${wizard_password_ffsync}@localhost/syncstorage_rs" \
             migration --migration-dir syncstorage-mysql/migrations run
         # tokenserver db initialization
-        $HOME/.cargo/bin/diesel --database-url "mysql://${SPK_NAME}:${wizard_password_ffsync}@localhost/tokenserver_rs" \
+        ${DIESEL} --database-url "mysql://${DBUSER}:${wizard_password_ffsync}@localhost/tokenserver_rs" \
             migration --migration-dir tokenserver-db/migrations run
 
         # Add sync endpoint to database
-        ${MYSQL} -u ${SPK_NAME} -p "$(wizard_password_ffsync)" <<EOF
+        ${MYSQL} -u ${DBUSER} -p "$(wizard_password_ffsync)" <<EOF
 USE tokenserver_rs
 INSERT INTO services (id, service, pattern) VALUES
     (1, "sync-1.5", "{node}/1.5/{uid}");
@@ -68,7 +71,7 @@ EOF
 
         # Add syncserver node
         # the 10 is the user capacity.
-        SYNC_TOKENSERVER__DATABASE_URL="mysql://${SPK_NAME}:${wizard_password_ffsync}@localhost/tokenserver_rs" \
+        SYNC_TOKENSERVER__DATABASE_URL="mysql://${DBUSER}:${wizard_password_ffsync}@localhost/tokenserver_rs" \
             python3 tools/tokenserver/add_node.py \
             ${wizard_ffsync_public_url} 10
 
@@ -79,14 +82,12 @@ EOF
         # Escape vertical bars in the replacement values
         WIZARD_PASSWORD=$(echo "${wizard_password_ffsync}" | sed 's/|/\\|/g')
 
-        # Copy configuration template into folder
-        ${CP} "${SYNOPKG_PKGDEST}/var/local.toml" "${CFG_FILE}"
-
         # Perform replacements using sed with | as the delimiter
-        sed -i "s|{{MASTER_SECRET}}|${MASTER_SECRET}|g" "${CFG_FILE}"
-        sed -i "s|{{TCP_PORT}}|${SERVICE_PORT}|g" "${CFG_FILE}"
-        sed -i "s|{{SQL_USER}}|${SPK_NAME}|g" "${CFG_FILE}"
-        sed -i "s|{{SQL_PASS}}|${WIZARD_PASSWORD}|g" "${CFG_FILE}"
-        sed -i "s|{{METRICS_HASH_SECRET}}|${METRICS_HASH_SECRET}|g" "${CFG_FILE}"
+        sed -e "s|{{MASTER_SECRET}}|${MASTER_SECRET}|g"             \
+            -e "s|{{TCP_PORT}}|${SERVICE_PORT}|g"                   \
+            -e "s|{{SQL_USER}}|${DBUSER}|g"                         \
+            -e "s|{{SQL_PASS}}|${WIZARD_PASSWORD}|g"                \
+            -e "s|{{METRICS_HASH_SECRET}}|${METRICS_HASH_SECRET}|g" \
+            -i "${CFG_FILE}"
     fi
 }
