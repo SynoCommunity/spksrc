1e5e33269f76be1a14906bfdc4822ed9b411dfd7
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2023-12-24 06:12:20 -0400

    Modify restore of .htaccess file

--- spk/cops/src/service-setup.sh
+++ spk/cops/src/service-setup.sh
@@ -168,12 +168,11 @@ service_save ()
 
 service_restore ()
 {
-    if [ -f "${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/config_local.php" ] && [ -f "${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/.htaccess" ]; then
+    if [ -f "${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/config_local.php" ]; then
         # Restore some stuff
         echo "Restore previous data from ${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}"
-        # Restore cops configuration files
+        # Restore cops configuration file
         rsync -aX --update -I ${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/config_local.php ${WEB_ROOT}/config_local.php 2>&1
-        rsync -aX --update -I ${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/.htaccess ${WEB_ROOT}/.htaccess 2>&1
     else
         # Backup missing, re-initialise default values
         CFG_FILE="${WEB_ROOT}/config_local.php"
@@ -187,6 +186,20 @@ service_restore ()
             sed -i -e "s|@cops_title@|${wizard_cops_title:=COPS}|g" ${CFG_FILE}
             sed -i -e "s|@use_url_rewriting@|${url_rewriting:=0}|g" ${CFG_FILE}
             chmod ga+w "${CFG_FILE}"
+        fi
+    fi
+    if [ -f "${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/.htaccess" ]; then
+        # Check if the .htaccess file has been modified
+        if [ -f ${WEB_ROOT}/.htaccess ]; then
+            SRC_FILE=${SYNOPKG_TEMP_UPGRADE_FOLDER}/${PACKAGE}/web/.htaccess
+            DST_FILE=${WEB_ROOT}/.htaccess
+            OUTPUT_FILE=${WEB_ROOT}/.htaccess.diff
+            DIFFERENCES=$(diff "$SRC_FILE" "$DST_FILE")
+            if [ -n "$DIFFERENCES" ]; then
+                # Write the differences to the output file
+                echo "$DIFFERENCES" > "$OUTPUT_FILE"
+                echo "Modifications to .htaccess file detected. Details saved to $OUTPUT_FILE"
+            fi
         fi
     fi
 
