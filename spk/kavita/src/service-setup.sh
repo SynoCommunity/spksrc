b9b9e1545103907442d9812c78b9da65953a1edd
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2024-01-11 05:05:10 -0400

    Fix startup config

--- spk/kavita/src/service-setup.sh
+++ spk/kavita/src/service-setup.sh
@@ -3,25 +3,33 @@
 KAVITA="${SYNOPKG_PKGDEST}/share/Kavita"
 
 # Kavita uses custom Config and PID directories
-HOME_DIR="${SYNOPKG_PKGVAR}"
-CONFIG_DIR="${HOME_DIR}/config"
+HOME_DIR="${SYNOPKG_PKGDEST}/share"
+CONFIG_DIR="${SYNOPKG_PKGVAR}/config"
 PID_FILE="${CONFIG_DIR}/kavita.pid"
-
-if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
-    SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${KAVITA}"
-else
-    SERVICE_COMMAND="env HOME=${HOME_DIR} ${KAVITA}"
-fi
 
 SVC_BACKGROUND=y
 SVC_WAIT_TIMEOUT=90
+
+service_prestart ()
+{
+    # Replace generic service startup, fork process in background
+    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+        COMMAND="env LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${KAVITA}"
+    else
+        COMMAND="env ${KAVITA}"
+    fi
+    echo "Starting Kavita at ${HOME_DIR}" >> ${LOG_FILE}
+    cd ${HOME_DIR};
+    ${COMMAND} >> ${LOG_FILE} 2>&1 &
+    echo "$!" > "${PID_FILE}"
+}
 
 service_postinst ()
 {
     echo "Setup config directory"
     # Remove default config directory and link to var
-    ${RM} "${SYNOPKG_PKGDEST}/share/config"
-    ${LN} "${CONFIG_DIR}" "${SYNOPKG_PKGDEST}/share/config"
+    ${RM} "${HOME_DIR}/config"
+    ${LN} "${CONFIG_DIR}" "${HOME_DIR}/config"
 
     if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
         set_unix_permissions "${CONFIG_DIR}"
