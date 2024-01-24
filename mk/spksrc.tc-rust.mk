b57b66bd43f7771cc59a50546b7c714b0e01b92a
Vincent Fortier <th0ma7@gmail.com>
2024-01-24 22:04:19 +0000

    rust: Fix typo in precedent commit

--- mk/spksrc.tc-rust.mk
+++ mk/spksrc.tc-rust.mk
@@ -109,7 +109,7 @@ else
 	@$(MAKE) -C ../../native/cmake
 	@$(MSG) "Building Tier-3 rust target: $(RUST_TARGET)"
 	@(cd $(WORK_DIR) && [ ! -d rust ] && git clone --depth 1 https://github.com/rust-lang/rust.git || true)
-	@(cd $(WORK_DIR) && git submodule init && git submodule update --depth 1 src/llvm-project)
+	@(cd $(WORK_DIR)/rust && git submodule init && git submodule update --depth 1 src/llvm-project)
 	@(cd $(WORK_DIR)/rust && rm -f config.toml && ./x setup compiler)
 	(cd $(WORK_DIR)/rust && \
 	   CFLAGS_$(subst -,_,$(RUST_TARGET))="$(TC_EXTRA_CFLAGS)" \
