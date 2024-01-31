fffff69df034112228e108c1683581e7b86fa7a2
Vincent Fortier <th0ma7@users.noreply.github.com>
2024-01-31 07:02:01 -0500

    Revert "cross-cc.mk: Msg. to run make setup first when using all-supported (#…" (#6001)
    This reverts commit 5bf0ab506ab9b1688d0e94d233c05beebeb234ab.

--- mk/spksrc.cross-cc.mk
+++ mk/spksrc.cross-cc.mk
@@ -89,17 +89,8 @@ all-supported:
 	    env $(ENV) $(MAKE) -C ../../$$depend 2>&1 | tee --append build-$${depend%/*}-$${depend#*/}.log ; \
 	    [ $${PIPESTATUS[0]} -eq 0 ] || false ; \
 	  fi ; \
-	done
-ifeq ($(strip $(SUPPORTED_ARCHS)),)
-	$(MAKE) supported-arch-error
-else
+	done ; \
 	$(MAKE) $(addprefix supported-arch-,$(SUPPORTED_ARCHS))
-endif
-
-supported-arch-error:
-	@$(MSG) ########################################################
-	@$(MSG) ERROR - Please run make setup from spksrc root directory
-	@$(MSG) ########################################################
 
 supported-arch-%:
 	@$(MSG) BUILDING package for arch $* with SynoCommunity toolchain
