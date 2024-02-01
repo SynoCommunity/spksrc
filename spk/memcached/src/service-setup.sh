df14b70f2284d3900f9d2768a1e496bf0ae3563a
hgy59 <hpgy59@gmail.com>
2024-02-02 00:46:03 +0100

    memcached: fix uninstall on DSM < 7 (#6006)
    * memcached: fix uninstall on DSM < 7
    * limit to DSM version supporting PHP 7.4

--- spk/memcached/src/service-setup.sh
+++ spk/memcached/src/service-setup.sh
@@ -1,27 +1,28 @@
 
-if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
-    # for DSM < 7
-    # for owner of var folder
-    GROUP=http
-fi
-
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
+CONFIG_DIR="${SYNOPKG_PKGVAR}/phpmemcachedadmin.config"
+
 MEMCACHED="${SYNOPKG_PKGDEST}/bin/memcached"
 MEMORY=$(awk '/MemTotal/{memory=$2/1024*0.15; if (memory > 64) memory=64; printf "%0.f", memory}' /proc/meminfo)
 SERVICE_COMMAND="${MEMCACHED} -d -m ${MEMORY} -P ${PID_FILE}"
 
+
+if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
+    # for DSM < 7
+    # for owner of var folder
+    GROUP=http
+    WEB_DIR=/var/services/web
+fi
+
 service_postinst ()
 {
    # create config file on demand
-   if [ ! -e "${SYNOPKG_PKGVAR}/phpmemcachedadmin.config/Memcache.php" ]; then
+   if [ ! -e ${CONFIG_DIR}/Memcache.php ]; then
       echo "Create default config file Memcache.php"
-      cp -f ${SYNOPKG_PKGVAR}/phpmemcachedadmin.config/Memcache.sample.php ${SYNOPKG_PKGVAR}/phpmemcachedadmin.config/Memcache.php
+      cp -f ${CONFIG_DIR}/Memcache.sample.php ${CONFIG_DIR}/Memcache.php
    fi
 
    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
-
-      CONFIG_DIR="${SYNOPKG_PKGVAR}/phpmemcachedadmin.config"
-      WEB_DIR="/var/services/web"
 
       # Install the web interface
       cp -R "${SYNOPKG_PKGDEST}/share/phpMemcachedAdmin" ${WEB_DIR}
