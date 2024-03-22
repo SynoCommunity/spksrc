a379daeb4e3d975847d3dfaa249754c1745509bc
hgy59 <hpgy59@gmail.com>
2024-03-22 18:00:09 +0100

    mpd: update to v0.23.15
    - update mpd and libmpdclient

--- spk/mpd/src/service-setup.sh
+++ spk/mpd/src/service-setup.sh
@@ -13,3 +13,24 @@ service_postinst ()
     mkdir -p "${SYNOPKG_PKGVAR}/playlists"
 }
 
+service_preupgrade ()
+{
+    # create file with installer variables on the fly
+    if [ ! -e "${INST_VARIABLES}" ]; then
+        if [ -e "${CFG_FILE}" ]; then
+            if [ -z "${SHARE_PATH}" ]; then
+                SHARE_PATH=$(cat ${CONFIG_FILE} | grep music_directory | awk '{print $2}')
+            fi
+            if [ -z "${SHARE_NAME}" -a -n "${SHARE_PATH}" ]; then
+                SHARE_NAME=$(basename ${SHARE_PATH})
+            fi
+            echo "Create ${INST_VARIABLES} [SHARE_PATH=${SHARE_PATH}, SHARE_NAME=${SHARE_NAME}]"
+            save_wizard_variables
+        else
+            echo "WARNING: cannot create ${INST_VARIABLES}. Config not found: ${CONFIG_FILE}"
+        fi
+    else
+        echo "Installer variables available"
+        cat "${INST_VARIABLES}"
+    fi
+}
