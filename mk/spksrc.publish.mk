SynoCommunity/spksrc

commit ecf67ddc45f88032f00fdb9b6bafd9b5c713d58e

Add generic spksrc.supported.mk (#6002)
* cross-cc.mk: Make usage of generic spksrc.supported.mk

* common.mk: Adjust pstat filename

* python311: Transparent change to force builds

* cross-rust.mk: Use generic spksrc.supported.mk helper

* supported.mk: Ensure pre-build-native is a grouped target using &:

group target (&:) tells make that instead of building each target
with a different invocation of the recipe, which is the default,
a single invocation of the 'pre-build-native' recipe builds all
the targets.

* cross-go.mk: Use generic spksrc.supported.mk helper

* cross-go.mk: Fix mod file download missing u+rw flags

Ref https://github.com/golang/go/issues/31481
Otherwise files under pkg/mod/golang.org/* are read-only and
make clean cannot proceed with it's deletion.

* spk.mk: Major clean-up and simplification to the build narrative

* spksrc: Rename kernel.mk to cross-kernel.mk

* common.mk: Enable PSTAT only with MAKELEVEL <= 2

* cross-rust.mk: Remove kerne-required.mk left over

* common-rules.mk: Move common rules out of common.mk as multi-loop

* spk.mk: Fix PSTAT_TIME to only provide stats on entire arch build

* common-rules.mk: Add make rustup to check rust toolchain status

* cross-cmake.mk: Make independent from cross-cc.mk

* main-depends.mk: Convert to supported.mk

* .gitignore: Add status* to ignore list

* cross-cmake.mk: Missing download rule dependency

* Limit PATH populating and allow rustup command from all cross*

* supported.mk: Further reduce code duplication

* Move *tree, <smart>-clean to common-rules and provide native access

* qoriq-rust: Optimize code by including DEPENDS in toolchains

* spksrc: Basics for a first iteration of framework testing facility

* tc.mk: Re-order depend processing to be done afterwards rustc

* meson+ninja+cmake: Fix build using ninja and allow new *_BASE_DIR

While fixing build process I came accross zstd special case where

mk/spksrc.publish.mk
@@ -1,0 +1,62 @@
+###
+
+# make publish-all-<supported|latest>
+ifeq (publish,$(findstring publish,$(subst -, ,$(firstword $(MAKECMDGOALS)))))
+ACTION=publish-
+endif
+ifeq ($(PUBLISH_URL),)
+TARGET_ARCH=error-publish_url
+endif
+ifeq ($(PUBLISH_API_KEY),)
+TARGET_ARCH=error-api_key
+endif
+
+####
+
+.PHONY: $(ACTION)$(TARGET_TYPE)-arch-error-publish_url $(ACTION)$(TARGET_TYPE)-arch-error-api_key
+
+$(ACTION)$(TARGET_TYPE)-arch-error-publish_url:
+	@$(MSG) ########################################################
+	@$(MSG) ERROR - Set PUBLISH_URL in local.mk
+	@$(MSG) ########################################################
+	$(error)
+
+$(ACTION)$(TARGET_TYPE)-arch-error-api_key:
+	@$(MSG) ########################################################
+	@$(MSG) ERROR - Set PUBLISH_API_KEY in local.mk
+	@$(MSG) ########################################################
+	$(error)
+
+###
+
+.PHONY: publish-all-$(TARGET_TYPE)
+
+publish-all-$(TARGET_TYPE): $(addprefix publish-$(TARGET_TYPE)-arch-,$(TARGET_ARCH))
+
+$(ACTION)$(TARGET_TYPE)-arch-%: $(TARGET_TYPE)-arch-%
+	-@MAKEFLAGS= $(MAKE) publish-arch-$*
+
+publish-arch-%:
+	$(MAKE) $(addprefix publish-build-arch-, $(or $(filter $(addprefix %, $(DEFAULT_TC)), $(filter %$(word 2,$(subst -, ,$*)), $(filter $(firstword $(subst -, ,$*))%, $(AVAILABLE_TOOLCHAINS)))),$*))
+
+###
+
+publish-build-arch-%: SHELL:=/bin/bash
+publish-build-arch-%:
+	@$(MSG) PUBLISHING package for arch $* to http://synocommunity.com | tee --append build-$*.log
+	@MAKEFLAGS= $(MAKE) ARCH=$(firstword $(subst -, ,$*)) TCVERSION=$(lastword $(subst -, ,$*)) publish 2>&1 | tee --append publish-$*.log >(grep -e '^http' -e '^{"package":' -e '^{"message":' >> status-publish.log) ; \
+	status=$${PIPESTATUS[0]} ; \
+	[ $${status[0]} -eq 0 ] || false
+	
+###
+
+publish:
+	@response=$$(http --verify=no --ignore-stdin --auth $(PUBLISH_API_KEY): POST $(PUBLISH_URL)/packages @$(SPK_FILE_NAME) --print=hb) ; \
+	response_code=$$(echo "$$response" | grep -Fi "HTTP/1.1" | awk '{print $$2}') ; \
+	if [ "$$response_code" = "201" ] ; then \
+		output=$$(echo "$$response" | awk '/^[[:space:]]*$$/ {p=1;next} p') ; \
+		echo "Package published successfully\n$$output" | tee --append publish-$*.log ; \
+	else \
+		echo "ERROR: Failed to publish package - HTTP response code $$response_code\n$$response" | tee --append publish-$*.log ; \
+		exit 1 ; \
+	fi

.gitignore
@@ -1,12 +1,15 @@
 distrib
 packages
 native/*/work*
+native/*/status*
 cross/*/work*
 cross/*/build*
+cross/*/status*
 kernel/*/work*
 spk/*/work*
 spk/*/build*
 spk/*/publish*
+spk/*/status*
 diyspk/*/work*
 toolchain/*/work*
 toolchain/*/*_done

Makefile
@@ -1,20 +1,26 @@
 
+# Include framework self-test
+include mk/spksrc.test-rules.mk
+
 AVAILABLE_TCS = $(notdir $(wildcard toolchain/syno-*))
 AVAILABLE_ARCHS = $(notdir $(subst syno-,/,$(AVAILABLE_TCS)))
 SUPPORTED_SPKS = $(sort $(patsubst spk/%/Makefile,%,$(wildcard spk/*/Makefile)))
 
 
+ifneq ($(firstword $(MAKECMDGOALS)),test)
 all: $(SUPPORTED_SPKS)
+endif
 
 all-noarch:
-	@for spk in $(sort $(dir $(wildcard spk/*/Makefile))) ; \
+	@for spk in $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile))) ; \
 	do \
 	   grep -q "override ARCH" "$${spk}/Makefile" && $(MAKE) -C $${spk} ; \
 	done
 
-
+ifneq ($(firstword $(MAKECMDGOALS)),test)
 clean: $(addsuffix -clean,$(SUPPORTED_SPKS))
 clean: native-clean cross-clean
+endif
 
 dist-clean: clean
 dist-clean: kernel-clean toolchain-clean toolkit-clean
@@ -50,7 +56,7 @@ cross-clean:
 	done
 
 spk-clean:
-	@for spk in $(dir $(wildcard spk/*/Makefile)) ; \
+	@for spk in $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile))) ; \
 	do \
 	    $(MAKE) -C $${spk} clean ; \
 	done
@@ -58,27 +64,36 @@ spk-clean:
 %: spk/%/Makefile
 	cd $(dir $^) && env $(MAKE)
 
-%-clean: spk/%/Makefile
-	cd $(dir $^) && env $(MAKE) clean
-
 native-%: native/%/Makefile
 	cd $(dir $^) && env $(MAKE)
 
 native-%-clean: native/%/Makefile
 	cd $(dir $^) && env $(MAKE) clean
 
-# build dependency tree for all packages
-# and take the tree output only (starting with a tab)
-dependency-tree:
-	@for spk in $(dir $(wildcard spk/*/Makefile)) ; \
+# build dependency flat list for all packages
+dependency-flat:
+	@echo $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile)))
+	@for spk in $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile))) ; \
 	do \
-	    $(MAKE) -C $${spk} dependency-tree | grep -P "^[\t]" ; \
+	    echo "$(MAKE) -s -C $${spk} dependency-flat" ; \
+	    $(MAKE) -s -C $${spk} dependency-flat ; \
+	done
+
+# build dependency tree for all packages
+dependency-tree:
+	@echo $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile)))
+	@for spk in $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile))) ; \
+	do \
+	    echo "$(MAKE) --no-print-directory -C $${spk} dependency-tree" ; \
+	    $(MAKE) --no-print-directory -C $${spk} dependency-tree ; \
 	done
 
 # build dependency list for all packages
 dependency-list:
-	@for spk in $(dir $(wildcard spk/*/Makefile)) ; \
+	@echo $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile)))
+	@for spk in $(filter-out $(dir $(wildcard spk/*/BROKEN)),$(dir $(wildcard spk/*/Makefile))) ; \
 	do \
+	    echo "$(MAKE) -s -C $${spk} dependency-list" ; \
 	    $(MAKE) -s -C $${spk} dependency-list ; \
 	done
 
@@ -166,6 +181,7 @@ local.mk:
 	@echo "DISTRIBUTOR_URL =" >> $@
 	@echo "REPORT_URL =" >> $@
 	@echo "DEFAULT_TC =" >> $@
+	@echo "#PSTAT = on" >> $@
 	@echo "#PARALLEL_MAKE = max" >> $@
 
 dsm-%: local.mk

cross/libwebp/Makefile
@@ -12,23 +12,12 @@ HOMEPAGE = https://developers.google.com/speed/webp
 COMMENT  = WebP codec: library to encode and decode images in WebP format.
 LICENSE  = https://github.com/webmproject/libwebp/blob/master/COPYING
 
-GNU_CONFIGURE = 1
-PRE_CONFIGURE_TARGET = libwebp_pre_configure
-CONFIGURE_ARGS  = --disable-static
-CONFIGURE_ARGS += --enable-libwebpdecoder
+CMAKE_USE_NINJA = 1
 
-ADDITIONAL_CPPFLAGS = -O
+include ../../mk/spksrc.cross-cmake.mk
 
-include ../../mk/spksrc.common.mk
-# newer gcc require definition of -std=c11
-ifneq ($(call version_lt,$(TCVERSION),6.1)$(call version_ge,$(TCVERSION),3.0),11)
-ifneq ($(findstring $(ARCH),$(ARMv5_ARCHS)),$(ARCH))
-ADDITIONAL_CPPFLAGS += -std=c11
+# gcc version below 5.x requires enforcing c11
+# fallback to default for armv5 using older gcc
+ifeq ($(call version_le, $(TC_GCC), 5)$(call version_gt,$(TC_GCC),4.7),11)
+ADDITIONAL_CFLAGS += -std=c11
 endif
-endif
-
-include ../../mk/spksrc.cross-cc.mk
-
-.PHONY: libwebp_pre_configure
-libwebp_pre_configure:
-	$(RUN) ./autogen.sh

cross/mysql-connector-c/Makefile
@@ -13,6 +13,7 @@ COMMENT  = MySQL C API (libmysqlclient)
 LICENSE  = GNU GPLv2
 
 USE_NATIVE_CMAKE_LEGACY = 1
+CMAKE_USE_NINJA = 1
 
 include ../../mk/spksrc.cross-cmake.mk
 

cross/openjpeg/Makefile
@@ -13,6 +13,7 @@ COMMENT  = OpenJPEG is an open-source JPEG 2000 codec written in C language.
 LICENSE  = BSD 2-clause simplified
 
 CMAKE_USE_NINJA = 1
+CMAKE_ARGS += -Wno-dev
 CMAKE_ARGS += -DBUILD_STATIC_LIBS=OFF
 CMAKE_ARGS += -DOPENJP2_COMPILE_OPTIONS="-O"
 # deactivate build of tools (we use the library only)

cross/x265/Makefile
@@ -3,7 +3,7 @@ PKG_VERS = 3.5
 PKG_EXT = tar.gz
 PKG_DIST_NAME = $(PKG_NAME)_$(PKG_VERS).$(PKG_EXT)
 PKG_DIST_SITE = https://bitbucket.org/multicoreware/x265_git/downloads
-PKG_DIR = $(PKG_NAME)_$(PKG_VERS)/source
+PKG_DIR = $(PKG_NAME)_$(PKG_VERS)
 
 OPTIONAL_DEPENDS = native/nasm
 
@@ -11,78 +11,46 @@ HOMEPAGE = http://x265.org/
 COMMENT  = x265 is an open source HEVC encoder.
 LICENSE  = GPL
 
-CONFIGURE_TARGET = x265_configure
+PRE_CONFIGURE_TARGET = x265_pre_configure
+PRE_COMPILE_TARGET = x265_pre_compile
 
-CMAKE_ARGS  = -DCMAKE_INSTALL_PREFIX=$(INSTALL_PREFIX)
-CMAKE_ARGS += -DCMAKE_CROSSCOMPILING=TRUE
-CMAKE_ARGS += -DCMAKE_SYSTEM_NAME=Linux
-CMAKE_ARGS += -DCMAKE_C_COMPILER=$(TC_PATH)$(TC_PREFIX)gcc
-CMAKE_ARGS += -DCMAKE_CXX_COMPILER=$(TC_PATH)$(TC_PREFIX)g++
-CMAKE_ARGS += -DCMAKE_INSTALL_RPATH=$(INSTALL_PREFIX)/lib
-CMAKE_ARGS += -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=TRUE
-CMAKE_ARGS += -DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE
-CMAKE_ARGS += -DENABLE_LIBNUMA=OFF
-CMAKE_ARGS += -DENABLE_PIC=ON
-#CMAKE_ARGS += -DENABLE_CLI=OFF
+CMAKE_USE_NINJA = 1
+CMAKE_USE_NASM = 1
+CMAKE_SOURCE_DIR = source
+
+# Build directories for default, 12 and 10 bit depth
+CMAKE_BUILD_DIR = $(WORK_DIR)/$(PKG_DIR)/buildAll
+CMAKE_BUILD_12B = $(WORK_DIR)/$(PKG_DIR)/build12bit
+CMAKE_BUILD_10B = $(WORK_DIR)/$(PKG_DIR)/build10bit
 
 # additional cmake args for the build of 10 and 12 bit depth libraries
-CMAKE_BITLIB_ARGS =
+CMAKE_ARGS_BITLIB += -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF
+CMAKE_ARGS_BITLIB += -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DEXTRA_LINK_FLAGS="-ldl"
+# provide cmake toolchain path when building bitlibs
+CMAKE_ARGS_BITLIB += -DCMAKE_TOOLCHAIN_FILE=$(CMAKE_TOOLCHAIN_PKG)
 
 # additional cmake args for the final multilib build
-CMAKE_MULTILIB_ARGS =
+ADDITIONAL_CMAKE_ARGS += -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS="-ldl -L."
+ADDITIONAL_CMAKE_ARGS += -DLINKED_10BIT=ON -DLINKED_12BIT=ON -DENABLE_SHARED=ON
 
 include ../../mk/spksrc.common.mk
 
-ifeq ($(findstring $(ARCH),$(ARMv5_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=armv5
-CMAKE_ARGS += -DCROSS_COMPILE_ARM=ON
-CMAKE_ARGS += -DENABLE_ASSEMBLY=OFF
-endif
-
-ifeq ($(findstring $(ARCH),$(ARMv7_ARCHS) $(ARMv7L_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=armv7
-CMAKE_ARGS += -DCROSS_COMPILE_ARM=ON
-CMAKE_ARGS += -DENABLE_ASSEMBLY=OFF
-endif
-
-ifeq ($(findstring $(ARCH),$(ARMv8_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=aarch64
-CMAKE_ARGS += -DCROSS_COMPILE_ARM=ON
 # asm code for HIGH_BIT_DEPTH is not compatible with arm64/aarch64
 # https://bitbucket.org/multicoreware/x265_git/issues/549/fail-to-build-for-aarch64-and-armhf
-CMAKE_BITLIB_ARGS += -DENABLE_ASSEMBLY=OFF
-endif
-
-ifeq ($(findstring $(ARCH),$(PPC_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=ppc64
-CMAKE_ARGS += -DCMAKE_CXX_FLAGS=-m32
-CMAKE_ARGS += -DENABLE_ALTIVEC=OFF
-CMAKE_ARGS += -DCPU_POWER8=OFF
+ifeq ($(findstring $(ARCH),$(ARM_ARCHS) $(PPC_ARCHS)),$(ARCH))
 CMAKE_ARGS += -DENABLE_ASSEMBLY=OFF
 endif
 
-# Define x86asm
 ifeq ($(findstring $(ARCH),$(i686_ARCHS) $(x64_ARCHS)),$(ARCH))
-BUILD_DEPENDS = native/nasm
-NASM_PATH = $(abspath $(PWD)/../../native/nasm/work-native/install/usr/local/bin)
-CMAKE_ARGS += -DNASM_EXECUTABLE=$(NASM_PATH)/nasm
-ENV += AS=$(NASM_PATH)/nasm
-ENV += PATH=$(NASM_PATH):$$PATH
-endif
-
-ifeq ($(findstring $(ARCH),$(x64_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=x86_64
 CMAKE_ARGS += -DCMAKE_ASM_NASM_FLAGS=-w-macro-params-legacy
 endif
 
-ifeq ($(findstring $(ARCH),$(i686_ARCHS)),$(ARCH))
-CMAKE_ARGS += -DCMAKE_SYSTEM_PROCESSOR=x86
 # asm code for HIGH_BIT_DEPTH is not compatible with 32-bit arch
-CMAKE_BITLIB_ARGS += -DENABLE_ASSEMBLY=OFF
-CMAKE_MULTILIB_ARGS += -DCMAKE_ASM_NASM_FLAGS=-w-macro-params-legacy
+ifeq ($(findstring $(ARCH),$(i686_ARCHS)),$(ARCH))
+CMAKE_ARGS_BITLIB += -DENABLE_ASSEMBLY=OFF
 endif
 
-include ../../mk/spksrc.cross-cc.mk
+include ../../mk/spksrc.cross-cmake.mk
 
 # gcc version above 4.8 required to support hdr10plus
 ifeq ($(call version_le, $(TC_GCC), 4.8),1)
@@ -102,36 +70,18 @@ CMAKE_ARGS += -DNO_ATOMICS=ON
 CMAKE_ARGS += -DCMAKE_CXX_FLAGS="-DSPKSRC_FIX_PPC853X_4_3"
 endif
 
+.PHONY: x265_pre_configure
+x265_pre_configure: $(CMAKE_TOOLCHAIN_PKG)
+	@$(MSG) Configure 12BIT libraries
+	$(RUN) cmake -S $(WORK_DIR)/$(PKG_DIR)/source -B $(CMAKE_BUILD_12B) $(CMAKE_ARGS) $(CMAKE_ARGS_BITLIB) -DMAIN12=ON -G Ninja
+	@$(MSG) Configure 10BIT libraries
+	$(RUN) cmake -S $(WORK_DIR)/$(PKG_DIR)/source -B $(CMAKE_BUILD_10B) $(CMAKE_ARGS) $(CMAKE_ARGS_BITLIB) -G Ninja
 
-.PHONY: $(PKG_DIR)/libx265_main12.a $(PKG_DIR)/libx265_main10.a x265_configure
-
-# First create specific static libraries with build options for 12 and 10 bit depth
-PKG_WORK_DIR = $(WORK_DIR)/$(PKG_NAME)_$(PKG_VERS)
-PKG_BUILD_12BIT = $(PKG_WORK_DIR)/build12bit
-PKG_BUILD_10BIT = $(PKG_WORK_DIR)/build10bit
-
-CMAKE_BITLIB_ARGS += -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF
-CMAKE_BITLIB_ARGS += -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DEXTRA_LINK_FLAGS="-ldl"
-
-$(PKG_DIR)/libx265_main12.a:
-	mkdir --parents $(PKG_BUILD_12BIT)
-	@$(MSG) Configure for 12BIT libraries
-	cd $(PKG_BUILD_12BIT) && cmake $(CMAKE_ARGS) $(CMAKE_BITLIB_ARGS) -DMAIN12=ON ../source
+.PHONY: x265_pre_compile
+x265_pre_compile:
 	@$(MSG) Build 12BIT libraries
-	cd $(PKG_BUILD_12BIT) && $(MAKE) $(COMPILE_MAKE_OPTIONS)
-	@$(RUN) ln -sf $(PKG_BUILD_12BIT)/libx265.a libx265_main12.a
-
-$(PKG_DIR)/libx265_main10.a:
-	@$(MSG) Configure for 10BIT libraries
-	mkdir --parents $(PKG_BUILD_10BIT)
-	cd $(PKG_BUILD_10BIT) && cmake $(CMAKE_ARGS) $(CMAKE_BITLIB_ARGS) ../source
+	@$(RUN) ninja -C $(CMAKE_BUILD_12B)
 	@$(MSG) Build 10BIT libraries
-	cd $(PKG_BUILD_10BIT) && $(MAKE) $(COMPILE_MAKE_OPTIONS)
-	@$(RUN) ln -sf $(PKG_BUILD_10BIT)/libx265.a libx265_main10.a
-
-# Aggregate them all together in default 8 bit depth build
-CMAKE_MULTILIB_ARGS += -DEXTRA_LIB="x265_main10.a;x265_main12.a" -DEXTRA_LINK_FLAGS="-ldl -L."
-CMAKE_MULTILIB_ARGS += -DLINKED_10BIT=ON -DLINKED_12BIT=ON -DENABLE_SHARED=ON
-
-x265_configure: $(PKG_DIR)/libx265_main12.a $(PKG_DIR)/libx265_main10.a
-	@$(RUN) cmake $(CMAKE_ARGS) $(CMAKE_MULTILIB_ARGS) .
+	@$(RUN) ninja -C $(CMAKE_BUILD_10B)
+	@$(RUN) ln -sf $(CMAKE_BUILD_12B)/libx265.a $(CMAKE_BUILD_DIR)/libx265_main12.a
+	@$(RUN) ln -sf $(CMAKE_BUILD_10B)/libx265.a $(CMAKE_BUILD_DIR)/libx265_main10.a

cross/x265/patches/001-fix-dynamicHDR-flags-arm64.patch
@@ -1,7 +1,7 @@
 # set ARM64 flags the same way as in parent CMakeLists.txt
 # 
---- dynamicHDR10/CMakeLists.txt.orig	2021-03-16 12:53:00.000000000 +0000
-+++ dynamicHDR10/CMakeLists.txt	2022-03-26 13:06:56.201819727 +0000
+--- source/dynamicHDR10/CMakeLists.txt.orig	2021-03-16 12:53:00.000000000 +0000
++++ source/dynamicHDR10/CMakeLists.txt	2022-03-26 13:06:56.201819727 +0000
 @@ -43,7 +43,11 @@
          endif()
      endif()

commit e8c1673ef14e5b6c57ff0ac810a38e3b3fcdfa5d

fix make publish
- make publish-arch-* must include make arch-* (broken by #6002)

mk/spksrc.publish.mk
@@ -42,7 +42,7 @@ publish-arch-%:
 ###
 
 publish-build-arch-%: SHELL:=/bin/bash
-publish-build-arch-%:
+publish-build-arch-%: build-arch-%
 	@$(MSG) PUBLISHING package for arch $* to http://synocommunity.com | tee --append build-$*.log
 	@MAKEFLAGS= $(MAKE) ARCH=$(firstword $(subst -, ,$*)) TCVERSION=$(lastword $(subst -, ,$*)) publish 2>&1 | tee --append publish-$*.log >(grep -e '^http' -e '^{"package":' -e '^{"message":' >> status-publish.log) ; \
 	status=$${PIPESTATUS[0]} ; \