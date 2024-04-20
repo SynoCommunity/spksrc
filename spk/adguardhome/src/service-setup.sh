92fbcf5424c293bb4a136972c488d4019d9cd6a4
publicarray <publicarray@users.noreply.github.com>
2024-04-20 17:32:05 +1000

    Patched out the privilege check as it wasn't working even with the correct capabilities.

--- spk/adguardhome/src/service-setup.sh
+++ spk/adguardhome/src/service-setup.sh
@@ -2,5 +2,9 @@ ADGUARDHOME="${SYNOPKG_PKGDEST}/bin/adguardhome"
 CFG_FILE="${SYNOPKG_PKGVAR}/AdGuardHome.yml"
 PID_FILE="${SYNOPKG_PKGVAR}/adguardhome.pid"
 WEB_UI="0.0.0.0:${SYNOPKG_PKGPORT}"
-SERVICE_COMMAND="${ADGUARDHOME} --web-addr ${WEB_UI} -c ${CFG_FILE} --pidfile ${PID_FILE} -l ${LOG_FILE} --work-dir ${SYNOPKG_PKGVAR}"
+SERVICE_COMMAND="${ADGUARDHOME} --web-addr ${WEB_UI} --config ${CFG_FILE} --pidfile ${PID_FILE} --logfile ${LOG_FILE} --work-dir ${SYNOPKG_PKGVAR}"
 SVC_BACKGROUND=y
+
+service_postinst () {
+    mkdir -p "${SYNOPKG_PKGVAR}/data/"
+}
