c3a20a702b509196406f8cf07c157940f06ee60f
publicarray <publicarray@users.noreply.github.com>
2024-04-21 10:29:36 +1000

    revert and use export again

--- spk/kopia/src/service-setup.sh
+++ spk/kopia/src/service-setup.sh
@@ -1,5 +1,7 @@
 export XDG_CACHE_HOME=${SYNOPKG_PKGVAR}/cache
-ENV="USER= KOPIA_LOG_DIR=${SYNOPKG_PKGVAR} KOPIA_CACHE_DIR=${SYNOPKG_PKGVAR} KOPIA_CONFIG_PATH=${SYNOPKG_PKGVAR}/config"
+export KOPIA_LOG_DIR=${SYNOPKG_PKGVAR}
+export KOPIA_CACHE_DIR=${SYNOPKG_PKGVAR}
+export KOPIA_CONFIG_PATH=${SYNOPKG_PKGVAR}/config/config.json
 TLS_CONFIG="--tls-cert-file ${SYNOPKG_PKGVAR}/default.crt --tls-key-file ${SYNOPKG_PKGVAR}/default.key"
 KOPIA=${SYNOPKG_PKGDEST}/bin/kopia
 SERVICE_COMMAND="${KOPIA} server start --ui ${TLS_CONFIG} --address=0.0.0.0:${SERVICE_PORT} --grpc --control-api --enable-actions"
