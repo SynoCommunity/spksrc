fd93d2d4a96bc0f0d5a11e06f4127bb49855c7b5
publicarray <publicarray@users.noreply.github.com>
2024-04-21 10:07:07 +1000

    Fix blank username/password from wizard and Fix TLS cert generation

--- spk/kopia/src/service-setup.sh
+++ spk/kopia/src/service-setup.sh
@@ -1,12 +1,24 @@
 export XDG_CACHE_HOME=${SYNOPKG_PKGVAR}/cache
 ENV="USER= KOPIA_LOG_DIR=${SYNOPKG_PKGVAR} KOPIA_CACHE_DIR=${SYNOPKG_PKGVAR} KOPIA_CONFIG_PATH=${SYNOPKG_PKGVAR}/config"
-TLS_CONFIG="--tls-generate-cert --tls-cert-file ${SYNOPKG_PKGVAR}/default.crt --tls-key-file ${SYNOPKG_PKGVAR}/default.key"
+TLS_CONFIG="--tls-cert-file ${SYNOPKG_PKGVAR}/default.crt --tls-key-file ${SYNOPKG_PKGVAR}/default.key"
 KOPIA=${SYNOPKG_PKGDEST}/bin/kopia
 SERVICE_COMMAND="${KOPIA} server start --ui ${TLS_CONFIG} --address=0.0.0.0:${SERVICE_PORT} --grpc --control-api --enable-actions"
 SVC_BACKGROUND=yes
 SVC_WRITE_PID=yes
 
 service_prestart () {
-    call_func "load_variables_from_file"
+    wizard_password=$(cat "${SYNOPKG_PKGVAR}/pwd")
+    wizard_username=$(cat "${SYNOPKG_PKGVAR}/usr")
+
+    if [ ! -f "${SYNOPKG_PKGVAR}/default.crt" ];then
+        SERVICE_COMMAND+=" --tls-generate-cert"
+    fi
     SERVICE_COMMAND+=" --server-username=${wizard_username} --server-password=${wizard_password}"
+}
+
+service_postinst () {
+    if [ ! -f "${SYNOPKG_PKGVAR}/pwd" ]; then
+        echo "${wizard_password}" > "${SYNOPKG_PKGVAR}/pwd"
+        echo "${wizard_username}" > "${SYNOPKG_PKGVAR}/usr"
+    fi
 }
