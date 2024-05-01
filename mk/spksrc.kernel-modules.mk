8efe65aaf0f14cbf8ec8fff73fcc84216d0aee9f
Vincent Fortier <th0ma7@gmail.com>
2024-05-01 00:27:36 +0000

    kernel.mk: Filter out unsupported kernel archs

--- mk/spksrc.kernel-modules.mk
+++ mk/spksrc.kernel-modules.mk
@@ -5,7 +5,7 @@ kernel-modules-%:
 	@if [ "$(filter $(DEFAULT_TC),lastword $(subst -, ,$(MAKECMDGOALS)))" ]; then \
 	   archs2process="$(filter $(addprefix %-,$(SUPPORTED_KERNEL_VERSIONS)),$(filter $(addsuffix -$(word 1,$(subst ., ,$(word 2,$(subst -, ,$*))))%,$(shell sed -n -e '/TC_ARCH/ s/.*= *//p' ../../toolchain/syno-$*/Makefile)), $(LEGACY_ARCHS)))" ; \
 	elif [ "$(filter $(GENERIC_ARCHS),$(subst -, ,$(MAKECMDGOALS)))" ]; then \
-	   archs2process="$(filter $(addprefix %-,$(lastword $(subst -, ,$(MAKECMDGOALS)))),$(filter $(addsuffix -$(word 1,$(subst ., ,$(word 2,$(subst -, ,$*))))%,$(shell sed -n -e '/TC_ARCH/ s/.*= *//p' ../../toolchain/syno-$*/Makefile)), $(LEGACY_ARCHS)))" ; \
+	   archs2process="$(filter $(addprefix %-,$(lastword $(subst -, ,$(MAKECMDGOALS)))),$(filter $(addsuffix -$(word 1,$(subst ., ,$(word 2,$(subst -, ,$*))))%,$(filter-out $(UNSUPPORTED_ARCHS),$(shell sed -n -e '/TC_ARCH/ s/.*= *//p' ../../toolchain/syno-$*/Makefile))), $(LEGACY_ARCHS)))" ; \
 	else \
 	   archs2process=$* ; \
 	fi ; \
@@ -18,13 +18,13 @@ kernel-modules-%:
 	  echo "REQUIRE_KERNEL_MODULE: [$(REQUIRE_KERNEL_MODULE)]" ; \
 	  echo "KERNEL_ROOT: [$(KERNEL_ROOT)]" ; \
 	  echo "DEPENDS: [$(DEPENDS)]" ; \
-#	  MAKEFLAGS= $(PSTAT_TIME) $(MAKE) WORK_DIR=$(CURDIR)/work-$* ARCH=$$(echo $${arch} | cut -f1 -d-) TCVERSION=$$(echo $${arch} | cut -f2 -d-) strip 2>&1 | tee --append build-$*-kernel-modules.log ; \
-	  echo MAKEFLAGS= $(PSTAT_TIME) $(MAKE) WORK_DIR=$(CURDIR)/work-$* ARCH=$$(echo $${arch} | cut -f1 -d-) TCVERSION=$$(echo $${arch} | cut -f2 -d-) strip 2>&1 | tee --append build-$*-kernel-modules.log ; \
 	  MAKEFLAGS= $(PSTAT_TIME) $(MAKE) WORK_DIR=$(CURDIR)/work-$* ARCH=$$(echo $${arch} | cut -f1 -d-) TCVERSION=$$(echo $${arch} | cut -f2 -d-) strip 2>&1 | tee --append build-$*-kernel-modules.log ; \
 	  [ $${PIPESTATUS[0]} -eq 0 ] || false ; \
 	  $(MAKE) spkclean ; \
-	  rm -fr $(CURDIR)/work-$*/$(addprefix linux-, $${arch}) ; \
-	  $(MAKE) -C ../../toolchain/syno-$${arch} clean ; \
+	  if [ "$$(basename $(abspath $(CURDIR)/$(BASEDIR)))" = "workspace" ]; then \
+	     rm -fr $(CURDIR)/work-$*/$(addprefix linux-, $${arch}) ; \
+	     $(MAKE) -C ../../toolchain/syno-$${arch} clean ; \
+	  fi ; \
 	done
 
 kernel-arch-%:
