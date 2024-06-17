e5b2b445de0f4f403a1eb114e0d24e37dd030c54
hgy59 <hpgy59@gmail.com>
2024-06-17 07:18:21 +0200

    fix output of make dependency-list (#6144)
    * fix output of make dependency-list
    - avoid the make command and the list of all packages in the output
    - this is a leftover of PR #6002
    
    * remove global dependency-flat target
    - dependency-flat is an internal target used dependency-list, dependency-tree and for some internal targets
    
    * znc: fix for dependency-list
    
    * avoid duplicates in dependency-flat
    
    * build-action: fix prepare
    - fix rename of spk names: remove only package with exact name from list
    
    * remove dependency-kernel-list
    - this was introduced in #5095
    - BUT: we must not download kernel sources in prepare.sh, just as we don't do with toolchains
    - if we want to optimize the download, we could cache the (extracted) kernel sources as we do for toolchains
      but since the cache is already at its limit, it is not possible to cache kernel sources without
      dropping other cached data.

--- mk/spksrc.dependency-tree.mk
+++ mk/spksrc.dependency-tree.mk
@@ -41,7 +41,7 @@ dependency-flat:
 	@echo "$(CURDIR)" | grep -Po "/\K(spk|cross|native|diyspk|toolchain)/.*"
 	@for depend in $$(echo "$(BUILD_DEPENDS) $(DEPENDS) $(OPTIONAL_DEPENDS)" | tr ' ' '\n' | sort -u | tr '\n' ' ') ; \
 	do \
-	  DEPENDENCY_WALK=1 $(MAKE) -s -C ../../$$depend dependency-flat ; \
+	  DEPENDENCY_WALK=1 $(MAKE) -s -C ../../$$depend dependency-flat | sort -u ; \
 	done
 
 ### 
