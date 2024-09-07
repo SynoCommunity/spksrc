99cd8f49e83edb91c52870b20407bdc68d02839e
hgy59 <hpgy59@gmail.com>
2024-09-08 01:48:57 +0200

    update install and upgrade wizards
    - add notes to the installation wizard that the "Migration notes" apply only when the shared folder already contains a minio data structure
    - evaluate existing data format in upgrade wizard and show related "Migration notes"
      - reusing an existing installation with the new format will have no upgrade issues
      - reusing an existing installation with the filesystem format will not get any future update
      - when changing the shared folder on upgrade, the data format is unknown and it is unknown whether future updates will work
    - add link to the 'Migrate from Gateway or Filesystem Mode' page

--- spk/minio/src/wizard/upgrade_uifile.sh
+++ spk/minio/src/wizard/upgrade_uifile.sh
@@ -2,22 +2,105 @@
 
 INST_ETC="/var/packages/${SYNOPKG_PKGNAME}/etc"
 INST_VARIABLES="${INST_ETC}/installer-variables"
+MINIO_DATA_FORMAT="unknown"
 
 # Reload wizard variables stored by postinst
 if [ -r "${INST_VARIABLES}" ]; then
-    # we cannot source the file to reload variables, when values have special characters.
-    # This works even with following characers (e.g. for passwords): " ' < \ > :space: = $ | ...
-    while read -r _line; do
-        _key="$(echo ${_line} | cut --fields=1 --delimiter='=')"
-        _value="$(echo ${_line} | cut --fields=2- --delimiter='=')"
-        declare -g "${_key}=${_value}"
-    done < ${INST_VARIABLES}
+   # we cannot source the file to reload variables, when values have special characters.
+   # This works even with following characers (e.g. for passwords): " ' < \ > :space: = $ | ...
+   while read -r _line; do
+      _key="$(echo ${_line} | cut --fields=1 --delimiter='=')"
+      _value="$(echo ${_line} | cut --fields=2- --delimiter='=')"
+      declare -g "${_key}=${_value}"
+   done < ${INST_VARIABLES}
 fi
+
+# Evaluate the current data format
+if [ -d "${SHARE_PATH}" ]; then
+   if [ -d "${SHARE_PATH}/.minio.sys" ]; then
+      if [ -r "${SHARE_PATH}/.minio.sys/format.json" ]; then
+         MINIO_DATA_FORMAT=$(cat "${SHARE_PATH}/.minio.sys/format.json" | jq '.format' | tr -d '"')
+      fi
+   fi
+fi
+
+
+TITLE_STEP_1="MinIO configuration"
+TITLE_STEP_2="MinIO - Migration Notes"
+
+UPDATE_TEXT_INFO="<b>This MinIO installation uses already the new data format.</b><br/> <b style=\\\"color: green\\\">You will be able to update to future minio package versions.</b>"
+UPDATE_TEXT_WARNING1="<b>This MinIO installation uses the FS data format.</b><br/> <b style=\\\"color: red\\\">Future minio versions will not be installable without prior manual data migration.</b>"
+UPDATE_TEXT_WARNING2="<b>You changed the MinIO data directory or the data format could not be evaluated.</b><br/> <b style=\\\"color: orange\\\">Future minio versions will only be installable when the folder does not contain MinIO data in the FS Format.</b>"
+
+quote_json ()
+{
+   sed -e 's|\\|\\\\|g' -e 's|\"|\\\"|g'
+}
+
+deactivateFunction ()
+{
+   DEACTIVATE=$(/bin/cat<<EOF
+{
+   function findStepByTitle(wizardDialog, title) {
+      for (var i = 0; i < wizardDialog.customuiIds.length; i++) {
+         var step = wizardDialog.getStep(wizardDialog.customuiIds[i]);
+         if (title === step.headline) {
+            return step;
+         }
+      }
+      return null;
+   }
+   function getValue(wizardDialog, stepTitle, key)
+   {
+      var step = findStepByTitle(wizardDialog, stepTitle);
+      if (!step) {
+         return null;
+      } else {
+         var component = step.getComponent(key);
+         if (!component) {
+            return null;
+         } else {
+            return component.getValue();
+         }
+      }
+   }
+   function getDataFormat(wizardDialog) {
+      var wizard_data_dir = getValue(wizardDialog,"${TITLE_STEP_1}","wizard_data_directory");
+      if (wizard_data_dir === "${SHARE_NAME}") {
+         return "${MINIO_DATA_FORMAT}";
+      } else {
+         return "unknown";
+      }
+   }
+   var currentStep = arguments[0];
+   var wizardDialog = currentStep.owner;
+   var step2 = findStepByTitle(wizardDialog, "${TITLE_STEP_2}");
+   var dataFormat = getDataFormat(wizardDialog);
+   if (currentStep.headline === "${TITLE_STEP_1}") {
+      var textComponent = step2.getComponent(0);
+      if (textComponent) {
+         if ( dataFormat == "xl" || dataFormat == "xl-single" ) {
+            textComponent.setValue("${UPDATE_TEXT_INFO}");
+         } else if (dataFormat == "fs") {
+            textComponent.setValue("${UPDATE_TEXT_WARNING1}");
+         } else {
+            textComponent.setValue("${UPDATE_TEXT_WARNING2}");
+         }
+      } else {
+         console.log("ERROR:", "textComponent not found");
+      }
+   }
+}
+EOF
+)
+   echo "${DEACTIVATE}" | quote_json
+}
 
 cat <<EOF > $SYNOPKG_TEMP_LOGFILE
 [
    {
-      "step_title": "MinIO configuration",
+      "step_title": "${TITLE_STEP_1}",
+      "deactivate_v2": "$(deactivateFunction)",
       "items": [
          {
             "type": "textfield",
@@ -82,19 +165,16 @@ cat <<EOF > $SYNOPKG_TEMP_LOGFILE
       ]
    },
    {
-      "step_title": "MinIO - end of life for filesystem mode",
+      "step_title": "${TITLE_STEP_2}",
       "items": [
          {
-            "desc": "MinIO removed support for the filesystem mode which is a 1:1 mapping of buckets to files on disk. If your deployment uses filesystem mode you need to manually migrate to single-node single-drive deployment setup. This is the only way to receive future updates of MinIO. A new MinIO package with a different name will be provided in future."
+            "desc": "This text is set before activating this page."
          },
          {
-            "desc": "To check if you're affected by this change check your buckets manually by looking into each folder on your diskstation. If there is exactly a single file per S3 object you need to migrate your data. Otherwise you're already running a single-node single-drive deployment an can upgrade to the new MinIO package in future without a migration."
+            "desc": "MinIO removed support for the filesystem mode which is a 1:1 mapping of buckets to files on disk. If your deployment uses filesystem mode you need to manually migrate to single-node single-drive deployment setup. Future MinIO packages will support the new format only."
          },
          {
-            "desc": "Because of the complexity and risks involved an automatic upgrade process is not available. The official MinIO documentation provides a guide for doing so called 'Migrate from Gateway or Filesystem Mode'"
-         },
-         {
-            "desc": "For a regular user it is suggested to run an instance of MinIO on a separate system and migrate your data to this new instance. This system could be a desktop, single board computer or a server. If possible you can mount the storage from your diskstation over the network and use it directly during the migration process. After a successful migration replace the data of your current MinIO installation on your diskstation. Please stop MinIO before replacing your data and restart it afterwards."
+            "desc": "Because of the complexity and risks involved, an automatic upgrade process is not available. The official MinIO documentation provides a guide for doing so, see <a target=\"_blank\" href=\"https://min.io/docs/minio/linux/operations/install-deploy-manage/migrate-fs-gateway.html\">Migrate from Gateway or Filesystem Mode</a>."
          }
       ]
    }
