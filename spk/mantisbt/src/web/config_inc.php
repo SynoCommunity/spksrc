b54a77cd7c90a5c66d5a0d4bbb0dc096fa3b3394
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2024-09-24 16:34:17 -0400

    Add resource workers and PHP profiles

--- spk/mantisbt/src/web/config_inc.php
+++ spk/mantisbt/src/web/config_inc.php
@@ -1,7 +1,8 @@
 <?php
 	$g_hostname = 'localhost';
-	$g_db_type = 'mysql';
+	$g_db_type = 'mysqli';
 	$g_database_name = 'mantisbt';
 	$g_db_username = 'mantisbt';
 	$g_db_password = '@password@';
+	$g_crypto_master_salt = '@rand_str@';
 ?>
