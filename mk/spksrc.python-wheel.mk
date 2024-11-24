8a052a72b23e0705527663e024df5e12c172a026
Vincent Fortier <th0ma7@gmail.com>
2024-11-24 14:54:28 +0000

    spksrc.python-wheel.mk: Fix wheel building

--- mk/spksrc.python-wheel.mk
+++ mk/spksrc.python-wheel.mk
@@ -19,30 +19,30 @@ endif
 # Resume with standard spksrc.cross-cc.mk
 include ../../mk/spksrc.cross-cc.mk
 
-# Fetch python variables
--include $(WORK_DIR)/crossenv/build/python-cc.mk
+# Define where is located the crossenv
+CROSSENV_MODULE_PATH = $(firstword $(wildcard $(WORK_DIR)/crossenv-$(PKG_NAME)-$(PKG_VERS) $(WORK_DIR)/crossenv-$(PKG_NAME) $(WORK_DIR)/crossenv-default))
 
-# If using spk.python.mk with PYTHON_STAGING_PREFIX defined
+# If using spksrc.python.mk with PYTHON_STAGING_PREFIX defined
 # then redirect STAGING_INSTALL_PREFIX so rust
 # wheels can find openssl and other libraries
 ifneq ($(wildcard $(PYTHON_STAGING_PREFIX)),)
 STAGING_INSTALL_PREFIX := $(PYTHON_STAGING_PREFIX)
 endif
 
-# Python module variables
-ifeq ($(strip $(PYTHONPATH)),)
-PYTHONPATH = $(PYTHON_LIB_NATIVE):$(INSTALL_DIR)$(INSTALL_PREFIX)/$(PYTHON_LIB_DIR)/site-packages/
-endif
-
 ## python wheel specific configurations
 include ../../mk/spksrc.wheel-env.mk
 
+### Prepare crossenv
+build_crossenv_module:
+	@$(MSG) WHEEL="$(PKG_NAME)-$(PKG_VERS)" $(MAKE) crossenv-$(ARCH)-$(TCVERSION)
+	@WHEEL="$(PKG_NAME)-$(PKG_VERS)" $(MAKE) crossenv-$(ARCH)-$(TCVERSION)
+
 ### Python wheel rules
-build_python_wheel_target:
-	@$(MSG) "PYTHON WHEEL: activate crossenv found: $(CROSSENV)"
-	@$(MSG) "CROSSENV_PATH = $(CROSSENV_PATH)"
-	@. $(CROSSENV_PATH)/bin/activate ; \
-	$(RUN) _PYTHON_HOST_PLATFORM=$(TC_TARGET) python3 -m build $(BUILD_ARGS) --wheel $(WHEELS_BUILD_ARGS) --outdir $(WHEELHOUSE)
+build_python_wheel_target: build_crossenv_module
+	$(foreach e,$(shell cat $(CROSSENV_MODULE_PATH)/build/python-cc.mk),$(eval $(e)))
+	@. $(CROSSENV) ; \
+	$(MSG) _PYTHON_HOST_PLATFORM=$(TC_TARGET) cross-python3 -m build $(BUILD_ARGS) --wheel $(WHEELS_BUILD_ARGS) --outdir $(WHEELHOUSE) ; \
+	$(RUN) _PYTHON_HOST_PLATFORM=$(TC_TARGET) cross-python3 -m build $(BUILD_ARGS) --wheel $(WHEELS_BUILD_ARGS) --outdir $(WHEELHOUSE)
 	@$(RUN) echo "$(PKG_NAME)==$(PKG_VERS)" >> $(WHEELHOUSE)/$(WHEELS_CROSS_COMPILE)
 
 post_install_python_wheel_target: $(WHEEL_TARGET) install_python_wheel
