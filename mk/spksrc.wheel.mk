4e246e95aaf992b1a1330c8083a0636bd80a289b
Vincent Fortier <th0ma7@gmail.com>
2025-01-06 23:10:36 +0000

    wheel.mk: Remove unused PATH and LD_LIBRARY_PATH to cross building

--- mk/spksrc.wheel.mk
+++ mk/spksrc.wheel.mk
@@ -222,9 +222,6 @@ else
 	@$(MSG) "[SKIP] Building pure-python"
 endif
 
-# PATH modified to access native/python* linked to crossenv-<wheel>/build/bin
-#      this also provides access to maturin and its dependencies needed for building wheels
-# LD_LIBRARY_PATH modified to access native/python libraries so related binary works
 cross-compile-wheel-%: SHELL:=/bin/bash
 cross-compile-wheel-%:
 	@if [ "$(PIP_GLOBAL_OPTION)" ]; then \
@@ -233,8 +230,6 @@ cross-compile-wheel-%:
 	fi ; \
 	$(MSG) \
 	   _PYTHON_HOST_PLATFORM="$(TC_TARGET)" \
-	   PATH="$(CROSSENV_PATH)/build/bin:$(PATH)" \
-	   LD_LIBRARY_PATH="$(abspath $(WORK_DIR)/../../../native/$(PYTHON_PKG_NAME)/work-native/install/usr/local/lib):$(LD_LIBRARY_PATH)" \
 	   MESON_CROSS_FILE="$(MESON_TOOLCHAIN_WRK)" \
 	   $(PIP_CROSSENV) \
 	   $(PIP_WHEEL_ARGS_CROSSENV) \
@@ -244,8 +239,6 @@ cross-compile-wheel-%:
 	   $(REQUIREMENT) ; \
 	$(RUN) \
 	   _PYTHON_HOST_PLATFORM="$(TC_TARGET)" \
-	   PATH="$(CROSSENV_PATH)/build/bin:$(PATH)" \
-	   LD_LIBRARY_PATH="$(abspath $(WORK_DIR)/../../../native/$(PYTHON_PKG_NAME)/work-native/install/usr/local/lib):$(LD_LIBRARY_PATH)" \
 	   MESON_CROSS_FILE="$(MESON_TOOLCHAIN_WRK)" \
 	   $(PIP_CROSSENV) \
 	   $(PIP_WHEEL_ARGS_CROSSENV) \
