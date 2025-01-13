813d86f1b61cfd3e5fad7e2c70b9479257e75f99
hgy59 <hpgy59@gmail.com>
2025-01-13 01:18:20 +0100

    add SRM 1.3 toolchains (#6318)
    * add SRM 1.3 toolchains
    - add toolchains extracted from toolkit from Synology downloads
    - hawkeye is an early release and seems incorrect
    
    * add SRM 1.3 to new build matrix
    
    * build.yml: limit manual build to 10 input fields
    
    * fix Makefile of armv7-1.3 toolchain
    
    * fix check for required SRM versions
    
    * add remarks for hawkeye-1.3 toolchain
    
    * kernel version of cypress-1.3 verified

--- mk/spksrc.archs.mk
+++ mk/spksrc.archs.mk
@@ -16,7 +16,11 @@ endif
 
 # Distinct SRM and DSM archs to allow handling of different TCVERSION ranges.
 # SRM - Synology Router Manager
-SRM_ARMv7_ARCHS = northstarplus ipq806x dakota
+SRM_ARMv7_ARCHS = northstarplus ipq806x dakota hawkeye
+SRM_ARMv8_ARCHS = cypress
+# required in spksrc.pre-check.mk
+SRM_ARCHS = $(SRM_ARMv7_ARCHS) $(SRM_ARMv8_ARCHS)
+
 # DSM - all ARMv7 except SRM specific archs
 DSM_ARMv7_ARCHS = alpine alpine4k armada370 armada375 armada38x armadaxp monaco
 # comcerto2k is the only ARMv7 arch that uses an GCC (4.9.3) and GLIBC (2.20)
@@ -32,7 +36,7 @@ ARMv5_ARCHS = 88f6281
 ARMv7_ARCHS = $(GENERIC_ARMv7_ARCH) $(DSM_ARMv7_ARCHS) $(SRM_ARMv7_ARCHS)
 # hi3535 is not supported by generic ARMv7 arch
 ARMv7L_ARCHS = hi3535
-ARMv8_ARCHS = $(GENERIC_ARMv8_ARCH) rtd1296 rtd1619b armada37xx
+ARMv8_ARCHS = $(GENERIC_ARMv8_ARCH) $(SRM_ARMv8_ARCHS) rtd1296 rtd1619b armada37xx
 ARM_ARCHS = $(ARMv5_ARCHS) $(ARMv7_ARCHS) $(ARMv7L_ARCHS) $(ARMv8_ARCHS)
 
 PPC_ARCHS = powerpc ppc824x ppc853x ppc854x qoriq
