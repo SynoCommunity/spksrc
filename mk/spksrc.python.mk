70c83e4ebd10b2f1b22d9f9fcfbbd8d414a50026
Vincent Fortier <th0ma7@gmail.com>
2025-01-25 20:24:30 +0000

    python.mk: Do not create symlinks for bzip2 against python dir

--- mk/spksrc.python.mk
+++ mk/spksrc.python.mk
@@ -42,12 +42,12 @@ export ADDITIONAL_LDFLAGS  += -L$(OPENSSL_STAGING_PREFIX)/lib
 export ADDITIONAL_LDFLAGS  += -Wl,--rpath-link,$(OPENSSL_STAGING_PREFIX)/lib -Wl,--rpath,$(OPENSSL_PREFIX)/lib
 endif
 
-# Re-use all default python mandatory libraries (with exception of xz, zlib)
-PYTHON_LIBS_EXCLUDE = %lzma.pc %zlib.pc
+# Re-use all default python mandatory libraries (with exception of bzip2, xz, zlib)
+PYTHON_LIBS_EXCLUDE = %bzip2.pc %lzma.pc %zlib.pc
 PYTHON_LIBS := $(filter-out $(PYTHON_LIBS_EXCLUDE),$(wildcard $(PYTHON_STAGING_INSTALL_PREFIX)/lib/pkgconfig/*.pc))
 
-# Re-use all python dependencies and mark as already done (with exceltion of xz, zlib)
-PYTHON_DEPENDS_EXCLUDE = xz zlib
+# Re-use all python dependencies and mark as already done (with exceltion of bzip2, xz, zlib)
+PYTHON_DEPENDS_EXCLUDE = bzip2 xz zlib
 PYTHON_DEPENDS := $(foreach cross,$(filter-out $(PYTHON_DEPENDS_EXCLUDE),$(foreach pkg_name,$(shell $(MAKE) dependency-list -C $(realpath $(PYTHON_PACKAGE_WORK_DIR)/../) 2>/dev/null | grep ^$(PYTHON_PACKAGE) | cut -f2 -d:),$(shell sed -n 's/^PKG_NAME = \(.*\)/\1/p' $(realpath $(CURDIR)/../../$(pkg_name)/Makefile)))),$(wildcard $(PYTHON_PACKAGE_WORK_DIR)/.$(cross)-*_done))
 
 # call-up pre-depend to prepare the shared python build environment
