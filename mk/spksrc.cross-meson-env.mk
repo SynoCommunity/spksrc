32d1d597aa4ae50c327644ee6f0e5f9b4c2270f7
Vincent Fortier <th0ma7@gmail.com>
2025-03-18 23:55:31 +0000

    cross-meson-env.mk: Testing completed and added details on issue

--- mk/spksrc.cross-meson-env.mk
+++ mk/spksrc.cross-meson-env.mk
@@ -29,14 +29,31 @@ CONFIGURE_ARGS += --cross-file=$(MESON_CROSS_TOOLCHAIN_PKG)
 
 # Enforce unsetting all flags as using cross-file with the
 # exception of PKG_CONFIG_LIBDIR to force default pkgconfig.
+#
 # Also due to meson bug, keep LDFLAGS for rpath management.
+# This only occurs when there are multiple rpath definitions.
 # Ref: https://github.com/mesonbuild/meson/issues/14354
 #      https://github.com/mesonbuild/meson/issues/6541
+#
+# Note0: The only way to get rpath defined in resulting *.so
+#        is from using LDFLAGS variable part of environment.
+# Note1: Adding options --enable-new-dtags or --strip-all to
+#        *_link_args has no effect on resulting rpath and runpath.
+# Note2: Defining build_rpath and install_rpath has no effect.
+#        It should normally affect the runpath and not the rpath.
+# Note3: Similarly, re-defining -Wl,--rpath-link and -Wl,--rpath
+#        part of *_link_args has no effect neither as LDFLAGS
+#        needs to be set anyway and already has rpath defined.
+# Note4: Defining runpath (instead of rpath) using install_rpath
+#        or -Wl,--enable-new-dtags has no effect neither.
+#
 ENV_MESON  = -u AR -u AS -u CC -u CPP -u CXX -u LD -u LDSHARED
 ENV_MESON += -u OBJCOPY -u OBJDUMP -u RANLIB -u READELF -u STRIP
-ENV_MESON += -u CFLAGS -u CPPFLAGS -u CXXFLAGS -u FFLAGS
-#ENV_MESON += -u CFLAGS -u CPPFLAGS -u CXXFLAGS -u FFLAGS -u LDFLAGS
-ENV_MESON += -u ADDITIONAL_CFLAGS -u ADDITIONAL_CPPFLAGS -u ADDITIONAL_CXXFLAGS -u ADDITIONAL_FFLAGS -u ADDITIONAL_LDFLAGS
+ENV_MESON += -u CFLAGS -u ADDITIONAL_CFLAGS
+ENV_MESON += -u CPPFLAGS -u ADDITIONAL_CPPFLAGS
+ENV_MESON += -u CXXFLAGS -u ADDITIONAL_CXXFLAGS
+ENV_MESON += -u FFLAGS -u ADDITIONAL_FFLAGS
+#ENV_MESON += -u LDFLAGS -u ADDITIONAL_LDFLAGS
 ENV_MESON += -u PKG_CONFIG_PATH -u SYSROOT
 
 ifeq ($(findstring $(ARCH),$(ARMv5_ARCHS)),$(ARCH))
@@ -111,10 +128,11 @@ endif
 	echo "prefix = '$(INSTALL_PREFIX)'"
 	@echo
 	@echo "[properties]" ; \
-	echo "# pkg_config_path not needed due to PKG_CONFIG_LIBDIR" ; \
+	echo "# Not needed due to PKG_CONFIG_LIBDIR" ; \
 	echo "#pkg_config_path = '$(abspath $(PKG_CONFIG_LIBDIR))'" ; \
-	echo "build_rpath = '"$$(echo $(LDFLAGS) | grep -oP '(?<=-Wl,--rpath-link,)[^ ]+' | tr '\n' ':' | sed 's/:$$//')"'" ; \
-	echo "install_rpath = '"$$(echo $(LDFLAGS) | grep -oP '(?<=-Wl,--rpath,)[^ ]+' | tr '\n' ':' | sed 's/:$$//')"'"
+	echo "# Not needed due to rpath and rpath-link defined in LDFLAGS" ; \
+	echo "#build_rpath = '"$$(echo $(LDFLAGS) | grep -oP '(?<=-Wl,--rpath-link,)[^ ]+' | tr '\n' ':' | sed 's/:$$//')"'" ; \
+	echo "#install_rpath = '"$$(echo $(LDFLAGS) | grep -oP '(?<=-Wl,--rpath,)[^ ]+' | tr '\n' ':' | sed 's/:$$//')"'"
 	@echo
 	@echo "[built-in]" ; \
 	echo "c_args = ["
@@ -129,7 +147,6 @@ ifneq ($(strip $(MESON_BUILTIN_C_LINK_ARGS)),)
 	@echo -ne "\t'$(MESON_BUILTIN_C_LINK_ARGS)',\n"
 endif
 	@echo $(LDFLAGS) | tr ' ' '\n' | grep -v rpath | sed -e "s/^/\t'/" -e "s/$$/',/" ; \
-	echo -ne "\t'-Wl,--strip-all'\n" ; \
 	echo -ne "\t]\n"
 	@echo
 	@echo "cpp_args = ["
@@ -144,7 +161,6 @@ ifneq ($(strip $(MESON_BUILTIN_CPP_LINK_ARGS)),)
 	@echo -ne "\t'$(MESON_BUILTIN_CPP_LINK_ARGS)',\n"
 endif
 	@echo $(LDFLAGS) | tr ' ' '\n' | grep -v rpath | sed -e "s/^/\t'/" -e "s/$$/',/" ; \
-	echo -ne "\t'-Wl,--strip-all'\n" ; \
 	echo -ne "\t]\n"
 	@echo
 	@echo "cxx_args = ["
@@ -163,5 +179,4 @@ ifneq ($(strip $(MESON_BUILTIN_FC_LINK_ARGS)),)
 	@echo -ne "\t'$(MESON_BUILTIN_FC_LINK_ARGS)',\n"
 endif
 	@echo $(LDFLAGS) | tr ' ' '\n' | grep -v rpath | sed -e "s/^/\t'/" -e "s/$$/',/" ; \
-	echo -ne "\t'-Wl,--strip-all'\n" ; \
 	echo -ne "\t]\n"
