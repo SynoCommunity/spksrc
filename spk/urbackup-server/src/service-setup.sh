36b128627a8fcee320d075cf66a58487bc5ee428
hgy59 <hpgy59@gmail.com>
2025-05-15 23:10:12 +0200

    urbackup: create dedicated packages for server and client
    - urbackup-client update to v2.5.25
    - urbackup-server update to v2.5.33 and redesign shared folder configuration

--- spk/urbackup-server/src/service-setup.sh
+++ spk/urbackup-server/src/service-setup.sh
@@ -2,7 +2,6 @@
 PATH="${SYNOPKG_PKGDEST}/bin:${PATH}"
 
 SERVER=${SYNOPKG_PKGDEST}/bin/urbackupsrv
-
 SERVICE_COMMAND="${SERVER} run --daemon --user ${EFF_USER} --http-port ${SERVICE_PORT} --pidfile ${PID_FILE} --loglevel info --logfile ${LOG_FILE}"
 
 service_postinst ()
