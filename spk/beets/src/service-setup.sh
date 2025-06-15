d143bf40c97822cb9a448e733f6e13c870e5e6ee
hgy59 <hpgy59@gmail.com>
2025-06-15 15:18:48 +0200

    beets: remove python/pycairo dependency (again)
    - install cross wheels as prebuilt whl files (to install pygobject without pycairo dependency)
    - install pure python wheels with requirements-pure.txt

--- spk/beets/src/service-setup.sh
+++ spk/beets/src/service-setup.sh
@@ -10,8 +10,14 @@ service_postinst ()
     install_python_virtualenv
 
     echo ${separator}
-    install_python_wheels
+    echo "Install packages from wheelhouse"
+    pip install --disable-pip-version-check --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
 
-    # Log installation information
-    echo "Installed version: $(${SYNOPKG_PKGDEST}/env/bin/beet version 2>&1)"
+    echo ${separator}
+    echo "Install pure python packages from index"
+    pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${SYNOPKG_PKGVAR}/pip-cache --requirement ${SYNOPKG_PKGDEST}/share/wheelhouse/requirements-pure.txt
+
+    echo ${separator}
+    echo "Installed version:"
+    ${SYNOPKG_PKGDEST}/env/bin/beet version
 }
