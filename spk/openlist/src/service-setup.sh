c3eb2674e2d73af7f909a9b1010ba1371efae20b
hgy59 <hpgy59@gmail.com>
2025-06-28 10:37:38 +0200

    fix openlist config and define admin password
    - fix config.json to use package var folder
    - use different log files for openlist server and openlist package service
    - define SynoCommunity specific version properties
    - adjust package maintainer
    - use default admin password (until wizard is implemented)

--- spk/openlist/src/service-setup.sh
+++ spk/openlist/src/service-setup.sh
@@ -1,5 +1,16 @@
-CONFIG_FILE="${SYNOPKG_PKGVAR}/config.json"
+OPENLIST="${SYNOPKG_PKGDEST}/bin/openlist --data ${SYNOPKG_PKGVAR}"
 
-SERVICE_COMMAND="${SYNOPKG_PKGDEST}/bin/openlist server --data ${SYNOPKG_PKGVAR}"
+SERVICE_COMMAND="${OPENLIST} server"
 SVC_BACKGROUND=yes
 SVC_WRITE_PID=yes
+
+# TODO: get admin password from installation wizard
+wizard_admin_password=admin
+
+service_postinst ()
+{
+   if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
+      echo "Set OpenList amdin password"
+      ${OPENLIST} password set ${wizard_admin_password}
+   fi
+}
