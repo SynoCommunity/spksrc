ebfd1bd4d90244ae3ec64bb597e1b08ab8339939
hgy59 <hpgy59@gmail.com>
2025-06-28 11:31:20 +0200

    update openlist and add wizard
    - update openlist to v4.0.4
    - add installation wizard from template

--- spk/openlist/src/service-setup.sh
+++ spk/openlist/src/service-setup.sh
@@ -4,9 +4,6 @@ SERVICE_COMMAND="${OPENLIST} server"
 SVC_BACKGROUND=yes
 SVC_WRITE_PID=yes
 
-# TODO: get admin password from installation wizard
-wizard_admin_password=admin
-
 service_postinst ()
 {
    if [ "${SYNOPKG_PKG_STATUS}" == "INSTALL" ]; then
