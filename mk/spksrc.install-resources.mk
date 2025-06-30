fdf138995c12f4393f9f173fff3589c5bc37cb9d
Vincent Fortier <th0ma7@gmail.com>
2025-06-30 13:28:41 +0000

    Fix WORK_DIR for toolchain configuration file location
    Since a while when invoking a build it hapens that all dependencies
    ends-up also with a work-<arch>-<version>/tc_vars.cmake|meson*|mk
    Expected behavior is for the location to be unique under the current
    directory from where the build was invoked.

--- mk/spksrc.install-resources.mk
+++ mk/spksrc.install-resources.mk
@@ -7,7 +7,6 @@
 
 # Common makefiles
 include ../../mk/spksrc.common.mk
-include ../../mk/spksrc.directories.mk
 
 # Configure the included makefiles
 URLS          = $(PKG_DIST_SITE)/$(PKG_DIST_NAME)
@@ -28,6 +27,8 @@ TC = syno$(ARCH_SUFFIX)
 endif
 endif
 
+# Common directories (must be set after ARCH_SUFFIX)
+include ../../mk/spksrc.directories.mk
 
 #####
 
