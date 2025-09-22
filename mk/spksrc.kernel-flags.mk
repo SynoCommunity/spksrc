ece55ca9bfbfc0d76579d26d4b4c00bca704c6f0
hgy59 <hpgy59@gmail.com>
2025-09-22 16:24:02 +0200

    simplify SRM kernel downloads

--- mk/spksrc.kernel-flags.mk
+++ mk/spksrc.kernel-flags.mk
@@ -123,17 +123,8 @@ endif
 #
 else ifeq ($(strip $(TC_TYPE)),SRM)
 
-#
-# For SRM version >= 1.3
-#
-ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 9346),1)
-
-ifeq ($(KERNEL_URL_VERSION),)
-KERNEL_URL_VERSION = $(KERNEL_VERS)
-endif
-
 ifeq ($(strip $(KERNEL_DIST_SITE)),)
-KERNEL_DIST_SITE = https://github.com/SynoCommunity/spksrc/releases/download/kernels/srm1.3
+KERNEL_DIST_SITE = https://github.com/SynoCommunity/spksrc/releases/download/kernels/srm$(KERNEL_VERS)
 endif
 
 ifeq ($(strip $(KERNEL_DIST_NAME)),)
@@ -152,31 +143,4 @@ ifeq ($(strip $(KERNEL_STRIP)),)
 KERNEL_STRIP = 0
 endif
 
-
-#
-# For SRM version >= 1.2
-#
-else ifeq ($(shell expr "$(KERNEL_BUILD)" \>= 7742),1)
-
-ifeq ($(strip $(KERNEL_DIST_SITE)),)
-KERNEL_DIST_SITE = https://github.com/SynoCommunity/spksrc/releases/download/kernels/srm1.2
 endif
-
-ifeq ($(strip $(KERNEL_DIST_NAME)),)
-KERNEL_DIST_NAME = $(KERNEL_ARCH)-$(KERNEL_DIST).$(KERNEL_EXT)
-endif
-
-ifeq ($(strip $(KERNEL_URL_DIR)),)
-KERNEL_URL_DIR = $(KERNEL_ARCH)
-endif
-
-ifeq ($(strip $(KERNEL_PREFIX)),)
-KERNEL_PREFIX = $(KERNEL_DIST)
-endif
-
-ifeq ($(strip $(KERNEL_STRIP)),)
-KERNEL_STRIP = 0
-endif
-endif
-
-endif  # For SRM
