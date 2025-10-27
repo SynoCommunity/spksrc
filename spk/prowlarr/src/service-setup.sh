d5106282c4c5c304d9e3b76050c6f09c93320416
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2025-10-26 20:11:57 -0400

    Update distribution files for DSM < 7.2

--- spk/prowlarr/src/service-setup.sh
+++ spk/prowlarr/src/service-setup.sh
@@ -9,7 +9,7 @@ PROWLARR_CONFIG_DIR="${CONFIG_DIR}/Prowlarr"
 PID_FILE="${PROWLARR_CONFIG_DIR}/prowlarr.pid"
 CMD_ARGS="-nobrowser -data=${PROWLARR_CONFIG_DIR}"
 
-if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
     GROUP="sc-download"
     SERVICE_COMMAND="env HOME=${HOME_DIR} LD_LIBRARY_PATH=${SYNOPKG_PKGDEST}/lib ${PROWLARR} ${CMD_ARGS}"
 else
@@ -21,35 +21,43 @@ SVC_WAIT_TIMEOUT=90
 
 service_postinst ()
 {
-    echo "Set update required"
-    # Make Prowlarr do an update check on start
-    touch ${PROWLARR_CONFIG_DIR}/update_required 2>&1
+    if [ "${SYNOPKG_PKG_STATUS}" = "INSTALL" ]; then
+        echo "Set update required"
+        # Make Prowlarr do an update check on start
+        touch "${PROWLARR_CONFIG_DIR}/update_required" 2>&1
 
-    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
-        set_unix_permissions "${CONFIG_DIR}"
+        if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
+            set_unix_permissions "${CONFIG_DIR}"
+        fi
     fi
 }
 
 service_preupgrade ()
 {
-    ## never update Prowlarr distribution, use internal updater only
-    [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup ] && rm -rf ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup
-    echo "Backup existing distribution to ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
-    mkdir -p ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup 2>&1
-    rsync -aX ${SYNOPKG_PKGDEST}/share ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/ 2>&1
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ] || \
+        { [ "${SYNOPKG_DSM_VERSION_MAJOR}" -eq 7 ] && [ "${SYNOPKG_DSM_VERSION_MINOR}" -lt 2 ]; }; then
+        ## earlier DSM < 7.2 with auto-update disabled, update Prowlarr distribution
+        return 0
+    else
+        ## DSM >= 7.2; don't update Prowlarr distribution, use internal updater only
+        [ -d "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup" ] && rm -rf "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+        echo "Backup existing distribution to ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
+        mkdir -p "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup" 2>&1
+        rsync -aX "${SYNOPKG_PKGDEST}/share" "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/" 2>&1
+    fi
 }
 
 service_postupgrade ()
 {
     ## restore Prowlarr distribution
-    if [ -d ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share ]; then
+    if [ -d "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share" ]; then
         echo "Restore previous distribution from ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup"
-        rm -rf ${SYNOPKG_PKGDEST}/share/Prowlarr/bin 2>&1
+        rm -rf "${SYNOPKG_PKGDEST}/share/Prowlarr/bin" 2>&1
         # prevent overwrite of updated package_info
-        rsync -aX --exclude=package_info ${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/ ${SYNOPKG_PKGDEST}/share 2>&1
+        rsync -aX --exclude=package_info "${SYNOPKG_TEMP_UPGRADE_FOLDER}/backup/share/" "${SYNOPKG_PKGDEST}/share" 2>&1
     fi
 
-    if [ ${SYNOPKG_DSM_VERSION_MAJOR} -lt 7 ]; then
+    if [ "${SYNOPKG_DSM_VERSION_MAJOR}" -lt 7 ]; then
         set_unix_permissions "${SYNOPKG_PKGDEST}/share"
     fi
 }
