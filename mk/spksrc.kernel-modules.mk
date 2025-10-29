33d6ba623b9be2296b96c8facd5b9888915825fe
Vincent Fortier <th0ma7@gmail.com>
2025-10-29 22:42:51 +0000

    kernel-modules.mk: Remove uneeded stat call and merge logs to arch

--- mk/spksrc.kernel-modules.mk
+++ mk/spksrc.kernel-modules.mk
@@ -16,13 +16,13 @@ kernel-modules:
 	      $(MAKE) spkclean ; \
 	      $(MSG) "Building kernel-modules for $${depend} ARCH" ; \
 	      $(MSG) $$(printf "%s MAKELEVEL: %02d, PARALLEL_MAKE: %s, ARCH: %s, NAME: %s\n" "$$(date +%Y%m%d-%H%M%S)" $(MAKELEVEL) "$(PARALLEL_MAKE)" "$(ARCH)-$(TCVERSION)" "kernel-modules-$${depend}") | tee --append $(STATUS_LOG) ; \
-	      $(PSTAT_TIME) $(MAKE) WORK_DIR=$(CURDIR)/work$(ARCH_SUFFIX) \
+	      $(MAKE) WORK_DIR=$(CURDIR)/work$(ARCH_SUFFIX) \
 	          REQUIRE_KERNEL_MODULE="$(REQUIRE_KERNEL_MODULE)" \
 	          ARCH=$$(echo $${depend} | cut -f1 -d-) \
 	          TCVERSION=$$(echo $${depend} | cut -f2 -d-) \
 	          -C ../../kernel/syno-$$depend ; \
 	      rm -fr $(CURDIR)/work$(ARCH_SUFFIX)/linux-$${depend} ; \
-	    } > >(tee --append build$(ARCH_SUFFIX)-kernel-modules-$${depend}.log) 2>&1 ; \
+	    } > >(tee --append build-$${depend}.log) 2>&1 ; \
 	    done ; \
 	    rsync -ah work$(ARCH_SUFFIX)/tc_vars-backup/tc_vars* work$(ARCH_SUFFIX)/. ; \
 	} > >(tee --append $(DEFAULT_LOG)) 2>&1 \
