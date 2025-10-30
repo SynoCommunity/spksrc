1130c05e3413d2bba882046b2803076cd60a58ee
Vincent Fortier <th0ma7@gmail.com>
2025-10-30 08:57:59 +0000

    kernel-modules.mk: Set LOGGING_ENABLED to avoid double logging TC

--- mk/spksrc.kernel-modules.mk
+++ mk/spksrc.kernel-modules.mk
@@ -16,7 +16,8 @@ kernel-modules:
 	      $(MAKE) spkclean ; \
 	      $(MSG) "Building kernel-modules for $${depend} ARCH" ; \
 	      $(MSG) $$(printf "%s MAKELEVEL: %02d, PARALLEL_MAKE: %s, ARCH: %s, NAME: %s\n" "$$(date +%Y%m%d-%H%M%S)" $(MAKELEVEL) "$(PARALLEL_MAKE)" "$(ARCH)-$(TCVERSION)" "kernel-modules-$${depend}") | tee --append $(STATUS_LOG) ; \
-	      $(MAKE) WORK_DIR=$(CURDIR)/work$(ARCH_SUFFIX) \
+	      $(MAKE) LOGGING_ENABLED=1 \
+	          WORK_DIR=$(CURDIR)/work$(ARCH_SUFFIX) \
 	          REQUIRE_KERNEL_MODULE="$(REQUIRE_KERNEL_MODULE)" \
 	          ARCH=$$(echo $${depend} | cut -f1 -d-) \
 	          TCVERSION=$$(echo $${depend} | cut -f2 -d-) \
