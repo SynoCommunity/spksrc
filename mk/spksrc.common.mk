39fb15b2dba5aeb178d9ff4b008017f844857e6f
Vincent Fortier <th0ma7@gmail.com>
2025-11-01 14:19:04 +0000

    common.mk: Move rust filtering to top-level $(AVAILABLE_TOOLCHAINS)

--- mk/spksrc.common.mk
+++ mk/spksrc.common.mk
@@ -67,7 +67,7 @@ PIP_DOWNLOAD_ARGS = download --no-index --find-links $(PIP_DISTRIB_DIR) --disabl
 LANGUAGES = chs cht csy dan enu fre ger hun ita jpn krn nld nor plk ptb ptg rus spn sve trk
 
 # Available toolchains formatted as '{ARCH}-{TC}'
-AVAILABLE_TOOLCHAINS = $(subst syno-,,$(sort $(notdir $(wildcard $(BASEDIR)toolchain/syno-*))))
+AVAILABLE_TOOLCHAINS = $(subst syno-,,$(filter-out %-rust,$(sort $(notdir $(wildcard $(BASEDIR)toolchain/syno-*)))))
 AVAILABLE_TCVERSIONS = $(sort $(foreach arch,$(AVAILABLE_TOOLCHAINS),$(shell echo ${arch} | cut -f2 -d'-')))
 
 # Available toolchains formatted as '{ARCH}-{TC}'
@@ -93,10 +93,10 @@ ARCHS_DUPES_DEPRECATED += $(addsuffix %,$(DEPRECATED_ARCHS))
 ARCHS_DUPES = $(ARCHS_WITH_GENERIC_SUPPORT) $(ARCHS_DUPES_DEPRECATED) $(TCVERSION_DUPES)
 
 # supported: used for all-supported target
-SUPPORTED_ARCHS = $(sort $(filter-out %-rust $(ARCHS_DUPES), $(AVAILABLE_TOOLCHAINS)))
+SUPPORTED_ARCHS = $(sort $(filter-out $(ARCHS_DUPES), $(AVAILABLE_TOOLCHAINS)))
 
 # default: used for all-latest target
-LATEST_ARCHS = $(foreach arch,$(sort $(basename $(subst -,.,$(basename $(subst .,,$(SUPPORTED_ARCHS)))))),$(arch)-$(notdir $(subst -,/,$(sort $(filter %$(lastword $(notdir $(subst -,/,$(sort $(filter $(arch)%, $(filter-out %-rust,$(AVAILABLE_TOOLCHAINS))))))),$(sort $(filter $(arch)%,$(AVAILABLE_TOOLCHAINS))))))))
+LATEST_ARCHS = $(foreach arch,$(sort $(basename $(subst -,.,$(basename $(subst .,,$(SUPPORTED_ARCHS)))))),$(arch)-$(notdir $(subst -,/,$(sort $(filter %$(lastword $(notdir $(subst -,/,$(sort $(filter $(arch)%, $(AVAILABLE_TOOLCHAINS)))))),$(sort $(filter $(arch)%, $(AVAILABLE_TOOLCHAINS))))))))
 
 # legacy: used for all-legacy and when kernel support is used
 #         all archs except generic archs
