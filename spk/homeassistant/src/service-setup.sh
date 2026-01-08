eec969218cb6aa0a3a7b3e7ea8fff6a0675f74af
hgy59 <hpgy59@gmail.com>
2026-01-08 21:09:36 +0100

    homeassistant: update to v2025.11.3
    - update homeassistant to the last officially supported version of Home Assistant Core
    - update to Python 3.13
    - include only a minimal set of cross compiled wheels
    - this release is restricted to x64 and aarch64 and requires DSM 7.2
    - download and install HACS at installation time
    - patch several installed wheels to fix dependencies and to find native libraries

--- spk/homeassistant/src/service-setup.sh
+++ spk/homeassistant/src/service-setup.sh
@@ -1,6 +1,7 @@
 
-PYTHON_DIR="/var/packages/python312/target/bin"
+PYTHON_DIR="/var/packages/python313/target/bin"
 PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
+HACS_VERS=2.0.5
 
 CONFIG_DIR="${SYNOPKG_PKGVAR}/config"
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/env/bin/hass -v --config ${CONFIG_DIR} --log-file ${LOG_FILE}"
@@ -52,28 +53,57 @@ service_postinst ()
     install_python_virtualenv
     
     echo ${separator}
-    echo "Install HACS into: ${CONFIG_DIR}/custom_components/hacs"
-    mkdir -p "${CONFIG_DIR}/custom_components/hacs"
-    tar -xzf ${SYNOPKG_PKGDEST}/share/hacs.tar.gz -C ${CONFIG_DIR}/custom_components/hacs
+    echo "Download HACS ${HACS_VERS} and install to: ${CONFIG_DIR}/custom_components/hacs"
+    wget -nv -O ${SYNOPKG_PKGVAR}/hacs.zip https://github.com/hacs/integration/releases/download/${HACS_VERS}/hacs.zip
+    mkdir -p "${CONFIG_DIR}/custom_components"
+    ${SYNOPKG_PKGDEST}/bin/unzip -qo ${SYNOPKG_PKGVAR}/hacs.zip -d ${CONFIG_DIR}/custom_components/hacs
+    rm -f ${SYNOPKG_PKGVAR}/hacs.zip
 
     echo ${separator}
     echo "Install packages from wheelhouse"
     pip install --disable-pip-version-check --no-deps --no-input --no-index ${SYNOPKG_PKGDEST}/share/wheelhouse/*.whl
 
     echo ${separator}
-    echo "Install cross python packages from index"
+    echo "Install cross python packages"
     pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/requirements-cross_from_index.txt
 
     echo ${separator}
-    echo "Install pure python packages from index"
+    echo "Install pure python packages"
     pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/requirements-pure.txt
 
+    echo ${separator}
+    echo "Install additional packages for some integrations"
+    pip install --disable-pip-version-check --no-deps --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGDEST}/share/requirements-integrations.txt
 
     if [ "${SYNOPKG_PKG_STATUS}" == "UPGRADE" ]; then
         if [ -e ${SYNOPKG_PKGVAR}/requirements-custom.txt ]; then
             echo ${separator}
-            echo "Install custom packages from index"
+            echo "Install custom packages"
             pip install --disable-pip-version-check --no-input --cache-dir ${PIP_CACHE_DIR} --requirement ${SYNOPKG_PKGVAR}/requirements-custom.txt
         fi
     fi
+
+    echo ${separator}
+    echo "Patch some packages after installation"
+    ### patch integrations
+    SITE_PACKAGES=$(realpath ${SYNOPKG_PKGDEST}/env/lib/python*/site-packages)
+    # aioasuswrt==1.5.1 does not exist, let it find aioasuswrt==1.5.4
+    sed -e 's/aioasuswrt==1.5.1/aioasuswrt>=1.5.1,<2.0.0/g' -i.orig ${SITE_PACKAGES}/homeassistant/components/asuswrt/manifest.json
+    # opuslib does not find libopus.so with 'find_library'
+    sed -e "s|lib_location = find_library('opus')|lib_location = '${SYNOPKG_PKGDEST}/lib/libopus.so'|g" -i.orig ${SITE_PACKAGES}/opuslib/api/__init__.py
+    # aiodhcpwatcher does not find libpcap.so
+    sed -e "s|find_library(\"pcap\")|\"${SYNOPKG_PKGDEST}/lib/libpcap.so\"|g" -i.orig ${SITE_PACKAGES}/scapy/libs/winpcapy.py
+
+    echo "Provide chunk for tami4 integration"
+    ### install chunk stub required by tami4 integration
+    cp -p ${SYNOPKG_PKGDEST}/share/chunk.py ${SITE_PACKAGES}/
+    
+    echo "Restrict packages to explicit versions"
+    CONSTRAINTS=${SITE_PACKAGES}/homeassistant/package_constraints.txt
+    echo ""                                 >> ${CONSTRAINTS}
+    echo "# Added by SynoCommunity package" >> ${CONSTRAINTS}
+    echo "pycares==4.11.0"                  >> ${CONSTRAINTS}
+    echo "caio==0.9.24"                     >> ${CONSTRAINTS}
+    echo "scapy==2.6.1"                     >> ${CONSTRAINTS}
+    echo "voip_utils==0.3.4"                >> ${CONSTRAINTS}
 }
