c2a83fc4683f62d94ada3ab7f0606e121514633c
fgma <fgma@users.noreply.github.com>
2026-01-20 22:42:16 +0100

    create default config from template file

--- spk/garage/src/service-setup.sh
+++ spk/garage/src/service-setup.sh
@@ -18,38 +18,13 @@ service_postinst()
       RPC_SECRET=$(openssl rand -hex 32)
       ADMIN_TOKEN=$(openssl rand -hex 32)
       METRICS_TOKEN=$(openssl rand -hex 32)
-      cat > "${CONFIG_FILE}" <<EOF
-metadata_dir = "${SHARE_PATH}/garage/meta"
-data_dir = "${SHARE_PATH}/garage/data"
-db_engine = "sqlite"
 
-replication_factor = 1
-
-block_ram_buffer_max = "128MiB"
-
-rpc_bind_addr = "[::]:3901"
-rpc_public_addr = "127.0.0.1:3901"
-rpc_secret = "${RPC_SECRET}"
-
-[s3_api]
-s3_region = "garage"
-api_bind_addr = "[::]:3900"
-root_domain = ".s3.garage.localhost"
-
-[s3_web]
-bind_addr = "[::]:3902"
-root_domain = ".web.garage.localhost"
-index = "index.html"
-
-[k2v_api]
-api_bind_addr = "[::]:3904"
-
-[admin]
-api_bind_addr = "[::]:3903"
-admin_token = "${ADMIN_TOKEN}"
-metrics_token = "${METRICS_TOKEN}"
-
-EOF
+      sed \
+         -e "s|@SHARE_PATH@|$SHARE_PATH|g" \
+         -e "s|@RPC_SECRET@|$RPC_SECRET|g" \
+         -e "s|@ADMIN_TOKEN@|$ADMIN_TOKEN|g" \
+         -e "s|@METRICS_TOKEN@|$METRICS_TOKEN|g" \
+         "${SYNOPKG_PKGDEST}/garage_config_template.toml" > "${CONFIG_FILE}"
    fi
 }
 
