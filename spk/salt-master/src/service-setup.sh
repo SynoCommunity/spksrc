f63cae39ff40aa3efca935e61d061e8f1a10ba68
Michael Reid <943378+mreid-tt@users.noreply.github.com>
2026-01-26 18:42:51 +0000

    Salt: Update to v3007.11 (#6928)

--- spk/salt-master/src/service-setup.sh
+++ spk/salt-master/src/service-setup.sh
@@ -1,76 +1,78 @@
 # Define python312 binary path
-PYTHON_DIR="/var/packages/python312/target/bin"
-# Add local bin, virtualenv along with python312 to the default PATH
-PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:${PYTHON_DIR}:${PATH}"
-LANGUAGE="env LANG=en_US.UTF-8"
-SYNOPKG_PKGETC=/var/packages/${SYNOPKG_PKGNAME}/etc
+PATH="${SYNOPKG_PKGDEST}/env/bin:${SYNOPKG_PKGDEST}/bin:/var/packages/python312/target/bin:${PATH}"
+export LANG=en_US.UTF-8
 
-service_prestart ()
+SALT_CONFIG="/var/packages/${SYNOPKG_PKGNAME}/etc"
+SALT_RUN_DIR="${SYNOPKG_PKGVAR}/run"
+PID_FILE_MASTER="${SALT_RUN_DIR}/salt-master.pid"
+PID_FILE_API="${SALT_RUN_DIR}/salt-api.pid"
+MASTER_SOCKET="${SALT_RUN_DIR}/master/master_event_pub.ipc"
+
+# Wait for a file to exist (with timeout)
+wait_for_file()
 {
-    # Define variables and commands
-    LOG_FILE_MASTER="${SYNOPKG_PKGVAR}/salt-master.log"
-    LOG_FILE_API="${SYNOPKG_PKGVAR}/salt-api.log"
-    PID_FILE_MASTER="${SYNOPKG_PKGVAR}/salt-master-runtime.pid"
-    PID_FILE_API="${SYNOPKG_PKGVAR}/salt-api-runtime.pid"
-    COMMAND_MASTER="salt-master --pid-file ${PID_FILE_MASTER} -c ${SYNOPKG_PKGETC} --log-file=${LOG_FILE_MASTER} -d"
-    COMMAND_API="salt-api --pid-file ${PID_FILE_API} -c ${SYNOPKG_PKGETC} --log-file=${LOG_FILE_API} -d"
-    # Execute salt-master command
-    $COMMAND_MASTER
-    # Wait until salt-master is populated
-    i=0
-    while [ $i -lt 10 ]; do
-        [ -s "${PID_FILE_MASTER}" ] && break
+    timeout=${2:-10}
+    while [ $timeout -gt 0 ] && [ ! -e "$1" ]; do
         sleep 1
-        i=$((i + 1))
+        timeout=$((timeout - 1))
     done
-    # Execute salt-api command
-    $COMMAND_API
-    # Wait until salt-api is populated
-    i=0
-    while [ $i -lt 10 ]; do
-        [ -s "${PID_FILE_API}" ] && break
-        sleep 1
-        i=$((i + 1))
-    done
-    # Combine PID files
-    : > "${PID_FILE}"
-    [ -s "${PID_FILE_API}" ] && echo "$(cat "${PID_FILE_API}")" >> "${PID_FILE}"
-    [ -s "${PID_FILE_MASTER}" ] && echo "$(cat "${PID_FILE_MASTER}")" >> "${PID_FILE}"
+    [ -e "$1" ]
 }
 
-service_poststop ()
+service_prestart()
 {
-    # Define variables
-    PID_FILE_MASTER="${SYNOPKG_PKGVAR}/salt-master-runtime.pid"
-    PID_FILE_API="${SYNOPKG_PKGVAR}/salt-api-runtime.pid"
-    # Remove any runtime PID files
-    [ -f "${PID_FILE_API}" ] && rm -f "${PID_FILE_API}"
-    [ -f "${PID_FILE_MASTER}" ] && rm -f "${PID_FILE_MASTER}"
+    # Clean up stale PID files from previous run
+    rm -f "${PID_FILE_MASTER}" "${PID_FILE_API}" "${PID_FILE}"
+
+    # Start salt-master and wait for IPC socket (indicates readiness)
+    salt-master --pid-file "${PID_FILE_MASTER}" -c "${SALT_CONFIG}" -d
+    wait_for_file "${MASTER_SOCKET}" 15
+
+    # Start salt-api (requires salt-master to be ready)
+    salt-api --pid-file "${PID_FILE_API}" -c "${SALT_CONFIG}" -d
+    wait_for_file "${PID_FILE_API}" 10
+
+    # Combine PIDs for framework's stop handling (one PID per line)
+    { cat "${PID_FILE_MASTER}"; echo; cat "${PID_FILE_API}"; } > "${PID_FILE}" 2>/dev/null
 }
 
-service_postinst ()
+service_poststop()
 {
-    # Create a Python virtualenv
+    rm -f "${PID_FILE_MASTER}" "${PID_FILE_API}"
+}
+
+service_preupgrade()
+{
+    # Remove saltgui folder to allow clean upgrade (static web assets, not user config)
+    rm -rf "${SYNOPKG_PKGVAR}/saltgui"
+}
+
+service_postinst()
+{
     install_python_virtualenv
-
-    # Install wheels
     install_python_wheels
 
-    # patch rsax931.py file to find libcrypto lib provided by python312
-    # (rely on patch-ng==1.18.1 included in requirements-pure.txt)
+    # Patch rsax931.py to find libcrypto from python312
     python ${SYNOPKG_PKGDEST}/env/lib/python3.12/site-packages/patch_ng.py \
            --directory=${SYNOPKG_PKGDEST}/env/lib/python3.12/site-packages/salt/utils \
            ${SYNOPKG_PKGDEST}/share/rsax931.py.patch
 
-    # Prepare salt-master config in /var/packages/salt-master/etc
-    test -d ${SYNOPKG_PKGETC}/master.d || install -m 755 -d ${SYNOPKG_PKGETC}/master.d
-    test -f ${SYNOPKG_PKGETC}/master || install -m 644 ${SYNOPKG_PKGDEST}/share/master ${SYNOPKG_PKGETC}/master
-    test -f ${SYNOPKG_PKGETC}/master.d/01_pidfile.conf || echo "pidfile: run" > ${SYNOPKG_PKGETC}/master.d/01_pidfile.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/02_sockdir.conf || echo "sock_dir: run/master" > ${SYNOPKG_PKGETC}/master.d/02_sockdir.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/03_cachedir.conf || echo "cachedir: cache" > ${SYNOPKG_PKGETC}/master.d/03_cachedir.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/04_logging.conf || echo "log_file: ${SYNOPKG_PKGNAME}.log" > ${SYNOPKG_PKGETC}/master.d/04_logging.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/05_loglevel.conf || echo "log_level_logfile: info" >> ${SYNOPKG_PKGETC}/master.d/05_loglevel.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/06_pkidir.conf || echo "pki_dir: pki/master" > ${SYNOPKG_PKGETC}/master.d/06_pkidir.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/07_rootdir.conf || echo "root_dir: ${SYNOPKG_PKGVAR}" > ${SYNOPKG_PKGETC}/master.d/07_rootdir.conf
-    test -f ${SYNOPKG_PKGETC}/master.d/08_extmodsdir.conf || echo "extension_modules: extensions" > ${SYNOPKG_PKGETC}/master.d/08_extmodsdir.conf
+    # Initialize configuration directory
+    install -m 755 -d "${SALT_CONFIG}/master.d"
+    [ -f "${SALT_CONFIG}/master" ] || install -m 644 "${SYNOPKG_PKGDEST}/share/master" "${SALT_CONFIG}/master"
+
+    # Create default configuration (only if not already present)
+    for conf in \
+        "01_pidfile.conf:pidfile: run" \
+        "02_sockdir.conf:sock_dir: run/master" \
+        "03_cachedir.conf:cachedir: cache" \
+        "04_logging.conf:log_file: ${SYNOPKG_PKGNAME}.log" \
+        "05_loglevel.conf:log_level_logfile: info" \
+        "06_pkidir.conf:pki_dir: pki/master" \
+        "07_rootdir.conf:root_dir: ${SYNOPKG_PKGVAR}" \
+        "08_extmodsdir.conf:extension_modules: extensions"
+    do
+        file="${SALT_CONFIG}/master.d/${conf%%:*}"
+        [ -f "$file" ] || echo "${conf#*:}" > "$file"
+    done
 }
