d1db7656321187726e697f5dd4b8e8e08f7b3e71
Vincent Fortier <th0ma7@gmail.com>
2026-01-26 00:45:05 +0000

    spk.mk: Fix stage1 by adding missing status file

--- mk/spksrc.spk.mk
+++ mk/spksrc.spk.mk
@@ -500,6 +500,8 @@ package: $(SPK_FILE_NAME)
 # -----------------------------------------------------------------------------
 # Stage1: Toolchain bootstrap
 # -----------------------------------------------------------------------------
+TCVARS_DONE := $(WORK_DIR)/.tcvars_done
+
 .PHONY: spk-stage1
 spk-stage1: $(TCVARS_DONE)
 
