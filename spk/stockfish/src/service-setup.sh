9123cd4082015702805ef38e39dbee9de36ac70a
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2026-03-03 19:35:40 +0000

    Stockfish: Update to v16

--- spk/stockfish/src/service-setup.sh
+++ spk/stockfish/src/service-setup.sh
@@ -1,13 +1,10 @@
-
-INSTALL_DIR=${SYNOPKG_PKGDEST}/webgui
-WEB_DIR="/var/services/web/stockfish"
+# Config file location (WebStation manages the web directory via resource file)
+WEB_DIR="/var/services/web_packages/${SYNOPKG_PKGNAME}"
 CONFIG_TEMPLATE=${SYNOPKG_PKGDEST}/var/webgui-config-template.php
 CONFIG_FILE="${WEB_DIR}/config.php"
 
 service_postinst ()
 {
-    # Install the web interface
-    cp -pR ${INSTALL_DIR} ${WEB_DIR}
     # Overwrite config file from template
     cp -f ${CONFIG_TEMPLATE} ${CONFIG_FILE}
 
@@ -15,11 +12,3 @@ service_postinst ()
     sed -i -e "s|@@_wizard_security_code_@@|${wizard_security_code}|g" ${CONFIG_FILE}
     sed -i -e "s|@@_wizard_thinking_time_ms_@@|${wizard_thinking_time_ms}|g" ${CONFIG_FILE}
 }
-
-
-service_postuninst ()
-{
-    # Remove the web interface
-    rm -rf ${WEB_DIR}
-}
-
