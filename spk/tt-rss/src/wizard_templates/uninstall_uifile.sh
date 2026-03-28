a15b3a221619d66b46a51c02aa74a7b60861a0fb
mreid-tt <943378+mreid-tt@users.noreply.github.com>
2026-03-28 06:23:25 +0000

    Tiny Tiny RSS: Switch from MariaDB to PostgreSQL
    - Update to latest tt-rss git commit
    - Change dependency from MariaDB10 to PostgreSQL
    - Require DSM 7.2+ (PHP 8.4)
    - Update PHP backend to 13 (PHP 8.4)
    - Remove legacy DSM 6.x support files
    - Add OPML export/import for user migration
    - Add validate_preinst/validate_preupgrade/validate_preuninst functions
    - Helper functions: exec_update_schema(), create_pg_user_db(), cleanup_mariadb()
    - Add version_static.txt with git hash
    - Combine install wizard pages, improve wording
    - Consistent wizard variable naming
    - Fix indentation consistency in wizard scripts
    - Clean up Makefile
    - Update digests for git hash

---
+++ spk/tt-rss/src/wizard_templates/uninstall_uifile.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+page_append ()
+{
+	if [ -z "$1" ]; then
+		echo "$2"
+	elif [ -z "$2" ]; then
+		echo "$1"
+	else
+		echo "$1,$2"
+	fi
+}
+
+PAGE_ADMIN=$(/bin/cat<<EOF
+{
+    "step_title": "{{UNINSTALL_TITLE}}",
+    "invalid_next_disabled_v2": true,
+    "items": [{
+        "type": "textfield",
+        "desc": "{{POSTGRESQL_USERNAME_DESC}}",
+        "subitems": [{
+            "key": "wizard_pg_username_admin",
+            "desc": "{{POSTGRESQL_USERNAME_LABEL}}",
+            "defaultValue": "pgadmin"
+        }]
+    }, {
+        "type": "password",
+        "desc": "{{POSTGRESQL_PASSWORD_DESC}}",
+        "subitems": [{
+            "key": "wizard_pg_password_admin",
+            "desc": "{{POSTGRESQL_PASSWORD_LABEL}}",
+            "validator": {
+                "allowBlank": false
+            }
+        }]
+    }, {
+        "type": "textfield",
+        "desc": "{{POSTGRESQL_PORT_DESC}}",
+        "subitems": [{
+            "key": "wizard_pg_port",
+            "desc": "{{POSTGRESQL_PORT_LABEL}}",
+            "defaultValue": "5433"
+        }]
+    }]
+}
+EOF
+)
+
+PAGE_EXPORT=$(/bin/cat<<EOF
+{
+    "step_title": "{{REMOVE_TT_RSS_TITLE}}",
+    "invalid_next_disabled_v2": true,
+    "items": [{
+        "desc": "{{REMOVE_TT_RSS_DESC}}"
+    }, {
+        "type": "textfield",
+        "desc": "{{DB_EXPORT_PATH_DESC}}",
+        "subitems": [{
+            "key": "wizard_dbexport_path",
+            "desc": "{{DB_EXPORT_PATH_LABEL}}",
+            "validator": {
+                "allowBlank": true,
+                "regex": {
+                    "expr": "/^\\\\/(volume|volumeUSB)[0-9]+\\\\//",
+                    "errorText": "{{DB_EXPORT_PATH_ERROR}}"
+                }
+            }
+        }]
+    }, {
+        "type": "password",
+        "desc": "{{DB_EXPORT_PASSWORD_DESC}}",
+        "subitems": [{
+            "key": "wizard_pg_password_ttrss",
+            "desc": "{{DB_EXPORT_PASSWORD_LABEL}}"
+        }]
+    }]
+}
+EOF
+)
+
+main ()
+{
+	local uninstall_page=""
+	uninstall_page=$(page_append "$uninstall_page" "$PAGE_ADMIN")
+
+	# Add export screen - always show it to allow skipping export
+	uninstall_page=$(page_append "$uninstall_page" "$PAGE_EXPORT")
+
+	echo "[$uninstall_page]" > "${SYNOPKG_TEMP_LOGFILE}"
+}
+
+main "$@"
