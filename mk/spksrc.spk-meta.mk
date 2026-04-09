5e227d259c23bf1d54dcf074d70c6012ebda6fde
Vincent Fortier <th0ma7@gmail.com>
2026-04-09 23:39:51 +0000

    spk-meta.mk: Handle conditional synocli-videodriver inclusion

--- mk/spksrc.spk-meta.mk
+++ mk/spksrc.spk-meta.mk
@@ -10,15 +10,18 @@ ifneq ($(filter spk-stage2,$(MAKECMDGOALS)),)
   include ../../mk/spksrc.spk/base.mk
 endif
 
-ifdef FFMPEG_PACKAGE
-  include ../../mk/spksrc.spk/ffmpeg.mk
-endif
+##
 
 ifdef PYTHON_PACKAGE
   include ../../mk/spksrc.spk/python.mk
 endif
 
-ifdef VIDEODRV_PACKAGE
+ifdef FFMPEG_PACKAGE
+  include ../../mk/spksrc.spk/ffmpeg.mk
+  FFMPEG_RPATH := $(shell readelf -d $(FFMPEG_STAGING_INSTALL_PREFIX)/bin/ffmpeg 2>/dev/null | grep -E 'RPATH|RUNPATH')
+endif
+
+ifneq ($(or $(findstring synocli-videodriver,$(FFMPEG_RPATH)),$(VIDEODRV_PACKAGE)),)
   include ../../mk/spksrc.spk/videodriver.mk
 endif
 
