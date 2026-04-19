a340677b272e866088d2163e7fd6ce419b0698cd
Vincent Fortier <th0ma7@gmail.com>
2026-04-19 12:58:25 +0000

    patch.mk: Avoid duplicate patches to be applied
    In rare case armv7 and x64 patches could be double counted as
    they are both an 'arch' and a 'group'.  This fix ensure evey
    patch are unique within the PATCHES variable.

--- mk/spksrc.patch.mk
+++ mk/spksrc.patch.mk
@@ -36,7 +36,7 @@ PATCHES += $(sort $(foreach group,ARM_ARCHS ARMv5_ARCHS ARMv7_ARCHS ARMv7L_ARCHS
 	   $(wildcard patches/$(shell echo $(group) | cut -f1 -d '_' | tr 'A-Z' 'a-z')/*.patch \
 	              patches/$(shell echo $(group) | cut -f1 -d '_' | tr 'A-Z' 'a-z')-$(TCVERSION)/*.patch))))
 endif
-PATCHES := $(realpath $(PATCHES))
+PATCHES := $(call uniq,$(realpath $(PATCHES)))
 
 PATCH_COOKIE = $(WORK_DIR)/.$(COOKIE_PREFIX)patch_done
 
