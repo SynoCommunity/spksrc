55b8a34346dbd0a3eee8a1579b5fe9d127d83472
Michael Reid <943378+mreid-tt@users.noreply.github.com>
2026-06-29 18:36:13 +0000

    Netdata: Use POST_PATCH_TARGET to redirect writable paths at cmake time
    - Replace runtime var symlink with cmake path patching via POST_PATCH_TARGET
    - Remove _setup_runtime_dirs, service_postinst, service_postupgrade
    - CACHE_DIR, CONFIG_DIR, LOG_DIR, VARLIB_DIR use INSTALL_PREFIX_VAR

--- spk/netdata/src/service-setup.sh
+++ spk/netdata/src/service-setup.sh
@@ -2,14 +2,3 @@ SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/usr/sbin/netdata -D -P ${PID_FILE}"
-
-_setup_runtime_dirs ()
-{
-    ln -sf "${SYNOPKG_PKGVAR}" "${SYNOPKG_PKGDEST}/var"
-    mkdir -p "${SYNOPKG_PKGVAR}/lib/netdata" \
-             "${SYNOPKG_PKGVAR}/log/netdata" \
-             "${SYNOPKG_PKGVAR}/cache/netdata"
-}
-
-service_postinst () { _setup_runtime_dirs; }
-service_postupgrade () { _setup_runtime_dirs; }
