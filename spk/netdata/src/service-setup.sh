0878f34444cb87108266d6ae9c5be555ce747e01
Michael Reid <943378+mreid-tt@users.noreply.github.com>
2026-06-30 02:36:42 +0000

    Netdata: Create runtime directories in service_postinst
    With cmake paths now pointing to /var/packages/netdata/var/..., the
    lib/, log/, cache/, and etc/ directories must exist before netdata
    starts. The service runs as sc-netdata which cannot create them.

--- spk/netdata/src/service-setup.sh
+++ spk/netdata/src/service-setup.sh
@@ -2,3 +2,13 @@ SVC_BACKGROUND=y
 SVC_WRITE_PID=y
 
 SERVICE_COMMAND="${SYNOPKG_PKGDEST}/usr/sbin/netdata -D -P ${PID_FILE}"
+
+service_postinst ()
+{
+    mkdir -p "${SYNOPKG_PKGVAR}/lib/netdata" \
+             "${SYNOPKG_PKGVAR}/log/netdata" \
+             "${SYNOPKG_PKGVAR}/cache/netdata" \
+             "${SYNOPKG_PKGVAR}/etc/netdata"
+}
+
+service_postupgrade () { service_postinst; }
