034a715d9f1a8b7ac46cd856121f695c129be00c
fakerr <111339712+not-first@users.noreply.github.com>
2026-07-01 15:06:42 +1000

    move all options into singular environment.txt file

--- spk/beszel-agent/src/service-setup.sh
+++ spk/beszel-agent/src/service-setup.sh
@@ -1,9 +1,6 @@
 export PATH="/usr/local/bin:$PATH"
 
-KEY_FILE="${SYNOPKG_PKGVAR}/key.pub"
-EXTRA_FS_FILE="${SYNOPKG_PKGVAR}/extra_fs.conf"
-SMART_DEVICES_FILE="${SYNOPKG_PKGVAR}/smart_devices.conf"
-CUSTOM_ENV_FILE="${SYNOPKG_PKGVAR}/custom_env.conf"
+ENV_FILE="${SYNOPKG_PKGVAR}/environment.txt"
 
 
 export_variables_from_file ()
@@ -34,26 +31,7 @@ export_variables_from_file ()
     fi
 }
 
-# Read the public key from the saved file
-if [ -f "${KEY_FILE}" ]; then
-    BESZEL_PUBLIC_KEY=$(cat "${KEY_FILE}")
-    export KEY="${BESZEL_PUBLIC_KEY}"
-fi
-
-# Read the extra filesystems from the saved file
-if [ -f "${EXTRA_FS_FILE}" ]; then
-    EXTRA_FS=$(cat "${EXTRA_FS_FILE}")
-    export EXTRA_FILESYSTEMS="${EXTRA_FS}"
-fi
-
-# Read the SMART devices from the saved file
-if [ -f "${SMART_DEVICES_FILE}" ]; then
-    SMART_DEVS=$(cat "${SMART_DEVICES_FILE}")
-    export SMART_DEVICES="${SMART_DEVS}"
-fi
-
-# Read any custom environment variables from file (the file is newline-separated)
-export_variables_from_file "${CUSTOM_ENV_FILE}"
+export_variables_from_file "${ENV_FILE}"
 
 BESZEL_AGENT="${SYNOPKG_PKGDEST}/bin/beszel-agent"
 SERVICE_COMMAND="${BESZEL_AGENT}"
@@ -63,23 +41,21 @@ SVC_WRITE_PID=y
 
 service_postinst ()
 {
-    # Save the public key only if provided
+    printf 'SKIP_SYSTEMD=true\n' > "${ENV_FILE}"
+
     if [ -n "${wizard_pub_key}" ]; then
-        echo "${wizard_pub_key}" > "${KEY_FILE}"
+        printf 'KEY=%s\n' "${wizard_pub_key}" >> "${ENV_FILE}"
     fi
 
-    # Save the extra filesystems only if provided
     if [ -n "${wizard_extra_fs}" ]; then
-        echo "${wizard_extra_fs}" > "${EXTRA_FS_FILE}"
+        printf 'EXTRA_FILESYSTEMS=%s\n' "${wizard_extra_fs}" >> "${ENV_FILE}"
     fi
 
-    # Save the SMART devices only if provided
     if [ -n "${wizard_smart_devices}" ]; then
-        echo "${wizard_smart_devices}" > "${SMART_DEVICES_FILE}"
+        printf 'SMART_DEVICES=%s\n' "${wizard_smart_devices}" >> "${ENV_FILE}"
     fi
 
-    # Save any custom environment variables only if provided
     if [ -n "${wizard_extra_env}" ]; then
-        printf '%s\n' "${wizard_extra_env}" > "${CUSTOM_ENV_FILE}"
+        printf '%s\n' "${wizard_extra_env}" >> "${ENV_FILE}"
     fi
 }
