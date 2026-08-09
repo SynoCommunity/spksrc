ecd84ffd9facbec436ee96d922c8887e30357ee2
Michael Reid <943378+mreid-tt@users.noreply.github.com>
2026-08-09 13:34:56 -0400

    Netdata: Update to v2.10.4 (#7379)

--- spk/netdata/src/service-setup.sh
+++ spk/netdata/src/service-setup.sh
@@ -8,6 +8,10 @@ service_postinst ()
              "${SYNOPKG_PKGVAR}/log/netdata" \
              "${SYNOPKG_PKGVAR}/cache/netdata" \
              "${SYNOPKG_PKGVAR}/etc/netdata"
+    # ensure the shipped default config is writable by the service user
+    if [ -f "${SYNOPKG_PKGVAR}/etc/netdata/netdata.conf" ]; then
+        chown "${EFF_USER}:synocommunity" "${SYNOPKG_PKGVAR}/etc/netdata/netdata.conf"
+    fi
 }
 
 service_postupgrade () { service_postinst; }
